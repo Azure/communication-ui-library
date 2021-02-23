a6dd4337e2ae2b54e3a3a3508601ab1b5d6baaa6
Alex Kwan (ONENOTE) <alkwa@microsoft.com>
2021-02-23 09:17:24 -0800

    updating with correct static content path

--- samples/Server/src/app.ts
+++ samples/Server/src/app.ts
@@ -21,7 +21,7 @@ app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 
 app.use('/createThread', cors(), createThread);
 app.use('/addUser', cors(), addUser);
