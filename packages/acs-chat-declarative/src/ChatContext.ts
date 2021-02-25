9e4c172e45bdf9fc2480e360b294d82cf5336c8f
Nan Jiang <jinan@microsoft.com>
2021-02-24 16:28:15 -0800

    Initial branch for chat-declarative - chat declarative generates a chatthread declarative - a context is sharing between chat and thread -  all state change will trigger state change event

---
+++ packages/acs-chat-declarative/src/ChatContext.ts
@@ -0,0 +1,75 @@
+// © Microsoft Corporation. All rights reserved.
+import EventEmitter from 'events';
+import produce from 'immer';
+import { ChatClientState, ChatMessageWithLocalId, ChatThreadClientState } from './ChatClientState';
+
+// have separated ClientState and ChatThreadState?
+export class ChatContext {
+  private _state: ChatClientState = {
+    userId: '',
+    displayName: '',
+    threads: new Map()
+  };
+  private _emitter: EventEmitter = new EventEmitter();
+  public setState(state: ChatClientState): void {
+    this._state = state;
+    this._emitter.emit('stateChanged', this._state);
+  }
+
+  public getState(): ChatClientState {
+    return this._state;
+  }
+
+  public setThread(threadId: string, threadState: ChatThreadClientState): void {
+    this.setState(
+      produce(this._state, (draft: ChatClientState) => {
+        draft.threads.set(threadId, threadState);
+      })
+    );
+  }
+
+  public setChatMessages(threadId: string, messages: Map<string, ChatMessageWithLocalId>): void {
+    this.setState(
+      produce(this._state, (draft: ChatClientState) => {
+        const threadState = draft.threads.get(threadId);
+        if (threadState) {
+          threadState.chatMessages = messages;
+        }
+      })
+    );
+  }
+
+  // This function changes messageKey from localId to remoteId, which indicates message synced with server successfully
+  setLocalMessageSynced = (threadId: string, localId: string): void => {
+    this.setState(
+      produce(this._state, (draft: ChatClientState) => {
+        const chatMessages = draft.threads.get(threadId)?.chatMessages;
+        const message: ChatMessageWithLocalId | undefined = chatMessages ? chatMessages.get(localId) : undefined;
+        if (chatMessages && message && message.clientMessageId && message.messageId) {
+          chatMessages.delete(message.clientMessageId);
+          chatMessages.set(message.messageId, message);
+        }
+      })
+    );
+  };
+
+  setChatMessage = (threadId: string, message: ChatMessageWithLocalId): void => {
+    const { messageId, clientMessageId } = message;
+    if (messageId || clientMessageId) {
+      this.setState(
+        produce(this._state, (draft: ChatClientState) => {
+          const threadMessages = draft.threads.get(threadId)?.chatMessages;
+          const isLocalIdInMap = threadMessages && clientMessageId && threadMessages.get(clientMessageId);
+          const messageKey = !messageId || isLocalIdInMap ? clientMessageId : messageId;
+          if (threadMessages && messageKey) {
+            threadMessages.set(messageKey, message);
+          }
+        })
+      );
+    }
+  };
+
+  public onStateChange(handler: (state: ChatClientState) => void): void {
+    this._emitter.on('stateChanged', handler);
+  }
+}
---
+++ packages/acs-chat-declarative/src/ChatThreadClientDeclarative.ts
@@ -0,0 +1,89 @@
+// © Microsoft Corporation. All rights reserved.
+import { ChatMessage, ChatThreadClient } from '@azure/communication-chat';
+import { MessageStatus } from './types/ChatMessage';
+import { Constants } from './Constants';
+import { ChatMessageWithLocalId } from './ChatClientState';
+import { ChatContext } from './ChatContext';
+
+let context: ChatContext;
+
+export const convertChatMessage = (
+  message: ChatMessage,
+  status: MessageStatus = MessageStatus.DELIVERED,
+  clientMessageId?: string
+): ChatMessageWithLocalId => {
+  return {
+    ...message,
+    senderId: message.sender?.communicationUserId,
+    senderDisplayName: message.senderDisplayName,
+    clientMessageId: clientMessageId,
+    messageId: message.id,
+    status
+  };
+};
+
+const proxyChatThreadClient: ProxyHandler<ChatThreadClient> = {
+  get: function <P extends keyof ChatThreadClient>(target: ChatThreadClient, prop: P) {
+    switch (prop) {
+      case 'listMessages': {
+        // TODO: wrap the async iterator with another async iterator
+        // every time we call/user calls this function, it will update state in context
+        return function (...args: Parameters<ChatThreadClient['listMessages']>) {
+          return target.listMessages(...args);
+        };
+      }
+      case 'getMessage': {
+        return async function (...args: Parameters<ChatThreadClient['getMessage']>) {
+          const message = await target.getMessage(...args);
+          context.setChatMessage(target.threadId, convertChatMessage(message));
+          return message;
+        };
+      }
+      case 'sendMessage': {
+        // Retry logic?
+        return async function (...args: Parameters<ChatThreadClient['sendMessage']>) {
+          const { content } = args[0];
+          const clientMessageId = (Math.floor(Math.random() * Constants.MAXIMUM_INT64) + 1).toString(); //generate a random unsigned Int64 number
+          const newMessage = {
+            content,
+            clientMessageId,
+            createdOn: undefined,
+            status: MessageStatus.SENDING
+          };
+          context.setChatMessage(target.threadId, newMessage);
+
+          const result = await target.sendMessage(...args);
+          if (result._response.status === Constants.CREATED) {
+            if (result.id) {
+              context.setChatMessage(target.threadId, {
+                ...newMessage,
+                status: MessageStatus.DELIVERED,
+                messageId: result.id
+              });
+              context.setLocalMessageSynced(target.threadId, clientMessageId);
+            }
+          } else if (result._response.status === Constants.PRECONDITION_FAILED_STATUS_CODE) {
+            context.setChatMessage(target.threadId, { ...newMessage, status: MessageStatus.FAILED });
+          }
+          return result;
+        };
+      }
+      default:
+        return Reflect.get(target, prop);
+    }
+  }
+};
+
+export const chatThreadClientDeclaratify = (
+  chatThreadClient: ChatThreadClient,
+  chatContext: ChatContext
+): ChatThreadClient => {
+  context = chatContext;
+  context.setThread(chatThreadClient.threadId, {
+    failedMessageIds: [],
+    chatMessages: new Map(),
+    threadId: chatThreadClient.threadId,
+    threadMembers: []
+  });
+  return new Proxy(chatThreadClient, proxyChatThreadClient) as ChatThreadClient;
+};
