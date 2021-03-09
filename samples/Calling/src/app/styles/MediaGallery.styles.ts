9c0cea7b79e437fa5c6b62bbc9e3995d9e682fb0
Eason <eason.yty@gmail.com>
2021-03-09 12:24:05 -0800

    Refactor GalleryTile, localPreview, mediaGalleyOneToOne with VideoTile (#56)
    * refactor GalleryTile, localPreview, mediaGalleyOneToOne with VideoTile
    
    * rename StreamMediaComponent to StreamMedia
    
    * address comments
    
    Co-authored-by: Eason Yang <easony@microsoft.com>

--- samples/Calling/src/app/styles/MediaGallery.styles.ts
+++ samples/Calling/src/app/styles/MediaGallery.styles.ts
@@ -1,6 +1,8 @@
 // © Microsoft Corporation. All rights reserved.
 
-import { mergeStyles } from '@fluentui/react';
+import { getTheme, mergeStyles } from '@fluentui/react';
+
+const palette = getTheme().palette;
 
 const videoBaseStyle = mergeStyles({
   border: 0,
@@ -26,4 +28,39 @@ export const aspectRatioBoxContentStyle = mergeStyles({
   left: 0,
   width: '100%',
   height: '100%'
+});
+
+export const stackContainerStyle = mergeStyles({
+  height: '100%',
+  width: '25%'
+});
+
+export const screenShareContainerStyle = mergeStyles({
+  height: '100%',
+  width: '75%',
+  position: 'relative'
+});
+
+export const videoHint = mergeStyles({
+  backgroundColor: palette.neutralSecondary,
+  bottom: '5%',
+  height: '1.75rem',
+  boxShadow: '0 0 1px 0 rgba(0,0,0,.16)',
+  color: palette.neutralLighter,
+  fontSize: '1.25rem',
+  lineHeight: '1.0625rem',
+  textAlign: 'left',
+  left: '2%',
+  overflow: 'hidden',
+  position: 'absolute',
+  padding: '0.25rem',
+  whiteSpace: 'nowrap',
+  maxWidth: '95%',
+  borderRadius: 4
+});
+
+export const disabledVideoHint = mergeStyles(videoHint, {
+  backgroundColor: 'transparent',
+  color: palette.neutralSecondary,
+  boxShadow: 'none'
 });
