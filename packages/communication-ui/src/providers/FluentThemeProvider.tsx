07e3ecec54f37351dd47918b33f980781233dbda
Miguel Gamis <miguelgamis@microsoft.com>
2021-03-10 15:37:01 -0800

    bug fixed

--- packages/communication-ui/src/providers/FluentThemeProvider.tsx
+++ packages/communication-ui/src/providers/FluentThemeProvider.tsx
@@ -1,8 +1,8 @@
 // © Microsoft Corporation. All rights reserved.
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider, Theme, PartialTheme } from '@fluentui/react-theme-provider';
-import { mergeThemes, Provider, teamsTheme } from '@fluentui/react-northstar';
+import { mergeThemes, Provider, teamsTheme, ThemeInput } from '@fluentui/react-northstar';
 
 interface FluentThemeProviderProps {
   children: React.ReactNode;
@@ -11,24 +11,29 @@ interface FluentThemeProviderProps {
 
 export const FluentThemeProvider = (props: FluentThemeProviderProps): JSX.Element => {
   const { theme, children } = props;
+  const [themeState, setTheme] = useState<PartialTheme | Theme | undefined>(undefined);
+  const [northstarTheme, setNorthstarTheme] = useState<ThemeInput<any>>({});
 
-  const northstarTheme = {
-    componentVariables: {
-      Chat: {
-        backgroundColor: theme?.palette?.white
-      },
-      ChatMessage: {
-        authorColor: theme?.palette?.neutralDark,
-        contentColor: theme?.palette?.neutralDark,
-        backgroundColor: theme?.palette?.neutralLight,
-        backgroundColorMine: theme?.palette?.themeLight
+  useEffect(() => {
+    setTheme(theme);
+    setNorthstarTheme({
+      componentVariables: {
+        Chat: {
+          backgroundColor: theme?.palette?.white
+        },
+        ChatMessage: {
+          authorColor: theme?.palette?.neutralDark,
+          contentColor: theme?.palette?.neutralDark,
+          backgroundColor: theme?.palette?.neutralLight,
+          backgroundColorMine: theme?.palette?.themeLight
+        }
+        // add more here to align theme for northstar components
       }
-      // add more here to align theme for northstar components
-    }
-  };
+    });
+  }, [theme]);
 
   return (
-    <ThemeProvider theme={theme} className="wrapper" applyTo="body" style={{ display: 'inherit' }}>
+    <ThemeProvider theme={themeState} className="wrapper" applyTo="body" style={{ display: 'inherit' }}>
       <Provider theme={mergeThemes(teamsTheme, northstarTheme)} className="wrapper">
         {children}
       </Provider>
