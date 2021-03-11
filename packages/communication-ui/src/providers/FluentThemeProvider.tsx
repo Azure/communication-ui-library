6b5c73091da097fd934fbc08dd204e0ad4387886
Miguel Gamis <miguelgamis@microsoft.com>
2021-03-10 18:32:56 -0800

    refactor

--- packages/communication-ui/src/providers/FluentThemeProvider.tsx
+++ packages/communication-ui/src/providers/FluentThemeProvider.tsx
@@ -12,29 +12,31 @@ interface FluentThemeProviderProps {
 export const FluentThemeProvider = (props: FluentThemeProviderProps): JSX.Element => {
   const { theme, children } = props;
   const [fluentTheme, setFluentTheme] = useState<PartialTheme | Theme | undefined>(undefined);
-  const [fluentNorthStarTheme, setFluentNorthStarTheme] = useState<ThemeInput<any>>({});
+  const [fluentNorthStarTheme, setFluentNorthStarTheme] = useState<ThemeInput<any>>(teamsTheme);
 
   useEffect(() => {
     setFluentTheme(theme);
-    setFluentNorthStarTheme({
-      componentVariables: {
-        Chat: {
-          backgroundColor: theme?.palette?.white
-        },
-        ChatMessage: {
-          authorColor: theme?.palette?.neutralDark,
-          contentColor: theme?.palette?.neutralDark,
-          backgroundColor: theme?.palette?.neutralLight,
-          backgroundColorMine: theme?.palette?.themeLight
+    setFluentNorthStarTheme(
+      mergeThemes(fluentNorthStarTheme, {
+        componentVariables: {
+          Chat: {
+            backgroundColor: theme?.palette?.white
+          },
+          ChatMessage: {
+            authorColor: theme?.palette?.neutralDark,
+            contentColor: theme?.palette?.neutralDark,
+            backgroundColor: theme?.palette?.neutralLight,
+            backgroundColorMine: theme?.palette?.themeLight
+          }
+          // add more here to align theme for northstar components
         }
-        // add more here to align theme for northstar components
-      }
-    });
+      })
+    );
   }, [theme]);
 
   return (
     <ThemeProvider theme={fluentTheme} className="wrapper" applyTo="body" style={{ display: 'inherit' }}>
-      <Provider theme={mergeThemes(teamsTheme, fluentNorthStarTheme)} className="wrapper">
+      <Provider theme={fluentNorthStarTheme} className="wrapper">
         {children}
       </Provider>
     </ThemeProvider>
