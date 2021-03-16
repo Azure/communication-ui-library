54b17d5712c7d858f56374be16866e1ed9f4b8a5
Nan Jiang <jinan@microsoft.com>
2021-03-15 17:05:17 -0700

    Merge export type sentence

--- packages/acs-chat-declarative/src/index.ts
+++ packages/acs-chat-declarative/src/index.ts
@@ -2,15 +2,6 @@
 export { chatClientDeclaratify } from './ChatClientDeclarative';
 export { chatThreadClientDeclaratify } from './ChatThreadClientDeclarative';
 
-import { DeclarativeChatClient as _DeclarativeChatClient } from './ChatClientDeclarative';
-import { ChatMessageWithStatus as _ChatMessageWithStatus } from './types/ChatMessageWithStatus';
-
-import {
-  ChatThreadClientState as _ChatThreadClientState,
-  ChatClientState as _ChatClientState
-} from './ChatClientState';
-
-export type DeclarativeChatClient = _DeclarativeChatClient;
-export type ChatThreadClientState = _ChatThreadClientState;
-export type ChatMessageWithStatus = _ChatMessageWithStatus;
-export type ChatClientState = _ChatClientState;
+export type { DeclarativeChatClient } from './ChatClientDeclarative';
+export type { ChatMessageWithStatus } from './types/ChatMessageWithStatus';
+export type { ChatThreadClientState, ChatClientState } from './ChatClientState';
