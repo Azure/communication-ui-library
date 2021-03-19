cdaf8610eb0a8ad433b546599ddbc0190d966218
Anjul Garg <anjulgarg@live.com>
2021-03-19 15:05:52 -0700

    [#2397386] Improving Control Bar storybook docs

--- packages/communication-ui/src/stories/docs/ControlBarDocs.tsx
+++ packages/communication-ui/src/stories/docs/ControlBarDocs.tsx
@@ -1,6 +1,7 @@
 // © Microsoft Corporation. All rights reserved.
 
 import { Stack } from '@fluentui/react';
+import { CallEndIcon } from '@fluentui/react-northstar';
 import { Canvas, Description, Heading, Props, Source, Title } from '@storybook/addon-docs/blocks';
 import React from 'react';
 import { audioButtonProps, ControlBar, ControlButton, hangupButtonProps, optionsButtonProps, screenShareButtonProps, videoButtonProps } from '../../components';
@@ -27,8 +28,8 @@ const defaultOptionsMenuProps = {
       iconProps: { iconName: 'LocationCircle' },
       subMenuProps: {
         items: [
-          { key: 'camera1', text: 'Full HD Webcam', title: 'Full HD Webcam', canCheck: true, isChecked: true },
-          { key: 'camera2', text: 'Macbook Pro Webcam', title: 'Macbook Pro Webcam' }
+          { key: 'cam1', text: 'Full Webcam', title: 'Full Webcam', canCheck: true, isChecked: true },
+          { key: 'cam2', text: 'Macbook Webcam', title: 'Macbook Webcam' }
         ]
       }
     },
@@ -38,8 +39,8 @@ const defaultOptionsMenuProps = {
       iconProps: { iconName: 'LocationCircle' },
       subMenuProps: {
         items: [
-          { key: 'mic1', text: 'Realtek HD Audio', title: 'Realtek HD Audio' },
-          { key: 'mic2', text: 'Macbook Pro Mic', title: 'Macbook Pro Mic', canCheck: true, isChecked: true }
+          { key: 'mic1', text: 'Realtek Audio', title: 'Realtek Audio' },
+          { key: 'mic2', text: 'Macbook Mic', title: 'Macbook Mic', canCheck: true, isChecked: true }
         ]
       }
     }
@@ -71,8 +72,8 @@ const defaultOptionsMenuProps = {
       iconProps: { iconName: 'LocationCircle' },
       subMenuProps: {
         items: [
-          { key: 'camera1', text: 'Full HD Webcam', title: 'Full HD Webcam', canCheck: true, isChecked: true },
-          { key: 'camera2', text: 'Macbook Pro Webcam', title: 'Macbook Pro Webcam' }
+          { key: 'cam1', text: 'HD Webcam', title: 'HD Webcam', canCheck: true, isChecked: true, onClick: handleClick('cam1') },
+          { key: 'cam2', text: 'Macbook Webcam', title: 'Macbook Webcam', onClick: handleClick('cam2') }
         ]
       }
     },
@@ -82,8 +83,8 @@ const defaultOptionsMenuProps = {
       iconProps: { iconName: 'LocationCircle' },
       subMenuProps: {
         items: [
-          { key: 'mic1', text: 'Realtek HD Audio', title: 'Realtek HD Audio' },
-          { key: 'mic2', text: 'Macbook Pro Mic', title: 'Macbook Pro Mic', canCheck: true, isChecked: true }
+          { key: 'mic1', text: 'Realtek Audio', title: 'Realtek Audio', onClick: handleClick('mic1') },
+          { key: 'mic2', text: 'Macbook Mic', title: 'Macbook Mic', canCheck: true, isChecked: true, onClick: handleClick('mic2') }
         ]
       }
     }
@@ -93,30 +94,150 @@ const defaultOptionsMenuProps = {
 const ControlBarExample: () => JSX.Element = () => {
   return (
     <ControlBar layout={'horizontal'}>
-      <ControlButton {...videoButtonProps} />
-      <ControlButton {...audioButtonProps} />
-      <ControlButton {...screenShareButtonProps} />
-      <ControlButton {...optionsButtonProps} menuProps={defaultOptionsMenuProps} />
-      <ControlButton {...hangupButtonProps} />
+      <ControlButton {...videoButtonProps} onClick={toggleVideo} showLabel={false} />
+      <ControlButton {...audioButtonProps} onClick={toggleAudio} showLabel={false} />
+      <ControlButton {...screenShareButtonProps} onClick={toggleScreenShare} showLabel={false} />
+      <ControlButton {...optionsButtonProps} menuProps={defaultOptionsMenuProps} showLabel={false} />
+      <ControlButton {...hangupButtonProps} onClick={hangUpCall} showLabel={false} />
     </ControlBar>
   );
 };
 `;
+
+const ControlBarLayoutExample: () => JSX.Element = () => {
+  return (
+    <Stack style={{ flexFlow: 'row', minHeight: '250px' }}>
+      <FluentThemeProvider>
+        <ControlBar layout='floatingLeft'>
+          <ControlButton {...videoButtonProps} />
+          <ControlButton {...audioButtonProps} />
+          <ControlButton {...screenShareButtonProps} />
+          <ControlButton {...optionsButtonProps} menuProps={defaultOptionsMenuProps} />
+          <ControlButton {...hangupButtonProps} />
+        </ControlBar>
+      </FluentThemeProvider>
+    </Stack>
+  );
+};
+
+const controlBarLayoutCode = `
+<ControlBar layout='floatingLeft'>
+  <ControlButton {...videoButtonProps} />
+  <ControlButton {...audioButtonProps} />
+  <ControlButton {...screenShareButtonProps} />
+  <ControlButton {...optionsButtonProps} menuProps={defaultOptionsMenuProps} />
+  <ControlButton {...hangupButtonProps} />
+</ControlBar>
+`;
+
+const customControlButtonUsage = `
+const HangUpButton: () => JSX.Element = () => {
+  const styles = {
+    root: {
+      marginTop: '10px !important',
+      minHeight: '1rem',
+      background: 'firebrick',
+      color: 'white',
+      ':hover': { background: 'red', color: 'white' }
+    },
+    flexContainer: { flexFlow: 'row' },
+    label: { color: 'white', paddingLeft: '0.5rem' }
+  };
+  return (
+    <ControlButton 
+      defaultIcon={<CallEndIcon />} 
+      defaultLabel={<Stack>End Call</Stack>} 
+      showLabel={true} 
+      styles={styles} 
+      onClick={hangUpCall}
+    />
+  );
+};
+
+const CustomControlButtonExample: () => JSX.Element = () => {
+  return (
+    <Stack style={{ flexFlow: 'row' }}>
+      <FluentThemeProvider>
+        <ControlBar layout={'horizontal'}>
+          <ControlButton {...videoButtonProps} />
+          <ControlButton {...audioButtonProps} />
+          <HangUpButton />
+        </ControlBar>
+      </FluentThemeProvider>
+    </Stack>
+  );
+};
+`;
+
+const HangUpButton: () => JSX.Element = () => {
+  return (
+    <ControlButton 
+      defaultIcon={<CallEndIcon />}
+      defaultLabel={<Stack>End Call</Stack>}
+      showLabel={true}
+      styles={{
+        root: {
+          marginTop: '10px !important',
+          minHeight: '1rem',
+          background: 'firebrick',
+          color: 'white',
+          ':hover': { background: 'red', color: 'white' }
+        },
+        flexContainer: { flexFlow: 'row' },
+        label: { color: 'white', paddingLeft: '0.5rem' }
+      }}
+    />
+  );
+};
+
+const CustomControlButtonExample: () => JSX.Element = () => {
+  return (
+    <Stack style={{ flexFlow: 'row' }}>
+      <FluentThemeProvider>
+        <ControlBar layout={'horizontal'}>
+          <ControlButton {...videoButtonProps} />
+          <ControlButton {...audioButtonProps} />
+          <HangUpButton />
+        </ControlBar>
+      </FluentThemeProvider>
+    </Stack>
+  );
+};
+
 
 export const getDocs: () => JSX.Element = () => {
   return (
     <>
       <Title>ControlBar & ControlButton</Title>
       <Description of={ControlBar} />
+      <Canvas>
+        <ControlBarExample />
+      </Canvas>
+
       <Heading>Importing</Heading>
       <Source code={importStatement} />
-      <Heading>Example - Control Bar with Default Buttons</Heading>
+
+      <Heading>Default Usage</Heading>
       <Canvas>
         <ControlBarExample />
       </Canvas>
       <Source code={exampleCode} />
+
+      <Heading>Control Bar Layouts</Heading>
+      <Canvas>
+        <ControlBarLayoutExample />
+      </Canvas>
+      <Source code={controlBarLayoutCode} />
+
+      <Heading>Custom ControlButton</Heading>
+      <Canvas>
+        <CustomControlButtonExample />
+      </Canvas>
+      <Source code={customControlButtonUsage} />
+
       <Heading>ControlBar Props</Heading>
       <Props of={ControlBar} />
+
       <Heading>ControlButton Props</Heading>
       <Props of={ControlButton} />
     </>
