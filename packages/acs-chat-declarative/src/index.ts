68a394a1f1bb5d0b6a529e838b49c14e3052e5f6
Nan Jiang <jinan@microsoft.com>
2021-03-30 09:43:35 -0700

    Add index exporting type

--- packages/acs-chat-declarative/src/index.ts
+++ packages/acs-chat-declarative/src/index.ts
@@ -6,4 +6,5 @@ export type { DeclarativeChatClient } from './ChatClientDeclarative';
 export type { ChatMessageWithStatus, MessageStatus } from './types/ChatMessageWithStatus';
 export type { ChatConfig } from './types/ChatConfig';
 export type { ReadReceipt } from './types/ReadReceipt';
+export type { TypingIndicator } from './types/TypingIndicator';
 export type { ChatThreadClientState, ChatClientState } from './ChatClientState';
