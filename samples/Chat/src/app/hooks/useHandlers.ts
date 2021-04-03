2741064ae6121bbf100b43c8eb66caf9a1501d6f
Nan Jiang <jinan@microsoft.com>
2021-04-03 01:28:42 -0700

    Fix typo

--- samples/Chat/src/app/hooks/useHandlers.ts
+++ samples/Chat/src/app/hooks/useHandlers.ts
@@ -7,7 +7,7 @@ import { useChatClient, useChatThreadClient } from '@azure/communication-ui';
 
 import { ReactElement } from 'react';
 
-// This will be moved into selector folde rwhen ChatClientProvide when refactor finished
+// This will be moved into selector folder when ChatClientProvide when refactor finished
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
 export const useHandlers = <PropsT>(component: (props: PropsT) => ReactElement | null) => {
   const chatClient: DeclarativeChatClient = useChatClient() as any;
