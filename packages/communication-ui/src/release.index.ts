7748b252f68b9d4650a1cd8131b1cc0022b0b2bd
James Burnside <mail@jamesburnside.com>
2021-04-06 12:50:30 -0700

    fix sample builds

--- packages/communication-ui/src/release.index.ts
+++ packages/communication-ui/src/release.index.ts
@@ -3,6 +3,8 @@
 /**
  * This file exists only while we ready the api for review and refactor out code.
  * This will replace index.ts once that work is complete.
+ * Do not forget to update the `types` in the package.json when this is complete to
+ * `dist/communication-ui.d.ts`.
  */
 
 export * from './components';
