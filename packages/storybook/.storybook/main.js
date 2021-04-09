c3be7973062d111b9d24978ef9c470d6c1481b6d
James Burnside <mail@jamesburnside.com>
2021-04-09 16:33:48 -0700

    merge webpack configs

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -19,6 +19,14 @@ module.exports = {
     '@storybook/addon-knobs'
   ],
   webpackFinal: async (config, { configType }) => {
+    // Remove entrypoint size warning (rush build sees warnings as errors). Ideally we should use code splitting to reduce the entry point instead.
+    // For more information on how this might be possible see: https://github.com/storybookjs/storybook/issues/6885
+    config.performance.hints = false;
+
+    // Resolve local packages directly to the source file.
+    // This means props in the docs for each component can be directly retrieved from the component (we are seeing issues when 
+    // referencing the dist/component.js file, these are fixed when referencing the /component.ts typescript file).
+    // Note: This triggers babel to retranspile all package dependency files during webpack's compilation step.
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       "@azure/communication-ui": path.resolve(__dirname, "../../communication-ui/src")
