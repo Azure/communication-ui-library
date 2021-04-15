a93ebc51e23ac64a81a2a0919bd6e76136d594b3
Nan Jiang <jinan@microsoft.com>
2021-04-15 14:36:27 -0700

    Remove unused files

--- packages/acs-chat-selector/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const Constants = {
-  GUID_FOR_INITIAL_TOPIC_NAME: 'c774da81-94d5-4652-85c7-6ed0e8dc67e6'
-};
--- packages/acs-chat-selector/src/utils/existsTopicName.ts
+++
@@ -1,4 +0,0 @@
-import { Constants } from '../constants';
-
-export const existsTopicName = (topicName?: string): boolean =>
-  !!topicName && topicName !== Constants.GUID_FOR_INITIAL_TOPIC_NAME;
