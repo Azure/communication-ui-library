a4de2d36a35516bbd7ea963802d0eb3e3dce7bd0
Miguel Gamis <miguelgamis@microsoft.com>
2021-04-15 15:59:45 -0700

    refactored typingIndictorSelector

--- packages/acs-chat-selector/src/typingIndicatorSelector.ts
+++ packages/acs-chat-selector/src/typingIndicatorSelector.ts
@@ -14,10 +14,7 @@ const PARTICIPANTS_THRESHOLD = 20;
 const MAXIMUM_LENGTH_OF_TYPING_USERS = 35;
 
 const filterTypingIndicators = (typingIndicators: TypingIndicator[], userId: string): TypingIndicator[] => {
-  if (!typingIndicators) {
-    return [];
-  }
-  const newTypingIndicators: TypingIndicator[] = [];
+  const filteredTypingIndicators: TypingIndicator[] = [];
   const seen = new Set();
   const dateNow = new Date();
   const date5SecondsAgo = new Date(dateNow.getTime() - MINIMUM_TYPING_INTERVAL_IN_MILLISECONDS);
@@ -34,46 +31,60 @@ const filterTypingIndicators = (typingIndicators: TypingIndicator[], userId: str
       continue;
     }
     seen.add(typingIndicator.sender.user.communicationUserId);
-    newTypingIndicators.push(typingIndicator);
+    filteredTypingIndicators.push(typingIndicator);
   }
-  return newTypingIndicators;
+  return filteredTypingIndicators;
+};
+
+const convertSdkTypingIndicatorsToTypingUsers = (
+  typingIndicators: TypingIndicator[],
+  participants: Map<string, ChatParticipant>
+): TypingUser[] => {
+  const typingUsersMentioned: TypingUser[] = [];
+  let countOfUsersMentioned = 0;
+  let totalCharacterCount = 0;
+
+  for (const typingIndicator of typingIndicators) {
+    const displayName = participants.get(typingIndicator.sender.user.communicationUserId)?.displayName ?? 'unknown';
+    countOfUsersMentioned += 1;
+    // The typing users above will be separated by ', '. We account for that additional length and with this length in
+    // mind we generate the final string.
+    const additionalCharCount = 2 * (countOfUsersMentioned - 1) + displayName.length;
+    if (totalCharacterCount + additionalCharCount <= MAXIMUM_LENGTH_OF_TYPING_USERS || countOfUsersMentioned === 1) {
+      typingUsersMentioned.push({ prefixImageUrl: '', displayName });
+      totalCharacterCount += additionalCharCount;
+    } else {
+      break;
+    }
+  }
+
+  return typingUsersMentioned;
 };
 
 export const typingIndicatorSelector = reselect.createSelector(
   [getTypingIndicators, getParticipants, getUserId],
   (typingIndicators: TypingIndicator[], participants: Map<string, ChatParticipant>, userId: string) => {
-    // filter out typing indicators that are from the duplicate users or current user as well as those are older than threshold
+    // filter typing indicators to remove those that are from the duplicate users or current user as well as those older than a threshold
     const filteredTypingIndicators = filterTypingIndicators(typingIndicators, userId);
 
     if (filteredTypingIndicators.length === 0 || participants.size >= PARTICIPANTS_THRESHOLD) {
       return { typingUsers: [], typingString: '' };
     }
 
-    const countOfUsersTyping = filteredTypingIndicators.length;
-    let typingString = filteredTypingIndicators.length > 1 ? ' are typing...' : ' is typing...';
+    const typingUsersMentioned: TypingUser[] = convertSdkTypingIndicatorsToTypingUsers(
+      filteredTypingIndicators,
+      participants
+    );
 
-    let countOfUsersMentioned = 0;
-    let totalCharacterCount = 0;
-
-    const typingUsers: TypingUser[] = [];
-    for (const typingIndicator of filteredTypingIndicators) {
-      const displayName = participants.get(typingIndicator.sender.user.communicationUserId)?.displayName ?? 'unknown';
-      countOfUsersMentioned += 1;
-      // The typing users above will be separated by ', '. We account for that additional length and with this length in
-      // mind we generate the final string.
-      const additionalCharCount = 2 * (countOfUsersMentioned - 1) + displayName.length;
-      if (totalCharacterCount + additionalCharCount <= MAXIMUM_LENGTH_OF_TYPING_USERS || countOfUsersMentioned === 1) {
-        typingUsers.push({ prefixImageUrl: '', displayName });
-        totalCharacterCount += additionalCharCount;
-      } else {
-        const countOthers = countOfUsersTyping - countOfUsersMentioned + 1;
-        typingString = ` and ${countOthers} other ${countOthers === 1 ? '' : 's'}` + typingString;
-        break;
-      }
+    let typingString = '';
+    const countOfUsersNotMentioned = filteredTypingIndicators.length - typingUsersMentioned.length;
+    if (countOfUsersNotMentioned > 0) {
+      typingString = ` and ${countOfUsersNotMentioned} other${countOfUsersNotMentioned === 1 ? '' : 's'}`;
     }
+    typingString += filteredTypingIndicators.length > 1 ? ' are typing...' : ' is typing...';
 
     return {
-      typingUsers: typingUsers,
+      typingUsers: typingUsersMentioned,
       typingString: typingString
     };
   }
