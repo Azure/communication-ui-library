89c5c249889d62bfe166efab02701a0b34df5523
Miguel Gamis <miguelgamis@microsoft.com>
2021-04-16 10:13:40 -0700

    eslint fix

--- packages/acs-chat-selector/src/typingIndicatorSelector.ts
+++ packages/acs-chat-selector/src/typingIndicatorSelector.ts
@@ -1,6 +1,9 @@
 // © Microsoft Corporation. All rights reserved.
 
-import { getTypingIndicators, getParticipants, getUserId } from './baseSelectors';
+// @ts-ignore
+import { ChatClientState } from '@azure/acs-chat-declarative';
+// @ts-ignore
+import { BaseSelectorProps, getTypingIndicators, getParticipants, getUserId } from './baseSelectors';
 import * as reselect from 'reselect';
 import { ChatParticipant } from '@azure/communication-chat';
 import { TypingIndicator } from '@azure/acs-chat-declarative';
