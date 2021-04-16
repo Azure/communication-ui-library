adead07d7687c07c421983790fe74b5df257269c
Eason Yang <easony@microsoft.com>
2021-04-16 16:14:42 -0700

    ChatScreen to new architecture

--- packages/communication-ui/src/consumers/index.ts
+++ packages/communication-ui/src/consumers/index.ts
@@ -8,7 +8,6 @@ export * from './MapToVideoProps';
 export * from './MapToLocalDeviceSettingsProps';
 
 export * from './MapToChatMessageProps';
-export * from './MapToChatThreadProps';
 export * from './MapToErrorsProps';
 export * from './MapToErrorBarProps';
 export * from './MapToSendBoxProps';
