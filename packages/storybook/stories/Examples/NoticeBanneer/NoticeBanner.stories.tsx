8878c130bda83cf410b4c983bbca3b1fd3e493ea
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-04-21 16:16:26 -0700

    Remove knobs and overlays

--- packages/storybook/stories/Examples/NoticeBanneer/NoticeBanner.stories.tsx
+++ packages/storybook/stories/Examples/NoticeBanneer/NoticeBanner.stories.tsx
@@ -1,4 +1,3 @@
-import { boolean, text } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/react/types-6-0';
 import React from 'react';
 import { EXAMPLES_FOLDER_PREFIX } from '../../constants';
@@ -6,10 +5,7 @@ import { CallComponent } from './snippets/CallComponent.snippet';
 import { getDocs } from './Docs';
 
 export const Component: () => JSX.Element = () => {
-  const callStateText = text('Call State Text', 'Waiting for others to join');
-  const isVideoReady = boolean('Show Video', false);
-
-  return <CallComponent isVideoReady={isVideoReady} callStateText={callStateText} />;
+  return <CallComponent />;
 };
 
 export default {
