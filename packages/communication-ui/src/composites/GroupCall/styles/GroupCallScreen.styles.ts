d4724016f54689707eeba6d4872a240feed47b86
Miguel Gamis <miguelgamis@microsoft.com>
2021-04-22 17:58:02 -0700

    removed unused import

--- packages/communication-ui/src/composites/GroupCall/styles/GroupCallScreen.styles.ts
+++ packages/communication-ui/src/composites/GroupCall/styles/GroupCallScreen.styles.ts
@@ -1,6 +1,6 @@
 // © Microsoft Corporation. All rights reserved.
 
-import { IStackItemStyles, IStackStyles, getTheme, mergeStyles } from '@fluentui/react';
+import { IStackItemStyles, IStackStyles, mergeStyles } from '@fluentui/react';
 
 const headerShadow = 'rgba(0, 0, 0, 0.133) 0px 1.6px 3.6px 0px, rgba(0, 0, 0, 0.11) 0px 0.3px 0.9px 0px';
 
