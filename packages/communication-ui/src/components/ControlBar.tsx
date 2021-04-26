5d100d714b295aa24ad0a1bdaac245c4f4fdc9db
Miguel Gamis <miguelgamis@microsoft.com>
2021-04-26 15:18:36 -0700

    getPerceptualBrightnessOfHexColor refactored

--- packages/communication-ui/src/components/ControlBar.tsx
+++ packages/communication-ui/src/components/ControlBar.tsx
@@ -198,24 +198,25 @@ export interface ControlBarProps {
 }
 
 const isDarkThemed = (theme: Theme): boolean => {
-  const blackBrightness = getPerceptualBrightnessOfHexColor(theme.palette.black);
-  const whiteBrightness = getPerceptualBrightnessOfHexColor(theme.palette.white);
-  if (Number.isNaN(blackBrightness) || Number.isNaN(whiteBrightness)) {
+  const themeBlackBrightness = getPerceptualBrightnessOfHexColor(theme.palette.black);
+  const themeWhiteBrightness = getPerceptualBrightnessOfHexColor(theme.palette.white);
+  if (Number.isNaN(themeBlackBrightness) || Number.isNaN(themeWhiteBrightness)) {
     return false;
   }
-  return blackBrightness > whiteBrightness;
+  return themeBlackBrightness > themeWhiteBrightness;
 };
 
-// Will return Nan if hexColor is not a hex code
 const getPerceptualBrightnessOfHexColor = (hexColor: string): number => {
-  if (hexColor.substring(0, 1) !== '#' || hexColor.length !== 7) {
+  // return NaN if hexColor is not a hex code
+  if (!/^#[0-9A-Fa-f]{6}$/i.test(hexColor)) {
     return NaN;
   }
   const r = parseInt(hexColor.substring(1, 3), 16);
   const g = parseInt(hexColor.substring(3, 5), 16);
   const b = parseInt(hexColor.substring(5, 7), 16);
 
-  return r * 2 + g * 3 + b;
+  // arithmetic mean μ of the red, green, and blue color coordinates. Source: https://en.wikipedia.org/wiki/Brightness
+  return (r + g + b) / 3;
 };
 
 /**
