8b2ce4ca50fd31908c48aa4ac9ec8b4f3dfa8dfc
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-04-27 16:10:54 -0700

    Document state machine

--- packages/storybook/stories/Examples/TeamsInterop/snippets/TeamsInterop.snippet.tsx
+++ packages/storybook/stories/Examples/TeamsInterop/snippets/TeamsInterop.snippet.tsx
@@ -19,39 +19,72 @@ export interface TeamsInteropHistory {
 //
 // Returns null if no banner needs to be displayed.
 export function bannerMessage(props: TeamsInteropHistory): JSX.Element | null {
+  /*
+  This function implements a single transition in a state machine.
+  Nodes of the state machine correspond to the possible values of the TeamsInterp tuple:
+  (recordingEnabled, transcriptionEnabled).
+  Thus there are 4 distinct states.
+
+  There are 12 transitions between the 4 states.
+  The diagram below omits the transitions 2->3, 3->2, 1->4 and 4->1 for clarity.
+
+  1.                                                       2.
+    ┌────────────────┐ 1->2              ┌────────────────┐
+    │ ! recording    ├──────────────────►│ ! recording    │
+    │ ! transcribing │                   │   transcribing │
+    └──┬─────────────┘◄──────────────────┴─┬──────────────┘
+      │1->3       ▲               2->1    │2->4        ▲
+      │           │                       │            │
+      │           │                       │            │
+      │           │                       │            │
+      ▼      3->1 │                       ▼       4->2 │
+    ┌──────────────┴─┐ 3->4              ┌──────────────┴─┐
+    │   recording    ├──────────────────►│   recording    │
+    │ ! transcribing │                   │   transcribing │
+    └────────────────┘◄──────────────────┴────────────────┘
+  3.                               4->3                    4.
+
+  The implementation consists of nested if blocks. The outer conditional selects the source node
+  and the inner conditional selects the transition out of the node.
+  */
   const prev = props.teamsInteropPrevious;
   const cur = props.teamsInteropCurrent;
 
-  if (!prev.recordingEnabled && !prev.transcriptionEnabled) {
-    if (cur.recordingEnabled && cur.transcriptionEnabled) {
-      return (
-        <>
-          <b>Recording and transcription have started.</b> By joining, you are giving consent for this meeting to be
-          transcribed.
-          <PrivacyPolicy />
-        </>
-      );
-    }
+  // Source:  4
+  if (prev.recordingEnabled && prev.transcriptionEnabled) {
+    // Transition: 4 -> 3
     if (cur.recordingEnabled && !cur.transcriptionEnabled) {
       return (
         <>
-          <b>Recording has started.</b> By joining, you are giving consent for this meeting to be transcribed.
+          <b>Transcription has stopped.</b> You are now only recording this meeting.
           <PrivacyPolicy />
         </>
       );
     }
+    // Transition: 4 -> 2
     if (!cur.recordingEnabled && cur.transcriptionEnabled) {
       return (
         <>
-          <b>Transcription has started.</b> By joining, you are giving consent for this meeting to be transcribed.
+          <b>Recording has stopped.</b> You are now only transcribing this meeting.
           <PrivacyPolicy />
+        </>
+      );
+    }
+    // Transition: 4 -> 1
+    if (!cur.recordingEnabled && !cur.transcriptionEnabled) {
+      return (
+        <>
+          <b>Recording and transcription are being saved. </b> Recording and transcription have stopped.
+          <LearnMore />
         </>
       );
     }
     return null;
   }
 
+  // Source: 3
   if (prev.recordingEnabled && !prev.transcriptionEnabled) {
+    // Transition: 3 -> 4
     if (cur.recordingEnabled && cur.transcriptionEnabled) {
       return (
         <>
@@ -61,14 +94,7 @@ export function bannerMessage(props: TeamsInteropHistory): JSX.Element | null {
         </>
       );
     }
-    if (!cur.recordingEnabled && !cur.transcriptionEnabled) {
-      return (
-        <>
-          <b>Recording is being saved.</b> Recording has stopped.
-          <LearnMore />
-        </>
-      );
-    }
+    // Transition: 3 -> 2
     if (!cur.recordingEnabled && cur.transcriptionEnabled) {
       // This could be considered either:
       // - Recording was stopped
@@ -81,10 +107,21 @@ export function bannerMessage(props: TeamsInteropHistory): JSX.Element | null {
         </>
       );
     }
+    // Transition: 3 -> 1
+    if (!cur.recordingEnabled && !cur.transcriptionEnabled) {
+      return (
+        <>
+          <b>Recording is being saved.</b> Recording has stopped.
+          <LearnMore />
+        </>
+      );
+    }
     return null;
   }
 
+  // Source:  2
   if (!prev.recordingEnabled && prev.transcriptionEnabled) {
+    // Transition: 3 -> 4
     if (cur.recordingEnabled && cur.transcriptionEnabled) {
       return (
         <>
@@ -94,14 +131,7 @@ export function bannerMessage(props: TeamsInteropHistory): JSX.Element | null {
         </>
       );
     }
-    if (!cur.recordingEnabled && !cur.transcriptionEnabled) {
-      return (
-        <>
-          <b>Transcription is being saved.</b> Transcription has stopped.
-          <LearnMore />
-        </>
-      );
-    }
+    // Transition: 3 -> 2
     if (!cur.recordingEnabled && cur.transcriptionEnabled) {
       // This could be considered either:
       // - Transcription was stopped
@@ -114,36 +144,51 @@ export function bannerMessage(props: TeamsInteropHistory): JSX.Element | null {
         </>
       );
     }
-    return null;
-  }
-
-  if (prev.recordingEnabled && prev.transcriptionEnabled) {
-    if (cur.recordingEnabled && !cur.transcriptionEnabled) {
-      return (
-        <>
-          <b>Transcription has stopped.</b> You are now only recording this meeting.
-          <PrivacyPolicy />
-        </>
-      );
-    }
-    if (!cur.recordingEnabled && cur.transcriptionEnabled) {
-      return (
-        <>
-          <b>Recording has stopped.</b> You are now only transcribing this meeting.
-          <PrivacyPolicy />
-        </>
-      );
-    }
+    // Transition: 3 -> 1
     if (!cur.recordingEnabled && !cur.transcriptionEnabled) {
       return (
         <>
-          <b>Recording and transcription are being saved. </b> Recording and transcription have stopped.
+          <b>Transcription is being saved.</b> Transcription has stopped.
           <LearnMore />
         </>
       );
     }
     return null;
   }
+
+  // Source: 1
+  if (!prev.recordingEnabled && !prev.transcriptionEnabled) {
+    // Transition: 1 -> 4
+    if (cur.recordingEnabled && cur.transcriptionEnabled) {
+      return (
+        <>
+          <b>Recording and transcription have started.</b> By joining, you are giving consent for this meeting to be
+          transcribed.
+          <PrivacyPolicy />
+        </>
+      );
+    }
+    // Transition: 1 -> 3
+    if (cur.recordingEnabled && !cur.transcriptionEnabled) {
+      return (
+        <>
+          <b>Recording has started.</b> By joining, you are giving consent for this meeting to be transcribed.
+          <PrivacyPolicy />
+        </>
+      );
+    }
+    // Transition: 1 -> 2
+    if (!cur.recordingEnabled && cur.transcriptionEnabled) {
+      return (
+        <>
+          <b>Transcription has started.</b> By joining, you are giving consent for this meeting to be transcribed.
+          <PrivacyPolicy />
+        </>
+      );
+    }
+    return null;
+  }
+
   return null;
 }
 
