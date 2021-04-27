b82c9617d1a0548e4c01edcacbd9bd26d60528a1
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2021-04-27 15:05:45 -0700

    Custom theme tool in storybook. ControlBar theming remap when dark and floating. (#175)
    * ControlBar boolean prop added to remap background if floating layout and dark theme is used.
    
    * using neutralQuaternaryAlt for floating dark ControlBar. polished off Calling composite.
    
    * storybook tests updated
    
    * removed unused import
    
    * updated storybook tests
    
    * updated storybook tests
    
    * revert Lobby story changes
    
    * MediaGallery video hints no longer using getTheme
    
    * updated storybook tests
    
    * revert Lobby story changes
    
    * updated storybook tests
    
    * Custom theme tool added to storybook
    
    * fixed background color logic for ControlBar story
    
    * wrote function to check if theme is dark in ControlBar
    
    * if fluentTheme is not provided or if palette property is missing then default to light theme
    
    * JSON validation now accepting palette object only
    
    * added back telemetry
    
    * updated pnpm-lock.yaml
    
    * proper merge of package json with main
    
    * getPerceptualBrightnessOfHexColor refactored
    
    * Logging to console if theme JSON cannot be parsed
    
    * moved isDarkThemed function to themeUtils
    
    * remove WithTheme file

---
+++ packages/storybook/.storybook/manager.tsx
@@ -0,0 +1,26 @@
+import { create } from '@storybook/theming';
+import addons, { types } from '@storybook/addons';
+import React from 'react';
+import { ThemeToolTipWithPanel } from './ThemeToolTipWithPanel';
+import { initTelemetry } from './telemetry';
+
+initTelemetry();
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Azure Communication Services - UI Toolkit'
+  }),
+  enableShortcuts: false
+});
+
+addons.register('toolbar', () => {
+  addons.add('toolbar-addon/theme', {
+    title: 'Theme',
+    //👇 Sets the type of UI element in Storybook
+    type: types.TOOL,
+    //👇 Shows the Toolbar UI element if either the Canvas or Docs tab is active
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
+    render: ({ active }) => <ThemeToolTipWithPanel active={active} />
+  });
+});
