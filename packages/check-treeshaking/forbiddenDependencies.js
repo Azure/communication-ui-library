ff90a03c52962a3e8430cb692feece34cea53370
Dominik Messinger <domessin@microsoft.com>
2021-04-30 19:18:43 -0700

    New tool to check dependencies for optimal treeshaking

---
+++ packages/check-treeshaking/forbiddenDependencies.js
@@ -0,0 +1,8 @@
+// © Microsoft Corporation. All rights reserved.
+module.exports = {
+  '../communication-ui/dist/dist-esm/components/SendBox.js': ['@azure/communication-chat'],
+  '../acs-chat-declarative/dist/dist-esm/index.js': ['@azure/communication-ui', '@fluentui', 'react'],
+  '../acs-calling-declarative/dist/dist-esm/index.js': ['@azure/communication-ui', '@fluentui', 'react'],
+  '../acs-chat-selector/dist/dist-esm/index.js': ['@azure/communication-ui'],
+  '../acs-calling-selector/dist/dist-esm/index.js': ['@azure/communication-ui']
+};
