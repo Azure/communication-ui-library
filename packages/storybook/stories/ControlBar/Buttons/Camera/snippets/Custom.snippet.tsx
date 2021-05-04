2194cf2a483030ef2b89ea36ce7df925c66e896f
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-05-04 12:00:57 -0700

    Fix strings for Camera example

--- packages/storybook/stories/ControlBar/Buttons/Camera/snippets/Custom.snippet.tsx
+++ packages/storybook/stories/ControlBar/Buttons/Camera/snippets/Custom.snippet.tsx
@@ -5,45 +5,29 @@ import { CameraButton } from '@azure/communication-ui';
 export const CustomCameraButtonExample: () => JSX.Element = () => {
   const customOnRenderIcon = (props?: IButtonProps): JSX.Element => {
     if (props?.checked) {
-      return <Icon key={'micCustomIcon1'} iconName={'Camera'} style={{ color: 'green', fontSize: '25px' }} />;
+      return <Icon iconName={'Camera'} style={{ color: 'green', fontSize: '25px' }} />;
     }
 
-    return (
-      <Icon
-        key={'micCustomIcon2'}
-        iconName={'Camera'}
-        style={{ color: 'red', fontSize: '25px', fontWeight: 'bolder' }}
-      />
-    );
+    return <Icon iconName={'Camera'} style={{ color: 'red', fontSize: '25px', fontWeight: 'bolder' }} />;
   };
 
   const customOnRenderText = (props?: IButtonProps): JSX.Element => {
     if (props?.checked) {
-      return (
-        <Text key={'micCustomText'} style={{ fontStyle: 'italic' }}>
-          unmuted
-        </Text>
-      );
+      return <Text style={{ fontStyle: 'italic' }}>Turn off</Text>;
     }
 
-    return <Label key={'micCustomLabel'}>muted</Label>;
+    return <Label key={'micCustomLabel'}>Turn on</Label>;
   };
 
   return (
     <Stack horizontal horizontalAlign={'center'}>
       <CameraButton
-        key={'micCustomBtn1'}
         checked={true}
         showLabel={true}
         onRenderIcon={customOnRenderIcon}
         onRenderText={customOnRenderText}
       />
-      <CameraButton
-        key={'micCustomBtn2'}
-        showLabel={true}
-        onRenderIcon={customOnRenderIcon}
-        onRenderText={customOnRenderText}
-      />
+      <CameraButton showLabel={true} onRenderIcon={customOnRenderIcon} onRenderText={customOnRenderText} />
     </Stack>
   );
 };
