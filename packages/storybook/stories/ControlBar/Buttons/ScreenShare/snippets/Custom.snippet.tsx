15d8f97590dec46df5f1384d9a0b493ceee38bf6
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-05-04 13:03:10 -0700

    Fix screenshare icon

--- packages/storybook/stories/ControlBar/Buttons/ScreenShare/snippets/Custom.snippet.tsx
+++ packages/storybook/stories/ControlBar/Buttons/ScreenShare/snippets/Custom.snippet.tsx
@@ -5,45 +5,29 @@ import { ScreenShareButton } from '@azure/communication-ui';
 export const CustomScreenShareButtonExample: () => JSX.Element = () => {
   const customOnRenderIcon = (props?: IButtonProps): JSX.Element => {
     if (props?.checked) {
-      return <Icon key={'micCustomIcon1'} iconName={'ScreenShare'} style={{ color: 'green', fontSize: '25px' }} />;
+      return <Icon iconName={'Presentation'} style={{ color: 'green', fontSize: '25px' }} />;
     }
 
-    return (
-      <Icon
-        key={'micCustomIcon2'}
-        iconName={'ScreenShare'}
-        style={{ color: 'red', fontSize: '25px', fontWeight: 'bolder' }}
-      />
-    );
+    return <Icon iconName={'Presentation'} style={{ color: 'red', fontSize: '25px', fontWeight: 'bolder' }} />;
   };
 
   const customOnRenderText = (props?: IButtonProps): JSX.Element => {
     if (props?.checked) {
-      return (
-        <Text key={'micCustomText'} style={{ fontStyle: 'italic' }}>
-          unmuted
-        </Text>
-      );
+      return <Text style={{ fontStyle: 'italic' }}>sharing screen</Text>;
     }
 
-    return <Label key={'micCustomLabel'}>muted</Label>;
+    return <Label>not sharing screen</Label>;
   };
 
   return (
     <Stack horizontal horizontalAlign={'center'}>
       <ScreenShareButton
-        key={'micCustomBtn1'}
         checked={true}
         showLabel={true}
         onRenderIcon={customOnRenderIcon}
         onRenderText={customOnRenderText}
       />
-      <ScreenShareButton
-        key={'micCustomBtn2'}
-        showLabel={true}
-        onRenderIcon={customOnRenderIcon}
-        onRenderText={customOnRenderText}
-      />
+      <ScreenShareButton showLabel={true} onRenderIcon={customOnRenderIcon} onRenderText={customOnRenderText} />
     </Stack>
   );
 };
