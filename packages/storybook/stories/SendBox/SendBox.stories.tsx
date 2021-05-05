427648c23c93825f93ec9cd80f4c789b7b0e5bd2
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-05-05 11:50:59 -0700

    Flatten story hierarchy for simple stories (#208)
    Many components have a single story. Instead of adding a folder with a single story, the story is now hoisted to an item in the containing folder.
    
    Storybook avoids creating the extra folder as long as there is only one named export from the component module, and the name matches the content path for the story.

--- packages/storybook/stories/SendBox/SendBox.stories.tsx
+++ packages/storybook/stories/SendBox/SendBox.stories.tsx
@@ -3,13 +3,13 @@
 import React from 'react';
 import { boolean, text } from '@storybook/addon-knobs';
 import { getDocs } from './SendBoxDocs';
-import { SendBox } from 'react-components';
+import { SendBox as SendBoxComponent } from 'react-components';
 import { COMPONENT_FOLDER_PREFIX } from '../constants';
 import { Meta } from '@storybook/react/types-6-0';
 
 export default {
-  title: `${COMPONENT_FOLDER_PREFIX}/SendBox`,
-  component: SendBox,
+  title: `${COMPONENT_FOLDER_PREFIX}/Send Box`,
+  component: SendBoxComponent,
   parameters: {
     docs: {
       page: () => getDocs()
@@ -17,10 +17,12 @@ export default {
   }
 } as Meta;
 
-export const SendBoxComponent = (): JSX.Element => {
+// This must be the only named export from this module, and must be named to match the storybook path suffix.
+// This ensures that storybook hoists the story instead of creating a folder with a single entry.
+export const SendBox = (): JSX.Element => {
   return (
     <div style={{ width: '31.25rem' }}>
-      <SendBox
+      <SendBoxComponent
         disabled={boolean('Block button from sending', false, 'Injected by ACS Context')}
         onMessageSend={async (message) => alert(`sent message: ${message} `)}
         onTyping={(): Promise<void> => {
