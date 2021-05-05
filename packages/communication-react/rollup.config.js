7da034c3217f4dd24dc84c5a31af4da7a7b58f77
James Burnside <mail@jamesburnside.com>
2021-05-05 09:51:18 -0700

    remove beachball for the meta package until changelog story  is workeded out - otherwise there will be lots of duplicate chagnelog entries

--- packages/communication-react/rollup.config.js
+++ packages/communication-react/rollup.config.js
@@ -6,9 +6,9 @@ import commonConfig from '../../common/config/rollup/rollup.config';
 export default [
   {
     ...commonConfig(Package),
-    input: './dist/dist-esm/react-components/src/index.js',
+    input: './dist/dist-esm/communication-react/src/index.js',
     output: {
-      file: './dist/dist-cjs/react-components/index.js',
+      file: './dist/dist-cjs/communication-react/index.js',
       format: 'cjs',
       sourcemap: true
     }
