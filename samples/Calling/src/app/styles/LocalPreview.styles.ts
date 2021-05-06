d7b62e0ea82df089d5fc994170ee0d6cd68f8d0b
Dominik <domessin@microsoft.com>
2021-05-06 16:34:10 -0700

    Fix copyright header to MIT and add LICENSE files (#225)

--- samples/Calling/src/app/styles/LocalPreview.styles.ts
+++ samples/Calling/src/app/styles/LocalPreview.styles.ts
@@ -1,4 +1,5 @@
-// © Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
 import { IStackTokens, IToggleStyles, getTheme, mergeStyles, IImageStyles } from '@fluentui/react';
 
