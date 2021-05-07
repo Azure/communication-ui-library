216561e88f87b6bc24e899e1813d5e53ee38e4ff
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-05-07 10:44:49 -0700

    Revert tests to plain string IDs
    This reverts commit 836e0e181c56681ef11fa403d688e3acc246ab3e.

--- packages/acs-chat-selector/src/typingIndicatorSelector.test.ts
+++ packages/acs-chat-selector/src/typingIndicatorSelector.test.ts
@@ -1,6 +1,6 @@
 // © Microsoft Corporation. All rights reserved.
 
-import { TypingIndicator, communicationIdentifierAsKey } from '@azure/acs-chat-declarative';
+import { TypingIndicator } from '@azure/acs-chat-declarative';
 import { typingIndicatorSelector } from './typingIndicatorSelector';
 
 describe('typingIndicatorSelector tests', () => {
@@ -43,24 +43,15 @@ describe('typingIndicatorSelector tests', () => {
         receivedOn: new Date()
       }
     ];
-
     const participants = new Map();
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '1' }), {
-      id: { communicationUserId: '1' },
-      displayName: 'User1'
-    });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '2' }), {
-      id: { communicationUserId: '2' },
-      displayName: 'User2'
-    });
+    participants.set('1', { id: '1', displayName: 'User1' });
+    participants.set('2', { id: '2', displayName: 'User2' });
     const result = typingIndicatorSelector.resultFunc(orderedTypingIndicators, participants, {
       kind: 'communicationUser',
       communicationUserId: '1'
     });
     expect(result.typingUsers.length).toEqual(1);
-    expect(result.typingUsers).toEqual([
-      { userId: { kind: 'communicationUser', communicationUserId: '2' }, displayName: 'User2' }
-    ]);
+    expect(result.typingUsers).toEqual([{ userId: '2', displayName: 'User2' }]);
   });
 
   test('should list filtered typing indicators from oldest to most recent', async (): Promise<void> => {
@@ -91,28 +82,18 @@ describe('typingIndicatorSelector tests', () => {
       }
     ];
     const participants = new Map();
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '2' }), {
-      id: { communicationUserId: '2' },
-      displayName: 'User2'
-    });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '3' }), {
-      id: { communicationUserId: '1' },
-      displayName: 'User3'
-    });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '4' }), {
-      id: { communicationUserId: '2' },
-      displayName: 'User4'
-    });
-
+    participants.set('2', { id: '2', displayName: 'User2' });
+    participants.set('3', { id: '3', displayName: 'User3' });
+    participants.set('4', { id: '4', displayName: 'User4' });
     const result = typingIndicatorSelector.resultFunc(orderedTypingIndicators, participants, {
       kind: 'communicationUser',
       communicationUserId: '1'
     });
     expect(result.typingUsers.length).toEqual(3);
     expect(result.typingUsers).toEqual([
-      { userId: { kind: 'communicationUser', communicationUserId: '4' }, displayName: 'User4' },
-      { userId: { kind: 'communicationUser', communicationUserId: '2' }, displayName: 'User2' },
-      { userId: { kind: 'communicationUser', communicationUserId: '3' }, displayName: 'User3' }
+      { userId: '4', displayName: 'User4' },
+      { userId: '2', displayName: 'User2' },
+      { userId: '3', displayName: 'User3' }
     ]);
   });
 
@@ -147,27 +128,12 @@ describe('typingIndicatorSelector tests', () => {
     participants.set('5', { id: '5', displayName: 'User5' });
     participants.set('6', { id: '6', displayName: 'User6' });
     participants.set('7', { id: '7', displayName: 'User7' });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '5' }), {
-      id: { communicationUserId: '5' },
-      displayName: 'User5'
-    });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '6' }), {
-      id: { communicationUserId: '6' },
-      displayName: 'User6'
-    });
-    participants.set(communicationIdentifierAsKey({ communicationUserId: '7' }), {
-      id: { communicationUserId: '7' },
-      displayName: 'User7'
-    });
-
     const result = typingIndicatorSelector.resultFunc(orderedTypingIndicators, participants, {
       kind: 'communicationUser',
       communicationUserId: '1'
     });
     expect(result.typingUsers.length).toEqual(1);
-    expect(result.typingUsers).toEqual([
-      { userId: { kind: 'communicationUser', communicationUserId: '6' }, displayName: 'User6' }
-    ]);
+    expect(result.typingUsers).toEqual([{ userId: '6', displayName: 'User6' }]);
   });
 
   test('should return empty array if there are 20 participants', async (): Promise<void> => {
