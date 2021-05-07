62acefc29d9326112505b83fc6e18f60434032c4
Eason Yang <easony@microsoft.com>
2021-05-07 14:55:56 -0700

    update header

--- packages/acs-calling-selector/src/videoGallerySelector.ts
+++ packages/acs-calling-selector/src/videoGallerySelector.ts
@@ -1,4 +1,5 @@
-// © Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
 // @ts-ignore
 import { Call, LocalVideoStream, RemoteParticipant, RemoteVideoStream } from '@azure/acs-calling-declarative';
