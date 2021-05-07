40c015f6c6c0ace8d11fdb00b5667911ae959cf7
Nan Jiang <jinan@microsoft.com>
2021-05-07 15:24:29 -0700

    Revert ChatArea

--- samples/Chat/src/app/ChatArea.tsx
+++ samples/Chat/src/app/ChatArea.tsx
@@ -3,7 +3,7 @@
 import { ErrorBar as ErrorBarComponent, SendBox, TypingIndicator, MessageThread } from 'react-components';
 import { connectFuncsToContext, MapToErrorBarProps } from 'react-composites';
 import { Stack } from '@fluentui/react';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { chatAreaContainerStyle, sendBoxParentStyle } from './styles/ChatArea.styles';
 import { usePropsFor } from './hooks/usePropsFor';
 
@@ -26,16 +26,6 @@ export const ChatArea = (props: ChatAreaProps): JSX.Element => {
 
   const onLoadPreviousChatMessages = chatThreadProps.onLoadPreviousChatMessages;
 
-  // This state is a temporary contoso fix and will be the typingUsers prop passed to TypingIndicator
-  const [transientTypingUsers, setTransientTypingUsers] = useState(typingIndicatorProps.typingUsers);
-  // This useEffect is to set transientTypingUsers to be empty after a timeout of no updates from usePropsFor
-  useEffect(() => {
-    setTransientTypingUsers(typingIndicatorProps.typingUsers);
-    setTimeout(() => {
-      setTransientTypingUsers([]);
-    }, 8000); //wait 8000 milliseconds to make sure it cleans up well
-  }, [typingIndicatorProps.typingUsers]);
-
   // Initialize the Chat thread with history messages
   useEffect(() => {
     (async () => {
@@ -48,7 +38,7 @@ export const ChatArea = (props: ChatAreaProps): JSX.Element => {
       <MessageThread {...chatThreadProps} onRenderAvatar={props.onRenderAvatar} numberOfChatMessagesToReload={5} />
       <Stack.Item align="center" className={sendBoxParentStyle}>
         <div style={{ paddingLeft: '0.5rem', paddingRight: '0.5rem' }}>
-          <TypingIndicator typingUsers={transientTypingUsers} />
+          <TypingIndicator {...typingIndicatorProps} />
         </div>
         <ErrorBar />
         <SendBox {...sendBoxProps} />
