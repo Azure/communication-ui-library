74bed81a0701f96251e165c1a390479e810c42fd
Aline Caillaud <alcail@microsoft.com>
2021-05-10 10:03:20 -0700

    update ErrorBar story

--- packages/storybook/stories/ErrorBar/ErrorBar.stories.tsx
+++ packages/storybook/stories/ErrorBar/ErrorBar.stories.tsx
@@ -2,11 +2,50 @@
 // Licensed under the MIT license.
 
 import { CommunicationUiErrorSeverity, ErrorBar as ErrorBarComponent } from '@azure/communication-react';
+import { Canvas, Description, Heading, Props, Source, Title } from '@storybook/addon-docs/blocks';
 import { text, select } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/react/types-6-0';
 import React, { useState } from 'react';
+
 import { COMPONENT_FOLDER_PREFIX } from '../constants';
-import { getDocs } from './ErrorBarDocs';
+import { ErrorBarExample } from './snippets/ErrorBar.snippet';
+import { OtherSeverityErrorBarExample } from './snippets/OtherSeverityErrorBar.snippet';
+
+const ErrorBarExampleText = require('!!raw-loader!./snippets/ErrorBar.snippet.tsx').default;
+const OtherSeverityErrorBarExampleText = require('!!raw-loader!./snippets/OtherSeverityErrorBar.snippet.tsx').default;
+
+const importStatement = `
+import { ErrorBarComponent, CommunicationUiErrorSeverity } from '@azure/communication-react';
+`;
+
+const getDocs: () => JSX.Element = () => {
+  return (
+    <>
+      <Title>ErrorBar</Title>
+      <Description of={ErrorBarComponent} />
+
+      <Heading>Importing</Heading>
+      <Source code={importStatement} />
+
+      <Heading>Example</Heading>
+      <Canvas mdxSource={ErrorBarExampleText}>
+        <ErrorBarExample />
+      </Canvas>
+
+      <Heading>Other severity types</Heading>
+      <Description>
+        The default severity type is ERROR. There are 3 other severity types that can be set on the severity property
+        shown in the example below. `ErrorBar` is not rendered when severity is set to IGNORE.
+      </Description>
+      <Canvas mdxSource={OtherSeverityErrorBarExampleText}>
+        <OtherSeverityErrorBarExample />
+      </Canvas>
+
+      <Heading>ErrorBar Props</Heading>
+      <Props of={ErrorBarComponent} />
+    </>
+  );
+};
 
 // This must be the only named export from this module, and must be named to match the storybook path suffix.
 // This ensures that storybook hoists the story instead of creating a folder with a single entry.
