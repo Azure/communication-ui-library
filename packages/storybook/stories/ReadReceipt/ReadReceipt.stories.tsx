19c24ac0586410fd6149bc39f24bf7c1f065e974
Aline Caillaud <alcail@microsoft.com>
2021-05-10 14:47:34 -0700

    update ReadReceipt story

--- packages/storybook/stories/ReadReceipt/ReadReceipt.stories.tsx
+++ packages/storybook/stories/ReadReceipt/ReadReceipt.stories.tsx
@@ -1,16 +1,64 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { MessageStatus, ReadReceipt as ReadRecieptComponent } from '@azure/communication-react';
+import { MessageStatus, ReadReceipt as ReadReceiptComponent } from '@azure/communication-react';
+import { Provider, teamsTheme } from '@fluentui/react-northstar';
+import { Title, Description, Props, Heading, Source, Canvas } from '@storybook/addon-docs/blocks';
 import { select, text } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/react/types-6-0';
 import React from 'react';
+
 import { COMPONENT_FOLDER_PREFIX } from '../constants';
-import { getDocs } from './ReadReceiptDocs';
+
+const ReadReceiptExampleText = require('!!raw-loader!./snippets/ReadReceipt.snippet.tsx').default;
+
+const importStatement = `import { ReadReceipt, MessageStatus } from '@azure/communication-react';`;
+
+const ExampleReadReceipts: () => JSX.Element = () => (
+  <>
+    <span style={{ margin: '0 5px' }}>
+      <ReadReceiptComponent messageStatus={'delivered'} />
+    </span>
+    <span style={{ margin: '0 5px' }}>
+      <ReadReceiptComponent messageStatus={'seen'} />
+    </span>
+    <span style={{ margin: '0 5px' }}>
+      <ReadReceiptComponent messageStatus={'sending'} />
+    </span>
+    <span style={{ margin: '0 5px' }}>
+      <ReadReceiptComponent messageStatus={'failed'} />
+    </span>
+  </>
+);
+
+const getDocs: () => JSX.Element = () => {
+  return (
+    <>
+      <Title>ReadReceipts</Title>
+      <Description>
+        Read Receipt is used to indicate whether a message has been read, delivered, currently sending, or failed to
+        send.
+      </Description>
+
+      <Heading>Importing</Heading>
+      <Source code={importStatement} />
+
+      <Heading>Example</Heading>
+      <Canvas mdxSource={ReadReceiptExampleText}>
+        <Provider theme={teamsTheme}>
+          <ExampleReadReceipts />
+        </Provider>
+      </Canvas>
+
+      <Heading>Props</Heading>
+      <Props of={ReadReceiptComponent} />
+    </>
+  );
+};
 
 export default {
   title: `${COMPONENT_FOLDER_PREFIX}/Read Receipt`,
-  component: ReadRecieptComponent,
+  component: ReadReceiptComponent,
   parameters: {
     docs: {
       page: () => getDocs()
@@ -22,7 +70,7 @@ export default {
 // This ensures that storybook hoists the story instead of creating a folder with a single entry.
 export const ReadReceipt = (): JSX.Element => {
   return (
-    <ReadRecieptComponent
+    <ReadReceiptComponent
       messageStatus={select<MessageStatus>('Message Status', ['delivered', 'sending', 'seen', 'failed'], 'delivered')}
       deliveredTooltipText={text('Delivered icon tooltip text', 'Sent')}
       sendingTooltipText={text('Sending icon tooltip text', 'Sending')}
