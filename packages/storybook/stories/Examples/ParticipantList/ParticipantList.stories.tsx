0c6db558fd1a953c53faf449f5b7a3d324aa861a
Aline Caillaud <alcail@microsoft.com>
2021-05-10 10:06:55 -0700

    update ParticipantList story

--- packages/storybook/stories/Examples/ParticipantList/ParticipantList.stories.tsx
+++ packages/storybook/stories/Examples/ParticipantList/ParticipantList.stories.tsx
@@ -4,11 +4,49 @@
 import { ParticipantItem } from '@azure/communication-react';
 import { Stack, PersonaPresence } from '@fluentui/react';
 import { MicOffIcon, CallControlPresentNewIcon } from '@fluentui/react-northstar';
+import { Title, Heading, Description, Canvas } from '@storybook/addon-docs/blocks';
 import { boolean, text, select } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/react/types-6-0';
 import React from 'react';
+
 import { EXAMPLES_FOLDER_PREFIX } from '../../constants';
-import { getDocs } from './ParticipantListDocs';
+import { BasicParticipantListExample } from './snippets/BasicParticipantList.snippet';
+import { InteractiveParticipantListExample } from './snippets/InteractiveParticipantList.snippet';
+
+const BasicParticipantListExampleText = require('!!raw-loader!./snippets/BasicParticipantList.snippet.tsx').default;
+const InteractiveParticipantListExampleText = require('!!raw-loader!./snippets/InteractiveParticipantList.snippet.tsx')
+  .default;
+
+export const getDocs: () => JSX.Element = () => {
+  return (
+    <>
+      <Title>Participant List</Title>
+
+      <Heading>Basic example</Heading>
+      <Description>
+        To build a list of [ParticipantItem](./?path=/docs/ui-components-participantitem--participant-item) components,
+        we recommend using the Fluent UI [Stack](https://developer.microsoft.com/en-us/fluentui#/controls/web/stack) as
+        a container like shown in the code below.
+      </Description>
+      <Canvas mdxSource={BasicParticipantListExampleText}>
+        <BasicParticipantListExample />
+      </Canvas>
+
+      <Heading>Interactive example</Heading>
+      <Description>
+        The participant item is designed with a context menu style affordance which allows you to sub menu items for
+        interacting with this participant. For example, let us add menu items and icons to the participants using
+        `menuItems` and `onRenderIcon` properties of
+        [ParticipantItem](./?path=/docs/ui-components-participantitem--participant-item#props) like in the code below.
+        For simplicity, we are using React `useState` to keep the state of every participant to decide which menu items
+        and icons to show. You can now mute and unmute by clicking a participant in the rendered example below.
+      </Description>
+      <Canvas mdxSource={InteractiveParticipantListExampleText}>
+        <InteractiveParticipantListExample />
+      </Canvas>
+    </>
+  );
+};
 
 const onlyUnique = (value: string, index: number, self: string[]): boolean => {
   return self.indexOf(value) === index;
