7636cee63d12db3c74fb4ee9ea7e1b9deb44ff69
Aline Caillaud <alcail@microsoft.com>
2021-05-10 12:12:17 -0700

    update OneToOneCall story

--- packages/storybook/stories/OneToOneCallComposite/OneToOneCallComposite.stories.tsx
+++ packages/storybook/stories/OneToOneCallComposite/OneToOneCallComposite.stories.tsx
@@ -3,6 +3,7 @@
 
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { CommunicationIdentityClient, CommunicationUserToken } from '@azure/communication-identity';
+import { Description, Heading, Props, Source, Title } from '@storybook/addon-docs/blocks';
 import { text } from '@storybook/addon-knobs';
 import { Meta } from '@storybook/react/types-6-0';
 import React, { useEffect, useState } from 'react';
@@ -13,7 +14,63 @@ import {
   COMPOSITE_STRING_REQUIREDCONNECTIONSTRING
 } from '../CompositeStringUtils';
 import { COMPOSITE_EXPERIENCE_CONTAINER_STYLE, COMPOSITE_FOLDER_PREFIX } from '../constants';
-import { getDocs } from './OneToOneCallCompositeDocs';
+
+const importStatement = `
+import { OneToOneCall } from 'react-composites';
+import { Provider, teamsTheme } from '@fluentui/react-northstar';
+import { CommunicationIdentityClient, CommunicationUserToken } from '@azure/communication-identity';
+`;
+
+const exampleCode = `
+const containerStyle = {
+  width: '90vw',
+  height: '90vh'
+};
+
+/**
+ * Helper function to generate a user token using your ACS connection string.
+ * This code should reside in the server-side logic.
+ * WARNING: Never expose/use your ACS Connection String in your front-end code.
+ */
+const createUserToken = async (connectionString: string): Promise<CommunicationUserToken> => {
+  if (!connectionString) {
+    throw new Error('No ACS connection string provided');
+  }
+  const tokenClient = new CommunicationIdentityClient(connectionString);
+  const user = await tokenClient.createUser();
+  const token = await tokenClient.issueToken(user, ['voip']);
+  return token;
+};
+
+export const OneToOneCallComposite: () => JSX.Element = () => {
+  const [token, setToken] = useState<string>('GENERATED_TOKEN_USING_createUserToken()');
+  const [userId, setUserId] = useState<string>('CALLEE_ID');
+
+  return (
+    <Provider theme={teamsTheme} style={containerStyle}>
+      <OneToOneCall displayName={'YOUR_DISPLAY_NAME'} calleeId={userId} token={token} />
+    </Provider>
+  )
+};
+`;
+
+const getDocs: () => JSX.Element = () => {
+  return (
+    <>
+      <Title>OneToOneCall</Title>
+      <Description>OneToOneCall is an one-stop component that you can make a 1-1 call using ACS.</Description>
+
+      <Heading>Importing</Heading>
+      <Source code={importStatement} />
+
+      <Heading>Example Code</Heading>
+      <Source code={exampleCode} />
+
+      <Heading>Props</Heading>
+      <Props of={OneToOneCallComposite} />
+    </>
+  );
+};
 
 export default {
   title: `${COMPOSITE_FOLDER_PREFIX}/One To One Call`,
