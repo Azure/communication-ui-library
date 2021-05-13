5aeba5d3ba08330e21c866fb418c5449a18442ce
James Burnside <mail@jamesburnside.com>
2021-05-13 11:26:45 -0700

    Remove TypingIndicatorEvent now the underlying data type has the received on event (#261)

--- packages/chat-stateful-client/src/ChatClientState.ts
+++ packages/chat-stateful-client/src/ChatClientState.ts
@@ -10,7 +10,7 @@ import {
   isPhoneNumberIdentifier
 } from '@azure/communication-common';
 import { ChatMessageWithStatus } from './types/ChatMessageWithStatus';
-import { TypingIndicatorEvent } from './types/TypingIndicatorEvent';
+import { TypingIndicatorReceivedEvent } from '@azure/communication-signaling';
 
 export type ChatClientState = {
   userId: CommunicationIdentifierKind;
@@ -34,7 +34,7 @@ export type ChatThreadClientState = {
   updateThreadMembersError?: boolean;
   failedMessageIds: string[];
   readReceipts: ChatMessageReadReceipt[];
-  typingIndicators: TypingIndicatorEvent[];
+  typingIndicators: TypingIndicatorReceivedEvent[];
   latestReadTime: Date;
 };
 
