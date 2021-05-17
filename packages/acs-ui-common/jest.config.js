83ff5d8081a56f380dd807342aaa21c1ac9f0510
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-05-17 14:29:29 -0700

    Add acs-ui-common packlet

---
+++ packages/acs-ui-common/jest.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const commonConfig = require('../../common/config/jest/jest.config');
+
+module.exports = {
+  ...commonConfig
+};
