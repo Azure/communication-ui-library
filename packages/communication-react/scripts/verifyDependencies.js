51c74cb366d265ef1d1b6a26dee2a567c81314d1
James Burnside <mail@jamesburnside.com>
2021-05-18 15:30:22 -0700

    Update missing dependencies in @azure/communication-react package (#308)

---
+++ packages/communication-react/scripts/verifyDependencies.js
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * Verify package contains all the dependencies of downstream packlets.
+ */
+
+const fs = require('fs');
+const path = require('path');
+const { exit } = require('process');
+
+const tsConfig = require(path.resolve(__dirname, '../tsconfig.json'));
+const downstreamPacklets = tsConfig['compilerOptions']['paths'];
+
+let allDependencies = new Set();
+let allPeerDependencies = new Set();
+
+for (const packlet of Object.keys(downstreamPacklets)) {
+  // special case while react-composites points to src/index.release and not src/index
+  const relativePath = packlet === 'react-composites' ? '../../' : '../';
+
+  const packletPackageData = require(path.resolve(
+    __dirname,
+    downstreamPacklets[packlet][0],
+    relativePath,
+    'package.json'
+  ));
+  allDependencies = new Set([...allDependencies, ...Object.keys(packletPackageData['dependencies'] || [])]);
+  allPeerDependencies = new Set([...allPeerDependencies, ...Object.keys(packletPackageData['peerDependencies'] || [])]);
+}
+
+if (allDependencies.size === 0) {
+  console.error('VERIFY DEPENDENCIES ERROR: No Dependencies found - something went wrong');
+  exit(1);
+}
+if (allPeerDependencies.size === 0) {
+  console.error('VERIFY DEPENDENCIES ERROR: No Peer Dependencies found - something went wrong');
+  exit(1);
+}
+
+const packageData = require(path.resolve(__dirname, '../package.json'));
+const currentPackageDependencies = packageData['dependencies'];
+for (const dep of allDependencies) {
+  // if the dependency is not listed or is not another internal packlet, throw error
+  if (!currentPackageDependencies[dep] && !downstreamPacklets[dep]) {
+    console.error(
+      `VERIFY DEPENDENCIES ERROR: ${dep} does not exist in @azure/communication-react package dependencies list`
+    );
+    exit(1);
+  }
+}
+
+const currentPackagePeerDependencies = packageData['peerDependencies'];
+for (const peerDep of allPeerDependencies) {
+  if (!currentPackagePeerDependencies[peerDep])
+    throw `VERIFY DEPENDENCIES ERROR: ${peerDep} does not exist in @azure/communication-react peerDependencies list`;
+}
+
+exit(0);
