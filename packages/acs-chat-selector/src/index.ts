624ef4b545e6c9efcddb01677f391ede382101f6
James Burnside <mail@jamesburnside.com>
2021-05-19 07:33:38 -0700

    cleanup meta package index.ts file

--- packages/acs-chat-selector/src/index.ts
+++ packages/acs-chat-selector/src/index.ts
@@ -2,14 +2,12 @@
 // Licensed under the MIT license.
 
 export { createDefaultChatHandlers, createDefaultChatHandlersForComponent } from './handlers/createHandlers';
-export { memoizeFnAll } from './utils/memoizeFnAll';
 export { ChatClientProvider, useChatClient } from './providers/ChatClientProvider';
 export { ChatThreadClientProvider, useChatThreadClient, useThreadId } from './providers/ChatThreadClientProvider';
 export { usePropsFor } from './hooks/usePropsFor';
-export { useSelector } from './hooks/useSelector';
+export { useSelector as useChatSelector } from './hooks/useSelector';
 
 // -- TODO: these exports should be removed in favor of useSelector -- //
-export { useHandlers } from './hooks/useHandlers';
 export { chatThreadSelector } from './chatThreadSelector';
 export { typingIndicatorSelector } from './typingIndicatorSelector';
 export { sendBoxSelector } from './sendBoxSelector';
@@ -18,7 +16,6 @@ export { chatParticipantListSelector } from './chatParticipantListSelector';
 
 export type { ChatClientProviderProps } from './providers/ChatClientProvider';
 export type { ChatThreadClientProviderProps } from './providers/ChatThreadClientProvider';
-export type { FunctionWithKey, CallbackType } from './utils/memoizeFnAll';
 export type { ChatBaseSelectorProps } from './baseSelectors';
 export type { DefaultChatHandlers } from './handlers/createHandlers';
 export type { GetSelector } from './hooks/usePropsFor';
--- packages/communication-react/src/index.ts
+++ packages/communication-react/src/index.ts
@@ -2,35 +2,9 @@
 // Licensed under the MIT license.
 
 export * from '../../acs-ui-common/src';
-export * from '../../calling-stateful-client/src';
 export * from '../../acs-calling-selector/src';
+export * from '../../acs-chat-selector/src';
+export * from '../../calling-stateful-client/src';
 export * from '../../chat-stateful-client/src';
 export * from '../../react-components/src';
 export * from '../../react-composites/src/index.release';
-
-// Manually re-export acs-chat-selector as several methods are duplicates of acs-calling-selector
-// TODO: integrate a better solution for sharing like code across packages
-export {
-  createDefaultChatHandlers,
-  createDefaultChatHandlersForComponent,
-  ChatClientProvider,
-  useChatClient,
-  ChatThreadClientProvider,
-  useChatThreadClient,
-  useThreadId,
-  usePropsFor,
-  useSelector as useChatSelector,
-  chatThreadSelector,
-  typingIndicatorSelector,
-  sendBoxSelector,
-  chatParticipantListSelector
-} from '../../acs-chat-selector/src';
-
-export type {
-  ChatClientProviderProps,
-  ChatThreadClientProviderProps,
-  ChatBaseSelectorProps,
-  DefaultChatHandlers,
-  GetSelector,
-  AreEqual
-} from '../../acs-chat-selector/src';
