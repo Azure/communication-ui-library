9482492a54a17bd72a5ae001661d7454ff9cfc37
James Burnside <mail@jamesburnside.com>
2021-05-20 13:33:51 -0700

    build fix

--- samples/Calling/src/app/selectors/baseSelectors.ts
+++ samples/Calling/src/app/selectors/baseSelectors.ts
@@ -1,16 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Call, CallClientState, DeviceManagerState, IncomingCall } from 'calling-stateful-client';
+import { Call, CallClientState, DeviceManagerState, IncomingCallState } from 'calling-stateful-client';
 import { CallingBaseSelectorProps } from '@azure/acs-calling-selector';
 
 export const getCalls = (state: CallClientState): Map<string, Call> => state.calls;
 
 export const getCallsEnded = (state: CallClientState): Call[] => state.callsEnded;
 
-export const getIncomingCalls = (state: CallClientState): Map<string, IncomingCall> => state.incomingCalls;
+export const getIncomingCalls = (state: CallClientState): Map<string, IncomingCallState> => state.incomingCalls;
 
-export const getIncomingCallsEnded = (state: CallClientState): IncomingCall[] => state.incomingCallsEnded;
+export const getIncomingCallsEnded = (state: CallClientState): IncomingCallState[] => state.incomingCallsEnded;
 
 export const getDeviceManager = (state: CallClientState): DeviceManagerState => state.deviceManager;
 
