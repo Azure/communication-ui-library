7fde346fc15bdad9162dfa892f22d1bc376f262b
James Burnside <mail@jamesburnside.com>
2021-05-21 07:24:55 -0700

    use correct type

--- packages/storybook/stories/MessageThread/snippets/CustomMessageContainer.snippet.tsx
+++ packages/storybook/stories/MessageThread/snippets/CustomMessageContainer.snippet.tsx
@@ -1,17 +1,15 @@
-import { FluentThemeProvider, MessageThread } from '@azure/communication-react';
+import { FluentThemeProvider, MessageThread, MessageThreadStylesProps } from '@azure/communication-react';
 import React from 'react';
 import { GetHistoryWithSystemMessages } from './placeholdermessages';
 
 export const MessageThreadWithCustomMessageContainerExample: () => JSX.Element = () => {
   // Show a customized message containers.
-  const messageThreadStyle = {
-    root: {
-      chatMessageContainer: {
-        fontStyle: 'italic',
-        boxShadow: '0px 3.2px 7.2px rgb(0 0 0 / 13\u0025), 0px 0.6px 1.8px rgb(0 0 0 / 11\u0025)'
-      },
-      systemMessageContainer: { fontWeight: 'bold', border: 'double red' }
-    }
+  const messageThreadStyle: MessageThreadStylesProps = {
+    chatMessageContainer: {
+      fontStyle: 'italic',
+      boxShadow: '0px 3.2px 7.2px rgb(0 0 0 / 13\u0025), 0px 0.6px 1.8px rgb(0 0 0 / 11\u0025)'
+    },
+    systemMessageContainer: { fontWeight: 'bold', border: 'double red' }
   };
 
   return (
