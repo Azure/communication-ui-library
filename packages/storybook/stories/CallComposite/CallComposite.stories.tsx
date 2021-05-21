e474d213a7d349fe30d120677225e4194f7a814b
James Burnside <mail@jamesburnside.com>
2021-05-21 11:20:41 -0700

    fix test command and update snapshots

--- packages/storybook/stories/CallComposite/CallComposite.stories.tsx
+++ packages/storybook/stories/CallComposite/CallComposite.stories.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import { CallComposite } from '@azure/communication-react';
-import { Title, Description, Heading, Source, Props } from '@storybook/addon-docs/blocks';
+import { Title, Description, Heading, Source } from '@storybook/addon-docs/blocks';
 import { Meta } from '@storybook/react/types-6-0';
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
