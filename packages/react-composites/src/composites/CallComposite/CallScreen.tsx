97b23872682d77767395027c16c2fc9ad94e700d
Anjul Garg <anjulgarg@live.com>
2021-05-21 20:29:08 -0700

    Teams Interop Call Denial Handler Error Page (#376)
    * Adding lobby denial error page
    
    * Add callId ref
    
    * Lobby call denial error handler
    
    * Removing console logs
    
    * Change files
    
    Co-authored-by: Nan Jiang <jinan@microsoft.com>

--- packages/react-composites/src/composites/CallComposite/CallScreen.tsx
+++ packages/react-composites/src/composites/CallComposite/CallScreen.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import { Label, Spinner, Stack } from '@fluentui/react';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   activeContainerClassName,
   containerStyles,
@@ -18,6 +18,7 @@ import { isInCall } from '../../utils/SDKUtils';
 import { complianceBannerSelector } from './selectors/complianceBannerSelector';
 import { useAdapter } from './adapter/CallAdapterProvider';
 import { useSelector } from './hooks/useSelector';
+import { getCall } from './selectors/baseSelectors';
 import { callStatusSelector } from './selectors/callStatusSelector';
 import { mediaGallerySelector } from './selectors/mediaGallerySelector';
 import { useHandlers } from './hooks/useHandlers';
@@ -26,26 +27,32 @@ import { CallControls } from './CallControls';
 import { ComplianceBanner } from './ComplianceBanner';
 import { lobbySelector } from './selectors/lobbySelector';
 import { Lobby } from './Lobby';
-import { AzureCommunicationCallAdapter } from './adapter';
+import { AzureCommunicationCallAdapter, CallCompositePage } from './adapter';
 
 export const MINI_HEADER_WINDOW_WIDTH = 450;
 
 export interface CallScreenProps {
   screenWidth: number;
   endCallHandler(): void;
+  callErrorHandler(customPage?: CallCompositePage): void;
   onRenderAvatar?: (props: PlaceholderProps, defaultOnRender: (props: PlaceholderProps) => JSX.Element) => JSX.Element;
 }
 
 const spinnerLabel = 'Initializing call client...';
 
 export const CallScreen = (props: CallScreenProps): JSX.Element => {
-  const { screenWidth, endCallHandler, onRenderAvatar } = props;
+  const { screenWidth, endCallHandler, callErrorHandler, onRenderAvatar } = props;
 
   const [joinedCall, setJoinedCall] = useState<boolean>(false);
 
   // To use useProps to get these states, we need to create another file wrapping Call,
   // It seems unnecessary in this case, so we get the updated states using this approach.
   const { callStatus, isScreenShareOn } = useSelector(callStatusSelector);
+  const call = useSelector(getCall);
+  const currentCallId = useRef('');
+  if (call) {
+    currentCallId.current = call.id;
+  }
 
   const mediaGalleryProps = useSelector(mediaGallerySelector);
   const mediaGalleryHandlers = useHandlers(MediaGallery);
@@ -67,6 +74,16 @@ export const CallScreen = (props: CallScreenProps): JSX.Element => {
     }
     setJoinedCall(true);
   }, [adapter, joinedCall]);
+
+  // Handle Call Join Errors
+  useEffect(() => {
+    const endedCall = adapter.getState().endedCall;
+    if (endedCall && currentCallId.current === endedCall?.id) {
+      if (endedCall?.callEndReason?.code === 0 && endedCall?.callEndReason.subCode === 5854) {
+        callErrorHandler('errorJoiningTeamsMeeting');
+      }
+    }
+  }, [adapter, call, callErrorHandler]);
 
   if ('isTeamsCall' in adapter) {
     const azureAdapter = adapter as AzureCommunicationCallAdapter;
--- packages/react-composites/src/composites/CallComposite/selectors/baseSelectors.ts
+++ packages/react-composites/src/composites/CallComposite/selectors/baseSelectors.ts
@@ -3,14 +3,14 @@
 
 import { CallState as SDKCallStatus } from '@azure/communication-calling';
 import { CallState, DeviceManagerState } from 'calling-stateful-client';
-import { CallAdapterState } from '../adapter/CallAdapter';
+import { CallAdapterState, CallCompositePage } from '../adapter/CallAdapter';
 
 export const getCall = (state: CallAdapterState): CallState | undefined => state.call;
 export const getCallStatus = (state: CallAdapterState): SDKCallStatus => state.call?.state ?? 'None';
 export const getDeviceManager = (state: CallAdapterState): DeviceManagerState => state.devices;
 export const getIsScreenShareOn = (state: CallAdapterState): boolean => state.call?.isScreenSharingOn ?? false;
 export const getIsPreviewCameraOn = (state: CallAdapterState): boolean => isPreviewOn(state.devices);
-export const getPage = (state: CallAdapterState): 'configuration' | 'call' => state.page;
+export const getPage = (state: CallAdapterState): CallCompositePage => state.page;
 export const getLocalMicrophoneEnabled = (state: CallAdapterState): boolean => state.isLocalPreviewMicrophoneEnabled;
 export const getDisplayName = (state: CallAdapterState): string | undefined => state.displayName;
 export const getIdentifier = (state: CallAdapterState): string => state.userId;
