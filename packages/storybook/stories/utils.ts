3f72c35ad2e6b087b31c3e0d88468edc789ef83b
Alex Kwan (ONENOTE) <alkwa@microsoft.com>
2021-05-22 09:11:18 -0700

    useCamera hook to disable camera when you unmount component

--- packages/storybook/stories/utils.ts
+++ packages/storybook/stories/utils.ts
@@ -1,25 +1,66 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const renderVideoStream = (scalingMode?: string): HTMLElement | null => {
-  if (navigator.mediaDevices?.getUserMedia) {
-    const video = document.createElement('video');
-    video.autoplay = true;
-    video.style.height = '100%';
-    video.style.width = '100%';
-    video.style.objectFit = scalingMode ?? 'cover';
+import { useEffect, useRef, useState } from 'react';
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        video.srcObject = stream;
-      })
-      .catch(function (error) {
-        console.log('Something went wrong!', error);
-      });
-
-    return video;
+const renderVideoStream = async (scalingMode?: string): Promise<HTMLVideoElement | null> => {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    return null;
   }
 
-  return null;
+  const video = document.createElement('video');
+  video.autoplay = true;
+  video.style.height = '100%';
+  video.style.width = '100%';
+  video.style.objectFit = scalingMode ?? 'cover';
+
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+  return video;
+};
+
+const stopVideoStream = (videoElement: HTMLVideoElement): void => {
+  const stream: MediaProvider | null = videoElement.srcObject;
+
+  if (!stream) {
+    return;
+  }
+
+  const mediaStream = stream as MediaStream;
+  mediaStream.getVideoTracks().forEach(function (track) {
+    if (track.readyState === 'live' && track.kind === 'video') {
+      track.stop();
+    }
+  });
+
+  return;
+};
+
+// utility hook to use a video element generated from your camera
+export const useCamera = (useCamera: boolean, scalingMode?: string): HTMLVideoElement | null => {
+  // we need to hold onto the exact same reference of the HTML video element
+  const streamElementRef = useRef<HTMLVideoElement | null>(null);
+  // we need to use a state variable so we can create changes that will force a re-render
+  const [streamElement, setStreamElement] = useState<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (useCamera) {
+      renderVideoStream(scalingMode).then((elem) => {
+        streamElementRef.current = elem;
+        setStreamElement(elem);
+      });
+    } else if (streamElementRef.current) {
+      stopVideoStream(streamElementRef.current);
+      setStreamElement(null);
+    }
+
+    return () => {
+      if (streamElementRef.current) {
+        stopVideoStream(streamElementRef.current);
+      }
+      setStreamElement(null);
+      streamElementRef.current = null;
+    };
+  }, [useCamera]);
+  return streamElement;
 };
