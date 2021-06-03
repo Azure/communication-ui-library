2ed5b2ca740e0536b24262e4aeb0b82b30f7641e
Prathmesh Prabhu <prprabhu@microsoft.com>
2021-06-02 21:23:54 -0700

    Fix sample build

--- samples/Calling/src/app/App.tsx
+++ samples/Calling/src/app/App.tsx
@@ -115,7 +115,7 @@ const App = (): JSX.Element => {
       };
       askPermissionAndQueryDevices();
     }
-  }, [page, token]);
+  }, [page, token, userId]);
 
   /**
    * Routing flow of the sample app: (happy path)
