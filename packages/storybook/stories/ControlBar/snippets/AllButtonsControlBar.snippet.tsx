754f1d2673757c7bde2a5a3a504a06c355b123d4
Aline Caillaud <alcail@microsoft.com>
2021-06-03 18:41:00 -0700

    add ParticipantsButton to ControlBar docs

--- packages/storybook/stories/ControlBar/snippets/AllButtonsControlBar.snippet.tsx
+++ packages/storybook/stories/ControlBar/snippets/AllButtonsControlBar.snippet.tsx
@@ -1,14 +1,53 @@
 import {
+  CallParticipant,
   CameraButton,
   ControlBar,
   EndCallButton,
   FluentThemeProvider,
   MicrophoneButton,
   OptionsButton,
+  ParticipantListProps,
+  ParticipantsButton,
   ScreenShareButton
 } from '@azure/communication-react';
 import { IContextualMenuProps } from '@fluentui/react';
 import React from 'react';
+
+const mockParticipants: CallParticipant[] = [
+  {
+    userId: 'user1',
+    displayName: 'You',
+    state: 'Connected',
+    isMuted: true,
+    isScreenSharing: false
+  },
+  {
+    userId: 'user2',
+    displayName: 'Hal Jordan',
+    state: 'Connected',
+    isMuted: true,
+    isScreenSharing: true
+  },
+  {
+    userId: 'user3',
+    displayName: 'Barry Allen',
+    state: 'Idle',
+    isMuted: false,
+    isScreenSharing: false
+  },
+  {
+    userId: 'user4',
+    displayName: 'Bruce Wayne',
+    state: 'Connecting',
+    isMuted: false,
+    isScreenSharing: false
+  }
+];
+
+const mockParticipantsProps: ParticipantListProps = {
+  participants: mockParticipants,
+  myUserId: 'user1'
+};
 
 export const AllButtonsControlBarExample: () => JSX.Element = () => {
   const exampleOptionsMenuProps: IContextualMenuProps = {
@@ -39,6 +78,11 @@ export const AllButtonsControlBarExample: () => JSX.Element = () => {
           onClick={() => {
             /*handle onClick*/
           }}
+        />
+        <ParticipantsButton
+          participantListProps={mockParticipantsProps}
+          callInvitationURL={'URL to copy'}
+          onMuteAll={() => {}}
         />
         <OptionsButton menuProps={exampleOptionsMenuProps} />
         <EndCallButton
