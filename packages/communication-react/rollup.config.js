1e477eb87a36365a7803adbb796453b3ad970730
Miguel Gamis <miguelgamis@microsoft.com>
2021-06-17 00:18:34 -0700

    simplified rollup

--- packages/communication-react/rollup.config.js
+++ packages/communication-react/rollup.config.js
@@ -3,27 +3,15 @@
 
 import Package from './package.json';
 import commonConfig from '../../common/config/rollup/rollup.config';
-import commonjs from '@rollup/plugin-commonjs';
-import sourcemaps from 'rollup-plugin-sourcemaps';
-import svg from 'rollup-plugin-svg';
-import json from '@rollup/plugin-json';
 
 export default [
   {
     ...commonConfig(Package),
     input: './dist/dist-esm/communication-react/src/index.js',
     output: {
-      dir: './dist/dist-cjs/communication-react',
+      file: './dist/dist-cjs/communication-react/index.js',
       format: 'cjs',
       sourcemap: true
-    },
-    plugins: [
-      commonjs(),
-      sourcemaps(),
-      svg(),
-      json({
-        compact: true
-      })
-    ]
+    }
   }
 ];
--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -2,29 +2,6 @@
 // Licensed under the MIT license.
 
 import Package from './package.json';
-import commonjs from '@rollup/plugin-commonjs';
-import sourcemaps from 'rollup-plugin-sourcemaps';
-import svg from 'rollup-plugin-svg';
-import json from '@rollup/plugin-json';
+import commonConfig from '../../common/config/rollup/rollup.config';
 
-export default {
-  context: 'window',
-  external: [
-    ...(Package.dependencies ? Object.keys(Package.dependencies) : []),
-    ...(Package.peerDependencies ? Object.keys(Package.peerDependencies) : [])
-  ],
-  input: './dist/dist-esm/index.js',
-  output: {
-    dir: './dist/dist-cjs',
-    format: 'cjs',
-    sourcemap: true
-  },
-  plugins: [
-    commonjs(),
-    sourcemaps(),
-    svg(),
-    json({
-      compact: true
-    })
-  ]
-};
+export default commonConfig(Package);
