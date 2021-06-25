a565c9d0b999ea29591424b02e263748a2f4dfd0
Anjul Garg <anjulgarg@live.com>
2021-06-25 15:53:56 -0700

    Moving tests to chat subdir

--- packages/react-composites/tests/browser/chat/ChatComposite.test.ts
+++ packages/react-composites/tests/browser/chat/ChatComposite.test.ts
@@ -1,9 +1,7 @@
-import path from 'path';
 import puppeteer, { Browser, Page } from 'puppeteer';
 import { startServer, stopServer } from './app/server';
 
 const SERVER_URL = 'http://localhost:3000';
-const SCREENSHOTS_DIR = path.join(__dirname, './screenshots');
 const PAGE_VIEWPORT = {
   width: 1200,
   height: 768,
@@ -34,6 +32,5 @@ describe('Chat Composite Puppeteer Tests', () => {
     await page.waitForSelector('#ChatComposite', { visible: true });
     const chatCompositeSelector = await page.$('#ChatComposite');
     expect(chatCompositeSelector).toBeTruthy();
-    await page.screenshot({ path: path.join(SCREENSHOTS_DIR, 'Home.png') });
   });
 });
