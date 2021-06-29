1a0b9a4e1c83dbb53c01d6796121964b05178f96
Anjul Garg <anjulgarg@live.com>
2021-06-29 13:28:36 -0700

    Excluding browser tests from this branch

--- packages/react-composites/jest.config.js
+++ packages/react-composites/jest.config.js
@@ -3,9 +3,13 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const commonConfig = require('../../common/config/jest/jest.config');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
   ...commonConfig,
-  roots: [path.join(__dirname, 'src'), path.join(__dirname, 'tests')]
+  roots: [
+    path.join(__dirname, 'src')
+    // path.join(__dirname, 'tests')
+  ]
 };
