ca65b0feaf10b5d2a135287f68f488f2bc17c480
Anjul Garg <anjulgarg@live.com>
2021-06-29 11:52:24 -0700

    Fixing linting errors

--- packages/react-composites/tests/browser/chat/app/serve.js
+++ packages/react-composites/tests/browser/chat/app/serve.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
 const server = express();
 server.use(express.static('./dist'));
--- packages/react-composites/tests/browser/chat/app/server.ts
+++ packages/react-composites/tests/browser/chat/app/server.ts
@@ -1,21 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 import express from 'express';
+import { Server } from 'http';
 
-let server: any;
+let server: Server;
 const app = express();
 app.use(express.static(__dirname + '/dist'));
 
-export const startServer = () =>
+export const startServer = (): Promise<void> =>
   new Promise((resolve, reject) => {
     server = app.listen(3000, () => {
       console.log('App listening on 3000');
-      resolve(true);
+      resolve();
     });
   });
 
-export const stopServer = () =>
+export const stopServer = (): Promise<void> =>
   new Promise((resolve, reject) => {
     if (!server) reject(false);
     server.close(() => {
-      resolve(true);
+      resolve();
     });
   });
