71b6472445a511aed6587c566637dcf65a445d4d
Porter Nan <jiangnanhello@live.com>
2021-07-07 17:14:29 -0700

    Small adjustments to the doc (#546)
    * Small adjustment to the doc
    
    * Update loadComposite api
    
    * Update guid to grouId
    
    Co-authored-by: Nan Jiang <jinan@microsoft.com>

--- samples/StaticHtmlComposites/src/callComposite.js
+++ samples/StaticHtmlComposites/src/callComposite.js
@@ -5,9 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CallComposite, createAzureCommunicationCallAdapter } from '@azure/communication-react';
 
-export const loadCallComposite = async function (args) {
-  const { containerId, userId, token, groupId, displayName } = args;
+export const loadCallComposite = async function (args, htmlElement) {
+  const { userId, token, groupId, displayName } = args;
   const adapter = await createAzureCommunicationCallAdapter(userId, token, { groupId }, displayName ?? 'anonymous');
-  ReactDOM.render(React.createElement(CallComposite, { adapter }, null), document.getElementById(containerId));
+  ReactDOM.render(React.createElement(CallComposite, { adapter }, null), htmlElement);
   return adapter;
 };
