4d6a2e45ecbe2a461bce30bb63ab277b6921295b
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-07-17 06:12:12 +0000

    Add test for sending a message with wrong thread ID

--- packages/react-composites/tests/browser/chat/ErrorBar.test.ts
+++ packages/react-composites/tests/browser/chat/ErrorBar.test.ts
@@ -1,10 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-
+import { IDS } from '../config';
 import { test } from './fixture';
-import { loadPage, stubMessageTimestamps, waitForCompositeToLoad } from '../utils';
-import { expect } from '@playwright/test';
+import { dataUiId, loadPage, stubMessageTimestamps, waitForCompositeToLoad } from '../utils';
+import { Page, expect } from '@playwright/test';
 
+// All tests in this suite *must be run sequentially*.
+// The tests are not isolated, tests may depend on the final-state of the chat thread after previous tests.
+//
+// We cannot use isolated tests because these are live tests -- the ACS chat service throttles our attempt to create
+// many threads using the same connection string in a short span of time.
 test.describe('ErrorBar is shown correctly', async () => {
   test('with incorrect thread ID', async ({ testBrowser, serverUrl, users }) => {
     const user = users[0];
@@ -13,5 +18,19 @@ test.describe('ErrorBar is shown correctly', async () => {
     await waitForCompositeToLoad(page);
     stubMessageTimestamps(page);
     expect(await page.screenshot()).toMatchSnapshot('error-bar-wrong-thread-id.png');
+
+    await sendAMessage(page);
+    await waitForSendFailure(page);
+    stubMessageTimestamps(page);
+    expect(await page.screenshot()).toMatchSnapshot('error-bar-send-message-with-wrong-thread-id.png');
   });
 });
+
+const sendAMessage = async (page: Page): Promise<void> => {
+  await page.type(dataUiId(IDS.sendboxTextfield), 'No, sir, this will not do.');
+  await page.keyboard.press('Enter');
+};
+
+const waitForSendFailure = async (page: Page): Promise<void> => {
+  await page.waitForSelector(`[data-ui-status="failed"]`);
+};
