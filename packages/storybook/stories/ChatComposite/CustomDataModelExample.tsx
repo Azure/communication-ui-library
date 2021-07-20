082b9f7fb517a067b0260b264e563e5d54ece150
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-07-20 09:46:07 -0700

    center all composite story content

--- packages/storybook/stories/ChatComposite/CustomDataModelExample.tsx
+++ packages/storybook/stories/ChatComposite/CustomDataModelExample.tsx
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { Stack } from '@fluentui/react';
 import { toFlatCommunicationIdentifier } from '@azure/communication-react';
 import { text, radios } from '@storybook/addon-knobs';
 import React, { useState, useEffect, useRef } from 'react';
 import { COMPOSITE_STRING_CONNECTIONSTRING } from '../CompositeStringUtils';
-import { COMPOSITE_EXPERIENCE_CONTAINER_STYLE } from '../constants';
+import { compositeExperienceContainerStyle } from '../constants';
 import {
   CustomDataModelExampleContainer,
   CustomDataModelExampleContainerProps
@@ -43,9 +44,9 @@ export const CustomDataModelExample: () => JSX.Element = () => {
   }, [knobs]);
 
   return (
-    <div style={COMPOSITE_EXPERIENCE_CONTAINER_STYLE}>
+    <Stack horizontalAlign="center" verticalAlign="center" styles={compositeExperienceContainerStyle}>
       {containerProps ? <CustomDataModelExampleContainer {...containerProps} /> : <ConfigHintBanner />}
-    </div>
+    </Stack>
   );
 };
 
