6b6ebaf2bb5cc6c2c4cae9c81db4734b119a035a
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-07-22 12:55:58 -0700

    [Meeting composite] Storybook setup (#600)

---
+++ packages/storybook/stories/MeetingComposite/snippets/Server.snippet.tsx
@@ -0,0 +1,63 @@
+// Contoso server to create a new user and thread.
+
+import { ChatClient, ChatParticipant } from '@azure/communication-chat';
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import { CommunicationIdentityClient } from '@azure/communication-identity';
+import { v1 as createGUID } from 'uuid';
+import { MeetingExampleProps } from './Meeting.snippet';
+
+const getChatThreadFromTeamsLink = (teamsMeetingLink: string): string => {
+  // Get the threadId from the url - this also contains the call locator ID that will be removed in the threadId.split
+  let threadId = teamsMeetingLink.replace('https://teams.microsoft.com/l/meetup-join/', '');
+  // Decode characters that outlook links encode
+  threadId = threadId.replaceAll('%3a', ':').replace('%40', '@');
+  // Extract just the chat guid from the link, stripping away the call locator ID
+  threadId = threadId.split(/^(.*?@thread\.v2)/gm)[1];
+
+  if (!threadId || threadId.length === 0) throw 'Could not get chat thread from teams link';
+
+  return threadId;
+};
+
+const createNewChatThread = async (chatClient: ChatClient, participants: ChatParticipant[]): Promise<string> => {
+  const chatThreadResponse = await chatClient.createChatThread(
+    { topic: 'Meeting with a friendly bot' },
+    { participants }
+  );
+  if (chatThreadResponse.invalidParticipants && chatThreadResponse.invalidParticipants.length > 0) {
+    throw 'Server could not add participants to the chat thread';
+  }
+
+  const chatThread = chatThreadResponse.chatThread;
+  if (!chatThread || !chatThread.id) {
+    throw 'Server could not create chat thread';
+  }
+
+  return chatThread.id;
+};
+
+export const createUserCredentials = async (
+  resourceConnectionString: string,
+  displayName: string,
+  teamsMeetingLink?: string
+): Promise<MeetingExampleProps> => {
+  const tokenClient = new CommunicationIdentityClient(resourceConnectionString);
+  const { user, token } = await tokenClient.createUserAndToken(['voip', 'chat']);
+  const endpointUrl = new URL(resourceConnectionString.replace('endpoint=', '').split(';')[0]).toString();
+  const chatClient = new ChatClient(endpointUrl, new AzureCommunicationTokenCredential(token));
+
+  const locator = teamsMeetingLink ? { meetingLink: teamsMeetingLink } : { groupId: createGUID() };
+
+  const threadId = teamsMeetingLink
+    ? getChatThreadFromTeamsLink(teamsMeetingLink)
+    : await createNewChatThread(chatClient, [{ id: user, displayName: displayName }]);
+
+  return {
+    userId: user,
+    token,
+    displayName,
+    endpointUrl,
+    locator,
+    threadId
+  };
+};
