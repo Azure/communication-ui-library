0159527ee525522546dfb4e77f3f6dd01e9537d7
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-07-22 11:54:24 -0700

    Center composite storybook pages' contents (#607)

--- packages/storybook/stories/ChatComposite/CustomBehavior.tsx
+++ packages/storybook/stories/ChatComposite/CustomBehavior.tsx
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { Stack } from '@fluentui/react';
 import { text } from '@storybook/addon-knobs';
 import React, { useState, useEffect, useRef } from 'react';
 import { COMPOSITE_STRING_CONNECTIONSTRING } from '../CompositeStringUtils';
-import { COMPOSITE_EXPERIENCE_CONTAINER_STYLE } from '../constants';
+import { compositeExperienceContainerStyle } from '../constants';
 import { ContosoChatContainer, ContainerProps } from './CustomBehaviorExampleContainer';
 import { createUserAndThread } from './snippets/Server.snippet';
 import { ConfigHintBanner, addParrotBotToThread } from './snippets/Utils';
@@ -29,9 +30,9 @@ export const CustomBehaviorExample: () => JSX.Element = () => {
   }, [knobs]);
 
   return (
-    <div style={COMPOSITE_EXPERIENCE_CONTAINER_STYLE}>
+    <Stack horizontalAlign="center" verticalAlign="center" styles={compositeExperienceContainerStyle}>
       {containerProps ? <ContosoChatContainer {...containerProps} /> : <ConfigHintBanner />}
-    </div>
+    </Stack>
   );
 };
 
