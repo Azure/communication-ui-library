5d35617b4e21d49a79acb6ac0fe1961c0eb21234
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-07-22 13:07:44 -0700

    [Meeting composite] Add chat and people pane with basic toggle buttons (#604)

--- packages/react-composites/src/composites/MeetingComposite/MeetingComposite.tsx
+++ packages/react-composites/src/composites/MeetingComposite/MeetingComposite.tsx
@@ -1,10 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { PartialTheme, Stack, Theme } from '@fluentui/react';
-import { CallComposite, CallAdapter } from '../CallComposite';
-import { ChatComposite, ChatAdapter } from '../ChatComposite';
+import { CallComposite, CallAdapter, CallCompositePage } from '../CallComposite';
+import { ChatAdapter } from '../ChatComposite';
+import { EmbeddedChatPane, EmbeddedPeoplePane } from './SidePane';
+import { ChatButton } from './ChatButton';
+import { PeopleButton } from './PeopleButton';
+import { CommunicationParticipant } from '@internal/react-components';
+import { CallState } from '@azure/communication-calling';
 
 export type MeetingCompositeProps = {
   callAdapter: CallAdapter;
@@ -18,29 +23,67 @@ export type MeetingCompositeProps = {
   meetingInvitationURL?: string;
 };
 
-const EmbeddedChatPane = (props: { chatAdapter: ChatAdapter; fluentTheme?: PartialTheme | Theme }): JSX.Element => (
-  <ChatComposite adapter={props.chatAdapter} fluentTheme={props.fluentTheme} />
-);
-
-const EmbeddedPeoplePane = (): JSX.Element => <></>;
-
-const SidePane = (props: { children?: React.ReactNode }): JSX.Element => <Stack>{props.children}</Stack>;
-
 export const MeetingComposite = (props: MeetingCompositeProps): JSX.Element => {
   const { callAdapter, fluentTheme } = props;
 
-  const [showChat] = useState(true);
-  const [showPeople] = useState(false);
-  const sidePaneShowing = showChat || showPeople;
+  const [currentCallState, setCurrentCallState] = useState<CallState>();
+  const [currentPage, setCurrentPage] = useState<CallCompositePage>();
+
+  // Future work item: get correct participants
+  const myUserId = callAdapter.getState().userId.communicationUserId;
+  const selfParticipant: CommunicationParticipant = {
+    displayName: callAdapter.getState().displayName,
+    userId: myUserId
+  };
+  const [participants] = useState<CommunicationParticipant[]>([selfParticipant]);
+  callAdapter.onStateChange((newState) => {
+    setCurrentPage(newState.page);
+    setCurrentCallState(newState.call?.state);
+  });
+  const hasJoinedCall =
+    currentPage === 'call' && currentCallState && !['Connecting', 'Ringing', 'InLobby'].includes(currentCallState);
+
+  const [showChat, setShowChat] = useState(false);
+  const [showPeople, setShowPeople] = useState(false);
+
+  const closePane = useCallback(() => {
+    setShowChat(false);
+    setShowPeople(false);
+  }, []);
+
+  const toggleChat = useCallback(() => {
+    setShowPeople(false);
+    setShowChat(!showChat);
+  }, [showChat]);
+
+  const togglePeople = useCallback(() => {
+    setShowChat(false);
+    setShowPeople(!showPeople);
+  }, [showPeople]);
 
   return (
-    <Stack horizontal>
+    <Stack styles={{ root: { width: '100%', height: '100%' } }} horizontal>
       <CallComposite adapter={callAdapter} fluentTheme={fluentTheme} />
-      {sidePaneShowing && (
-        <SidePane>
-          {showChat && <EmbeddedChatPane chatAdapter={props.chatAdapter} fluentTheme={props.fluentTheme} />}
-          {showPeople && <EmbeddedPeoplePane />}
-        </SidePane>
+      {hasJoinedCall && (
+        <Stack>
+          <Stack.Item grow>
+            {showChat && (
+              <EmbeddedChatPane chatAdapter={props.chatAdapter} fluentTheme={props.fluentTheme} onClose={closePane} />
+            )}
+            {showPeople && (
+              <EmbeddedPeoplePane
+                inviteLink={props.meetingInvitationURL}
+                myUserId={myUserId}
+                participants={participants}
+                onClose={closePane}
+              />
+            )}
+          </Stack.Item>
+          <Stack horizontal>
+            <ChatButton checked={showChat} showLabel={true} onClick={toggleChat} />
+            <PeopleButton checked={showPeople} showLabel={true} onClick={togglePeople} />
+          </Stack>
+        </Stack>
       )}
     </Stack>
   );
---
+++ packages/react-composites/src/composites/MeetingComposite/SidePane.tsx
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import React from 'react';
+import { ChatComposite, ChatAdapter } from '../ChatComposite';
+import { CommandBarButton, DefaultButton, PartialTheme, Theme, Stack } from '@fluentui/react';
+import {
+  sidePaneCloseButtonStyles,
+  sidePaneContainerStyles,
+  sidePaneContainerTokens,
+  sidePaneHeaderStyles,
+  sidePaneBodyStyles,
+  peoplePaneContainerTokens,
+  peopleSubheadingStyle
+} from './styles/SidePane.styles';
+import { ParticipantList, CommunicationParticipant } from '@internal/react-components';
+import copy from 'copy-to-clipboard';
+
+const SidePane = (props: { headingText: string; children: React.ReactNode; onClose: () => void }): JSX.Element => {
+  return (
+    <Stack styles={sidePaneContainerStyles} tokens={sidePaneContainerTokens}>
+      <Stack.Item>
+        <Stack horizontal horizontalAlign="space-between" styles={sidePaneHeaderStyles}>
+          <Stack.Item>{props.headingText}</Stack.Item>
+          <CommandBarButton
+            styles={sidePaneCloseButtonStyles}
+            iconProps={{ iconName: 'cancel' }}
+            onClick={props.onClose}
+          />
+        </Stack>
+      </Stack.Item>
+      <Stack.Item styles={sidePaneBodyStyles}>{props.children}</Stack.Item>
+    </Stack>
+  );
+};
+
+export const EmbeddedPeoplePane = (props: {
+  participants: CommunicationParticipant[];
+  myUserId: string;
+  inviteLink?: string;
+  onClose: () => void;
+}): JSX.Element => {
+  const { inviteLink } = props;
+  return (
+    <SidePane headingText={'People'} onClose={props.onClose}>
+      <Stack tokens={peoplePaneContainerTokens}>
+        {inviteLink && (
+          <DefaultButton text="Copy invite link" iconProps={{ iconName: 'Link' }} onClick={() => copy(inviteLink)} />
+        )}
+        <Stack.Item styles={peopleSubheadingStyle}>In this call</Stack.Item>
+        <ParticipantList myUserId={props.myUserId} participants={props.participants} />
+      </Stack>
+    </SidePane>
+  );
+};
+
+export const EmbeddedChatPane = (props: {
+  chatAdapter: ChatAdapter;
+  fluentTheme?: PartialTheme | Theme;
+  onClose: () => void;
+}): JSX.Element => {
+  return (
+    <SidePane headingText={'Chat'} onClose={props.onClose}>
+      <ChatComposite adapter={props.chatAdapter} fluentTheme={props.fluentTheme} />
+    </SidePane>
+  );
+};
