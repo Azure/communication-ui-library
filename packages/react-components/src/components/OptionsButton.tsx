3a67d4364f055b743783f235289cec9095daa514
Miguel Gamis <miguelgamis@microsoft.com>
2021-07-27 12:18:24 -0700

    Video device icon updated in OptionsButton

--- packages/react-components/src/components/OptionsButton.tsx
+++ packages/react-components/src/components/OptionsButton.tsx
@@ -2,8 +2,9 @@
 // Licensed under the MIT license.
 
 import React from 'react';
-import { MoreHorizontal20Filled } from '@fluentui/react-icons';
-import { IContextualMenuItem, ContextualMenuItemType } from '@fluentui/react';
+import { MoreHorizontal20Filled, Video20Regular } from '@fluentui/react-icons';
+import { IContextualMenuItem, ContextualMenuItemType, Theme } from '@fluentui/react';
+import { useTheme } from '../theming';
 import { useLocale } from '../localization';
 import { ControlBarButton, ControlBarButtonProps } from './ControlBarButton';
 
@@ -109,7 +110,8 @@ export interface OptionsButtonProps extends ControlBarButtonProps {
  */
 const generateDefaultMenuProps = (
   props: OptionsButtonProps,
-  strings: OptionsButtonStrings
+  strings: OptionsButtonStrings,
+  theme: Theme
 ): { items: IContextualMenuItem[] } | undefined => {
   const {
     microphones,
@@ -136,7 +138,9 @@ const generateDefaultMenuProps = (
           key: camera.id,
           text: camera.name,
           title: camera.name,
-          iconProps: { iconName: 'Camera' },
+          onRenderIcon: () => (
+            <Video20Regular style={{ color: theme.palette.themePrimary, display: 'flex', alignItems: 'center' }} />
+          ),
           canCheck: true,
           isChecked: camera.id === selectedCamera?.id,
           onClick: () => {
@@ -218,10 +222,11 @@ const onRenderOptionsIcon = (): JSX.Element => (
 export const OptionsButton = (props: OptionsButtonProps): JSX.Element => {
   const { onRenderIcon } = props;
 
+  const theme = useTheme();
   const localeStrings = useLocale().strings.optionsButton;
   const strings = { ...localeStrings, ...props.strings };
 
-  const defaultMenuProps = generateDefaultMenuProps(props, strings);
+  const defaultMenuProps = generateDefaultMenuProps(props, strings, theme);
 
   return (
     <ControlBarButton
