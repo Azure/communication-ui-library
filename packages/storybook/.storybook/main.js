cbaa06a3d5fae3ab1a9e284661938a287cd60f24
Aline Caillaud <alcail@microsoft.com>
2021-08-04 10:46:08 -0700

    fixing PostCss build warning

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -20,6 +20,9 @@ module.exports = {
     },
     '@storybook/addon-controls'
   ],
+  features: {
+    postcss: false,
+  },
   webpackFinal: async (config, { configType }) => {
     // Remove entrypoint size warning (rush build sees warnings as errors). Ideally we should use code splitting to reduce the entry point instead.
     // For more information on how this might be possible see: https://github.com/storybookjs/storybook/issues/6885
