73cf58d9cecf3e93f1b1a1b978eee3fbce414b16
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-08-06 22:12:59 +0000

    Dedupe waitWithBreakCondition

--- packages/calling-stateful-client/src/StatefulCallClient.test.ts
+++ packages/calling-stateful-client/src/StatefulCallClient.test.ts
@@ -27,15 +27,13 @@ import {
   MockCall,
   MockCallAgent,
   MockCommunicationUserCredential,
-  mockoutObjectFreeze,
   MockRecordingCallFeatureImpl,
   MockRemoteParticipant,
   MockRemoteVideoStream,
   MockTranscriptionCallFeatureImpl,
-  MockTransferCallFeatureImpl
+  MockTransferCallFeatureImpl,
+  waitWithBreakCondition
 } from './TestUtils';
-
-mockoutObjectFreeze();
 
 const mockCallId = 'a';
 const mockCallId2 = 'b';
@@ -161,30 +159,6 @@ async function createMockRemoteVideoStreamAndEmitVideoStreamsUpdated(
         ]?.videoStreams ?? {}
       ).length !== 0
   );
-}
-
-function waitMilliseconds(duration: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, duration);
-  });
-}
-
-/**
- * This will wait for up to 4 seconds and break when the given breakCondition is true. The reason for four seconds is
- * that by default the jest timeout for waiting for test is 5 seconds so ideally we want to break this and fail then
- * fail some expects check before the 5 seconds otherwise you'll just get a cryptic 'jest timeout error'.
- *
- * @param breakCondition
- */
-async function waitWithBreakCondition(breakCondition: () => boolean): Promise<void> {
-  for (let i = 0; i < 40; i++) {
-    await waitMilliseconds(100);
-    if (breakCondition()) {
-      break;
-    }
-  }
 }
 
 describe('Stateful call client', () => {
