dc989b87d74373c8c4f91d6d8ecd4ac628eb002b
Miguel Gamis <miguelgamis@microsoft.com>
2021-08-16 02:08:45 -0700

    fix local preview icon

--- packages/react-composites/src/composites/CallComposite/LocalPreview.tsx
+++ packages/react-composites/src/composites/CallComposite/LocalPreview.tsx
@@ -5,26 +5,20 @@ import { VideoOff20Filled } from '@fluentui/react-icons';
 import { Stack, Text } from '@fluentui/react';
 import React, { useCallback } from 'react';
 import { localPreviewContainerStyle, cameraOffLabelStyle, localPreviewTileStyle } from './styles/LocalPreview.styles';
-import { StreamMedia, VideoTile, MicrophoneButton, ControlBar, CameraButton } from '@internal/react-components';
+import {
+  StreamMedia,
+  VideoTile,
+  MicrophoneButton,
+  ControlBar,
+  CameraButton,
+  useTheme
+} from '@internal/react-components';
 import { usePropsFor } from './hooks/usePropsFor';
 import { localPreviewSelector } from './selectors/localPreviewSelector';
 import { useSelector } from './hooks/useSelector';
 import { getLocalMicrophoneEnabled } from './selectors/baseSelectors';
 import { useAdapter } from './adapter/CallAdapterProvider';
 import { devicePermissionSelector } from './selectors/devicePermissionSelector';
-
-const onRenderPlaceholder = (): JSX.Element => {
-  return (
-    <Stack style={{ width: '100%', height: '100%' }} verticalAlign="center">
-      <Stack.Item align="center">
-        <VideoOff20Filled primaryFill="currentColor" />
-      </Stack.Item>
-      <Stack.Item align="center">
-        <Text className={cameraOffLabelStyle}>Your camera is turned off.</Text>
-      </Stack.Item>
-    </Stack>
-  );
-};
 
 export const LocalPreview = (): JSX.Element => {
   const cameraButtonProps = usePropsFor(CameraButton);
@@ -37,6 +31,20 @@ export const LocalPreview = (): JSX.Element => {
   const onToggleMic = useCallback(async () => {
     isLocalMicrophoneEnabled ? adapter.mute() : adapter.unmute();
   }, [adapter, isLocalMicrophoneEnabled]);
+
+  const theme = useTheme();
+  const onRenderPlaceholder = useCallback((): JSX.Element => {
+    return (
+      <Stack style={{ width: '100%', height: '100%', color: theme.palette.black }} verticalAlign="center">
+        <Stack.Item align="center">
+          <VideoOff20Filled primaryFill="currentColor" />
+        </Stack.Item>
+        <Stack.Item align="center">
+          <Text className={cameraOffLabelStyle}>Your camera is turned off.</Text>
+        </Stack.Item>
+      </Stack>
+    );
+  }, [theme]);
 
   return (
     <Stack data-ui-id="call-composite-local-preview" className={localPreviewContainerStyle}>
