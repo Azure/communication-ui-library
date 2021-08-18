00c0dda13de28cd759e2827b10517f3e16d09a57
Miguel Gamis <miguelgamis@microsoft.com>
2021-08-18 12:36:05 -0700

    clean up styles

--- packages/react-components/src/components/styles/ParticipantItem.styles.ts
+++ packages/react-components/src/components/styles/ParticipantItem.styles.ts
@@ -13,22 +13,6 @@ export const participantItemContainerStyle = mergeStyles({
   alignItems: 'center'
 });
 
-export const participantItemNameStyle = mergeStyles({
-  fontSize: '0.875rem', // 14px
-  fontWeight: 400,
-  marginTop: '0.3125rem',
-  marginRight: '0.25rem',
-  paddingLeft: '0.25rem',
-  overflowY: 'hidden'
-});
-
-export const participantItemMeStyle = mergeStyles({
-  fontSize: '0.875rem', // 14px
-  fontWeight: 400,
-  color: '#A19F9D',
-  marginTop: '0.3125rem'
-});
-
 export const menuButtonContainerStyle = mergeStyles({
   root: {
     '&:hover': { background: 'none' },
