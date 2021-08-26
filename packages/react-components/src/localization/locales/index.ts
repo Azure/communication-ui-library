9682f8a4351051ae16fcd50204b9061dca1d7166
Miguel Gamis <miguelgamis@microsoft.com>
2021-08-26 14:56:34 -0700

    export other locales by merging locale files with en-US

--- packages/react-components/src/localization/locales/index.ts
+++ packages/react-components/src/localization/locales/index.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { Locale } from '..';
+import { PartialDeep } from 'type-fest';
+import { ComponentStrings, Locale } from '..';
 import _en_US from './en-US/strings.json';
 import _de_DE from './de-DE/strings.json';
 import _es_ES from './es-ES/strings.json';
@@ -16,29 +17,37 @@ import _tr_TR from './tr-TR/strings.json';
 import _zh_CN from './zh-CN/strings.json';
 import _zh_TW from './zh-TW/strings.json';
 
+const createComponentStrings = (localizedStrings: PartialDeep<ComponentStrings>): ComponentStrings => {
+  const strings: ComponentStrings = { ..._en_US };
+  Object.keys(localizedStrings).forEach((key: string) => {
+    strings[key] = { ...strings[key], ...localizedStrings[key] };
+  });
+  return strings;
+};
+
 /** Locale for English (US) */
 export const en_US: Locale = { strings: _en_US };
 /** Locale for German (Germany) */
-export const de_DE: Locale = { strings: _de_DE };
+export const de_DE: Locale = { strings: createComponentStrings(_de_DE) };
 /** Locale for Spanish (Spain) */
-export const es_ES: Locale = { strings: _es_ES };
+export const es_ES: Locale = { strings: createComponentStrings(_es_ES) };
 /** Locale for French (France) */
-export const fr_FR: Locale = { strings: _fr_FR };
+export const fr_FR: Locale = { strings: createComponentStrings(_fr_FR) };
 /** Locale for Italian (Italy) */
-export const it_IT: Locale = { strings: _it_IT };
+export const it_IT: Locale = { strings: createComponentStrings(_it_IT) };
 /** Locale for Japanese (Japan) */
-export const ja_JP: Locale = { strings: _ja_JP };
+export const ja_JP: Locale = { strings: createComponentStrings(_ja_JP) };
 /** Locale for Korean (South Korea) */
-export const ko_KR: Locale = { strings: _ko_KR };
+export const ko_KR: Locale = { strings: createComponentStrings(_ko_KR) };
 /** Locale for Dutch (Netherlands) */
-export const nl_NL: Locale = { strings: _nl_NL };
+export const nl_NL: Locale = { strings: createComponentStrings(_nl_NL) };
 /** Locale for Portuguese (Brazil) */
-export const pt_BR: Locale = { strings: _pt_BR };
+export const pt_BR: Locale = { strings: createComponentStrings(_pt_BR) };
 /** Locale for Russian (Russia) */
-export const ru_RU: Locale = { strings: _ru_RU };
+export const ru_RU: Locale = { strings: createComponentStrings(_ru_RU) };
 /** Locale for Turkish (Turkey) */
-export const tr_TR: Locale = { strings: _tr_TR };
+export const tr_TR: Locale = { strings: createComponentStrings(_tr_TR) };
 /** Locale for Chinese (Mainland China) */
-export const zh_CN: Locale = { strings: _zh_CN };
+export const zh_CN: Locale = { strings: createComponentStrings(_zh_CN) };
 /** Locale for Chinese (Taiwan) */
-export const zh_TW: Locale = { strings: _zh_TW };
+export const zh_TW: Locale = { strings: createComponentStrings(_zh_TW) };
--- packages/react-composites/src/composites/localization/locales/index.ts
+++ packages/react-composites/src/composites/localization/locales/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import { PartialDeep } from 'type-fest';
 import {
   en_US,
   de_DE,
@@ -16,7 +17,7 @@ import {
   zh_CN,
   zh_TW
 } from '@internal/react-components';
-import { CompositeLocale } from '../LocalizationProvider';
+import { CompositeLocale, CompositeStrings } from '../LocalizationProvider';
 import _en_US from './en-US/strings.json';
 import _de_DE from './de-DE/strings.json';
 import _es_ES from './es-ES/strings.json';
@@ -31,68 +32,76 @@ import _tr_TR from './tr-TR/strings.json';
 import _zh_CN from './zh-CN/strings.json';
 import _zh_TW from './zh-TW/strings.json';
 
+const createCompositeStrings = (localizedStrings: PartialDeep<CompositeStrings>): CompositeStrings => {
+  const strings: CompositeStrings = { ..._en_US };
+  Object.keys(localizedStrings).forEach((key: string) => {
+    strings[key] = { ...strings[key], ...localizedStrings[key] };
+  });
+  return strings;
+};
+
 /** Locale for English (US) */
 export const COMPOSITE_LOCALE_EN_US: CompositeLocale = {
   component: en_US,
-  strings: _en_US
+  strings: createCompositeStrings(_en_US)
 };
 /** Locale for German (Germany) */
 export const COMPOSITE_LOCALE_DE_DE: CompositeLocale = {
   component: de_DE,
-  strings: _de_DE
+  strings: createCompositeStrings(_de_DE)
 };
 /** Locale for Spanish (Spain) */
 export const COMPOSITE_LOCALE_ES_ES: CompositeLocale = {
   component: es_ES,
-  strings: _es_ES
+  strings: createCompositeStrings(_es_ES)
 };
 /** Locale for French (France) */
 export const COMPOSITE_LOCALE_FR_FR: CompositeLocale = {
   component: fr_FR,
-  strings: _fr_FR
+  strings: createCompositeStrings(_fr_FR)
 };
 /** Locale for Italian (Italy) */
 export const COMPOSITE_LOCALE_IT_IT: CompositeLocale = {
   component: it_IT,
-  strings: _it_IT
+  strings: createCompositeStrings(_it_IT)
 };
 /** Locale for Japanese (Japan) */
 export const COMPOSITE_LOCALE_JA_JP: CompositeLocale = {
   component: ja_JP,
-  strings: _ja_JP
+  strings: createCompositeStrings(_ja_JP)
 };
 /** Locale for Korean (South Korea) */
 export const COMPOSITE_LOCALE_KO_KR: CompositeLocale = {
   component: ko_KR,
-  strings: _ko_KR
+  strings: createCompositeStrings(_ko_KR)
 };
 /** Locale for Dutch (Netherlands) */
 export const COMPOSITE_LOCALE_NL_NL: CompositeLocale = {
   component: nl_NL,
-  strings: _nl_NL
+  strings: createCompositeStrings(_nl_NL)
 };
 /** Locale for Portuguese (Brazil) */
 export const COMPOSITE_LOCALE_PT_BR: CompositeLocale = {
   component: pt_BR,
-  strings: _pt_BR
+  strings: createCompositeStrings(_pt_BR)
 };
 /** Locale for Russian (Russia) */
 export const COMPOSITE_LOCALE_RU_RU: CompositeLocale = {
   component: ru_RU,
-  strings: _ru_RU
+  strings: createCompositeStrings(_ru_RU)
 };
 /** Locale for Turkish (Turkey) */
 export const COMPOSITE_LOCALE_TR_TR: CompositeLocale = {
   component: tr_TR,
-  strings: _tr_TR
+  strings: createCompositeStrings(_tr_TR)
 };
 /** Locale for Chinese (Mainland China) */
 export const COMPOSITE_LOCALE_ZH_CN: CompositeLocale = {
   component: zh_CN,
-  strings: _zh_CN
+  strings: createCompositeStrings(_zh_CN)
 };
 /** Locale for Chinese (Taiwan) */
 export const COMPOSITE_LOCALE_ZH_TW: CompositeLocale = {
   component: zh_TW,
-  strings: _zh_TW
+  strings: createCompositeStrings(_zh_TW)
 };
