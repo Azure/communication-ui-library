b6054eb89f670267a2c17b4fa3ae22185c8c4576
alcail <82416644+alcail@users.noreply.github.com>
2021-08-26 11:20:43 -0700

    Chat sample based on composite instead of components (#723)
    * fixing missing topics in Storybook
    
    * using Composite in Chat sample
    
    * get Emoji
    
    * Added header for Participants and Leave button
    
    * hide unnecessary control in Storybook
    
    * cleanup to remove unused code

--- samples/Chat/src/app/ChatHeader.tsx
+++ samples/Chat/src/app/ChatHeader.tsx
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React, { Dispatch } from 'react';
-import { DefaultButton, Icon, IconButton, mergeStyles, Pivot, PivotItem, Stack } from '@fluentui/react';
-import { Settings20Filled, Settings20Regular, People20Filled, People20Regular } from '@fluentui/react-icons';
+import React from 'react';
+import { DefaultButton, Icon, IconButton, mergeStyles, Stack } from '@fluentui/react';
+import { People20Filled, People20Regular } from '@fluentui/react-icons';
 import {
   chatHeaderContainerStyle,
   greyIconButtonStyle,
@@ -11,111 +11,49 @@ import {
   largeButtonContainerStyle,
   leaveButtonStyle,
   leaveIcon,
-  pivotItemStyle,
-  pivotItemStyles,
-  topicNameLabelStyle
+  leaveIconStyle,
+  panelButtonStyle
 } from './styles/ChatHeader.styles';
-import { SidePanelTypes } from './SidePanel';
-import { copyIconStyle } from './styles/InviteFooter.styles';
-import { CommunicationParticipant, useTheme } from '@azure/communication-react';
+import { useTheme } from '@azure/communication-react';
 
 export type ChatHeaderProps = {
-  myUserId: string;
-  topicName: string;
-  selectedPane: SidePanelTypes;
-  participants: CommunicationParticipant[];
-  setSelectedPane: Dispatch<SidePanelTypes>;
-  endChatHandler(): void;
-  removeThreadParticipant?: (userId: string) => Promise<void>;
-  updateThreadTopicName: (topicName: string) => Promise<void>;
+  isParticipantsDisplayed: boolean;
+  onEndChat(): void;
+  setShowParticipants(showParticipants: boolean): void;
 };
 
 export const ChatHeader = (props: ChatHeaderProps): JSX.Element => {
   const theme = useTheme();
 
-  const togglePeople = (selectedPane: SidePanelTypes, setSelectedPane: (pane: SidePanelTypes) => void): void => {
-    selectedPane !== SidePanelTypes.People
-      ? setSelectedPane(SidePanelTypes.People)
-      : setSelectedPane(SidePanelTypes.None);
-  };
-
-  const toggleSettings = (selectedPane: SidePanelTypes, setSelectedPane: (pane: SidePanelTypes) => void): void => {
-    selectedPane !== SidePanelTypes.Settings
-      ? setSelectedPane(SidePanelTypes.Settings)
-      : setSelectedPane(SidePanelTypes.None);
-  };
-
-  const togglePivotItem = (item: PivotItem | undefined): void => {
-    if (!item) return;
-    if (item.props.itemKey === SidePanelTypes.Settings) toggleSettings(props.selectedPane, props.setSelectedPane);
-    if (item.props.itemKey === SidePanelTypes.People) togglePeople(props.selectedPane, props.setSelectedPane);
-  };
-
-  const userId = props.myUserId;
-
   const leaveString = 'Leave';
 
   return (
-    <Stack
-      className={chatHeaderContainerStyle}
-      horizontal={true}
-      horizontalAlign="space-between"
-      verticalAlign={'baseline'}
-    >
-      <div className={topicNameLabelStyle}>{props.topicName}</div>
-      <Stack horizontal={true} verticalAlign={'baseline'}>
-        <Pivot
-          onKeyDownCapture={(e) => {
-            if ((e.target as HTMLElement).id === SidePanelTypes.People && e.keyCode === 39) e.preventDefault();
-          }}
-          getTabId={(itemKey: string) => itemKey}
-          onLinkClick={(item) => togglePivotItem(item)}
-          styles={pivotItemStyles}
-          defaultSelectedKey={SidePanelTypes.None}
-          selectedKey={props.selectedPane}
-        >
-          <PivotItem itemKey={SidePanelTypes.None} />
-          {/* To Toggle People's Panel */}
-          <PivotItem
-            itemKey={SidePanelTypes.People}
-            onRenderItemLink={() =>
-              props.selectedPane === SidePanelTypes.People ? (
-                <People20Filled primaryFill="currentColor" className={pivotItemStyle} />
-              ) : (
-                <People20Regular primaryFill="currentColor" className={pivotItemStyle} />
-              )
-            }
+    <Stack className={chatHeaderContainerStyle} horizontal={true} horizontalAlign="end">
+      <Stack horizontal={true} verticalAlign={'center'}>
+        {props.isParticipantsDisplayed ? (
+          <People20Filled
+            className={panelButtonStyle}
+            onClick={() => props.setShowParticipants(!props.isParticipantsDisplayed)}
           />
-          {/* To Toggle Settings's Panel */}
-          <PivotItem
-            itemKey={SidePanelTypes.Settings}
-            onRenderItemLink={() =>
-              props.selectedPane === SidePanelTypes.Settings ? (
-                <Settings20Filled primaryFill="currentColor" className={pivotItemStyle} />
-              ) : (
-                <Settings20Regular primaryFill="currentColor" className={pivotItemStyle} />
-              )
-            }
+        ) : (
+          <People20Regular
+            className={panelButtonStyle}
+            onClick={() => props.setShowParticipants(!props.isParticipantsDisplayed)}
           />
-        </Pivot>
+        )}
         <div className={iconButtonContainerStyle}>
           <IconButton
             iconProps={leaveIcon}
             className={mergeStyles(greyIconButtonStyle, { color: theme.palette.neutralPrimaryAlt })}
-            onClick={() => {
-              props.endChatHandler();
-            }}
+            onClick={() => props.onEndChat()}
           />
         </div>
         <div className={largeButtonContainerStyle}>
           <DefaultButton
             className={mergeStyles(leaveButtonStyle, { color: theme.palette.neutralPrimaryAlt })}
-            onClick={() => {
-              props.removeThreadParticipant && props.removeThreadParticipant(userId);
-              props.endChatHandler();
-            }}
+            onClick={() => props.onEndChat()}
           >
-            <Icon iconName="Leave" className={copyIconStyle} />
+            <Icon iconName={leaveIcon.iconName} className={leaveIconStyle} />
             {leaveString}
           </DefaultButton>
         </div>
