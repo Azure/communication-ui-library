892ac0c4b6f9fe512104d4279680bdd4d51d59d3
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-09-08 18:06:08 +0000

    Add tests for multiple errors etc

--- packages/react-components/src/components/ErrorBar.test.tsx
+++ packages/react-components/src/components/ErrorBar.test.tsx
@@ -4,13 +4,40 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { initializeIcons, MessageBar } from '@fluentui/react';
-import { ErrorBar } from './ErrorBar';
+import { ActiveError, ErrorBar } from './ErrorBar';
 import Enzyme, { ReactWrapper, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 const ONE_DAY_MILLISECONDS = 24 * 3600 * 1000;
 
-describe('ErrorBar dismissal tests for errors with timestamp', () => {
+describe('ErrorBar self-clearing error', () => {
+  beforeAll(() => {
+    Enzyme.configure({ adapter: new Adapter() });
+    initializeIcons();
+  });
+
+  test('error bar is hidden when an error with timestamp is cleared', () => {
+    const root = mountErrorBarWithDefaults();
+    setAccessDeniedErrorAt(root, new Date(Date.now()));
+    expect(messageBarCount(root)).toBe(1);
+    setNoActiveError(root);
+    expect(messageBarCount(root)).toBe(0);
+    setAccessDeniedErrorAt(root, new Date(Date.now()));
+    expect(messageBarCount(root)).toBe(1);
+  });
+
+  test('error bar is hidden when an error without timestamp is cleared', () => {
+    const root = mountErrorBarWithDefaults();
+    setAccessDeniedErrorWithoutTimestamp(root);
+    expect(messageBarCount(root)).toBe(1);
+    setNoActiveError(root);
+    expect(messageBarCount(root)).toBe(0);
+    setAccessDeniedErrorWithoutTimestamp(root);
+    expect(messageBarCount(root)).toBe(1);
+  });
+});
+
+describe('ErrorBar dismissal for errors with timestamp', () => {
   beforeAll(() => {
     Enzyme.configure({ adapter: new Adapter() });
     initializeIcons();
@@ -50,15 +77,15 @@ describe('ErrorBar dismissal tests for errors with timestamp', () => {
   });
 });
 
-describe('ErrorBar dismissal tests for errors without timestamp', () => {
+describe('ErrorBar dismissal for errors without timestamp', () => {
   beforeAll(() => {
     Enzyme.configure({ adapter: new Adapter() });
     initializeIcons();
   });
 
-  it('[xkcd] error can be dimissed', () => {
+  it('error can be dimissed', () => {
     const root = mountErrorBarWithDefaults();
-    setAccessDeniedErrorAt(root);
+    setAccessDeniedErrorWithoutTimestamp(root);
     expect(messageBarCount(root)).toBe(1);
     simulateDismissOneError(root);
     expect(messageBarCount(root)).toBe(0);
@@ -66,18 +93,28 @@ describe('ErrorBar dismissal tests for errors without timestamp', () => {
 
   it('new error after dismissal is not shown', () => {
     const root = mountErrorBarWithDefaults();
-    setAccessDeniedErrorAt(root);
+    setAccessDeniedErrorWithoutTimestamp(root);
     simulateDismissOneError(root);
-    setAccessDeniedErrorAt(root);
+    setAccessDeniedErrorWithoutTimestamp(root);
     expect(messageBarCount(root)).toBe(0);
   });
 
   it('new error after dismissal and intervening non-error is shown', () => {
     const root = mountErrorBarWithDefaults();
-    setAccessDeniedErrorAt(root);
+    setAccessDeniedErrorWithoutTimestamp(root);
     simulateDismissOneError(root);
     setNoActiveError(root);
-    setAccessDeniedErrorAt(root);
+    setAccessDeniedErrorWithoutTimestamp(root);
+    expect(messageBarCount(root)).toBe(1);
+  });
+});
+
+describe('ErrorBar dismissal with multiple errors', () => {
+  it('clearing an error with multiple errors leaves other errors untouched', () => {
+    const root = mountErrorBarWithDefaults();
+    setActiveErrors(root, [{ type: 'accessDenied', timestamp: new Date(Date.now()) }, { type: 'muteGeneric' }]);
+    expect(messageBarCount(root)).toBe(2);
+    simulateDismissOneError(root);
     expect(messageBarCount(root)).toBe(1);
   });
 });
@@ -99,20 +136,21 @@ const mountErrorBarWithDefaults = (): ReactWrapper => {
 
 const messageBarCount = (root: ReactWrapper): number => root.find(MessageBar).length;
 
-/**
- * Dismisses a MessageBar, when we know there is only one error being shown.
- */
 const simulateDismissOneError = (root: ReactWrapper) => {
   const messageBar = root.find(MessageBar).at(0);
   const button = messageBar.find('button').at(0);
   button.simulate('click');
 };
 
-const setAccessDeniedErrorAt = (root: ReactWrapper, timestamp?: Date): void => {
+const setAccessDeniedErrorAt = (root: ReactWrapper, timestamp: Date): void =>
+  setActiveErrors(root, [{ type: 'accessDenied', timestamp }]);
+
+const setAccessDeniedErrorWithoutTimestamp = (root: ReactWrapper): void =>
+  setActiveErrors(root, [{ type: 'accessDenied' }]);
+
+const setActiveErrors = (root: ReactWrapper, activeErrors: ActiveError[]): void => {
   act(() => {
-    root.setProps({
-      activeErrors: [{ type: 'accessDenied', timestamp }]
-    });
+    root.setProps({ activeErrors: activeErrors });
   });
 };
 
