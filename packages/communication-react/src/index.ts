56f6b5bb1508609575aa3f00cb70b4bf330cfe4f
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-16 09:22:05 -0700

    update docu

--- packages/communication-react/src/index.ts
+++ packages/communication-react/src/index.ts
@@ -2,7 +2,9 @@
 // Licensed under the MIT license.
 
 /**
- * Azure Communication Services - UI Library makes it easy for you to build modern communications user experiences using Azure Communication Services. It gives you a library of production-ready UI components that you can drop into your applications:
+ * @azure/communication-react is the npm package that exports functionality for the Azure Communication Services - UI Library.
+ *
+ * This package makes it easy for you to build modern communications user experiences using Azure Communication Services. It gives you a library of production-ready UI components that you can drop into your applications:
  *   - Composites: These components are turn-key solutions that implement common communication scenarios. You can quickly add video calling or chat experiences to your applications. Composites are open-source higher order components built using UI components.
  *   - UI Components - These components are open-source building blocks that let you build custom communications experience. Components are offered for both calling and chat capabilities that can be combined to build experiences.
  *
@@ -11,7 +13,7 @@
  * In conjunction to the UI components, the UI Library exposes a stateful client library for calling and chat. This client is agnostic to any specific state management framework and can be integrated with common state managers like Redux or React Context.
  * This stateful client library can be used with the UI Components to pass props and methods for the UI Components to render data. For more information, see Stateful Client Overview.
  *
- * For more documentation visit: https://aka.ms/acsstorybook
+ * For more information visit: https://aka.ms/acsstorybook
  *
  * @packageDocumentation
  */
