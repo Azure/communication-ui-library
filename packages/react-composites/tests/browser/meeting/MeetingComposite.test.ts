1c08418359dfc9df0d9850db42204b7998a391c6
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-16 12:13:22 -0700

    tests for meeting composite screens

--- packages/react-composites/tests/browser/meeting/MeetingComposite.test.ts
+++ packages/react-composites/tests/browser/meeting/MeetingComposite.test.ts
@@ -18,11 +18,66 @@ export const waitForMeetingCompositeToLoad = async (page: Page): Promise<void> =
   await page.waitForSelector(`${dataUiId('call-composite-start-call-button')}[data-is-focusable="true"]`);
 };
 
-test.describe('Meeting Composite E2E Tests', () => {
-  test('composite pages load completely', async ({ pages }) => {
-    for (const idx in pages) {
-      await waitForMeetingCompositeToLoad(pages[idx]);
-      expect(await pages[idx].screenshot()).toMatchSnapshot(`page-${idx}-meeting-screen.png`, { threshold: 0.5 });
+test.describe('Meeting Composite Pre-Join Tests', () => {
+  test.beforeEach(async ({ pages }) => {
+    for (const page of pages) {
+      page.reload();
+      page.bringToFront();
+      await waitForMeetingCompositeToLoad(page);
     }
   });
+
+  test('Pre-join screen loads correctly', async ({ pages }) => {
+    const page = pages[0];
+    expect(await page.screenshot()).toMatchSnapshot(`meeting-pre-join-screen.png`, { threshold: 0.5 });
+  });
+});
+
+test.describe('Meeting Composite Meeting Page Tests', () => {
+  test.beforeEach(async ({ pages }) => {
+    for (const page of pages) {
+      page.reload();
+      page.bringToFront();
+      await waitForMeetingCompositeToLoad(page);
+
+      // Join Meeting for each participant
+      await page.waitForSelector(dataUiId('call-composite-start-call-button'));
+      await page.click(dataUiId('call-composite-start-call-button'));
+
+      // Ensure meeting has successfully loaded
+      await page.waitForSelector(dataUiId('call-composite-hangup-button'));
+    }
+  });
+
+  test('Meeting gallery screen loads correctly', async ({ pages }) => {
+    const page = pages[0];
+    expect(await page.screenshot()).toMatchSnapshot(`meeting-basic-gallery-screen.png`, { threshold: 0.5 });
+
+    // @TODO: future tests to test toggling meeting controls
+  });
+
+  test('Chat pane opens and displays correctly', async ({ pages }) => {
+    const page = pages[0];
+    await page.click(dataUiId('meeting-composite-chat-button'));
+    await page.waitForSelector(dataUiId('meeting-composite-chat-pane'));
+    expect(await page.screenshot()).toMatchSnapshot(`meeting-chat-pane-open-screen.png`, { threshold: 0.5 });
+
+    // @TODO: future tests to test sending chat messages
+  });
+
+  test('People pane opens and displays correctly', async ({ pages }) => {
+    const page = pages[0];
+    await page.click(dataUiId('meeting-composite-people-button'));
+    await page.waitForSelector(dataUiId('meeting-composite-people-pane'));
+    expect(await page.screenshot()).toMatchSnapshot(`meeting-people-pane-screen.png`, { threshold: 0.5 });
+
+    // @TODO: future tests to verify expected people and interacting with participants
+  });
+
+  /**
+   * @TODO: Further tests:
+   *   - Teams lobby screen
+   *   - Leave meeting
+   *   - Viewport sizes
+   */
 });
