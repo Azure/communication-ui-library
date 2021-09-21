3b7f7de4b31d607a017bc205533f688a1ad372ca
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-21 11:40:54 -0700

    remove no-op check

--- packages/react-composites/src/composites/MeetingComposite/adapter/AzureCommunicationMeetingAdapter.ts
+++ packages/react-composites/src/composites/MeetingComposite/adapter/AzureCommunicationMeetingAdapter.ts
@@ -74,9 +74,6 @@ class MeetingContext {
   }
 
   public updateClientStateWithChatState(chatAdapterState: ChatAdapterState): void {
-    if (!this.state) {
-      console.warn('Cannot update chat state with meeting state - no meeting state exists');
-    }
     this.updateClientState(mergeChatAdapterStateIntoMeetingAdapterState(this.state, chatAdapterState));
   }
 
