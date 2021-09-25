1f38cad1818ad2dc2bb26086f00dba982af86805
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-09-25 04:49:03 +0000

    Fix chat bindings build

--- packages/chat-component-bindings/src/messageThreadSelector.ts
+++ packages/chat-component-bindings/src/messageThreadSelector.ts
@@ -11,7 +11,13 @@ import {
 import { toFlatCommunicationIdentifier } from '@internal/acs-ui-common';
 import { ChatMessageWithStatus } from '@internal/chat-stateful-client';
 import { memoizeFnAll } from '@internal/acs-ui-common';
-import { ChatMessage, Message, MessageAttachedStatus, CommunicationParticipant } from '@internal/react-components';
+import {
+  ChatMessage,
+  Message,
+  MessageAttachedStatus,
+  CommunicationParticipant,
+  SystemMessage
+} from '@internal/react-components';
 import { createSelector } from 'reselect';
 import { compareMessages } from './utils/compareMessages';
 import { ACSKnownMessageType } from './utils/constants';
@@ -23,7 +29,7 @@ const memoizedAllConvertChatMessage = memoizeFnAll(
     userId: string,
     isSeen: boolean,
     isLargeGroup: boolean
-  ): Message<'chat'> | Message<'system'> | Message<'custom'> => {
+  ): Message => {
     const messageType = chatMessage.type.toLowerCase();
     if (messageType === 'text' || messageType === 'richtext/html') {
       return convertToUiChatMessage(chatMessage, userId, isSeen, isLargeGroup);
@@ -40,56 +46,50 @@ const convertToUiChatMessage = (
   isLargeGroup: boolean
 ): ChatMessage => {
   return {
-    type: 'chat',
-    payload: {
-      createdOn: message.createdOn,
-      content: message.content?.message,
-      type: sanitizedMessageContentType(message.type),
-      status: !isLargeGroup && message.status === 'delivered' && isSeen ? 'seen' : message.status,
-      senderDisplayName: message.senderDisplayName,
-      senderId: message.sender !== undefined ? toFlatCommunicationIdentifier(message.sender) : userId,
-      messageId: message.id,
-      clientMessageId: message.clientMessageId,
-      editedOn: message.editedOn,
-      deletedOn: message.deletedOn
-    }
+    messageType: 'chat',
+    createdOn: message.createdOn,
+    content: message.content?.message,
+    contentType: sanitizedMessageContentType(message.type),
+    status: !isLargeGroup && message.status === 'delivered' && isSeen ? 'seen' : message.status,
+    senderDisplayName: message.senderDisplayName,
+    senderId: message.sender !== undefined ? toFlatCommunicationIdentifier(message.sender) : userId,
+    messageId: message.id,
+    clientMessageId: message.clientMessageId,
+    editedOn: message.editedOn,
+    deletedOn: message.deletedOn
   };
 };
 
-const convertToUiSystemMessage = (message: ChatMessageWithStatus): Message<'system'> => {
+const convertToUiSystemMessage = (message: ChatMessageWithStatus): SystemMessage => {
   const systemMessageType = message.type;
   if (systemMessageType === 'participantAdded' || systemMessageType === 'participantRemoved') {
     return {
-      type: 'system',
-      payload: {
-        createdOn: message.createdOn,
-        participants:
-          message.content?.participants
-            // TODO: In our moderator logic, we use undefined name as our displayName for moderator, which should be filtered out
-            // Once we have a better solution to identify the moderator, remove this line
-            ?.filter((participant) => participant.displayName && participant.displayName !== '')
-            .map(
-              (participant): CommunicationParticipant => ({
-                userId: toFlatCommunicationIdentifier(participant.id),
-                displayName: participant.displayName
-              })
-            ) ?? [],
-        type: systemMessageType,
-        messageId: message.id,
-        iconName: systemMessageType === 'participantAdded' ? 'PeopleAdd' : 'PeopleBlock'
-      }
+      messageType: 'system',
+      systemMessageType,
+      createdOn: message.createdOn,
+      participants:
+        message.content?.participants
+          // TODO: In our moderator logic, we use undefined name as our displayName for moderator, which should be filtered out
+          // Once we have a better solution to identify the moderator, remove this line
+          ?.filter((participant) => participant.displayName && participant.displayName !== '')
+          .map(
+            (participant): CommunicationParticipant => ({
+              userId: toFlatCommunicationIdentifier(participant.id),
+              displayName: participant.displayName
+            })
+          ) ?? [],
+      messageId: message.id,
+      iconName: systemMessageType === 'participantAdded' ? 'PeopleAdd' : 'PeopleBlock'
     };
   } else {
     // Only topic updated type left, according to ACSKnown type
     return {
-      type: 'system',
-      payload: {
-        createdOn: message.createdOn,
-        topic: message.content?.topic ?? '',
-        type: 'topicUpdated',
-        messageId: message.id,
-        iconName: 'Edit'
-      }
+      messageType: 'system',
+      systemMessageType: 'topicUpdated',
+      createdOn: message.createdOn,
+      topic: message.content?.topic ?? '',
+      messageId: message.id,
+      iconName: 'Edit'
     };
   }
 };
@@ -131,17 +131,14 @@ export const messageThreadSelector = createSelector(
   }
 );
 
-export const updateMessagesWithAttached = (
-  chatMessagesWithStatus: (Message<'chat'> | Message<'system'> | Message<'custom'>)[],
-  userId: string
-): void => {
+export const updateMessagesWithAttached = (chatMessagesWithStatus: Message[], userId: string): void => {
   chatMessagesWithStatus.sort(compareMessages);
 
   chatMessagesWithStatus.forEach((message, index, messages) => {
-    if (message.type !== 'chat') {
+    if (message.messageType !== 'chat') {
       return;
     }
-    const mine = message.payload.senderId === userId;
+    const mine = message.senderId === userId;
     /**
      * A block of messages: continuous messages that belong to the same sender and not intercepted by other senders.
      *
@@ -154,16 +151,16 @@ export const updateMessagesWithAttached = (
     if (index === 0) {
       if (index !== messages.length - 1) {
         //the next message has the same sender
-        if (message.payload.senderId === message.payload.senderId) {
+        if (message.senderId === message.senderId) {
           attached = 'top';
         }
       }
     } else {
       const previousMessage = messages[index - 1];
-      if (previousMessage.type === 'chat' && message.payload.senderId === previousMessage.payload.senderId) {
+      if (previousMessage.messageType === 'chat' && message.senderId === previousMessage.senderId) {
         //the previous message has the same sender
-        if (index !== messages.length - 1 && nextMessage.type === 'chat') {
-          if (message.payload.senderId === nextMessage.payload.senderId) {
+        if (index !== messages.length - 1 && nextMessage.messageType === 'chat') {
+          if (message.senderId === nextMessage.senderId) {
             //the next message has the same sender
             attached = true;
           } else {
@@ -176,8 +173,8 @@ export const updateMessagesWithAttached = (
         }
       } else {
         //the previous message has a different sender or is not a chat message
-        if (index !== messages.length - 1 && nextMessage.type === 'chat') {
-          if (message.payload.senderId === nextMessage.payload.senderId) {
+        if (index !== messages.length - 1 && nextMessage.messageType === 'chat') {
+          if (message.senderId === nextMessage.senderId) {
             //the next message has the same sender
             attached = 'top';
           }
@@ -185,7 +182,7 @@ export const updateMessagesWithAttached = (
       }
     }
 
-    message.payload.attached = attached;
-    message.payload.mine = mine;
+    message.attached = attached;
+    message.mine = mine;
   });
 };
