29a4fd5676c98e2778341045a4da0b47d95f363b
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-27 13:42:30 -0700

    Add comments to typedoc file

--- packages/communication-react/typedoc.js
+++ packages/communication-react/typedoc.js
@@ -1,6 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/**
+ * Typedoc is used by docs.microsoft.com to generate the API surface.
+ * This API surface can be seen here: https://docs.microsoft.com/en-us/javascript/api/@azure/communication-react
+ */
+
 module.exports = {
-  exclude: ['dist/dist-esm', 'dist/dist-cjs', 'node_modules']
+  /**
+   * Exclude files and folders that should not be part of the API surface.
+   */
+  exclude: ['dist-esm', 'dist/dist-cjs', 'node_modules']
 };
