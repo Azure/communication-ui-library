138db2aad7dd1b79fe4239c918e279e94d960ecf
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-28 15:44:10 -0700

    Ensure single workers across all tests

--- packages/react-composites/playwright.config.ts
+++ packages/react-composites/playwright.config.ts
@@ -1,6 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
-  timeout: 120000,
+  timeout: 120000, // Allow for more than the standard 30000 to ensure timeouts in waiting functions present debug stack traces.
+  workers: 1, // Ensure tests run sequentially.
   use: {
     video: 'retain-on-failure'
   }
