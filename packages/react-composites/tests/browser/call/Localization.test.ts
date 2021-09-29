d93d3009a23a93b50b3cb8c33df09868f1de430f
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-29 11:02:00 -0700

    run locale test in its own call id to isolate it

--- packages/react-composites/tests/browser/call/Localization.test.ts
+++ packages/react-composites/tests/browser/call/Localization.test.ts
@@ -4,21 +4,22 @@
 import { test } from './fixture';
 import { loadPageWithPermissionsForCalls, waitForCallCompositeToLoad, loadCallScreen } from '../common/utils';
 import { expect } from '@playwright/test';
+import { v1 as generateGUID } from 'uuid';
 
 test.describe('Localization tests', async () => {
-  test.beforeEach(async ({ pages }) => {
-    for (const page of pages) {
-      // Ensure any previous call users from prior tests have left the call
-      await page.reload();
-    }
-  });
-
   test('Configuration page title and participant button in call should be localized', async ({
     serverUrl,
     users,
     testBrowser
   }) => {
-    const page = await loadPageWithPermissionsForCalls(testBrowser, serverUrl, users[0], { useFrlocale: 'true' });
+    // Run this test in an isolated thread to ensure the test is totally isolated.
+    const newCallId = generateGUID();
+    const page = await loadPageWithPermissionsForCalls(
+      testBrowser,
+      serverUrl,
+      { ...users[0], threadId: newCallId },
+      { useFrlocale: 'true' }
+    );
     await page.bringToFront();
     await waitForCallCompositeToLoad(page);
     expect(await page.screenshot()).toMatchSnapshot('localized-call-configuration-page.png', { threshold: 0.5 });
