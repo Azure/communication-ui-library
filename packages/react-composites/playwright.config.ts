bf4b5d621e4578ce26d354cf285d49824c077289
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-29 09:42:05 -0700

    update config comments

--- packages/react-composites/playwright.config.ts
+++ packages/react-composites/playwright.config.ts
@@ -3,8 +3,20 @@
 
 import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
-  timeout: 120000, // Allow for more than the standard 30000. This ensures we get debug stack traces when await functions timeout.
-  workers: 1, // Ensure tests run sequentially.
+  // Allow for more than the standard 30s (currently set to 2minutes). This is to ensure we get debug stack traces
+  // when await functions timeout. Without this, if the test itself decides to timeout, playwright gives poor feedback
+  // on what was being awaited.
+  // Note: the default timeout of 30s still applies to `page.waitForX` functions, this is just the global timeout
+  // per test.
+  timeout: 120000,
+
+  // Ensure tests run sequentially. All tests in this suite *must be run sequentially*.
+  // The tests are not isolated, each test depends on the final-state of the chat thread after previous tests.
+  //
+  // We cannot use isolated tests because these are live tests -- the ACS chat service throttles our attempt to create
+  // many threads using the same connection string in a short span of time.
+  workers: 1,
+
   use: {
     headless: true,
     video: 'retain-on-failure'
