0ebc83207930484f18a252f4f66824eba0f93764
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-09-28 20:43:30 -0700

    Add playwright config

---
+++ packages/react-composites/playwright.config.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { PlaywrightTestConfig } from '@playwright/test';
+const config: PlaywrightTestConfig = {
+  timeout: 120000, // Allow for more than the standard 30000. This ensures we get debug stack traces when await functions timeout.
+  workers: 1, // Ensure tests run sequentially.
+  use: {
+    headless: true,
+    video: 'retain-on-failure'
+  }
+};
+export default config;
