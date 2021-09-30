8e9af6182783b3155d578e7989527eb478d84c7d
Anjul Garg <anjulgarg@live.com>
2021-09-30 11:06:27 -0700

    Re-using hooks

--- packages/react-components/src/components/utils/responsive.ts
+++ packages/react-components/src/components/utils/responsive.ts
@@ -4,30 +4,6 @@
 import { RefObject, useEffect, useState } from 'react';
 
 /**
- * A utility hook for identifying if the current viewport size is is mobile screen.
- * Returns updated result if parent/window resizes.
- * @param containerRef - Ref of a parent element whose width will be used to determine the viewport size
- */
-export const useIsMobileScreen = (containerRef: RefObject<HTMLDivElement>): boolean => {
-  const MOBILE_WIDTH_MAX = 480;
-  const [isMobileScreen, setIsMobileScreen] = useState(false);
-
-  useEffect(() => {
-    const updateWidth = (): void => {
-      const width = containerRef?.current?.offsetWidth ?? 0;
-      setIsMobileScreen(width <= MOBILE_WIDTH_MAX);
-    };
-    updateWidth();
-    window.addEventListener('resize', updateWidth);
-    return () => {
-      window.removeEventListener('resize', updateWidth);
-    };
-  }, [containerRef]);
-
-  return isMobileScreen;
-};
-
-/**
  * A utility hook for providing the width of a parent element.
  * Returns updated width if parent/window resizes.
  * @param containerRef - Ref of a parent element whose width will be returned.
@@ -47,4 +23,21 @@ export const useContainerWidth = (containerRef: RefObject<HTMLDivElement>): numb
   }, [containerRef]);
 
   return width;
+};
+
+/**
+ * A utility hook for identifying if the current viewport size is is mobile screen.
+ * Returns updated result if parent/window resizes.
+ * @param containerRef - Ref of a parent element whose width will be used to determine the viewport size
+ */
+export const useIsMobileScreen = (containerRef: RefObject<HTMLDivElement>): boolean => {
+  const MOBILE_WIDTH_MAX = 480;
+  const containerWidth = useContainerWidth(containerRef);
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+    setIsMobileScreen(containerWidth <= MOBILE_WIDTH_MAX);
+  }, [containerRef, containerWidth]);
+
+  return isMobileScreen;
 };
