88249eecd37d5bb260f9b6edfbe46b746ea3fbaf
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-10-01 16:15:58 -0700

    add property to fix participant overflow

--- packages/react-composites/src/composites/MeetingComposite/styles/MeetingCompositeStyles.ts
+++ packages/react-composites/src/composites/MeetingComposite/styles/MeetingCompositeStyles.ts
@@ -6,4 +6,15 @@ import { IStackStyles } from '@fluentui/react';
 /**
  * @private
  */
-export const compositeOuterContainerStyles: IStackStyles = { root: { width: '100%' } };
+export const compositeOuterContainerStyles: IStackStyles = {
+  root: {
+    width: '100%',
+
+    // This allows the composite to correctly contain the call composite by calculating a height for the available space.
+    // Items inside the call composite that fill 100% of the height will now fill the height of this container instead
+    // of the height of the child items.
+    // If this css property is to be removed, test the participant pane correctly uses the scroll overflow when there a lot
+    // of participants (i.e. beyond screen height) and where one participant is sharing their screen.
+    flexWrap: 'wrap'
+  }
+};
