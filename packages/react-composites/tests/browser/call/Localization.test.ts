042183219d369f4e3af0f54ecb2b0d94932b735b
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-10-01 17:48:08 -0700

    fix how locale test uses page

--- packages/react-composites/tests/browser/call/Localization.test.ts
+++ packages/react-composites/tests/browser/call/Localization.test.ts
@@ -2,17 +2,12 @@
 // Licensed under the MIT license.
 
 import { test } from './fixture';
-import {
-  loadPageWithPermissionsForCalls,
-  waitForCallCompositeToLoad,
-  loadCallScreen,
-  loadUrlInPage
-} from '../common/utils';
+import { waitForCallCompositeToLoad, loadCallScreen, loadUrlInPage } from '../common/utils';
 import { expect } from '@playwright/test';
 import { v1 as generateGUID } from 'uuid';
 
 test.describe('Localization tests', async () => {
-  test.beforeEach(async ({ page, users, serverUrl }) => {
+  test.beforeEach(async ({ pages, users, serverUrl }) => {
     // Each test *must* join a new call to prevent test flakiness.
     // We hit a Calling SDK service 500 error if we do not.
     // An issue has been filed with the calling team.
@@ -21,11 +16,13 @@ test.describe('Localization tests', async () => {
     user.groupId = newTestGuid;
 
     // Load different locale for locale tests
+    const page = pages[0];
     await loadUrlInPage(page, serverUrl, user, { useFrlocale: 'true' });
     await waitForCallCompositeToLoad(page);
   });
 
-  test('Configuration page title and participant button in call should be localized', async ({ page }) => {
+  test('Configuration page title and participant button in call should be localized', async ({ pages }) => {
+    const page = pages[0];
     await page.bringToFront();
     await waitForCallCompositeToLoad(page);
     expect(await page.screenshot()).toMatchSnapshot('localized-call-configuration-page.png', { threshold: 0.5 });
