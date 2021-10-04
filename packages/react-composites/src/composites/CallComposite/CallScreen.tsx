53e78bd088b3d2d346cd371c3e7c4cba2f75bc57
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-10-04 14:13:28 +0000

    allow developer to explicitly opt in

--- packages/react-composites/src/composites/CallComposite/CallScreen.tsx
+++ packages/react-composites/src/composites/CallComposite/CallScreen.tsx
@@ -132,10 +132,16 @@ export const CallScreen = (props: CallScreenProps): JSX.Element => {
     );
   }
 
-  // Do not show screen share button when composite is optimized for mobile.
+  // Do not show screen share button when composite is optimized for mobile unless the developer
+  // has explicitly opted in.
   const callControlOptions: boolean | CallControlOptions | undefined = options?.callControls;
-  if (options?.mobileView && callControlOptions && typeof props.options !== 'boolean') {
-    (callControlOptions as CallControlOptions).screenShareButton = false;
+  if (
+    options?.mobileView &&
+    callControlOptions &&
+    typeof callControlOptions !== 'boolean' &&
+    callControlOptions.screenShareButton !== true
+  ) {
+    callControlOptions.screenShareButton = false;
   }
 
   const screenShareModalHostId = 'UILibraryMediaGallery';
