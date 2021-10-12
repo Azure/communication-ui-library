427b639bb80b600fda63d410cc8c6895910a8c54
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-10-12 11:56:33 -0700

    cleanup

--- packages/react-composites/src/composites/MeetingComposite/FluidComponent.tsx
+++ packages/react-composites/src/composites/MeetingComposite/FluidComponent.tsx
@@ -17,6 +17,16 @@ const containerSchema = {
   initialObjects: { myMap: SharedMap }
 };
 
+const config: AzureConnectionConfig = {
+  tenantId: configDetails.tenantId,
+  tokenProvider: new AzureFunctionTokenProvider(configDetails.tokenProviderId, {
+    userId: 'UserId',
+    userName: 'Test User'
+  }),
+  orderer: configDetails.orderer,
+  storage: configDetails.storage
+};
+
 const createFluidContainer = async (
   client: AzureClient | TinyliciousClient
 ): Promise<{
@@ -24,39 +34,32 @@ const createFluidContainer = async (
   services: AzureContainerServices;
 }> => await client.createContainer(containerSchema);
 
+const initializeFluidMap = async (
+  client: AzureClient | TinyliciousClient
+): Promise<{ map: ISharedMap; containerId: string }> => {
+  const { container } = await createFluidContainer(client);
+  const map = container.initialObjects.myMap as ISharedMap;
+
+  const id = await container.attach();
+  return { map, containerId: id };
+};
+
 /**
  * @private
  */
 export const FluidComponent = (): JSX.Element => {
   const [fluidMap, setFluidMap] = React.useState<ISharedMap | undefined>(undefined);
 
-  const client = useMemo(() => {
-    const config: AzureConnectionConfig = {
-      tenantId: configDetails.tenantId,
-      tokenProvider: new AzureFunctionTokenProvider(configDetails.tokenProviderId, {
-        userId: 'UserId',
-        userName: 'Test User'
-      }),
-      orderer: configDetails.orderer,
-      storage: configDetails.storage
-    };
-
-    return USE_LOCAL_FLUID_SERVER ? new TinyliciousClient() : new AzureClient({ connection: config });
-  }, []);
-
   useEffect(() => {
-    if (client) {
-      (async () => {
-        const { container } = await createFluidContainer(client);
-        const map = container.initialObjects.myMap as ISharedMap;
-        map.set('myFirstCounter', 0);
-        const id = await container.attach();
-        console.log(id);
-        window.location.hash = id;
-        setFluidMap(map);
-      })();
-    }
-  }, [client]);
+    (async () => {
+      const client = USE_LOCAL_FLUID_SERVER ? new TinyliciousClient() : new AzureClient({ connection: config });
+      const { map, containerId } = await initializeFluidMap(client);
+
+      window.location.hash = containerId;
+
+      setFluidMap(map);
+    })();
+  }, []);
 
   useEffect(() => {
     fluidMap?.set('myFirstCounter', 1);
