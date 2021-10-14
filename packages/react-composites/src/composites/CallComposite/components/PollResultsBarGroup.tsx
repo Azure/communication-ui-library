0a265af2736d1ecce94779e6a2625c3626df8c02
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-10-14 16:27:56 -0700

    fix % value

--- packages/react-composites/src/composites/CallComposite/components/PollResultsBarGroup.tsx
+++ packages/react-composites/src/composites/CallComposite/components/PollResultsBarGroup.tsx
@@ -19,16 +19,17 @@ export interface PollResultsProps {
 export const PollResultBarGroup = (props: PollResultsProps): JSX.Element => {
   let totalVotes = 0;
   let maxVotesOnASingleResult = 0;
-  props.pollData.options.forEach((pollOption) => {
+  for (const pollOption of props.pollData.options) {
     totalVotes += pollOption?.votes || 0;
     maxVotesOnASingleResult = Math.max(maxVotesOnASingleResult, pollOption.votes ?? 0);
-  });
+  }
 
   return (
     <Stack tokens={{ childrenGap: '10px' }}>
       {props.pollData.options.map((pollResult, index) => {
-        const percentage = Math.floor((pollResult.votes ?? 0 / totalVotes) * 100) ?? 0;
+        const percentage = Math.floor(((pollResult.votes ?? 0) / totalVotes) * 100) ?? 0;
         const barPercentage = Math.floor(((pollResult.votes ?? 0) / maxVotesOnASingleResult) * 100) ?? 0;
+
         return (
           <Stack.Item key={index}>
             <PollResultBar percentage={percentage} barWidthPercentage={barPercentage} votes={pollResult.votes ?? 0} />
