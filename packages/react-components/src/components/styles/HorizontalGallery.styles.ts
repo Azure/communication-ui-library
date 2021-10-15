2f4ff531a13d742571e174212fa4d2ba1af4e242
Miguel Gamis <miguelgamis@microsoft.com>
2021-10-15 06:45:19 -0700

    removed getTheme

--- packages/react-components/src/components/styles/HorizontalGallery.styles.ts
+++ packages/react-components/src/components/styles/HorizontalGallery.styles.ts
@@ -1,18 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { getTheme, IStyle } from '@fluentui/react';
-
-const theme = getTheme();
+import { IStyle } from '@fluentui/react';
 
 /**
  * @private
  */
 export const leftRightButtonStyles: IStyle = {
   background: 'none',
-  border: `1px solid ${theme.palette.neutralLight}`,
   padding: 0,
-  borderRadius: theme.effects.roundedCorner4,
   height: '100%',
   minWidth: '1.75rem',
   maxWidth: '1.75rem'
