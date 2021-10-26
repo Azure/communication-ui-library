4a396d66e31a5593bf04e27c42f4e0b2704a44ce
Miguel Gamis <miguelgamis@microsoft.com>
2021-10-26 03:27:33 -0700

    remvoed is onlyUnique filter from VIdeoGallery story

--- packages/storybook/stories/VideoGallery/VideoGallery.stories.tsx
+++ packages/storybook/stories/VideoGallery/VideoGallery.stories.tsx
@@ -63,16 +63,11 @@ const MockLocalParticipant = {
   isMuted: true
 };
 
-const onlyUnique = (value: string, index: number, self: string[]): boolean => {
-  return self.indexOf(value) === index;
-};
-
 const VideoGalleryStory = (args): JSX.Element => {
   const remoteParticipants = args.remoteParticipants
     .split(',')
     .map((p) => p.trim())
     .filter((p) => p)
-    .filter(onlyUnique)
     .map((p, i) => {
       return {
         userId: `user${i}`,
