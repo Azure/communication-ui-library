47c5d44cd6314bf746b7715680e6dd175cc0ed0b
Nan Jiang <jinan@microsoft.com>
2021-11-01 10:10:12 -0700

    Add comments of how we fix warnings

--- samples/StaticHtmlComposites/webpack.config.dev.js
+++ samples/StaticHtmlComposites/webpack.config.dev.js
@@ -2,5 +2,7 @@ const config = require('./webpack.config.js');
 
 module.exports = {
   ...config,
+  // Chrome tries to load source map from node_modules folder which generates warnings in dev mode
+  // Embedding source map into js file fixes the problem
   devtool: 'eval-source-map'
 };
