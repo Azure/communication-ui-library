54f2ae4f81da15161107ded416b3db36d28bd348
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-11-03 11:33:23 -0700

    Make lobby screen strings and icons customizable (#1023)

--- packages/react-composites/src/composites/CallComposite/styles/LobbyTile.styles.ts
+++ packages/react-composites/src/composites/CallComposite/styles/LobbyTile.styles.ts
@@ -6,24 +6,7 @@ import { IPalette, IStyle } from '@fluentui/react';
 /**
  * @private
  */
-export const lobbyTileDarkenedOverlayStyles: (palette: IPalette, isVideoReady: boolean) => IStyle = (
-  palette,
-  isVideoReady
-) => {
-  return {
-    position: 'absolute',
-    background: isVideoReady ? '#201f1e' : palette.neutralLight,
-    opacity: 0.75
-  };
-};
-
-/**
- * @private
- */
-export const lobbyTileInformationStyles: (palette: IPalette, isVideoReady: boolean) => IStyle = (
-  palette,
-  isVideoReady
-) => {
+export const titleStyle: (palette: IPalette, isVideoReady: boolean) => IStyle = (palette, isVideoReady) => {
   return {
     fontSize: '1.75rem',
     color: isVideoReady ? 'white' : palette.neutralPrimary,
@@ -37,3 +20,12 @@ export const lobbyTileInformationStyles: (palette: IPalette, isVideoReady: boole
 export const videoTileStyles = {
   root: { height: '100%', width: '100%' }
 };
+
+/**
+ * @private
+ */
+export const moreDetailsStyle = (palette, isVideoReady): IStyle => ({
+  fontSize: '1rem',
+  color: isVideoReady ? 'white' : palette.neutralPrimary,
+  textAlign: 'center'
+});
