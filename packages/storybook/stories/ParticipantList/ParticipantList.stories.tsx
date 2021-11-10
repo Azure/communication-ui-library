8b53a86c0911f0d31aac3db41bd8717a2105a20a
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-11-10 08:43:23 -0800

    couple more components

--- packages/storybook/stories/ParticipantList/ParticipantList.stories.tsx
+++ packages/storybook/stories/ParticipantList/ParticipantList.stories.tsx
@@ -25,7 +25,7 @@ const getDocs: () => JSX.Element = () => {
   return (
     <>
       <Title>Participant List</Title>
-      <Description of={ParticipantListComponent} />
+      <Description>Component that renders a list of all calling or chat participants.</Description>
 
       <Heading>Default example for Chat</Heading>
       <Description>
