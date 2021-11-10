5f4e23db48d1cd5269cdec05a5239b16e70049f4
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2021-11-10 18:39:00 +0000

    style update

--- packages/react-composites/src/composites/CallComposite/pages/NoticePage.tsx
+++ packages/react-composites/src/composites/CallComposite/pages/NoticePage.tsx
@@ -3,7 +3,15 @@
 
 import React from 'react';
 import { Icon, mergeStyles, Stack, Text } from '@fluentui/react';
-import { containerStyle, moreDetailsStyles, stackItemGap, titleStyles } from '../styles/NoticePage.styles';
+import {
+  containerStyle,
+  moreDetailsStyles,
+  containerItemGap,
+  textContainerStyle,
+  titleStyles,
+  textContainerItemGap,
+  rejoinCallButtonContainerStyles
+} from '../styles/NoticePage.styles';
 import { useAdapter } from '../adapter/CallAdapterProvider';
 import { StartCallButton } from '../components/StartCallButton';
 
@@ -26,11 +34,13 @@ export function NoticePage(props: NoticePageProps): JSX.Element {
   const adapter = useAdapter();
   return (
     <Stack verticalFill verticalAlign="center" horizontalAlign="center" data-ui-id={props.dataUiId}>
-      <Stack className={mergeStyles(containerStyle)} tokens={stackItemGap}>
+      <Stack className={mergeStyles(containerStyle)} tokens={containerItemGap}>
         {props.iconName && <Icon iconName={props.iconName} />}
         <Text className={mergeStyles(titleStyles)}>{props.title}</Text>
         <Text className={mergeStyles(moreDetailsStyles)}>{props.moreDetails}</Text>
-        <StartCallButton onClickHandler={() => adapter.joinCall()} isDisabled={false} rejoinCall={true} />
+        <Stack styles={rejoinCallButtonContainerStyles}>
+          <StartCallButton onClickHandler={() => adapter.joinCall()} isDisabled={false} rejoinCall={true} />
+        </Stack>
       </Stack>
     </Stack>
   );
