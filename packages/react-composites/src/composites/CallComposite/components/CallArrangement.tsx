51e785fcac936a06c30d2696122c584477214c05
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-11-12 12:16:22 -0800

    Fix call screen background and call control drop shadow (and call control vertical alignment by side effect) (#1066)

--- packages/react-composites/src/composites/CallComposite/components/CallArrangement.tsx
+++ packages/react-composites/src/composites/CallComposite/components/CallArrangement.tsx
@@ -2,22 +2,19 @@
 // Licensed under the MIT license.
 
 import { Stack } from '@fluentui/react';
-import { ErrorBar, ErrorBarProps } from '@internal/react-components';
+import { ErrorBar, ErrorBarProps, useTheme } from '@internal/react-components';
 import React, { useMemo } from 'react';
 import { CallControls, CallControlsProps } from '../components/CallControls';
 import { ComplianceBanner, ComplianceBannerProps } from '../components/ComplianceBanner';
 import {
+  callControlsContainerStyles,
   notificationsContainerStyles,
-  callControlsContainer,
   containerStyleDesktop,
   containerStyleMobile,
   mediaGalleryContainerStyles,
-  subContainerStyles
+  galleryParentContainerStyles
 } from '../styles/CallPage.styles';
 import { MutedNotification, MutedNotificationProps } from './MutedNotification';
-
-// High enough to be above `onRenderGalleryContent()`.
-const NOTIFICATIONS_CONTAINER_ZINDEX = 9;
 
 /**
  * @private
@@ -38,17 +35,17 @@ export interface CallArrangementProps {
 export const CallArrangement = (props: CallArrangementProps): JSX.Element => {
   const containerClassName = useMemo(() => {
     return props.mobileView ? containerStyleMobile : containerStyleDesktop;
-  }, [props.mobileView ? containerStyleMobile : containerStyleDesktop]);
+  }, [props.mobileView]);
+
+  const theme = useTheme();
+  const callGalleryStyles = useMemo(
+    () => galleryParentContainerStyles(theme.palette.neutralLighterAlt),
+    [theme.palette.neutralLighterAlt]
+  );
 
   return (
-    <Stack
-      horizontalAlign="center"
-      verticalAlign="center"
-      className={containerClassName}
-      grow
-      data-ui-id={props.dataUiId}
-    >
-      <Stack.Item styles={notificationsContainerStyles(NOTIFICATIONS_CONTAINER_ZINDEX)}>
+    <Stack verticalFill horizontalAlign="stretch" className={containerClassName} data-ui-id={props.dataUiId}>
+      <Stack.Item styles={notificationsContainerStyles}>
         <Stack>
           <ComplianceBanner {...props.complianceBannerProps} />
         </Stack>
@@ -60,16 +57,16 @@ export const CallArrangement = (props: CallArrangementProps): JSX.Element => {
         {!!props.mutedNotificationProps && <MutedNotification {...props.mutedNotificationProps} />}
       </Stack.Item>
 
-      <Stack.Item styles={subContainerStyles} grow>
+      <Stack.Item styles={callGalleryStyles} grow>
         {props.onRenderGalleryContent && (
-          <Stack grow styles={mediaGalleryContainerStyles}>
+          <Stack verticalFill styles={mediaGalleryContainerStyles}>
             {props.onRenderGalleryContent()}
           </Stack>
         )}
       </Stack.Item>
 
       {props.callControlProps !== false && (
-        <Stack.Item className={callControlsContainer}>
+        <Stack.Item className={callControlsContainerStyles}>
           <CallControls {...props.callControlProps} />
         </Stack.Item>
       )}
