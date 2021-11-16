eb1a6d3996dff4285dec9d629beb9b33eaa0f332
Miguel Gamis <miguelgamis@microsoft.com>
2021-11-15 20:33:01 -0800

    HorizontalGalleryStyles children prop added

--- packages/storybook/stories/VideoGallery/snippets/CustomStyle.snippet.tsx
+++ packages/storybook/stories/VideoGallery/snippets/CustomStyle.snippet.tsx
@@ -12,6 +12,15 @@ const MockLocalParticipant = {
   isMuted: true
 };
 
+const mockVideoElement = document.createElement('div');
+mockVideoElement.style.maxWidth = decodeURIComponent('100%25');
+mockVideoElement.style.maxHeight = decodeURIComponent('100%25');
+const gif = document.createElement('img');
+gif.style.maxWidth = decodeURIComponent('100%25');
+gif.style.maxHeight = decodeURIComponent('100%25');
+gif.src = 'https://media.giphy.com/media/QvMUP3619500qb6mtw/giphy.gif';
+mockVideoElement.appendChild(gif);
+
 const MockRemoteParticipants = [
   {
     userId: 'user2',
@@ -27,7 +36,27 @@ const MockRemoteParticipants = [
   },
   {
     userId: 'user5',
-    displayName: 'Bruce Wayne'
+    displayName: 'Hal Jordan'
+  },
+  {
+    userId: 'user6',
+    displayName: 'Maria Hill'
+  },
+  {
+    userId: 'user7',
+    displayName: 'Kurt Wagner'
+  },
+  {
+    userId: 'user8',
+    displayName: 'Anna Marie LeBeau'
+  },
+  {
+    userId: 'user9',
+    displayName: 'Bruce Wayne',
+    videoStream: {
+      isAvailable: true,
+      renderElement: mockVideoElement
+    }
   }
 ];
 
@@ -39,6 +68,9 @@ export const CustomStyleVideoGalleryExample: () => JSX.Element = () => {
       border: 'solid 5px red'
     },
     gridLayout: {
+      children: { border: 'solid 5px red' }
+    },
+    horizontalGallery: {
       children: { border: 'solid 5px red' }
     }
   };
