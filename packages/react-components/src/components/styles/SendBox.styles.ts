603ae6247515fac4c58fb5c2e4cc2940b2d1cfa4
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2021-11-17 02:01:48 +0000

    setting explicit marginTop and marginRight instead of margin auto

--- packages/react-components/src/components/styles/SendBox.styles.ts
+++ packages/react-components/src/components/styles/SendBox.styles.ts
@@ -33,8 +33,8 @@ export const sendBoxStyle = mergeStyles({
 export const sendButtonStyle = mergeStyles({
   height: '1.25rem',
   width: '1.25rem',
-  marginLeft: '0.35rem',
-  marginRight: '0.35rem'
+  marginTop: '0.563rem', // 9px
+  marginRight: '0.313rem' // 5px
 });
 
 /**
