1118c0838d36761f5401eb0704c26706dd13e599
Anjul Garg <anjulgarg@live.com>
2021-11-17 21:54:07 +0000

    Adding curly braces for consistency

--- packages/calling-component-bindings/src/participantListSelector.ts
+++ packages/calling-component-bindings/src/participantListSelector.ts
@@ -43,7 +43,9 @@ const convertRemoteParticipantsToParticipantListParticipants = (
         return -1;
       } else if (nameA > nameB) {
         return 1;
-      } else return 0;
+      } else {
+        return 0;
+      }
     });
 };
 
