069b22587093f899171555afacd1788756b715ee
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-11-19 11:31:49 -0800

    Add linting to Storybook mdx files + fail Storybook build if there are lint warnings (#1128)

--- packages/storybook/.eslintrc.js
+++ packages/storybook/.eslintrc.js
@@ -68,6 +68,7 @@ module.exports = {
   root: true,
   settings: {
     react: {
+      'mdx/code-blocks': true,
       version: 'detect'
     }
   },
@@ -93,6 +94,14 @@ module.exports = {
       rules: {
         'react/prop-types': 'off',
         'react/jsx-key': 'off'
+      }
+    },
+    {
+      files: '*.mdx',
+      parser: 'eslint-mdx',
+      extends: 'plugin:mdx/recommended',
+      rules: {
+        'header/header': 'off'
       }
     }
   ]
