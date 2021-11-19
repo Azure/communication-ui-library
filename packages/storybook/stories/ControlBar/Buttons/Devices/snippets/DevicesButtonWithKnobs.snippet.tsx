f9c21088d9124ae3c5841646862f7d64286ccd29
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-11-19 09:31:10 -0800

    add linting to mdx files, fix linting errors

--- packages/storybook/stories/ControlBar/Buttons/Devices/snippets/DevicesButtonWithKnobs.snippet.tsx
+++ packages/storybook/stories/ControlBar/Buttons/Devices/snippets/DevicesButtonWithKnobs.snippet.tsx
@@ -5,7 +5,7 @@ import { DevicesButton, DevicesButtonProps } from '@azure/communication-react';
 import React, { useState } from 'react';
 import { defaultControlsCameras, defaultControlsMicrophones, defaultControlsSpeakers } from '../../../../controlsUtils';
 
-export const DevicesButtonWithKnobs = (args: any): JSX.Element => {
+export const DevicesButtonWithKnobs = (args: unknown): JSX.Element => {
   const [selectedCamera, setSelectedCamera] = useState<{ id: string; name: string }>(defaultControlsCameras[0]);
   const [selectedMicrophone, setSelectedMicrophone] = useState<{ id: string; name: string }>(
     defaultControlsMicrophones[0]
