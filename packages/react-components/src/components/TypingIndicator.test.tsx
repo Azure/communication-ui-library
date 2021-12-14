6e923a13904d75e979a116be63f82e4a7429ae74
Aline Caillaud <alcail@microsoft.com>
2021-12-13 18:57:57 -0800

    add more test to TypingIndicator

--- packages/react-components/src/components/TypingIndicator.test.tsx
+++ packages/react-components/src/components/TypingIndicator.test.tsx
@@ -9,14 +9,74 @@ import { mountWithLocalization, createTestLocale } from './utils/testUtils';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const randomText = Math.random().toString();
+const testLocale = createTestLocale({
+  typingIndicator: {
+    singleUser: '{user} ' + randomText,
+    multipleUsers: '{users} ' + randomText,
+    multipleUsersAbbreviateOne: '{users} and 1 other ' + randomText,
+    multipleUsersAbbreviateMany: '{users} and {numOthers} others ' + randomText
+  }
+});
+
 describe('TypingIndicator should format string correctly', () => {
   test('One user case', async () => {
-    const randomText = Math.random().toString();
-    const testLocale = createTestLocale({ typingIndicator: { singleUser: '{user} ' + randomText } });
     const component = mountWithLocalization(
-      <TypingIndicator typingUsers={[{ userId: 'user2', displayName: 'Claire' }]} />,
+      <TypingIndicator typingUsers={[{ userId: 'user1', displayName: 'Claire' }]} />,
       testLocale
     );
-    expect(component.text()).toBe('Claire ' + randomText);
+
+    const expectedString = 'Claire ' + randomText;
+    expect(component.text()).toBe(expectedString);
+    expect(component.html().includes(`aria-label=\"${expectedString}\"`)).toBe(true);
+  });
+
+  test('Two users case', async () => {
+    const component = mountWithLocalization(
+      <TypingIndicator
+        typingUsers={[
+          { userId: 'user1', displayName: 'Claire' },
+          { userId: 'user2', displayName: 'Christopher' }
+        ]}
+      />,
+      testLocale
+    );
+
+    const expectedString = 'Claire, Christopher ' + randomText;
+    expect(component.text()).toBe(expectedString);
+    expect(component.html().includes(`aria-label=\"${expectedString}\"`)).toBe(true);
+  });
+
+  test('Two users with abbreviation case', async () => {
+    const component = mountWithLocalization(
+      <TypingIndicator
+        typingUsers={[
+          { userId: 'user1', displayName: 'Claire Romanov' },
+          { userId: 'user2', displayName: 'Christopher Rutherford' }
+        ]}
+      />,
+      testLocale
+    );
+
+    const expectedString = 'Claire Romanov and 1 other ' + randomText;
+    expect(component.text()).toBe(expectedString);
+    expect(component.html().includes(`aria-label=\"${expectedString}\"`)).toBe(true);
+  });
+
+  test('Three users with abbreviation case', async () => {
+    const component = mountWithLocalization(
+      <TypingIndicator
+        typingUsers={[
+          { userId: 'user1', displayName: 'Claire Romanov' },
+          { userId: 'user2', displayName: 'Christopher Rutherford' },
+          { userId: 'user3', displayName: 'Jill Vernblom' }
+        ]}
+      />,
+      testLocale
+    );
+
+    const expectedString = 'Claire Romanov and 2 others ' + randomText;
+    expect(component.text()).toBe(expectedString);
+    expect(component.html().includes(`aria-label=\"${expectedString}\"`)).toBe(true);
   });
 });
