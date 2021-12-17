e39a4f1044ed8b749bf31eeea16ca6c649ee8c65
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2021-12-17 14:01:47 -0800

    update meeting test initialization

--- packages/react-composites/tests/browser/meeting/MeetingComposite.test.ts
+++ packages/react-composites/tests/browser/meeting/MeetingComposite.test.ts
@@ -12,24 +12,15 @@ import {
   waitForSelector
 } from '../common/utils';
 import { test } from './fixture';
-import { expect } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import { v1 as generateGUID } from 'uuid';
-import { sendMessage, waitForMessageDelivered } from '../common/chatTestHelpers';
+import { CHAT_PARTICIPANTS, sendMessage, waitForMessageDelivered, waitForMessageSeen } from '../common/chatTestHelpers';
+import { createChatThreadAndUsers } from '../common/fixtureHelpers';
+import { MeetingUserType } from '../common/fixtureTypes';
 
 test.describe('Meeting Composite Pre-Join Tests', () => {
   test.beforeEach(async ({ pages, users, serverUrl }) => {
-    // Each test *must* join a new call to prevent test flakiness.
-    // We hit a Calling SDK service 500 error if we do not.
-    // An issue has been filed with the calling team.
-    const newTestGuid = generateGUID();
-    for (let i = 0; i < pages.length; i++) {
-      const page = pages[i];
-      const user = users[i];
-      user.groupId = newTestGuid;
-
-      await page.goto(buildUrl(serverUrl, user));
-      await waitForMeetingCompositeToLoad(page);
-    }
+    await meetingTestSetup({ pages, users, serverUrl });
   });
 
   test('Pre-join screen loads correctly', async ({ pages }) => {
@@ -40,19 +31,7 @@ test.describe('Meeting Composite Pre-Join Tests', () => {
 
 test.describe('Meeting Composite Meeting Page Tests', () => {
   test.beforeEach(async ({ pages, users, serverUrl }) => {
-    // Each test *must* join a new call to prevent test flakiness.
-    // We hit a Calling SDK service 500 error if we do not.
-    // An issue has been filed with the calling team.
-    const newTestGuid = generateGUID();
-    for (let i = 0; i < pages.length; i++) {
-      const page = pages[i];
-      const user = users[i];
-      user.groupId = newTestGuid;
-
-      await page.goto(buildUrl(serverUrl, user));
-      await waitForMeetingCompositeToLoad(page);
-    }
-
+    await meetingTestSetup({ pages, users, serverUrl });
     await loadCallPageWithParticipantVideos(pages);
   });
 
@@ -66,18 +45,16 @@ test.describe('Meeting Composite Meeting Page Tests', () => {
     await pageClick(pages[0], dataUiId('meeting-composite-chat-button'));
     await waitForSelector(pages[0], dataUiId('meeting-composite-chat-pane'));
     await sendMessage(pages[0], 'Meeting composite is awesome!');
+
     // Open chat pane on page 1 and send a response
-    await pages[1].bringToFront();
     await pageClick(pages[1], dataUiId('meeting-composite-chat-button'));
     await waitForSelector(pages[1], dataUiId('meeting-composite-chat-pane'));
     await sendMessage(pages[1], 'I agree!');
-    await waitForMessageDelivered();
-    await waitForSelector(pages[1], `[data-ui-status="delivered"]`);
+    await waitForMessageDelivered(pages[1]);
+
     // Test page 0 has both sent message and received message
-    await pages[0].bringToFront();
-    await waitForSelector(pages[0], `[data-ui-status="seen"]`);
-    await pages[1].bringToFront();
-    await waitForSelector(pages[1], `[data-ui-status="seen"]`);
+    await waitForMessageSeen(pages[0]);
+    await waitForMessageSeen(pages[1]);
 
     // Ensure typing indicator has disappeared to prevent flakey test
     await pages[0].bringToFront();
@@ -101,3 +78,32 @@ test.describe('Meeting Composite Meeting Page Tests', () => {
    *   - Leave meeting
    */
 });
+
+export const meetingTestSetup = async ({
+  pages,
+  users,
+  serverUrl,
+  qArgs
+}: {
+  pages: Page[];
+  users: MeetingUserType[];
+  serverUrl: string;
+  /** optional query parameters for the page url */
+  qArgs?: { [key: string]: string };
+}): Promise<void> => {
+  // Each test *must* join a new call to prevent test flakiness.
+  // We hit a Calling SDK service 500 error if we do not.
+  // An issue has been filed with the calling team.
+  const newTestGuid = generateGUID();
+
+  // ensure chats are always unique per test
+  users = await createChatThreadAndUsers(CHAT_PARTICIPANTS);
+  for (let i = 0; i < pages.length; i++) {
+    const page = pages[i];
+    const user = users[i];
+    user.groupId = newTestGuid;
+
+    await page.goto(buildUrl(serverUrl, user, qArgs));
+    await waitForMeetingCompositeToLoad(page);
+  }
+};
