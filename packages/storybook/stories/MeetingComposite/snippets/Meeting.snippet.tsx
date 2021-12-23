df07453e1b93334a0873662a4ec4221b52720d75
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2021-12-23 22:19:56 +0000

    Added strings to the side pane from locale context.

--- packages/storybook/stories/MeetingComposite/snippets/Meeting.snippet.tsx
+++ packages/storybook/stories/MeetingComposite/snippets/Meeting.snippet.tsx
@@ -4,7 +4,8 @@ import {
   MeetingAdapter,
   MeetingComposite,
   createAzureCommunicationMeetingAdapter,
-  MeetingCallControlOptions
+  MeetingCallControlOptions,
+  COMPOSITE_LOCALE_FR_FR
 } from '@azure/communication-react';
 import { Theme, PartialTheme } from '@fluentui/react';
 import React, { useState, useEffect, useMemo } from 'react';
@@ -65,6 +66,7 @@ export const MeetingExperience = (props: MeetingExampleProps): JSX.Element => {
         meetingAdapter={meetingAdapter}
         fluentTheme={props.fluentTheme}
         meetingCallControlOptions={props.meetingCallControlOptions}
+        locale={COMPOSITE_LOCALE_FR_FR}
       />
     );
   }
