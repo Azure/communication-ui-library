7d65662ff00b8041525f4bd29bb8674a84cec766
edwardlee-msft <edwardlee@microsoft.com>
2022-01-06 22:06:23 +0000

    Fixed ScreenShareButton style to allow custom styles

--- packages/react-components/src/components/ScreenShareButton.tsx
+++ packages/react-components/src/components/ScreenShareButton.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import { useLocale } from '../localization';
 import { ControlBarButton, ControlBarButtonProps } from './ControlBarButton';
-import { DefaultPalette, IButtonStyles, Theme, useTheme } from '@fluentui/react';
+import { DefaultPalette, IButtonStyles, mergeStyles, Theme, useTheme } from '@fluentui/react';
 import { HighContrastAwareIcon } from './HighContrastAwareIcon';
 
 /**
@@ -66,9 +66,8 @@ export const ScreenShareButton = (props: ScreenShareButtonProps): JSX.Element =>
 
   return (
     <ControlBarButton
-      // merge in props
       {...props}
-      styles={styles}
+      className={mergeStyles(styles, props.styles)}
       onClick={props.onToggleScreenShare ?? props.onClick}
       onRenderOnIcon={props.onRenderOnIcon ?? onRenderScreenShareOnIcon}
       onRenderOffIcon={props.onRenderOffIcon ?? onRenderScreenShareOffIcon}
