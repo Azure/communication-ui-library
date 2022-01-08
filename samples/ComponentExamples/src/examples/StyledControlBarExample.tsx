8699cd297a114d36d9da05e13fed7081afaf43dd
edwardlee-msft <edwardlee@microsoft.com>
2022-01-08 00:34:34 +0000

    Add custom styled component buttons for e2e testing

--- samples/ComponentExamples/src/examples/StyledControlBarExample.tsx
+++ samples/ComponentExamples/src/examples/StyledControlBarExample.tsx
@@ -1,16 +1,40 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { CameraButton, ControlBar, usePropsFor } from '@azure/communication-react';
+import {
+  CameraButton,
+  ControlBar,
+  ControlBarButtonStyles,
+  DevicesButton,
+  EndCallButton,
+  MicrophoneButton,
+  ScreenShareButton,
+  usePropsFor
+} from '@azure/communication-react';
 import React from 'react';
 
 export const StyledControlBarExample = (): JSX.Element => {
   const cameraButtonProps = usePropsFor(CameraButton);
+  const microphoneButtonProps = usePropsFor(MicrophoneButton);
+  const screenShareButtonProps = usePropsFor(ScreenShareButton);
+  const deviceButtonProps = usePropsFor(DevicesButton);
+  const endCallButtonProps = usePropsFor(EndCallButton);
+
+  const controlBarButtonStyle: ControlBarButtonStyles = {
+    root: {
+      background: '#ffff00'
+    }
+  };
+
   return (
     <>
       <h3>An example of custom sytled Call ControlBar:</h3>
       <br />
       <ControlBar layout={'horizontal'}>
-        <CameraButton {...cameraButtonProps} />
+        <CameraButton styles={controlBarButtonStyle} {...cameraButtonProps} />
+        <MicrophoneButton styles={controlBarButtonStyle} {...microphoneButtonProps} />
+        <ScreenShareButton styles={controlBarButtonStyle} {...screenShareButtonProps} />
+        <DevicesButton styles={controlBarButtonStyle} {...deviceButtonProps} />
+        <EndCallButton styles={controlBarButtonStyle} {...endCallButtonProps} />
       </ControlBar>
     </>
   );
