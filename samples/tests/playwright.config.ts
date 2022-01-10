29888398b663c9d392d85492b4c2bad1ddbb5840
edwardlee-msft <edwardlee@microsoft.com>
2022-01-10 18:07:06 +0000

    Updated viewport to display all contents in ComponentExamples

--- samples/tests/playwright.config.ts
+++ samples/tests/playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
       use: {
         browserName: 'chromium',
         permissions: ['notifications', 'camera', 'microphone'],
-        viewport: null,
+        viewport: { width: 900, height: 900 },
         launchOptions: {
           args: [
             '--font-render-hinting=none', // Ensures that fonts are rendered consistently.
