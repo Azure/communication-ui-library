32b7b2aa6c345557ce0cc9f4ffdc9ff0f624c80c
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-01-14 09:48:30 -0800

    Dmceachernmsft/meetingSample (#1299)
    * added meeting composite into calling sample.
    
    * added Chat utils to meeting sameple
    
    * got it creating a meeting adapter.
    
    * working sample.
    
    * removed Calling changelogs.
    
    * updated readme for meeting
    
    * removed not needed comment
    
    * Changed more calling labelling to meeting.
    
    * fixed URL rewrite issue
    
    * Update samples/Meeting/public/index.html
    
    Co-authored-by: James Burnside <2684369+JamesBurnside@users.noreply.github.com>
    
    * made updates as per comments
    
    * made changes as per comments.
    removed endcall screen to use composite end call.
    
    * updated error page and token credentials.
    removed not needed images.
    
    * removed unused statements
    
    * created callArgs interface for call params
    
    * fix nits
    
    * finished nits.
    
    * removed not needed util file
    
    * removed depricated mobile detection.
    
    Co-authored-by: James Burnside <2684369+JamesBurnside@users.noreply.github.com>

---
+++ samples/Meeting/src/app/utils/constants.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export enum StatusCode {
+  OK = 200,
+  CREATED = 201
+}
+
+export const ENTER_KEY = 13;
+export const GUID_FOR_INITIAL_TOPIC_NAME = 'c774da81-94d5-4652-85c7-6ed0e8dc67e6';
