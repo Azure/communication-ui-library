a34bf51eee0e1805a372179633389cbddfad93f8
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2022-01-18 12:14:56 -0800

    Documented routes in Server/src/routes (#1316)
    * Documented routes in Server/src/routes
    
    * Updated type of params of addUser route

--- samples/Server/src/routes/getEndpointUrl.ts
+++ samples/Server/src/routes/getEndpointUrl.ts
@@ -6,6 +6,15 @@ import { getEndpoint } from '../lib/envHelper';
 
 const router = express.Router();
 
+/**
+ * route: /getEndpointUrl/
+ *
+ * purpose: Get the endpoint url of Azure Communication Services resource.
+ *
+ * @returns The endpoint url as string
+ *
+ */
+
 router.get('/', async function (req, res, next) {
   res.send(getEndpoint());
 });
