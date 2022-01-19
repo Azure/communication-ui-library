6a5f189c10525d70513c8799ca70a40bf26c1e52
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-01-19 01:33:11 +0000

    added back props, was passing in avatar options for style removed on accident

--- packages/react-composites/src/composites/common/AvatarPersona.tsx
+++ packages/react-composites/src/composites/common/AvatarPersona.tsx
@@ -78,6 +78,7 @@ export const AvatarPersona = (props: AvatarPersonaProps): JSX.Element => {
 
   return (
     <Persona
+      {...props}
       text={data?.text ?? text}
       imageUrl={data?.imageUrl ?? imageUrl}
       imageInitials={data?.imageInitials ?? imageInitials}
