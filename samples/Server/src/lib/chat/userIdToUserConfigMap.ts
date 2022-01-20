4cf80b0f49f321d59f52262db55605c0ddad4a79
carocao-msft <carolinecao@microsoft.com>
2022-01-20 14:44:26 -0500

    update comment

--- samples/Server/src/lib/chat/userIdToUserConfigMap.ts
+++ samples/Server/src/lib/chat/userIdToUserConfigMap.ts
@@ -9,5 +9,5 @@ interface UserConfig {
 
 // For the purpose of this sample, we opted to use an in-memory data store.
 // This means that if the web application is restarted any information maintained would be wiped.
-// For longer term storage solutions we suggest using Azure Blob Storage (https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blobs-introduction)
+// For longer term storage solutions we suggest referring to this document -> https://docs.microsoft.com/en-us/azure/architecture/guide/technology-choices/data-store-decision-tree
 export const userIdToUserConfigMap = new Map<string, UserConfig>();
