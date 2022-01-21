27ebbbaaedabdc50231dcbc9fd258ece554d55bf
edwardlee-msft <edwardlee@microsoft.com>
2022-01-21 18:54:22 +0000

    Set avatar initials by fluentui and remove unnecessary usernames on top of messages

--- packages/react-components/src/components/MessageThread.tsx
+++ packages/react-components/src/components/MessageThread.tsx
@@ -18,7 +18,17 @@ import {
   gutterWithAvatar,
   gutterWithHiddenAvatar
 } from './styles/MessageThread.styles';
-import { Icon, IStyle, mergeStyles, Persona, PersonaSize, PrimaryButton, Stack, IPersona } from '@fluentui/react';
+import {
+  Icon,
+  IStyle,
+  mergeStyles,
+  Persona,
+  PersonaSize,
+  PrimaryButton,
+  Stack,
+  IPersona,
+  getInitials
+} from '@fluentui/react';
 import { ComponentSlotStyle } from '@fluentui/react-northstar';
 import { LiveAnnouncer } from 'react-aria-live';
 import { delay } from './utils/delay';
@@ -322,7 +332,8 @@ const memoizeAllMessages = memoizeFnAll(
 
         const personaOptions: IPersona = {
           hidePersonalDetails: true,
-          size: PersonaSize.size32
+          size: PersonaSize.size32,
+          imageInitials: getInitials(message.senderDisplayName, false, true)
         };
 
         const chatItemMessageStyle =
