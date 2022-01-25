094211138fd1ce5a4c64cfa0821d2e3c18154547
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-01-25 14:05:15 -0800

    [Cherry Pick]Teams interop meeting sample (#1366)
    * updated logic as per nit
    
    * added thread Parsing function in for teams links
    
    * added missing import

--- samples/Meeting/src/app/App.tsx
+++ samples/Meeting/src/app/App.tsx
@@ -11,6 +11,7 @@ import {
   chatSDKVersion,
   createGroupId,
   fetchTokenResponse,
+  getChatThreadFromTeamsLink,
   getGroupIdFromUrl,
   getTeamsLinkFromUrl,
   isOnIphoneAndNotSafari
@@ -35,6 +36,8 @@ interface CallArgs {
   displayName: string;
 }
 
+const webAppTitle = document.title;
+
 const App = (): JSX.Element => {
   console.log(
     `ACS sample Meeting app. Last Updated ${buildTime} Using @azure/communication-calling:${callingSDKVersion} and Using @azure/communication-chat:${chatSDKVersion}`
@@ -44,7 +47,6 @@ const App = (): JSX.Element => {
 
   type AppPages = 'home' | 'meeting' | 'error';
 
-  const webAppTitle = document.title;
   const [page, setPage] = useState<AppPages>('home');
 
   // User credentials to join a meeting chat thread with - these are retrieved from the server
@@ -54,8 +56,8 @@ const App = (): JSX.Element => {
   const [callArgs, setCallArgs] = useState<CallArgs | undefined>(undefined);
 
   // Chat endpoint and thread id
-  const [endpointUrl, setEndpointUrl] = useState('');
-  const [threadId, setThreadId] = useState('');
+  const [endpointUrl, setEndpointUrl] = useState<string | undefined>(undefined);
+  const [threadId, setThreadId] = useState<string | undefined>(undefined);
 
   // Get Azure Communications Service token from the server
   useEffect(() => {
@@ -73,15 +75,17 @@ const App = (): JSX.Element => {
   useEffect(() => {
     (async () => {
       if (callArgs?.displayName && credentials !== undefined) {
-        const newThreadId = await getThread();
-        const result = await joinThread(newThreadId, credentials.userId.communicationUserId, callArgs.displayName);
-        if (!result) {
-          alert(ALERT_TEXT_TRY_AGAIN);
-          return;
+        if (threadId === undefined) {
+          const newThreadId = await getThread();
+          const result = await joinThread(newThreadId, credentials.userId.communicationUserId, callArgs.displayName);
+          if (!result) {
+            alert(ALERT_TEXT_TRY_AGAIN);
+            return;
+          }
+          setThreadId(newThreadId);
+          pushQSPUrl({ name: 'threadId', value: newThreadId });
         }
         setEndpointUrl(await getEndpointUrl());
-        setThreadId(newThreadId);
-        pushQSPUrl({ name: 'threadId', value: newThreadId });
       }
     })();
   }, [callArgs?.displayName, credentials?.userId.communicationUserId]);
@@ -105,7 +109,6 @@ const App = (): JSX.Element => {
               callLocator: callDetails.teamsLink || getTeamsLinkFromUrl() || getGroupIdFromUrl() || createGroupId()
             };
             setCallArgs(localCallArgs);
-
             // Update window URL to have a joinable link
             if (!joiningExistingMeeting) {
               const joinParam = isTeamsMeeting
@@ -115,6 +118,9 @@ const App = (): JSX.Element => {
                   }
                 : { name: 'groupId', value: (localCallArgs?.callLocator as GroupCallLocator).groupId };
               pushQSPUrl(joinParam);
+            }
+            if (isTeamsMeeting && callDetails.teamsLink) {
+              setThreadId(getChatThreadFromTeamsLink(callDetails.teamsLink.meetingLink.toString()));
             }
             setPage('meeting');
           }}
