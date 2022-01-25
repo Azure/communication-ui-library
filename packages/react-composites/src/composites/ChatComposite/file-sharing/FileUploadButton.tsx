d42ed0c1e7159c276376be72e1a004cb7e0a2fd6
Anjul Garg <anjulgarg@live.com>
2022-01-25 19:31:52 +0000

    Create file upload button component for chat composite

---
+++ packages/react-composites/src/composites/ChatComposite/file-sharing/FileUploadButton.tsx
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { Icon, mergeStyles, Stack, useTheme } from '@fluentui/react';
+import React, { useRef } from 'react';
+import { FileUploadHandler } from './FileUploadHandler';
+import { UploadedFile } from './UploadedFile';
+
+/**
+ * Props for {@link FileUploadButton} component.
+ * @internal
+ */
+export interface FileUploadButtonProps {
+  /**
+   * The ACS user ID of the user uploading the file.
+   */
+  userId: string;
+  /**
+   * The function of type {@link FileUploadHandler} for handling file uploads.
+   */
+  fileUploadHandler?: FileUploadHandler;
+  /**
+   * A string containing the comma separated list of accepted file types.
+   * Similar to the `accept` attribute of the `<input type="file" />` element.
+   */
+  accept?: string;
+  /**
+   * Allows multiple files to be selected if set to `true`.
+   * Default value is `false`.
+   */
+  multiple?: boolean;
+}
+
+/**
+ * @internal
+ */
+export const FileUploadButton = (props: FileUploadButtonProps): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const theme = useTheme();
+  const { userId, fileUploadHandler, accept, multiple = false } = props;
+
+  const onChange = (files: FileList | null): void => {
+    if (!files) {
+      return;
+    }
+
+    const uploadedFiles: UploadedFile[] = [];
+
+    for (const file of files) {
+      const uploadedFile = new UploadedFile(file);
+      uploadedFiles.push(uploadedFile);
+    }
+
+    fileUploadHandler && fileUploadHandler(userId, uploadedFiles);
+  };
+
+  return (
+    <>
+      <Stack
+        verticalAlign="center"
+        horizontalAlign="center"
+        className={mergeStyles({
+          width: '1.5rem',
+          height: '1.5rem',
+          cursor: 'pointer',
+          padding: '0.15rem',
+          ':hover': {
+            backgroundColor: theme.palette.neutralLighter
+          }
+        })}
+        onClick={() => inputRef.current?.click()}
+      >
+        <Icon iconName="Attach" />
+      </Stack>
+      <input
+        ref={inputRef}
+        hidden
+        multiple={multiple}
+        accept={accept}
+        type="file"
+        onChange={(e) => {
+          e.stopPropagation();
+          onChange(e.currentTarget.files);
+        }}
+      />
+    </>
+  );
+};
---
+++ packages/react-composites/src/composites/ChatComposite/file-sharing/FileUploadHandler.tsx
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { UploadedFile } from './UploadedFile';
+
+/**
+ * @beta
+ * A callback function for handling file uploads.
+ *
+ * @param userId The user ID of the user uploading the file.
+ * @param uploadedFiles The list of uploaded files. Each file is represented by an {@link UploadedFile} object.
+ */
+export type FileUploadHandler = (userId: string, uploadedFiles: UploadedFile[]) => void;
