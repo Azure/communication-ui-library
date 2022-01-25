08fb610b862fb8412b6eeba9313a83593ddce84c
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-01-25 10:13:57 -0800

    checkpoint for creating storybook page to play around with

--- packages/react-components/src/components/PictureInPictureInPicture/PictureInPictureInPicture.tsx
+++ packages/react-components/src/components/PictureInPictureInPicture/PictureInPictureInPicture.tsx
@@ -1,7 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React from 'react';
+import React, { ReactChild } from 'react';
+import {
+  PictureInPictureInPicturePrimaryTile,
+  PictureInPictureInPictureSecondaryTile,
+  _PictureInPictureInPictureTileProps
+} from './PictureInPictureInPictureTile';
 
 /**
  * Props for {@link _PictureInPictureInPicture} component.
@@ -13,6 +18,9 @@ export interface _PictureInPictureInPictureProps {
    * Callback when the {@link _PictureInPictureInPicture} is clicked.
    */
   onClick?: () => void;
+
+  primaryTile: _PictureInPictureInPictureTileProps;
+  secondaryTile: _PictureInPictureInPictureTileProps;
 }
 
 /* eslint-disable @typescript-eslint/no-unused-vars */ // REMOVE WHEN PROPS USED (BELOW)
@@ -26,5 +34,34 @@ export interface _PictureInPictureInPictureProps {
  * @internal
  */
 export const _PictureInPictureInPicture = (props: _PictureInPictureInPictureProps): JSX.Element => {
-  return <></>;
+  return (
+    <PictureInPictureInPictureContainer
+      primaryView={<PictureInPictureInPicturePrimaryTile {...props.primaryTile} />}
+      secondaryView={<PictureInPictureInPictureSecondaryTile {...props.secondaryTile} />}
+    />
+  );
+};
+/**
+ * Container for the picture in picture in picture component.
+ * This governs positioning and floating of the secondary PiP.
+ */
+const PictureInPictureInPictureContainer = (props: {
+  primaryView: ReactChild;
+  secondaryView: ReactChild;
+  onClick?: () => void;
+}): JSX.Element => (
+  <div style={tileContainerStyles} onClick={props.onClick}>
+    {props.primaryView}
+    <div style={secondaryTileFloatingStyles}>{props.secondaryView}</div>
+  </div>
+);
+
+const tileContainerStyles: React.CSSProperties = {
+  position: 'relative'
+};
+
+const secondaryTileFloatingStyles: React.CSSProperties = {
+  position: 'absolute',
+  bottom: '0.125rem',
+  right: '0.125rem'
 };
---
+++ packages/react-components/src/components/PictureInPictureInPicture/PictureInPictureInPictureTile.tsx
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { memoizeFunction } from '@fluentui/react';
+import React from 'react';
+import { useTheme } from '../../theming/FluentThemeProvider';
+import { VideoTile, VideoTileProps, VideoTileStylesProps } from '../VideoTile';
+
+type TileOrientation = 'portrait' | 'landscape';
+
+/** @internal */
+export interface _PictureInPictureInPictureTileProps
+  extends Pick<
+    VideoTileProps,
+    'styles' | 'displayName' | 'renderElement' | 'isMirrored' | 'noVideoAvailableAriaLabel'
+  > {
+  orientation: TileOrientation;
+}
+
+/** @private */
+export const PictureInPictureInPicturePrimaryTile = (props: _PictureInPictureInPictureTileProps): JSX.Element => {
+  const boxShadow = useTheme().effects.elevation8;
+  return <PictureInPictureInPictureTile {...props} styles={primaryTileStyles(props.orientation, boxShadow)} />;
+};
+
+/** @private */
+export const PictureInPictureInPictureSecondaryTile = (props: _PictureInPictureInPictureTileProps): JSX.Element => (
+  <PictureInPictureInPictureTile {...props} styles={secondaryTileStyles(props.orientation)} />
+);
+
+const PictureInPictureInPictureTile = (props: VideoTileProps): JSX.Element => (
+  <VideoTile {...props} showLabel={false} />
+);
+
+const primaryTileStyles = memoizeFunction(
+  (orientation: TileOrientation, themeElevation: string): VideoTileStylesProps => ({
+    root: {
+      borderRadius: '0.25rem',
+      height: orientation === 'landscape' ? '5.5rem' : '8rem',
+      width: orientation === 'landscape' ? '8rem' : '5.5rem',
+      boxShadow: themeElevation,
+      background: 'blue' // TODO: [jaburnsi] REMOVE
+    }
+  })
+);
+
+const secondaryTileStyles = memoizeFunction(
+  (orientation: TileOrientation): VideoTileStylesProps => ({
+    root: {
+      borderRadius: '0.25rem',
+      height: orientation === 'landscape' ? '1.625rem' : '2.0625rem',
+      width: orientation === 'landscape' ? '2.0625rem' : '1.625rem',
+      background: 'red' // TODO: [jaburnsi] REMOVE
+    }
+  })
+);
