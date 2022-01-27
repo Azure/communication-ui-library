a6f0fe52a357bb4e34226b82997f83a62393f2b6
Miguel Gamis <miguelgamis@microsoft.com>
2022-01-27 14:26:48 -0800

    removed default test remote participants

--- packages/react-composites/tests/browser/call/HorizontalGallery.test.ts
+++ packages/react-composites/tests/browser/call/HorizontalGallery.test.ts
@@ -5,36 +5,34 @@ import { test } from './fixture';
 import { expect } from '@playwright/test';
 import { buildUrlWithMockAdapter } from './utils';
 import { dataUiId, pageClick, clickOffPage, waitForPageFontsLoaded, waitForSelector } from '../common/utils';
-import { TestRemoteParticipant } from './TestCallingState';
 import { IDS } from '../common/constants';
-
-const defaultTestRemoteParticipants: TestRemoteParticipant[] = [
-  {
-    displayName: 'Paul Bridges',
-    isSpeaking: true,
-    isVideoStreamAvailable: true
-  },
-  {
-    displayName: 'Eryka Klein'
-  },
-  {
-    displayName: 'Fiona Harper',
-    isVideoStreamAvailable: true
-  }
-];
 
 test.describe('HorizontalGallery tests', async () => {
   test('HorizontalGallery should have 1 audio participant', async ({ pages, serverUrl }) => {
     const page = pages[0];
+    const testRemoteParticipants = [
+      {
+        displayName: 'Paul Bridges',
+        isSpeaking: true,
+        isVideoStreamAvailable: true
+      },
+      {
+        displayName: 'Eryka Klein'
+      },
+      {
+        displayName: 'Fiona Harper',
+        isVideoStreamAvailable: true
+      }
+    ];
     await page.goto(
       buildUrlWithMockAdapter(serverUrl, {
-        remoteParticipants: defaultTestRemoteParticipants
+        remoteParticipants: testRemoteParticipants
       })
     );
     // Click off page to turn away initial aria label
     await clickOffPage(page);
     await waitForPageFontsLoaded(page);
-    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery.png');
+    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-1-audio-participant.png');
   });
 
   test('HorizontalGallery should have multiple audio participants spanning multiple pages. Navigation buttons should work.', async ({
@@ -42,7 +40,19 @@ test.describe('HorizontalGallery tests', async () => {
     serverUrl
   }) => {
     const page = pages[0];
-    const testRemoteParticipants = defaultTestRemoteParticipants.concat([
+    const testRemoteParticipants = [
+      {
+        displayName: 'Paul Bridges',
+        isSpeaking: true,
+        isVideoStreamAvailable: true
+      },
+      {
+        displayName: 'Eryka Klein'
+      },
+      {
+        displayName: 'Fiona Harper',
+        isVideoStreamAvailable: true
+      },
       {
         displayName: 'Pardeep Singh'
       },
@@ -63,7 +73,7 @@ test.describe('HorizontalGallery tests', async () => {
       {
         displayName: 'Gerald Ho'
       }
-    ]);
+    ];
     await page.goto(
       buildUrlWithMockAdapter(serverUrl, {
         remoteParticipants: testRemoteParticipants
@@ -72,12 +82,12 @@ test.describe('HorizontalGallery tests', async () => {
     // Click off page to turn away initial aria label
     await clickOffPage(page);
     await waitForPageFontsLoaded(page);
-    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-page-1.png');
+    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-multiple-audio-participants-page-1.png');
     await waitForSelector(page, dataUiId(IDS.horizontalGalleryRightNavButton));
     await pageClick(page, dataUiId(IDS.horizontalGalleryRightNavButton));
-    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-page-2.png');
+    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-multiple-audio-participants-page-2.png');
     await waitForSelector(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
     await pageClick(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
-    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-page-1.png');
+    expect(await page.screenshot()).toMatchSnapshot('horizontal-gallery-multiple-audio-participants-page-1.png');
   });
 });
