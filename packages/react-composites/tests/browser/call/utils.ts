0ec2f58305b02b14ee7590bf5ccc410009e65458
Miguel Gamis <miguelgamis@microsoft.com>
2022-01-28 01:36:17 -0800

    buildUrlWithMockAdapter doc improvement

--- packages/react-composites/tests/browser/call/utils.ts
+++ packages/react-composites/tests/browser/call/utils.ts
@@ -8,7 +8,7 @@ import { TestCallingState } from './TestCallingState';
  * Create the test URL.
  * @param serverUrl - URL of webpage to test, this is typically https://localhost:3000
  * @param user - Test user the props of which populate query search parameters
- * @param state - State
+ * @param state - Calling state of type TestCallingState passed as a query search parameter
  * @param qArgs - Optional args to add to the query search parameters of the URL.
  * @returns URL string
  */
