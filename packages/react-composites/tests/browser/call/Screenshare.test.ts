f3859a5ad8b92316e7a29bf3cd64da7089acf2f5
Miguel Gamis <miguelgamis@microsoft.com>
2022-02-02 17:48:25 -0800

    shortened test name

--- packages/react-composites/tests/browser/call/Screenshare.test.ts
+++ packages/react-composites/tests/browser/call/Screenshare.test.ts
@@ -60,66 +60,61 @@ test.describe('Screenshare tests', async () => {
     expect(await page.screenshot()).toMatchSnapshot('local-screenshare.png');
   });
 
-  test(
-    'Remote screen share stream should be displayed in grid area of VideoGallery. ' +
-      'All remote participants should be HorizontalGallery spanning multiple pages. ' +
-      'Navigation buttons should work.',
-    async ({ pages, serverUrl }) => {
-      const page = pages[0];
-      const testRemoteParticipants = [
-        {
-          displayName: 'Pardeep Singh'
-        },
-        {
-          displayName: 'Reina Takizawa',
-          isSpeaking: true
-        },
-        {
-          displayName: 'Vasily Podkolzin',
-          isMuted: true
-        },
-        {
-          displayName: 'Luciana Rodriguez'
-        },
-        {
-          displayName: 'Antonie van Leeuwenhoek'
-        },
-        {
-          displayName: 'Gerald Ho'
-        },
-        {
-          displayName: 'Helen Sediq',
-          isScreenSharing: true
-        },
-        {
-          displayName: 'Paul Bridges',
-          isSpeaking: true,
-          isVideoStreamAvailable: true
-        },
-        {
-          displayName: 'Eryka Klein'
-        },
-        {
-          displayName: 'Fiona Harper',
-          isVideoStreamAvailable: true
-        }
-      ];
-      await page.goto(
-        buildUrlWithMockAdapter(serverUrl, {
-          remoteParticipants: testRemoteParticipants
-        })
-      );
-      // Click off page to turn away initial aria label
-      await clickOutsideOfPage(page);
-      await waitForSelector(page, dataUiId(IDS.videoGallery));
-      await waitForPageFontsLoaded(page);
-      expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-page-1.png');
-      await waitForSelector(page, dataUiId(IDS.horizontalGalleryRightNavButton));
-      await pageClick(page, dataUiId(IDS.horizontalGalleryRightNavButton));
-      expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-page-2.png');
-      await waitForSelector(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
-      await pageClick(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
-      expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-back-to-page-1.png');
-    }
-  );
+  test('Remote screen share stream should be displayed in grid area of VideoGallery.', async ({ pages, serverUrl }) => {
+    const page = pages[0];
+    const testRemoteParticipants = [
+      {
+        displayName: 'Pardeep Singh'
+      },
+      {
+        displayName: 'Reina Takizawa',
+        isSpeaking: true
+      },
+      {
+        displayName: 'Vasily Podkolzin',
+        isMuted: true
+      },
+      {
+        displayName: 'Luciana Rodriguez'
+      },
+      {
+        displayName: 'Antonie van Leeuwenhoek'
+      },
+      {
+        displayName: 'Gerald Ho'
+      },
+      {
+        displayName: 'Helen Sediq',
+        isScreenSharing: true
+      },
+      {
+        displayName: 'Paul Bridges',
+        isSpeaking: true,
+        isVideoStreamAvailable: true
+      },
+      {
+        displayName: 'Eryka Klein'
+      },
+      {
+        displayName: 'Fiona Harper',
+        isVideoStreamAvailable: true
+      }
+    ];
+    await page.goto(
+      buildUrlWithMockAdapter(serverUrl, {
+        remoteParticipants: testRemoteParticipants
+      })
+    );
+    // Click off page to turn away initial aria label
+    await clickOutsideOfPage(page);
+    await waitForSelector(page, dataUiId(IDS.videoGallery));
+    await waitForPageFontsLoaded(page);
+    expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-page-1.png');
+    await waitForSelector(page, dataUiId(IDS.horizontalGalleryRightNavButton));
+    await pageClick(page, dataUiId(IDS.horizontalGalleryRightNavButton));
+    expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-page-2.png');
+    await waitForSelector(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
+    await pageClick(page, dataUiId(IDS.horizontalGalleryLeftNavButton));
+    expect(await page.screenshot()).toMatchSnapshot('remote-screenshare-horizontal-gallery-back-to-page-1.png');
+  });
 });
