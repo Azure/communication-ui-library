a68893f5b2fb600949e1ec56429cf2c3a0db0cde
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-02-03 01:22:52 +0000

    removed un needed object

--- packages/react-composites/src/composites/MeetingComposite/ChatButton.tsx
+++ packages/react-composites/src/composites/MeetingComposite/ChatButton.tsx
@@ -41,7 +41,6 @@ export const ChatButton = (props: ChatButtonProps): JSX.Element => {
         onRenderOffIcon={props.onRenderOffIcon ?? onRenderOffIcon}
         onClick={props.onClick}
       />
-      {}
     </Stack>
   );
 };
