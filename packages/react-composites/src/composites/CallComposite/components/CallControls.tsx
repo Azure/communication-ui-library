180629502e68c4905045dee7809c644bf0edb0b8
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-02-10 08:50:49 +0000

    Drop unnecessary logic from CallControls

--- packages/react-composites/src/composites/CallComposite/components/CallControls.tsx
+++ packages/react-composites/src/composites/CallComposite/components/CallControls.tsx
@@ -3,12 +3,7 @@
 
 import { Stack } from '@fluentui/react';
 import { _isInLobbyOrConnecting } from '@internal/calling-component-bindings';
-import {
-  BaseCustomStyles,
-  ControlBar,
-  ControlBarButtonStyles,
-  ParticipantMenuItemsCallback
-} from '@internal/react-components';
+import { ControlBar, ParticipantMenuItemsCallback } from '@internal/react-components';
 import React, { useMemo } from 'react';
 import { CallControlOptions, CustomCallControlButtonCallback } from '../types/CallControlOptions';
 import { Camera } from './buttons/Camera';
@@ -31,23 +26,6 @@ export type CallControlsProps = {
    * Recommended for mobile devices.
    */
   increaseFlyoutItemSize?: boolean;
-  /**
-   * Whether to use split buttons to show device selection drop-downs
-   * Used by {@link MeetingComposite}.
-   */
-  splitButtonsForDeviceSelection?: boolean;
-  /**
-   * Styles for the {@link ControlBar}.
-   */
-  controlBarStyles?: BaseCustomStyles;
-  /**
-   * Styles for all buttons except {@link EndCallButton}.
-   */
-  commonButtonStyles?: ControlBarButtonStyles;
-  /**
-   * Styles for {@link EndCallButton}.
-   */
-  endCallButtonStyles?: ControlBarButtonStyles;
 };
 
 /**
@@ -75,54 +53,31 @@ export const CallControls = (props: CallControlsProps): JSX.Element => {
             dockedBottom it has position absolute and would therefore float on top of the media gallery,
             occluding some of its content.
          */}
-        <ControlBar layout="horizontal" styles={props.controlBarStyles}>
+        <ControlBar layout="horizontal">
           {customButtons['first']}
-          {options?.microphoneButton !== false && (
-            <Microphone
-              displayType={options?.displayType}
-              styles={props.commonButtonStyles}
-              splitButtonsForDeviceSelection={props.splitButtonsForDeviceSelection}
-            />
-          )}
+          {isEnabled(options?.microphoneButton) && <Microphone displayType={options?.displayType} />}
           {customButtons['afterMicrophoneButton']}
-          {options?.cameraButton !== false && (
-            <Camera
-              displayType={options?.displayType}
-              styles={props.commonButtonStyles}
-              splitButtonsForDeviceSelection={props.splitButtonsForDeviceSelection}
-            />
-          )}
+          {isEnabled(options?.cameraButton) && <Camera displayType={options?.displayType} />}
           {customButtons['afterCameraButton']}
-          {options?.screenShareButton !== false && (
-            <ScreenShare
-              option={options?.screenShareButton}
-              displayType={options?.displayType}
-              styles={props.commonButtonStyles}
-            />
+          {isEnabled(options?.screenShareButton) && (
+            <ScreenShare option={options?.screenShareButton} displayType={options?.displayType} />
           )}
           {customButtons['afterScreenShareButton']}
-          {options?.participantsButton !== false && (
+          {isEnabled(options?.participantsButton) && (
             <Participants
               option={options?.participantsButton}
               callInvitationURL={props.callInvitationURL}
               onFetchParticipantMenuItems={props.onFetchParticipantMenuItems}
               displayType={options?.displayType}
               increaseFlyoutItemSize={props.increaseFlyoutItemSize}
-              styles={props.commonButtonStyles}
             />
           )}
           {customButtons['afterParticipantsButton']}
-          {options?.devicesButton !== false && (
-            <Devices
-              displayType={options?.displayType}
-              increaseFlyoutItemSize={props.increaseFlyoutItemSize}
-              styles={props.commonButtonStyles}
-            />
+          {isEnabled(options?.devicesButton) && (
+            <Devices displayType={options?.displayType} increaseFlyoutItemSize={props.increaseFlyoutItemSize} />
           )}
           {customButtons['afterOptionsButton']}
-          {options?.endCallButton !== false && (
-            <EndCall displayType={options?.displayType} styles={props.endCallButtonStyles} />
-          )}
+          {isEnabled(options?.endCallButton) && <EndCall displayType={options?.displayType} />}
           {customButtons['afterEndCallButton']}
           {customButtons['last']}
         </ControlBar>
@@ -130,6 +85,9 @@ export const CallControls = (props: CallControlsProps): JSX.Element => {
     </Stack>
   );
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isEnabled = (option: any): boolean => option !== false;
 
 const onFetchCustomButtonPropsTrampoline = (
   options?: CallControlOptions
