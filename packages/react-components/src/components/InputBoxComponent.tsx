a4d422cdf84aee50d72ee826b721bde6a39da221
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-02-14 22:26:16 +0000

    Fix nits and api docs

--- packages/react-components/src/components/InputBoxComponent.tsx
+++ packages/react-components/src/components/InputBoxComponent.tsx
@@ -95,7 +95,7 @@ export const InputBoxComponent = (props: InputBoxComponentProps): JSX.Element =>
 
   const onTexFieldKeyDown = useCallback(
     (ev: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      if ((ev.nativeEvent as KeyboardEvent).isComposing) {
+      if (ev.nativeEvent.isComposing) {
         return;
       }
       if (ev.key === 'Enter' && (ev.shiftKey === false || !supportNewline)) {
