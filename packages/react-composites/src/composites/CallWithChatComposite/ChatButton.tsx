8df058f135d58abeee4f496b8301b32bcf7a7f11
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-02-16 05:11:50 +0000

    Fix chat button icon

--- packages/react-composites/src/composites/CallWithChatComposite/ChatButton.tsx
+++ packages/react-composites/src/composites/CallWithChatComposite/ChatButton.tsx
@@ -1,26 +1,26 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { ControlBarButton, ControlBarButtonProps } from '@internal/react-components';
 import { Chat20Filled, Chat20Regular } from '@fluentui/react-icons';
 
-const onRenderOnIcon = (): JSX.Element => <Chat20Filled key={'chatOnIconKey'} primaryFill="currentColor" />;
-const onRenderOffIcon = (): JSX.Element => <Chat20Regular key={'chatOffIconKey'} primaryFill="currentColor" />;
+const filledIcon = (): JSX.Element => <Chat20Filled key={'chatOnIconKey'} primaryFill="currentColor" />;
+const regularIcon = (): JSX.Element => <Chat20Regular key={'chatOffIconKey'} primaryFill="currentColor" />;
 
 /**
  * @private
  */
 export const ChatButton = (props: ControlBarButtonProps): JSX.Element => {
   const strings = { label: props.label, ...props.strings };
-
+  const defaultIcon = useMemo(() => (props.showLabel ? regularIcon : filledIcon), [props.showLabel]);
   return (
     <ControlBarButton
       {...props}
       labelKey={'chatButtonLabelKey'}
       strings={strings}
-      onRenderOnIcon={props.onRenderOnIcon ?? onRenderOnIcon}
-      onRenderOffIcon={props.onRenderOffIcon ?? onRenderOffIcon}
+      onRenderOnIcon={props.onRenderOnIcon ?? defaultIcon}
+      onRenderOffIcon={props.onRenderOffIcon ?? defaultIcon}
       onClick={props.onClick}
     />
   );
