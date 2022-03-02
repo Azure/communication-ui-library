b51a54f165a378e1892bfeda2ef28a2a509d4a59
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-03-02 10:18:00 -0800

    Fix call control bar wrapping to a new line (#1571)

--- packages/react-composites/src/composites/CallWithChatComposite/styles/CallWithChatCompositeStyles.ts
+++ packages/react-composites/src/composites/CallWithChatComposite/styles/CallWithChatCompositeStyles.ts
@@ -10,13 +10,6 @@ export const compositeOuterContainerStyles: IStackStyles = {
   root: {
     width: '100%',
 
-    // This allows the composite to correctly contain the call composite by calculating a height for the available space.
-    // Items inside the call composite that fill 100% of the height will now fill the height of this container instead
-    // of the height of the child items.
-    // If this css property is to be removed, test the participant pane correctly uses the scroll overflow when there a lot
-    // of participants (i.e. beyond screen height) and where one participant is sharing their screen.
-    flexWrap: 'wrap',
-
     // Create a new stacking context so that DrawerMenu can be positioned absolutely.
     position: 'relative'
   }
