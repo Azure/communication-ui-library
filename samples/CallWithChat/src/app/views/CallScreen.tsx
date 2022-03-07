d3041cfea32da46fcd8de7be0e2dde16f970c1c3
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-03-07 19:55:27 +0000

    Use new hook from sample

--- samples/CallWithChat/src/app/views/CallScreen.tsx
+++ samples/CallWithChat/src/app/views/CallScreen.tsx
@@ -9,10 +9,11 @@ import {
   CallAndChatLocator,
   CallWithChatAdapter,
   CallWithChatAdapterState,
-  CallWithChatComposite
+  CallWithChatComposite,
+  useAzureCommunicationCallWithChatAdapter
 } from '@azure/communication-react';
 import { Spinner } from '@fluentui/react';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useSwitchableFluentTheme } from '../theming/SwitchableFluentThemeProvider';
 import { createAutoRefreshingCredential } from '../utils/credential';
 import MobileDetect from 'mobile-detect';
@@ -30,13 +31,41 @@ export interface CallScreenProps {
 
 export const CallScreen = (props: CallScreenProps): JSX.Element => {
   const { token, userId, displayName, endpoint, locator } = props;
-  const [adapter, setAdapter] = useState<CallWithChatAdapter>();
   const callIdRef = useRef<string>();
-  const adapterRef = useRef<CallWithChatAdapter>();
   const { currentTheme, currentRtl } = useSwitchableFluentTheme();
-  const [isMobileSession, setIsMobileSession] = useState<boolean>(detectMobileSession());
+
+  const credential = useMemo(
+    () => createAutoRefreshingCredential(toFlatCommunicationIdentifier(userId), token),
+    [userId, token]
+  );
+  const adapter = useAzureCommunicationCallWithChatAdapter({ userId, displayName, credential, endpoint, locator });
+  useEffect(() => {
+    if (!adapter) {
+      return;
+    }
+    adapter.on('callError', (e) => {
+      // Error is already acted upon by the Call composite, but the surrounding application could
+      // add top-level error handling logic here (e.g. reporting telemetry).
+      console.log('Adapter error event:', e);
+    });
+    adapter.on('chatError', (e) => {
+      // Error is already acted upon by the Chat composite, but the surrounding application could
+      // add top-level error handling logic here (e.g. reporting telemetry).
+      console.log('Adapter error event:', e);
+    });
+    adapter.onStateChange((state: CallWithChatAdapterState) => {
+      const pageTitle = convertPageStateToString(state);
+      document.title = `${pageTitle} - ${WEB_APP_TITLE}`;
+
+      if (state?.call?.id && callIdRef.current !== state?.call?.id) {
+        callIdRef.current = state?.call?.id;
+        console.log(`Call Id: ${callIdRef.current}`);
+      }
+    });
+  }, [adapter]);
 
   // Whenever the sample is changed from desktop -> mobile using the emulator, make sure we update the formFactor.
+  const [isMobileSession, setIsMobileSession] = useState<boolean>(detectMobileSession());
   useEffect(() => {
     const updateIsMobile = (): void => {
       // The userAgent string is sometimes not updated synchronously when the `resize` event fires.
@@ -47,47 +76,6 @@ export const CallScreen = (props: CallScreenProps): JSX.Element => {
     window.addEventListener('resize', updateIsMobile);
     return () => window.removeEventListener('resize', updateIsMobile);
   }, []);
-
-  useEffect(() => {
-    (async () => {
-      if (!userId || !displayName || !locator || !token || !endpoint) {
-        return;
-      }
-
-      const adapter = await createAzureCommunicationCallWithChatAdapter({
-        userId,
-        displayName,
-        credential: createAutoRefreshingCredential(toFlatCommunicationIdentifier(userId), token),
-        endpoint,
-        locator: locator
-      });
-      adapter.on('callError', (e) => {
-        // Error is already acted upon by the Call composite, but the surrounding application could
-        // add top-level error handling logic here (e.g. reporting telemetry).
-        console.log('Adapter error event:', e);
-      });
-      adapter.on('chatError', (e) => {
-        // Error is already acted upon by the Chat composite, but the surrounding application could
-        // add top-level error handling logic here (e.g. reporting telemetry).
-        console.log('Adapter error event:', e);
-      });
-      adapter.onStateChange((state: CallWithChatAdapterState) => {
-        const pageTitle = convertPageStateToString(state);
-        document.title = `${pageTitle} - ${WEB_APP_TITLE}`;
-
-        if (state?.call?.id && callIdRef.current !== state?.call?.id) {
-          callIdRef.current = state?.call?.id;
-          console.log(`Call Id: ${callIdRef.current}`);
-        }
-      });
-      setAdapter(adapter);
-      adapterRef.current = adapter;
-    })();
-
-    return () => {
-      adapterRef?.current?.dispose();
-    };
-  }, [displayName, token, userId, locator, endpoint]);
 
   if (!adapter) {
     return <Spinner label={'Creating adapter'} ariaLive="assertive" labelPosition="top" />;
