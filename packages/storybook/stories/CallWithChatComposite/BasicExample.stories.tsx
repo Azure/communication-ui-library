18b7fcae9a9eaa10919ed3af9d883860ddd0ad5e
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-03-10 18:56:48 +0000

    fix lint errors

--- packages/storybook/stories/CallWithChatComposite/BasicExample.stories.tsx
+++ packages/storybook/stories/CallWithChatComposite/BasicExample.stories.tsx
@@ -5,7 +5,6 @@ import { CallWithChatComposite } from '@azure/communication-react';
 import { Stack } from '@fluentui/react';
 import { Meta } from '@storybook/react/types-6-0';
 import React, { useState, useEffect } from 'react';
-import { SingleLineBetaBanner } from '../BetaBanners/SingleLineBetaBanner';
 import { COMPOSITE_FOLDER_PREFIX, compositeExperienceContainerStyle } from '../constants';
 import { controlsToAdd, defaultCallWithChatCompositeHiddenControls } from '../controlsUtils';
 import { getDocs } from './CallWithChatCompositeDocs';
