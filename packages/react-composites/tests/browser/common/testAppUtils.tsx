f2540c0f2ff3c05811642ba7446d06ec06e184e4
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-03-11 21:06:02 +0000

    spelling

--- packages/react-composites/tests/browser/common/testAppUtils.tsx
+++ packages/react-composites/tests/browser/common/testAppUtils.tsx
@@ -30,7 +30,7 @@ export function initializeIconsForUITests(): void {
     icons: {
       Cancel: <Dismiss16Regular />,
       Clear: <Dismiss20Regular />,
-      CheckMark: <Checkmark16Regular />,
+      Checkmark: <Checkmark16Regular />,
       ChevronDown: <ChevronDown20Regular />,
       ChevronRight: <ChevronRight20Regular />,
       ErrorBadge: <DismissCircle20Regular />,
