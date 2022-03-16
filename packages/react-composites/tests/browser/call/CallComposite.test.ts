bedc25f570b4c0a23878e84583dcfe49a5332f06
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-03-16 20:23:38 +0000

    update the test to use a better selector to wait on tooltip

--- packages/react-composites/tests/browser/call/CallComposite.test.ts
+++ packages/react-composites/tests/browser/call/CallComposite.test.ts
@@ -111,7 +111,7 @@ test.describe('Call Composite E2E CallPage Tests', () => {
       await page.goto(buildUrl(serverUrl, user));
       await waitForCallCompositeToLoad(page);
       // we need to dismiss any tool tips if there are any so we press the keyboard.
-      await page.keyboard.down('Escape');
+      await page.locator('[aria-describedby="microphoneButtonLabel-tooltip"]');
     }
 
     await loadCallPageWithParticipantVideos(pages);
