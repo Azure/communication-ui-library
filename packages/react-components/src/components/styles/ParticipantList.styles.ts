81bd3d24fd96f6a333a7a1b9af303edee0469681
edwardlee-msft <edwardlee@microsoft.com>
2022-03-18 14:22:45 -0700

    Update ParticipantList.styles.ts

--- packages/react-components/src/components/styles/ParticipantList.styles.ts
+++ packages/react-components/src/components/styles/ParticipantList.styles.ts
@@ -8,8 +8,8 @@ import { ParticipantListItemStyles } from '../ParticipantList';
  * @private
  */
 export const participantListStyle = mergeStyles({
-  padding: '0.125rem',
-  height: '100%'
+  height: '100%',
+  padding: '0.125rem'
 });
 
 /**
