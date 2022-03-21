41b34e8e604a081ed5e01eddf1a583fbc2eec334
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-03-21 19:16:40 +0000

    Add SVG loader

--- samples/CallWithChat/esbuild.config.mjs
+++ samples/CallWithChat/esbuild.config.mjs
@@ -1,11 +1,12 @@
 import { NodeModulesPolyfillPlugin as esbuildPluginNodeModulePolyfills } from '@esbuild-plugins/node-modules-polyfill';
+import svg from 'esbuild-plugin-svg';
 import { build } from 'esbuild';
 
 build({
   entryPoints: ['./src/index.tsx'],
   outfile: 'dist/esbuild/index.js',
   bundle: true,
-  plugins: [esbuildPluginNodeModulePolyfills()],
+  plugins: [esbuildPluginNodeModulePolyfills(), svg()],
   sourcemap: true,
   logLevel: 'error'
 });
