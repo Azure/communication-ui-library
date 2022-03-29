1ed381ad096183eda3cc7ddf196f2e0a416d06fb
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-03-29 20:09:34 +0000

    updated the scripts

--- common/scripts/disable-beta-ci.js
+++ common/scripts/disable-beta-ci.js
@@ -1,8 +1,21 @@
+var fs = require('fs');
 
-const disableBetaCi = (branchName) => {
-    if(branchName.includes('beta')) {
-        return false;
-    } else {
-        return true;
-    }
+const disableBetaCi = () => {
+    var result = fs.readFile('./ci-stable.json', 'utf-8', (err, data) => {
+        if (err) {
+            return true;
+        } else {
+            const ci = JSON.parse(data);
+            if (ci.flavor === 'stable') {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+    return result;
+}
+
+const main = () => {
+    return disableBetaCi();
 }
--- common/scripts/set-ci-mode.js
+++ common/scripts/set-ci-mode.js
@@ -1,15 +1,14 @@
 var fs = require('fs');
 
-const createCi = (ciMode) => {
-    if(ciMode === 'beta'){
-        var stringOut = {
-            flavor : 'beta'
-        }
-        fs.writeFile('ci-mode.json', stringOut);
-    } else {
-        var stringOut = {
-            flavor : 'stable'
-        }
-        fs.writeFile('ci-mode.json', stringOut)
+const createCi = () => {
+    var stringOut = {
+        flavor : 'stable'
     }
+    fs.writeFile('ci-stable.json', stringOut);
 }
+
+const main = () => {
+    createCi();
+}
+
+main();
