1c70d254abc0c7141e931324157785e5e3e24ed2
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2022-03-30 22:57:52 +0000

    Added file sharing test for recieved messages

--- packages/react-composites/tests/browser/chat/FileSharing.test.ts
+++ packages/react-composites/tests/browser/chat/FileSharing.test.ts
@@ -4,11 +4,18 @@ import {
   waitForChatCompositeToLoad,
   buildUrl,
   isTestProfileStableFlavor,
-  stubMessageTimestamps
+  stubMessageTimestamps,
+  dataUiId
 } from '../common/utils';
-import { chatTestSetup, sendMessage } from '../common/chatTestHelpers';
+import {
+  chatTestSetup,
+  sendMessage,
+  waitForMessageDelivered,
+  waitForMessageWithContent
+} from '../common/chatTestHelpers';
 import { test } from './fixture';
 import { expect } from '@playwright/test';
+import { IDS } from '../common/constants';
 
 test.describe('Filesharing Attach file icon', async () => {
   test.skip(isTestProfileStableFlavor());
@@ -202,8 +209,10 @@ test.describe('Filesharing Message Thread', async () => {
     await chatTestSetup({ pages, users, serverUrl });
   });
 
-  test('contains File Download Card', async ({ serverUrl, users, page }) => {
-    await page.goto(
+  test('contains File Download Card', async ({ serverUrl, users, pages }) => {
+    const testMessageText = 'Hello!';
+    const page0 = pages[0];
+    await page0.goto(
       buildUrl(serverUrl, users[0], {
         useFileSharing: 'true',
         uploadedFiles: JSON.stringify([
@@ -216,9 +225,23 @@ test.describe('Filesharing Message Thread', async () => {
         ])
       })
     );
-    await waitForChatCompositeToLoad(page);
-    await sendMessage(page, 'Hi');
-    await stubMessageTimestamps(page);
-    expect(await page.screenshot()).toMatchSnapshot('filesharing-file-download-card.png');
+    await waitForChatCompositeToLoad(page0);
+    await sendMessage(page0, testMessageText);
+    await waitForMessageDelivered(page0);
+    await stubMessageTimestamps(page0);
+    expect(await page0.screenshot()).toMatchSnapshot('filesharing-file-download-card-in-sent-messages.png');
+
+    const page1 = pages[1];
+    await waitForChatCompositeToLoad(page1);
+    await waitForMessageWithContent(page1, testMessageText);
+
+    // It could be too slow to get typing indicator here, which makes the test flakey
+    // so wait for typing indicator disappearing, @Todo: stub out typing indicator instead.
+    await page1.waitForTimeout(1000); // ensure typing indicator has had time to appear
+    const typingIndicator = await page1.$(dataUiId(IDS.typingIndicator));
+    typingIndicator && (await typingIndicator.waitForElementState('hidden')); // ensure typing indicator has now disappeared
+
+    await stubMessageTimestamps(page1);
+    expect(await page1.screenshot()).toMatchSnapshot('filesharing-file-download-card-in-received-messages.png');
   });
 });
