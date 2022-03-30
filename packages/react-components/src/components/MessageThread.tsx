f0ea53ed5d061c82ca6331f01924a670aaea7013
carocao-msft <carolinecao@microsoft.com>
2022-03-30 12:54:44 -0700

    lint

--- packages/react-components/src/components/MessageThread.tsx
+++ packages/react-components/src/components/MessageThread.tsx
@@ -940,6 +940,7 @@ export const MessageThread = (props: MessageThreadProps): JSX.Element => {
       return <></>;
     },
     [
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       new Date().toDateString(),
       isNarrow,
       participantCount,
