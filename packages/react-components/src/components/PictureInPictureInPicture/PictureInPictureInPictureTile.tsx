c8067a9b259c8e33e7b63afe3eea79677d1921e4
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-04-04 22:38:23 +0000

    spelling

--- packages/react-components/src/components/PictureInPictureInPicture/PictureInPictureInPictureTile.tsx
+++ packages/react-components/src/components/PictureInPictureInPicture/PictureInPictureInPictureTile.tsx
@@ -40,7 +40,7 @@ const primaryTileStyles = memoizeFunction(
       height: orientation === 'landscape' ? '5.5rem' : '8rem',
       width: orientation === 'landscape' ? '8rem' : '5.5rem',
       boxShadow: themeElevation,
-      // PiPiP tiles were not deigned to host scrollable content. If removed check no scrollbar is showing in e2e tests
+      // PiPiP tiles were not designed to host scrollable content. If removed check no scrollbar is showing in e2e tests
       overflow: 'hidden'
     }
   })
@@ -52,7 +52,7 @@ const secondaryTileStyles = memoizeFunction(
       borderRadius: '0.25rem',
       height: orientation === 'landscape' ? '1.625rem' : '2rem',
       width: orientation === 'landscape' ? '2rem' : '1.625rem',
-      // PiPiP tiles were not deigned to host scrollable content. If removed check no scrollbar is showing in e2e tests
+      // PiPiP tiles were not designed to host scrollable content. If removed check no scrollbar is showing in e2e tests
       overflow: 'hidden'
     }
   })
