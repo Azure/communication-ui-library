763bfb6933e29401dbc5a5229ed12eff7d7ac055
Porter Nan <jiangnanhello@live.com>
2022-04-13 15:11:55 -0700

    Log the function called from stateful client (#1793)
    * Log the function called from stateful client

---
+++ packages/chat-stateful-client/src/Logger.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { createClientLogger } from '@azure/logger';
+
+/**
+ * @private
+ */
+export const chatStatefulLogger = createClientLogger('communication-react:chat-stateful');
