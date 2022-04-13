dd2e8da5c8b614da3f3d7f501e829ada027fa7b7
Anjul Garg <anjulgarg@live.com>
2022-04-13 21:27:15 +0000

    Updating api

--- packages/communication-react/review/communication-react.api.md
+++ packages/communication-react/review/communication-react.api.md
@@ -1933,7 +1933,6 @@ export type MessageThreadProps = {
     disableEditing?: boolean;
     strings?: Partial<MessageThreadStrings>;
     fileDownloadHandler?: FileDownloadHandler;
-    fileDownloadMetadataKey?: string;
 };
 
 // @public
--- packages/react-composites/src/composites/ChatComposite/file-sharing/index.ts
+++ packages/react-composites/src/composites/ChatComposite/file-sharing/index.ts
@@ -3,12 +3,11 @@
 export * from './FileUpload';
 export * from './FileUploadButton';
 export * from './FileUploadHandler';
-import { _FILE_SHARING_METADATA_KEY } from '@internal/acs-ui-common';
 
 /**
  * Metadata used for setting uploaded files by a user using chat composite in a group call.
  * @internal
  */
 export type FileSharingMetadata = {
-  [_FILE_SHARING_METADATA_KEY]: string;
+  fileSharingMetadata: string;
 };
