c4f09a124a5d127810d20fb644afc1d028ea5607
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-04-23 00:29:59 +0000

    add width and height back to announcer

--- packages/react-components/src/components/Announcer.tsx
+++ packages/react-components/src/components/Announcer.tsx
@@ -29,8 +29,8 @@ export const Announcer = (props: AnnouncerProps): JSX.Element => {
 const announcerStyles: IStackStyles = {
   root: {
     position: 'absolute',
-    width: '0px',
-    height: '0px',
+    width: '1px',
+    height: '1px',
     padding: 0,
     top: -1000,
     margin: '-1px',
