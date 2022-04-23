96bc4eb27f38824cfdeda8137f3488703da8e0fe
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-04-23 04:47:12 +0000

    update storyshots

--- packages/react-components/src/components/Announcer.tsx
+++ packages/react-components/src/components/Announcer.tsx
@@ -32,7 +32,6 @@ const announcerStyles: IStackStyles = {
     width: '1px',
     height: '1px',
     padding: 0,
-    top: -1000,
     margin: '-1px',
     overflow: 'hidden',
     clip: 'rect(0,0,0,0)',
