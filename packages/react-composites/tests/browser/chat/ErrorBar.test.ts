a34a00a1df09ab8c137c1f3525afc8c037cf7bc3
mgamis-msft <miguelgamis@microsoft.com>
2022-05-03 16:14:07 -0700

    helper function to stringify chat model

--- packages/react-composites/tests/browser/chat/ErrorBar.test.ts
+++ packages/react-composites/tests/browser/chat/ErrorBar.test.ts
@@ -1,7 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { stubMessageTimestamps, waitForChatCompositeToLoad, buildUrl, dataUiId } from '../common/utils';
+import {
+  stubMessageTimestamps,
+  waitForChatCompositeToLoad,
+  buildUrl,
+  dataUiId,
+  stringifyChatModel
+} from '../common/utils';
 import { test } from './fixture';
 import { expect } from '@playwright/test';
 import {
@@ -23,17 +29,13 @@ test.describe('ErrorBar is shown correctly', async () => {
   });
 
   test.only('not shown when nothing is wrong', async ({ serverUrl, users, page }) => {
-    await page.goto(
-      buildUrl(serverUrl, users[0], {
-        fakeModel: JSON.stringify({
-          users: JSON.stringify(
-            users.map((user) => {
-              return { displayName: user.displayName };
-            })
-          )
+    if (fakeAdapter) {
+      await page.goto(
+        buildUrl(serverUrl, users[0], {
+          fakeModel: stringifyChatModel(users)
         })
-      })
-    );
+      );
+    }
     await waitForChatCompositeToLoad(page);
     await stubMessageTimestamps(page);
     expect(await page.screenshot()).toMatchSnapshot('no-error-bar-for-valid-user.png');
