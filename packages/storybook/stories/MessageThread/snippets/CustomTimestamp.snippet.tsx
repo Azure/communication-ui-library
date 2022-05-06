65a53b7da545a8bdecf41507439b40bd33ec9b30
carocao-msft <carolinecao@microsoft.com>
2022-05-06 09:31:40 -0700

    edit time and date

--- packages/storybook/stories/MessageThread/snippets/CustomTimestamp.snippet.tsx
+++ packages/storybook/stories/MessageThread/snippets/CustomTimestamp.snippet.tsx
@@ -7,6 +7,16 @@ export const MessageThreadWithCustomTimestampExample: () => JSX.Element = () => 
   const dateTimeFormat = (messageDate: Date): string => {
     let hours = messageDate.getHours();
     let minutes = messageDate.getMinutes().toString();
+    let month = (messageDate.getMonth() + 1).toString();
+    let day = messageDate.getDate().toString();
+    const year = messageDate.getFullYear().toString();
+
+    if (month.length === 1) {
+      month = '0' + month;
+    }
+    if (day.length === 1) {
+      day = '0' + day;
+    }
     const isAm = hours < 12;
     if (hours > 12) {
       hours = hours - 12;
@@ -17,7 +27,20 @@ export const MessageThreadWithCustomTimestampExample: () => JSX.Element = () => 
     if (minutes.length < 2) {
       minutes = '0' + minutes;
     }
-    return 'TimeStamp: ' + hours.toString() + ':' + minutes + ' ' + (isAm ? 'a.m.' : 'p.m.');
+    return (
+      'TimeStamp: ' +
+      year +
+      '-' +
+      month +
+      '-' +
+      day +
+      ', ' +
+      hours.toString() +
+      ':' +
+      minutes +
+      ' ' +
+      (isAm ? 'a.m.' : 'p.m.')
+    );
   };
   return (
     <FluentThemeProvider>
