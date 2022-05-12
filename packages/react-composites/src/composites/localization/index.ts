895a9441a3d7cbbb2b997f37f95b97ca5ed6ebb1
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-05-12 17:37:12 +0000

    API extractor is grumpy

--- packages/react-composites/src/composites/localization/index.ts
+++ packages/react-composites/src/composites/localization/index.ts
@@ -9,6 +9,6 @@ import { useLocale } from './LocalizationProvider';
  * @internal
  *
  * Used by storybook INTERNAL stories to provide strings to components from
- * @internal/react-components that use composite strings, e.g., _ComplianceBanner.
+ * \@internal/react-components that use composite strings, e.g., _ComplianceBanner.
  */
 export const _useCompositeLocale = useLocale;
