33acf6492cfde757fd7cb185b3ec8eaaaadb564e
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-05-13 09:00:05 +0000

    address review comments

--- packages/react-components/src/components/ComplianceBanner/ComplianceBanner.tsx
+++ packages/react-components/src/components/ComplianceBanner/ComplianceBanner.tsx
@@ -77,14 +77,14 @@ export const _ComplianceBanner = (props: _ComplianceBannerProps): JSX.Element =>
 
   // Only update cached props and variant if there is _some_ change in the latest props.
   // This ensures that state machine is only updated if there is an actual change in the props.
-  const shouldUpdatedCached =
+  const shouldUpdateCached =
     props.callRecordState !== cachedProps.current.latestBooleanState.callRecordState ||
     props.callTranscribeState !== cachedProps.current.latestBooleanState.callTranscribeState;
 
   // The following three operations must be performed in this exact order:
 
   // [1]: Update cached state to transition the state machine.
-  if (shouldUpdatedCached) {
+  if (shouldUpdateCached) {
     cachedProps.current = {
       latestBooleanState: props,
       latestStringState: {
@@ -106,7 +106,7 @@ export const _ComplianceBanner = (props: _ComplianceBannerProps): JSX.Element =>
 
   // [3]: Transition the state machine again to deal with some end-states.
   if (
-    shouldUpdatedCached &&
+    shouldUpdateCached &&
     cachedProps.current.latestStringState.callRecordState === 'stopped' &&
     cachedProps.current.latestStringState.callTranscribeState === 'stopped'
   ) {
@@ -190,6 +190,7 @@ function DelayedUpdateBanner(props: {
     // We'll either update now, or schedule an update for later.
     if (pendingUpdateHandle.current) {
       clearTimeout(pendingUpdateHandle.current);
+      pendingUpdateHandle.current = null;
     }
 
     const now = Date.now();
