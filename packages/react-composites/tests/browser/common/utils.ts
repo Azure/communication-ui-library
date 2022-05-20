ca5d37b2754097f7b5e5ca630e4ce06a0b90fdbe
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2022-05-20 14:44:43 -0700

    File Sharing components are tab navigable (#1881)
    * Added Icon button for upload button, cancel file upload icon and download icon to make it tab navigable
    
    * Change files
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Style changes
    
    * Update composite automation snapshots
    
    * Update js bundle automation snapshots
    
    * Update js bundle automation snapshots
    
    * minor changes
    
    * Update composite automation snapshots
    
    * minor changes
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Style changes and test changes
    
    * Update composite automation snapshots
    
    * Added helper function to hide chat actions button
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * updated storybook snapshots
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Anjul Garg <anjulgarg@live.com>

--- packages/react-composites/tests/browser/common/utils.ts
+++ packages/react-composites/tests/browser/common/utils.ts
@@ -331,6 +331,8 @@ export interface StubOptions {
   stubMessageTimestamps?: boolean;
   // Disable tooltips on all buttons in the call control bar.
   dismissTooltips?: boolean;
+  // Hide chat message actions icon button.
+  dismissChatMessageActions?: boolean;
 }
 
 /**
@@ -350,6 +352,9 @@ export async function stableScreenshot(
   if (stubOptions?.dismissTooltips) {
     await disableTooltips(page);
   }
+  if (stubOptions?.dismissChatMessageActions) {
+    await hideChatMessageActionsButton(page);
+  }
   try {
     return await page.screenshot(screenshotOptions);
   } finally {
@@ -358,6 +363,13 @@ export async function stableScreenshot(
     }
   }
 }
+
+/**
+ * Helper function for hiding chat message actions icon button.
+ */
+const hideChatMessageActionsButton = async (page: Page): Promise<void> => {
+  await page.addStyleTag({ content: '.ui-chat__message__actions {display: none}' });
+};
 
 /**
  * Helper function for disabling all the tooltips on the page.
