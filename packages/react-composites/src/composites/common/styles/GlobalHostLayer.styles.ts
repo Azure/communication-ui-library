cbd5e86d4066d0b186bfea96296b5ca7919fb8c3
Nan Jiang <jinan@microsoft.com>
2022-06-01 17:49:06 -0700

    Add a default host layer in base provider 1. make our lib compatible with react-full-screen 2. avoid polluting global dom tree

---
+++ packages/react-composites/src/composites/common/styles/GlobalHostLayer.styles.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { IStyle } from '@fluentui/react/lib/Styling';
+
+/**
+ * @private
+ */
+export const globalLayerHostStyle: IStyle = {
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+  // pointer events for layerHost set to none to make descendants interactive
+  pointerEvents: 'none'
+};
