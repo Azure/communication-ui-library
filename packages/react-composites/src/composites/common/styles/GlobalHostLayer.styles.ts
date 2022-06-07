64e4114e698d20a952f04f9b390a949188de4c40
Nan Jiang <jinan@microsoft.com>
2022-06-07 01:12:45 -0700

    Update style to the same as fluent

--- packages/react-composites/src/composites/common/styles/GlobalHostLayer.styles.ts
+++ packages/react-composites/src/composites/common/styles/GlobalHostLayer.styles.ts
@@ -7,13 +7,8 @@ import { IStyle } from '@fluentui/react/lib/Styling';
  * @private
  */
 export const globalLayerHostStyle: IStyle = {
-  position: 'absolute',
-  left: 0,
-  top: 0,
-  width: '100%',
-  height: '100%',
-  overflow: 'hidden',
-  // pointer events for layerHost set to none to make descendants interactive
-  pointerEvents: 'none',
-  zIndex: 1000000
+  position: 'fixed',
+  zIndex: 1000000,
+  inset: '0px',
+  visibility: 'hidden'
 };
--- packages/react-composites/tests/browser/common/utils.ts
+++ packages/react-composites/tests/browser/common/utils.ts
@@ -33,10 +33,7 @@ async function screenshotOnFailure<T>(page: Page, fn: () => Promise<T>): Promise
  */
 export const pageClick = async (page: Page, selector: string): Promise<void> => {
   await page.bringToFront();
-  await screenshotOnFailure(
-    page,
-    async () => await page.click(selector, { timeout: PER_STEP_TIMEOUT_MS, force: true })
-  );
+  await screenshotOnFailure(page, async () => await page.click(selector, { timeout: PER_STEP_TIMEOUT_MS }));
   // Move the mouse off the screen
   await page.mouse.move(-1, -1);
 };
