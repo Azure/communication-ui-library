64a15f53d4c5c44f7d6be69df07f08c2613cb893
carocao-msft <carolinecao@microsoft.com>
2022-06-10 14:51:42 -0700

    change pr

--- packages/calling-component-bindings/src/participantListSelector.ts
+++ packages/calling-component-bindings/src/participantListSelector.ts
@@ -94,7 +94,7 @@ export const participantListSelector: ParticipantListSelector = createSelector(
       : [];
     participants.push({
       userId: userId,
-      displayName: displayName ?? '',
+      displayName: displayName,
       isScreenSharing: isScreenSharingOn,
       isMuted: isMuted,
       state: 'Connected',
