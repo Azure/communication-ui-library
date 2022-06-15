010577d08b1052f7cf09089f49fc41f40ad42d10
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2022-06-15 04:16:47 +0000

    updating multi line comments

--- packages/storybook/stories/ChatComposite/ChatCompositeDocs.tsx
+++ packages/storybook/stories/ChatComposite/ChatCompositeDocs.tsx
@@ -25,9 +25,9 @@ adapter={chatAdapter}
 options={{
   fileSharing: {
     uploadHandler: fileUploadHandler,
-    // If fileDownloadHandler is not provided. The file URL is opened in a new tab.
-    // You can find examples of fileDownloadHandler and fileUploadHandler in this tutorial
-    // https://docs.microsoft.com/en-us/azure/communication-services/tutorials/file-sharing-tutorial
+    /* If fileDownloadHandler is not provided. The file URL is opened in a new tab.
+    You can find examples of fileDownloadHandler and fileUploadHandler in this tutorial
+    https://docs.microsoft.com/en-us/azure/communication-services/tutorials/file-sharing-tutorial */
     downloadHandler: fileDownloadHandler,
     accept: 'image/png, image/jpeg, text/plain, .docx',
     multiple: true
