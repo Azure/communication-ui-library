4aaea6215e7bdb68e16b27b6e1d875912d59ae29
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2022-06-17 14:22:05 -0700

    Convert read receipt UI tests to use fake adapter (#1974)
    * Convert read receipt UI tests to use fake adapter
    
    * fix lint issues
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Both remote participants in context menu
    
    * unfocused test
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * use waitForSelector util function
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * delete unused snapshots
    
    * waitForSelector util function modified and used for hidden composites
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>

--- packages/react-composites/tests/browser/chat/ChatComposite.test.ts
+++ packages/react-composites/tests/browser/chat/ChatComposite.test.ts
@@ -1,17 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { dataUiId, stubMessageTimestamps, stubReadReceiptsToolTip, waitForChatCompositeToLoad } from '../common/utils';
+import { stubMessageTimestamps, waitForChatCompositeToLoad } from '../common/utils';
 import { test } from './fixture';
 import { expect } from '@playwright/test';
-import {
-  chatTestSetup,
-  sendMessage,
-  waitForMessageDelivered,
-  waitForMessageSeen,
-  waitForMessageWithContent,
-  waitForTypingIndicatorHidden
-} from '../common/chatTestHelpers';
+import { chatTestSetup, sendMessage, waitForMessageSeen, waitForMessageWithContent } from '../common/chatTestHelpers';
 
 test.describe('Chat Composite E2E Tests', () => {
   test.beforeEach(async ({ pages, users, serverUrl }) => {
@@ -20,49 +13,6 @@ test.describe('Chat Composite E2E Tests', () => {
 
   test('composite pages load completely', async ({ pages }) => {
     expect(await pages[0].screenshot()).toMatchSnapshot(`chat-screen.png`);
-  });
-
-  test('page[1] can receive message and send readReceipt when page[0] send message', async ({ pages }) => {
-    const testMessageText = 'How the turn tables';
-    const page0 = pages[0];
-    await sendMessage(page0, testMessageText);
-    await waitForMessageDelivered(page0);
-    await stubMessageTimestamps(page0);
-    expect(await page0.screenshot()).toMatchSnapshot('sent-messages.png');
-
-    const page1 = pages[1];
-    await waitForMessageWithContent(page1, testMessageText);
-    await waitForTypingIndicatorHidden(page1);
-
-    await stubMessageTimestamps(page1);
-    expect(await page1.screenshot()).toMatchSnapshot('received-messages.png');
-    await waitForMessageSeen(page0);
-
-    await stubMessageTimestamps(page0);
-    await page0.locator(dataUiId('chat-composite-message-status-icon')).click();
-    await page0.waitForSelector(dataUiId('chat-composite-message-tooltip'));
-    await stubReadReceiptsToolTip(page0);
-    expect(await page0.screenshot()).toMatchSnapshot('read-message-tooltip-text.png');
-  });
-
-  test('page[0] can receive read receipt from page[1] and show it in contextual menu', async ({ pages }) => {
-    const testMessageText = 'How the turn tables';
-    const page0 = pages[0];
-    await sendMessage(page0, testMessageText);
-    await waitForMessageDelivered(page0);
-    await stubMessageTimestamps(page0);
-    expect(await page0.screenshot()).toMatchSnapshot('sent-messages.png');
-
-    const page1 = pages[1];
-    await waitForMessageWithContent(page1, testMessageText);
-    await waitForTypingIndicatorHidden(page1);
-    await stubMessageTimestamps(page1);
-    await page0.locator(dataUiId('chat-composite-message')).click();
-    await page0.locator(dataUiId('chat-composite-message-action-icon')).click();
-    await page0.waitForSelector('[id="chat-composite-message-contextual-menu"]');
-    await page0.locator(dataUiId('chat-composite-message-contextual-menu-read-info')).click();
-    await page0.waitForSelector('[id="chat-composite-message-contextual-menu-read-name-list"]');
-    expect(await page0.screenshot()).toMatchSnapshot('read-message-contextualMenu.png');
   });
 
   test('page[1] can rejoin the chat', async ({ pages }) => {
