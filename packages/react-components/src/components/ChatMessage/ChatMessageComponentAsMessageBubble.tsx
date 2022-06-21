bb7b0e7c22718ffe09f42531efeb1b7bdb880b80
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2022-06-21 15:29:30 +0000

    minor change

--- packages/react-components/src/components/ChatMessage/ChatMessageComponentAsMessageBubble.tsx
+++ packages/react-components/src/components/ChatMessage/ChatMessageComponentAsMessageBubble.tsx
@@ -158,17 +158,25 @@ const MessageBubble = (props: ChatMessageComponentAsMessageBubbleProps): JSX.Ele
     setChatMessageActionFlyoutTarget(undefined);
   }, [setChatMessageActionFlyoutTarget]);
 
-  /* @conditional-compile-remove(file-sharing) */
   const defaultOnRenderFileDownloads = useCallback(
     () => (
       <_FileDownloadCards
         userId={userId}
         fileMetadata={message['attachedFilesMetadata'] || []}
         downloadHandler={fileDownloadHandler}
+        /* @conditional-compile-remove(file-sharing) */
         strings={{ downloadFile: props.strings.downloadFile ?? locale.strings.messageThread.downloadFile }}
       />
     ),
-    [userId, message, fileDownloadHandler, props, locale]
+    [
+      userId,
+      message,
+      /* @conditional-compile-remove(file-sharing) */
+      props,
+      /* @conditional-compile-remove(file-sharing) */
+      locale,
+      fileDownloadHandler
+    ]
   );
 
   const chatMessage = (
@@ -181,12 +189,9 @@ const MessageBubble = (props: ChatMessageComponentAsMessageBubbleProps): JSX.Ele
           content={
             <div>
               <ChatMessageContent message={message} liveAuthorIntro={strings.liveAuthorIntro} />
-              {
-                /* @conditional-compile-remove(file-sharing) */
-                props.onRenderFileDownloads
-                  ? props.onRenderFileDownloads(userId, message)
-                  : defaultOnRenderFileDownloads()
-              }
+              {props.onRenderFileDownloads
+                ? props.onRenderFileDownloads(userId, message)
+                : defaultOnRenderFileDownloads()}
             </div>
           }
           author={<Text className={chatMessageDateStyle}>{message.senderDisplayName}</Text>}
