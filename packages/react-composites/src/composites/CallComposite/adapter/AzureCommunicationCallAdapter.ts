670d5770a75e6655ef2266ace4ed230da0ddf15f
mgamis-msft <miguelgamis@microsoft.com>
2022-07-12 00:18:18 -0700

    Call adapter can join a room

--- packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
+++ packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
@@ -29,6 +29,8 @@ import {
   PropertyChangedEvent,
   StartCallOptions
 } from '@azure/communication-calling';
+/* @conditional-compile-remove(rooms) */
+import { RoomCallLocator } from '@azure/communication-calling';
 /* @conditional-compile-remove(PSTN-calls) */
 import { AddPhoneNumberOptions } from '@azure/communication-calling';
 import { EventEmitter } from 'events';
@@ -297,15 +299,30 @@ export class AzureCommunicationCallAdapter implements CallAdapter {
       const videoOptions = { localVideoStreams: this.localStream ? [this.localStream] : undefined };
 
       const isTeamsMeeting = !('groupId' in this.locator);
-      const call = isTeamsMeeting
-        ? this.callAgent.join(this.locator as TeamsMeetingLinkLocator, {
-            audioOptions,
-            videoOptions
-          })
-        : this.callAgent.join(this.locator as GroupCallLocator, {
-            audioOptions,
-            videoOptions
-          });
+      /* @conditional-compile-remove(rooms) */
+      const isRoomsCall = !('roomId' in this.locator);
+
+      let call;
+
+      if (isTeamsMeeting) {
+        call = this.callAgent.join(this.locator as TeamsMeetingLinkLocator, {
+          audioOptions,
+          videoOptions
+        });
+      } else {
+        call = this.callAgent.join(this.locator as GroupCallLocator, {
+          audioOptions,
+          videoOptions
+        });
+      }
+
+      /* @conditional-compile-remove(rooms) */
+      if (isRoomsCall) {
+        call = this.callAgent.join(this.locator as RoomCallLocator, {
+          audioOptions,
+          videoOptions
+        });
+      }
 
       this.processNewCall(call);
       return call;
@@ -642,6 +659,7 @@ export type CallParticipantsLocator = {
 export type CallAdapterLocator =
   | TeamsMeetingLinkLocator
   | GroupCallLocator
+  | /* @conditional-compile-remove(rooms) */ RoomCallLocator
   | /* @conditional-compile-remove(teams-adhoc-call) */ CallParticipantsLocator;
 
 /**
