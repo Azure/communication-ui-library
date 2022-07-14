975d130eec3505a68b729d9899e16789cb37d51b
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-07-14 06:10:37 +0000

    Force multiple workers in CI

--- packages/react-composites/playwright.config.hermetic.ts
+++ packages/react-composites/playwright.config.hermetic.ts
@@ -6,6 +6,10 @@ import common from './playwright.config.common';
 const config: PlaywrightTestConfig = {
   ...common,
 
+  // Use a fixed number of workers in CI because some of our CI bots have only one vCPU.
+  // Some parallelism still helps in this case.
+  workers: process.env.CI ? 3 : undefined,
+
   // All these tests are hermetic. It is safe to run them in parallel.
   fullyParallel: true
 };
