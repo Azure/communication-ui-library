69b32d11115adfb381c95f6ef59026999eea061a
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-07-18 16:14:18 +0530

    Convert remaining `CallComposite` e2e tests to `MockCallAdapterState` (#2086)

--- packages/react-composites/tests/browser/call/hermetic/CallControls.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/CallControls.test.ts
@@ -1,32 +1,24 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { buildUrlWithMockAdapter, test } from './fixture';
+import { buildUrlWithMockAdapterNext, defaultMockCallAdapterState, test } from './fixture';
 import { expect } from '@playwright/test';
 import { dataUiId, stableScreenshot, waitForSelector } from '../../common/utils';
 import { IDS } from '../../common/constants';
 
 test.describe('CallControls tests', async () => {
   test('CallControls when number of mics drops to zero', async ({ page, serverUrl }) => {
-    await page.goto(
-      buildUrlWithMockAdapter(serverUrl, {
-        devices: {
-          microphones: []
-        }
-      })
-    );
+    const initialState = defaultMockCallAdapterState();
+    initialState.devices.microphones = [];
+    await page.goto(buildUrlWithMockAdapterNext(serverUrl, initialState));
     await waitForSelector(page, dataUiId(IDS.videoGallery));
     expect(await stableScreenshot(page, { dismissTooltips: true })).toMatchSnapshot('no-mics.png');
   });
 
   test('CallControls when number of available cameras drops to zero', async ({ page, serverUrl }) => {
-    await page.goto(
-      buildUrlWithMockAdapter(serverUrl, {
-        devices: {
-          cameras: []
-        }
-      })
-    );
+    const initialState = defaultMockCallAdapterState();
+    initialState.devices.cameras = [];
+    await page.goto(buildUrlWithMockAdapterNext(serverUrl, initialState));
     await waitForSelector(page, dataUiId(IDS.videoGallery));
     expect(await stableScreenshot(page, { dismissTooltips: true })).toMatchSnapshot('no-videos.png');
   });
