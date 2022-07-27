5e0b805bd2be625c6f0876d52decb9913a6d0275
carocao-msft <carolinecao@microsoft.com>
2022-07-27 16:03:51 -0700

    revert

--- packages/react-components/src/components/Dialpad/Dialpad.test.tsx
+++ packages/react-components/src/components/Dialpad/Dialpad.test.tsx
@@ -1,35 +1,47 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/* @conditional-compile-remove(dialpad) */
 import React from 'react';
+/* @conditional-compile-remove(dialpad) */
 import Enzyme, { mount } from 'enzyme';
+/* @conditional-compile-remove(dialpad) */
 import Adapter from 'enzyme-adapter-react-16';
-import { Dialpad, DtmfTone, DialpadStrings } from './Dialpad';
+/* @conditional-compile-remove(dialpad) */
+import { DtmfTone, Dialpad, DialpadStrings } from './Dialpad';
 /* @conditional-compile-remove(dialpad) */
 import { createTestLocale, mountWithLocalization } from '../utils/testUtils';
-
+/* @conditional-compile-remove(dialpad) */
 Enzyme.configure({ adapter: new Adapter() });
 
+/* @conditional-compile-remove(dialpad) */
+const customDialpadStrings = {
+  placeholderText: Math.random().toString(),
+  deleteButtonAriaLabel: Math.random().toString()
+};
+
+test('workaround for conditional compilation. Test suite must contain at least one test', () => {
+  expect(true).toBeTruthy();
+});
+
+/* @conditional-compile-remove(dialpad) */
 const mockSendDTMF = jest.fn();
 
+/* @conditional-compile-remove(dialpad) */
 const onSendDtmfTone = (dtmfTone: DtmfTone): Promise<void> => {
   mockSendDTMF(dtmfTone);
   return Promise.resolve();
 };
 
+/* @conditional-compile-remove(dialpad) */
+const dialpadStrings: DialpadStrings = {
+  placeholderText: Math.random().toString(),
+  deleteButtonAriaLabel: Math.random().toString()
+};
+
+/* @conditional-compile-remove(dialpad) */
 describe('Dialpad tests', () => {
-  /*
-   * Localization depends on public API for `LocalizationProvider` that does not
-   * have the strings for the beta-only `Dialpad` component.
-   * skip this test for stable build.
-   *
-   * @conditional-compile-remove(dialpad)
-   */
   test('Should localize default text ', async () => {
-    const dialpadStrings: DialpadStrings = {
-      placeholderText: Math.random().toString(),
-      deleteButtonAriaLabel: Math.random().toString()
-    };
     const testLocale = createTestLocale({
       dialpad: dialpadStrings
     });
@@ -40,7 +52,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Clicking on dialpad button 1 should show 1 in input box', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     const button = component.find('[data-test-id="dialpad-button-0"]').first();
     if (button) {
       button.simulate('click');
@@ -50,7 +65,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Clicking on dialpad button 6 should send the corresponding dtmf tone Num6', async () => {
-    const component = mount(<Dialpad onSendDtmfTone={onSendDtmfTone} />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad onSendDtmfTone={onSendDtmfTone} />, testLocale);
 
     const button = component.find('[data-test-id="dialpad-button-5"]').first();
     if (button) {
@@ -60,7 +78,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Clicking on dialpad button 9 should not send other dtmf tones', async () => {
-    const component = mount(<Dialpad onSendDtmfTone={onSendDtmfTone} />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad onSendDtmfTone={onSendDtmfTone} />, testLocale);
 
     const button = component.find('[data-test-id="dialpad-button-8"]').first();
     if (button) {
@@ -72,10 +93,6 @@ describe('Dialpad tests', () => {
   });
 
   test('Dialpad should have customizable default input text', async () => {
-    const customDialpadStrings: DialpadStrings = {
-      placeholderText: Math.random().toString(),
-      deleteButtonAriaLabel: Math.random().toString()
-    };
     const component = mount(<Dialpad strings={customDialpadStrings} />);
     expect(component.find('[data-test-id="dialpad-input"]').first().props().placeholder).toBe(
       customDialpadStrings.placeholderText
@@ -83,7 +100,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Dialpad input box should be editable by keyboard', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     component
       .find('input')
       .first()
@@ -92,7 +112,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Dialpad input box should filter out non-valid input', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     component
       .find('input')
       .first()
@@ -101,7 +124,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Typing in 12345678900 should show 1 (234) 567-8900 in input box', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     component
       .find('input')
       .first()
@@ -110,7 +136,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Typing in 2345678900 should show (234) 567-8900 in input box', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     component
       .find('input')
       .first()
@@ -119,7 +148,10 @@ describe('Dialpad tests', () => {
   });
 
   test('Typing in 23456789000 should show  23456789000 in input box', async () => {
-    const component = mount(<Dialpad />);
+    const testLocale = createTestLocale({
+      dialpad: dialpadStrings
+    });
+    const component = mountWithLocalization(<Dialpad />, testLocale);
     component
       .find('input')
       .first()
