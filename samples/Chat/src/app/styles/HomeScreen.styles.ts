d788e8cc4fe8faa78970ce1ceca9a94c1cc2ad6c
carocao-msft <carolinecao@microsoft.com>
2022-07-27 11:09:01 -0700

    update

--- samples/Chat/src/app/styles/HomeScreen.styles.ts
+++ samples/Chat/src/app/styles/HomeScreen.styles.ts
@@ -54,7 +54,7 @@ export const listIconStyle = mergeStyles({
 });
 
 export const headerStyle = mergeStyles({
-  fontWeight: 400,
+  fontWeight: 600,
   fontSize: '2.25rem', // 36px
   width: 'inherit'
 });
