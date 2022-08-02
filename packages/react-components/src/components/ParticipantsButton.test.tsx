a2c0a5e02ce4ecb82f0b8f7e9fe87e1e96c84c06
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2022-08-02 20:50:01 +0000

    Enzyme tests for Participants button

--- packages/react-components/src/components/ParticipantsButton.test.tsx
+++ packages/react-components/src/components/ParticipantsButton.test.tsx
@@ -5,8 +5,10 @@ import React from 'react';
 import { ParticipantsButton } from './ParticipantsButton';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { createTestLocale, mountWithLocalization } from './utils/testUtils';
+import { createTestLocale, mountWithLocalization, mountWithPermissions } from './utils/testUtils';
 import { setIconOptions } from '@fluentui/react';
+import { _getPermissions } from '../permissions';
+import { ControlBarButton } from './ControlBarButton';
 
 // Suppress icon warnings for tests. Icons are fetched from CDN which we do not want to perform during tests.
 // More information: https://github.com/microsoft/fluentui/wiki/Using-icons#test-scenarios
@@ -31,5 +33,32 @@ describe('ParticipantsButton strings should be localizable and overridable', () 
       testLocale
     );
     expect(component.text()).toBe(participantButtonStrings.label);
+  });
+});
+
+describe('ParticipantListButton tests for different roles', () => {
+  test('ParticipantListButton should be enabled for Presenter role', async () => {
+    const wrapper = mountWithPermissions(
+      <ParticipantsButton participants={[{ displayName: 'User1', userId: '1', isRemovable: true }]} />,
+      _getPermissions('Presenter')
+    );
+    const participantButton = wrapper.find(ControlBarButton).first();
+    expect(participantButton.prop('disabled')).toBe(false);
+  });
+  test('ParticipantListButton should be enabled for Attendee role', async () => {
+    const wrapper = mountWithPermissions(
+      <ParticipantsButton participants={[{ displayName: 'User1', userId: '1', isRemovable: true }]} />,
+      _getPermissions('Attendee')
+    );
+    const participantButton = wrapper.find(ControlBarButton).first();
+    expect(participantButton.prop('disabled')).toBe(false);
+  });
+  test('ParticipantListButton should be disabled for Consumer role', async () => {
+    const wrapper = mountWithPermissions(
+      <ParticipantsButton participants={[{ displayName: 'User1', userId: '1', isRemovable: true }]} />,
+      _getPermissions('Consumer')
+    );
+    const participantButton = wrapper.find(ControlBarButton).first();
+    expect(participantButton.prop('disabled')).toBe(true);
   });
 });
