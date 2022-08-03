257c34dc1dbfca881fb5ccbe2e5228fa8b460ff3
Grisha Kruglov <gkruglov@microsoft.com>
2022-08-03 14:05:01 -0700

    Document template of the application ID used for telemetry

--- packages/acs-ui-common/src/telemetry.ts
+++ packages/acs-ui-common/src/telemetry.ts
@@ -18,6 +18,16 @@ export const sanitize = (version: string): string => {
 
 /**
  * Application ID to be included in telemetry data from the UI library.
+ * Template: acXYYY/<version>
+ * Where:
+ * - X describes a platform, [r: web, i: iOS, a: Android]
+ * - YYY describes what's running on this platform (optional, currently unused by this library):
+ *    Y[0] is high-level artifact,
+ *      [0: undefined, 1: AzureCommunicationLibrary, 2: ACS SampleApp]
+ *    Y[1] is specific implementation,
+ *      [0: undefined, 1: Call Composite, 2: Chat Composite, 3: CallWithChatComposite, 4: UI Components]
+ *    Y[2] is reserved for implementation details,
+ *      [0: undefined]
  *
  * @internal
  */
