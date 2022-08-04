788e4e42608fde4ad4b4d90433d450c4ac5fd890
Anjul Garg <anjulgarg@live.com>
2022-08-04 19:54:31 +0000

    fixing conditional compilation

--- packages/calling-component-bindings/src/utils/videoGalleryUtils.ts
+++ packages/calling-component-bindings/src/utils/videoGalleryUtils.ts
@@ -7,12 +7,11 @@ import {
 } from '@azure/communication-calling';
 import { memoizeFnAll, toFlatCommunicationIdentifier } from '@internal/acs-ui-common';
 import { RemoteParticipantState, RemoteVideoStreamState } from '@internal/calling-stateful-client';
-import {
-  VideoGalleryRemoteParticipant,
-  VideoGalleryRemoteParticipantState,
-  VideoGalleryStream
-} from '@internal/react-components';
+import { VideoGalleryRemoteParticipant, VideoGalleryStream } from '@internal/react-components';
 import { checkIsSpeaking } from './SelectorUtils';
+/* @conditional-compile-remove(one-to-n-calling) */
+/* @conditional-compile-remove(PSTN-calls) */
+import { VideoGalleryRemoteParticipantState } from '@internal/react-components';
 
 /** @internal */
 export const _dominantSpeakersWithFlatId = (dominantSpeakers?: DominantSpeakersInfo): undefined | string[] => {
