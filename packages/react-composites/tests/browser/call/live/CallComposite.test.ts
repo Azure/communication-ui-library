32ebc6aaade3c11728b2db8668b0143c840c6ac0
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-08-08 10:32:07 +0000

    Remove incomplete but flaky test

--- packages/react-composites/tests/browser/call/live/CallComposite.test.ts
+++ packages/react-composites/tests/browser/call/live/CallComposite.test.ts
@@ -160,41 +160,6 @@ test.describe('Call Composite E2E CallPage Tests', () => {
   });
 });
 
-/**
- * Mobile only tests for the call screen.
- * Each test should use the call:
- * ${test.skip(skipTestIfDesktop(testInfo));}
- * to ensure that the test is only run in the mobile project.
- */
-test.describe('Call Composite E2E CallPage [Mobile Only]', () => {
-  test.beforeEach(async ({ pages, users, serverUrl }) => {
-    // Each test *must* join a new call to prevent test flakiness.
-    // We hit a Calling SDK service 500 error if we do not.
-    // An issue has been filed with the calling team.
-    const newTestGuid = generateGUID();
-    for (let i = 0; i < pages.length; i++) {
-      const page = pages[i];
-      const user = users[i];
-      user.groupId = newTestGuid;
-
-      await page.goto(buildUrl(serverUrl, user));
-      await waitForCallCompositeToLoad(page);
-    }
-
-    await loadCallPageWithParticipantVideos(pages);
-  });
-
-  test('local camera switcher button cycles camera', async ({ pages }, testInfo) => {
-    // Mobile check
-    test.skip(isTestProfileDesktop(testInfo));
-    // Build Flavor check
-    test.skip(isTestProfileStableFlavor());
-
-    const page = pages[0];
-    await pageClick(page, dataUiId('local-camera-switcher-button'));
-  });
-});
-
 test.describe('Call Composite E2E Call Ended Pages', () => {
   // Make sure tests can still run well after retries
   test.beforeEach(async ({ pages, users, serverUrl }) => {
