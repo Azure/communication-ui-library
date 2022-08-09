26ed3497157842d7c0a0e595b4c3fcd3e107f21a
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-09 01:14:04 +0000

    fix ringing state

--- packages/react-composites/src/composites/CallComposite/components/MediaGallery.tsx
+++ packages/react-composites/src/composites/CallComposite/components/MediaGallery.tsx
@@ -19,7 +19,7 @@ import { useSelector } from '../hooks/useSelector';
 import { localVideoCameraCycleButtonSelector } from '../selectors/LocalVideoTileSelector';
 import { LocalVideoCameraCycleButton } from '@internal/react-components';
 /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
-import { participantStateStyle, participantStateStyleMobile } from '../styles/MediaGallery.styles';
+import { participantStateStyle } from '../styles/MediaGallery.styles';
 /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
 import { useLocale } from '../../localization';
 
@@ -78,24 +78,24 @@ export const MediaGallery = (props: MediaGalleryProps): JSX.Element => {
           <Stack styles={{ root: { margin: 'auto', maxHeight: '100%' } }}>
             <AvatarPersona userId={userId} {...options} dataProvider={props.onFetchAvatarPersonaData} />
             {
-              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */ options?.state &&
-                options?.state === 'Ringing' && (
-                  <Text className={mergeStyles(participantStateStyle)}>
-                    {videoTileStrings.participantStateConnecting}
-                  </Text>
-                )
+              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
+              options?.participantState === 'Ringing' && (
+                <Text className={mergeStyles(participantStateStyle)}>{videoTileStrings.participantStateRinging}</Text>
+              )
             }
             {
-              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */ options?.state &&
-                options?.state === 'Connecting' && (
-                  <Text className={mergeStyles(participantStateStyle)}>{videoTileStrings.participantStateRinging}</Text>
-                )
+              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
+              options?.participantState === 'Connecting' && (
+                <Text className={mergeStyles(participantStateStyle)}>
+                  {videoTileStrings.participantStateConnecting}
+                </Text>
+              )
             }
             {
-              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */ options?.state &&
-                options?.state === 'Hold' && (
-                  <Text className={mergeStyles(participantStateStyle)}>{videoTileStrings.participantStateHold}</Text>
-                )
+              /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
+              options?.participantState === 'Hold' && (
+                <Text className={mergeStyles(participantStateStyle)}>{videoTileStrings.participantStateHold}</Text>
+              )
             }
           </Stack>
         </Stack>
