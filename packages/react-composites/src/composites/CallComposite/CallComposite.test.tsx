0e3f02fae22fcc3a588a0ad94e6ac1dcdfdcae88
mgamis-msft <miguelgamis@microsoft.com>
2022-08-12 16:03:15 -0700

    CallComposite device permissions test with temporary MockCallAdapter

---
+++ packages/react-composites/src/composites/CallComposite/CallComposite.test.tsx
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { registerIcons } from '@fluentui/react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { MockCallAdapter } from './MockCallAdapter';
+import { CallComposite } from './CallComposite';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('CallComposite device permission test for different roles', () => {
+  let audioDevicePermissionRequests = 0;
+  let videoDevicePermissionRequests = 0;
+
+  const countDevicePermissionRequests = async (constrain: { audio: boolean; video: boolean }): Promise<void> => {
+    if (constrain.video) {
+      videoDevicePermissionRequests++;
+    }
+    if (constrain.audio) {
+      audioDevicePermissionRequests++;
+    }
+  };
+  const adapter = new MockCallAdapter({ askDevicePermission: countDevicePermissionRequests });
+
+  beforeEach(() => {
+    // Register icons used in CallComposite to avoid warnings
+    registerIcons({
+      icons: {
+        chevrondown: <></>
+      }
+    });
+
+    // Reset permission request counters to 0
+    audioDevicePermissionRequests = 0;
+    videoDevicePermissionRequests = 0;
+  });
+
+  test('Audio and video device permission should be requested when no role is assigned', async () => {
+    mount(<CallComposite adapter={adapter} />);
+    expect(audioDevicePermissionRequests).toBe(1);
+    expect(videoDevicePermissionRequests).toBe(1);
+  });
+
+  test('Audio and video device permission should be requested for Presenter role', async () => {
+    mount(<CallComposite adapter={adapter} role={'Presenter'} />);
+    expect(audioDevicePermissionRequests).toBe(1);
+    expect(videoDevicePermissionRequests).toBe(1);
+  });
+
+  test('Audio and video device permission should be requested for Attendee role', async () => {
+    mount(<CallComposite adapter={adapter} role={'Attendee'} />);
+    expect(audioDevicePermissionRequests).toBe(1);
+    expect(videoDevicePermissionRequests).toBe(1);
+  });
+
+  test('Only audio device permission should be requested for Consumer role', async () => {
+    mount(<CallComposite adapter={adapter} role={'Consumer'} />);
+    expect(audioDevicePermissionRequests).toBe(1);
+    expect(videoDevicePermissionRequests).toBe(0);
+  });
+});
---
+++ packages/react-composites/src/composites/CallComposite/MockCallAdapter.ts
@@ -0,0 +1,154 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { AudioDeviceInfo, Call, DtmfTone, PermissionConstraints, VideoDeviceInfo } from '@azure/communication-calling';
+import { CallAdapter, CallAdapterState } from './adapter';
+
+/**
+ * Temporary copy of the packages\react-composites\tests\browser\call\app\mocks\MockCallAdapter.ts
+ */
+// TODO: Remove this simplified copy of the MockCallAdapter when the original MockCallAdapter is moved to fake-backends package and can be imported
+export class MockCallAdapter implements CallAdapter {
+  constructor(testState: { askDevicePermission?: (constrain: PermissionConstraints) => Promise<void> }) {
+    this.state = defaultCallAdapterState;
+
+    if (testState.askDevicePermission) {
+      this.askDevicePermission = testState.askDevicePermission;
+    }
+  }
+
+  state: CallAdapterState;
+
+  addParticipant(): Promise<void> {
+    throw Error('addParticipant not implemented');
+  }
+  onStateChange(): void {
+    return;
+  }
+  offStateChange(): void {
+    return;
+  }
+  getState(): CallAdapterState {
+    return this.state;
+  }
+  dispose(): void {
+    throw Error('dispose not implemented');
+  }
+  joinCall(): Call | undefined {
+    throw Error('joinCall not implemented');
+  }
+  leaveCall(): Promise<void> {
+    throw Error('leaveCall not implemented');
+  }
+  startCamera(): Promise<void> {
+    throw Error('leaveCall not implemented');
+  }
+  stopCamera(): Promise<void> {
+    throw Error('stopCamera not implemented');
+  }
+  mute(): Promise<void> {
+    throw Error('mute not implemented');
+  }
+  unmute(): Promise<void> {
+    throw Error('unmute not implemented');
+  }
+  startCall(): Call | undefined {
+    throw Error('startCall not implemented');
+  }
+  holdCall(): Promise<void> {
+    return Promise.resolve();
+  }
+  resumeCall(): Promise<void> {
+    return Promise.resolve();
+  }
+  startScreenShare(): Promise<void> {
+    throw Error('startScreenShare not implemented');
+  }
+  stopScreenShare(): Promise<void> {
+    throw Error('stopScreenShare not implemented');
+  }
+  removeParticipant(): Promise<void> {
+    throw Error('removeParticipant not implemented');
+  }
+  createStreamView(): Promise<void> {
+    throw Error('createStreamView not implemented');
+  }
+  disposeStreamView(): Promise<void> {
+    throw Error('disposeStreamView not implemented');
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  askDevicePermission(constrain: PermissionConstraints): Promise<void> {
+    throw Error('askDevicePermission not implemented');
+  }
+  async queryCameras(): Promise<VideoDeviceInfo[]> {
+    return [];
+  }
+  async queryMicrophones(): Promise<AudioDeviceInfo[]> {
+    return [];
+  }
+  async querySpeakers(): Promise<AudioDeviceInfo[]> {
+    return [];
+  }
+  setCamera(): Promise<void> {
+    throw Error('setCamera not implemented');
+  }
+  setMicrophone(): Promise<void> {
+    throw Error('setMicrophone not implemented');
+  }
+  setSpeaker(): Promise<void> {
+    throw Error('setSpeaker not implemented');
+  }
+  sendDtmfTone(dtmfTone: DtmfTone): Promise<void> {
+    throw Error('sendDtmfTone not implemented');
+  }
+  on(): void {
+    throw Error('on not implemented');
+  }
+  off(): void {
+    throw Error('off not implemented');
+  }
+}
+
+/**
+ * Default call adapter state that the {@link MockCallAdapter} class is initialized with
+ */
+const defaultCallAdapterState: CallAdapterState = {
+  displayName: 'Agnes Thompson',
+  isLocalPreviewMicrophoneEnabled: true,
+  page: 'call',
+  call: {
+    id: 'call1',
+    callerInfo: { displayName: 'caller', identifier: { kind: 'communicationUser', communicationUserId: '1' } },
+    direction: 'Incoming',
+    transcription: { isTranscriptionActive: false },
+    recording: { isRecordingActive: false },
+    startTime: new Date(500000000000),
+    endTime: new Date(500000000000),
+    diagnostics: { network: { latest: {} }, media: { latest: {} } },
+    state: 'Connected',
+    localVideoStreams: [],
+    isMuted: false,
+    isScreenSharingOn: false,
+    remoteParticipants: {},
+    remoteParticipantsEnded: {}
+  },
+  userId: { kind: 'communicationUser', communicationUserId: '1' },
+  devices: {
+    isSpeakerSelectionAvailable: true,
+    selectedCamera: { id: 'camera1', name: '1st Camera', deviceType: 'UsbCamera' },
+    cameras: [{ id: 'camera1', name: '1st Camera', deviceType: 'UsbCamera' }],
+    selectedMicrophone: {
+      id: 'microphone1',
+      name: '1st Microphone',
+      deviceType: 'Microphone',
+      isSystemDefault: true
+    },
+    microphones: [{ id: 'microphone1', name: '1st Microphone', deviceType: 'Microphone', isSystemDefault: true }],
+    selectedSpeaker: { id: 'speaker1', name: '1st Speaker', deviceType: 'Speaker', isSystemDefault: true },
+    speakers: [{ id: 'speaker1', name: '1st Speaker', deviceType: 'Speaker', isSystemDefault: true }],
+    unparentedViews: [],
+    deviceAccess: { video: true, audio: true }
+  },
+  isTeamsCall: false,
+  latestErrors: {}
+};
