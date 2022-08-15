c4d897fafe007cb98f10ba5f2c1029d68b38854a
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-15 19:10:22 +0000

    fix test time

--- packages/react-composites/src/composites/CallComposite/components/HoldPane.test.tsx
+++ packages/react-composites/src/composites/CallComposite/components/HoldPane.test.tsx
@@ -5,14 +5,14 @@ import { getReadableTime } from './HoldPane';
 
 describe('Hold Pane tests', () => {
   test('getReadableTime should report the correct time in minutes to be 30 minutes', () => {
-    const timeInMiliSeconds = 18000000;
+    const timeInMiliSeconds = 1800000;
     const time = getReadableTime(timeInMiliSeconds);
 
     expect(time).toBe('30:00');
   });
 
   test.only('getReadableTime should report the correct time to be 1 hour', () => {
-    const timeInMiliSeconds = 36000000;
+    const timeInMiliSeconds = 3600000;
     const time = getReadableTime(timeInMiliSeconds);
     console.log(time);
 
