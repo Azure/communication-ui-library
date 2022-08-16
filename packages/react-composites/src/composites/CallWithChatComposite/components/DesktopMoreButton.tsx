618e135abbea732d8387e28633172edf2b02983d
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-16 08:27:14 -0700

    Add hold button to CallWithChat (#2213)
    * VideoTile call state
    
    * Change files
    
    * Update comment
    
    * add beta tag to videotilecallstate prop
    
    * Update api.md
    
    * Updating api.md
    
    * Tweak prop name
    
    * Update changelog
    
    * Tweak prop name
    
    * updating api.md
    
    * Tweaking types
    
    * update changelog
    
    * Update api.md
    
    * Return participant state from videoGallerySelector and wire it in gallery
    
    * Change files
    
    * Tweak changelog
    
    * Fixing build
    
    * Tweaking state conversion logic in videoGallerySelector
    
    * Localization for video tile participant state prop
    
    * Conditionally remove pstn code from participant item
    
    * Update api.md
    
    * Update Api.md
    
    * resolving PR comments
    
    * Change files
    
    * Debugging video tile participant state display logic
    
    * update api.md
    
    * Resolving PR comments
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * Fixing conditional compilation errors
    
    * Change files
    
    * Updating types to match video tile participant state
    
    * updating api.md
    
    * update video gallery logic
    
    * debugging
    
    * Change files
    
    * Remove duplicate interface
    
    * update Media Gallery
    
    * update vigeogallery tile creation
    
    * update mediagallery on render
    
    * update media gallery styles
    
    * Update locale strings to match figma
    
    * build api files
    
    * add tests for gridview and horizontal view
    
    * add tests for 1 video part and joining
    
    * remove test.only
    
    * update tests
    
    * add screensharing tests and fix typing issue
    
    * update api
    
    * fix Ringing error
    
    * update tests
    
    * fix deps array
    
    * add third case for ringing
    
    * fix ringing state
    
    * update names of tests
    
    * add new helper functions
    
    * remove test only
    
    * update participant item state name
    
    * update API docs
    
    * update initial state of PSTN and 1:n
    
    * add more button to call composite
    
    * move CallWithChatMoreButton to be reused
    
    * update control bar
    
    * add context menu with hold button
    
    * add hold button to more button
    
    * fix hold button strings
    
    * update strings for hold and more button
    
    * fix CC and isEnabled
    
    * Change files
    
    * add hold screen and timer
    
    * moved styles
    
    * update styles and timer
    
    * Change files
    
    * add people button to overflow
    
    * update api docs
    
    * add hold page
    
    * fix cc error
    
    * fix cc
    
    * update hold pane time string
    
    * fix cc
    
    * add more button to CallWithChat
    
    * fix control bar state when on hold
    
    * remove bad imports
    
    * add hold button to moreDrawer
    
    * fix lint
    
    * update for new people button location in Beta
    
    * remove test.only
    
    * remove not needed import
    
    * update cc
    
    * Change files
    
    * fix page build errors
    
    * add callPage CC
    
    * Update composite automation snapshots
    
    Co-authored-by: Anjul Garg <anjulgarg@live.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/react-composites/src/composites/CallWithChatComposite/components/DesktopMoreButton.tsx
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { IContextualMenuItem } from '@fluentui/react';
+import { ControlBarButtonProps } from '@internal/react-components';
+/*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+import { HoldButton } from '@internal/react-components';
+import React from 'react';
+/*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+import { useMemo } from 'react';
+/*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+import { usePropsFor } from '../../CallComposite/hooks/usePropsFor';
+/*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+import { buttonFlyoutIncreasedSizeStyles } from '../../CallComposite/styles/Buttons.styles';
+import { MoreButton } from '../../common/MoreButton';
+/*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+import { useLocale } from '../../localization';
+
+/**
+ *
+ * @private
+ */
+export const DesktopMoreButton = (props: ControlBarButtonProps): JSX.Element => {
+  /*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+  const localeStrings = useLocale();
+  /*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+  const holdButtonProps = usePropsFor(HoldButton);
+
+  /* @conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+  const moreButtonStrings = useMemo(
+    () => ({
+      label: localeStrings.strings.call.moreButtonCallingLabel,
+      tooltipOffContent: localeStrings.strings.callWithChat.moreDrawerButtonTooltip
+    }),
+    [localeStrings]
+  );
+
+  const moreButtonContextualMenuItems = (): IContextualMenuItem[] => {
+    const items: IContextualMenuItem[] = [];
+
+    /*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+    items.push({
+      key: 'holdButtonKey',
+      text: localeStrings.component.strings.holdButton.tooltipOffContent,
+      onClick: () => {
+        holdButtonProps.onToggleHold();
+      },
+      iconProps: { iconName: 'HoldCall', styles: { root: { lineHeight: 0 } } },
+      itemProps: {
+        styles: buttonFlyoutIncreasedSizeStyles
+      }
+    });
+
+    return items;
+  };
+
+  return (
+    <MoreButton
+      {...props}
+      data-ui-id="call-with-chat-composite-more-button"
+      /*@conditional-compile-remove(PSTN-calls) */ /* @conditional-compile-remove(one-to-n-calling) */
+      strings={moreButtonStrings}
+      menuIconProps={{ hidden: true }}
+      menuProps={{ items: moreButtonContextualMenuItems() }}
+    />
+  );
+};
