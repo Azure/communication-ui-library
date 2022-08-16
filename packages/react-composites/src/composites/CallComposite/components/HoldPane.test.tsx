23f63d3c80a655348b974dd2a0db9dbcbe50686e
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-15 17:07:49 -0700

    Add resume screen to Calling composite (#2202)
    * VideoTile call state
    
    * Change files
    
    * Update comment
    
    * add beta tag to videotilecallstate prop
    
    * Update api.md
    
    * Updating api.md
    
    * Tweak prop name
    
    * Update changelog
    
    * Tweak prop name
    
    * updating api.md
    
    * Tweaking types
    
    * update changelog
    
    * Update api.md
    
    * Return participant state from videoGallerySelector and wire it in gallery
    
    * Change files
    
    * Tweak changelog
    
    * Fixing build
    
    * Tweaking state conversion logic in videoGallerySelector
    
    * Localization for video tile participant state prop
    
    * Conditionally remove pstn code from participant item
    
    * Update api.md
    
    * Update Api.md
    
    * resolving PR comments
    
    * Change files
    
    * Debugging video tile participant state display logic
    
    * update api.md
    
    * Resolving PR comments
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * Fixing conditional compilation errors
    
    * Change files
    
    * Updating types to match video tile participant state
    
    * updating api.md
    
    * update video gallery logic
    
    * debugging
    
    * Change files
    
    * Remove duplicate interface
    
    * update Media Gallery
    
    * update vigeogallery tile creation
    
    * update mediagallery on render
    
    * update media gallery styles
    
    * Update locale strings to match figma
    
    * build api files
    
    * add tests for gridview and horizontal view
    
    * add tests for 1 video part and joining
    
    * remove test.only
    
    * update tests
    
    * add screensharing tests and fix typing issue
    
    * update api
    
    * fix Ringing error
    
    * update tests
    
    * fix deps array
    
    * add third case for ringing
    
    * fix ringing state
    
    * update names of tests
    
    * add new helper functions
    
    * remove test only
    
    * update participant item state name
    
    * update API docs
    
    * update initial state of PSTN and 1:n
    
    * add more button to call composite
    
    * move CallWithChatMoreButton to be reused
    
    * update control bar
    
    * add context menu with hold button
    
    * add hold button to more button
    
    * fix hold button strings
    
    * update strings for hold and more button
    
    * fix CC and isEnabled
    
    * Change files
    
    * add hold screen and timer
    
    * moved styles
    
    * update styles and timer
    
    * Change files
    
    * add people button to overflow
    
    * update api docs
    
    * add hold page
    
    * fix cc error
    
    * fix cc
    
    * update hold pane time string
    
    * fix cc
    
    * fix lint
    
    * update for new people button location in Beta
    
    * remove test.only
    
    * add tests and fix nits
    
    * fix test time
    
    * Change files
    
    * fix lint
    
    * fix time function
    
    * build
    
    * update call utils function and tests
    
    Co-authored-by: Anjul Garg <anjulgarg@live.com>

---
+++ packages/react-composites/src/composites/CallComposite/components/HoldPane.test.tsx
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { getReadableTime } from './HoldPane';
+
+describe('Hold Pane tests', () => {
+  test('getReadableTime should report the correct time in minutes to be 30 minutes', () => {
+    const timeInMiliSeconds = 1800000;
+    const time = getReadableTime(timeInMiliSeconds);
+
+    expect(time).toBe('30:00');
+  });
+
+  test('getReadableTime should report the correct time to be 1 hour', () => {
+    const timeInMiliSeconds = 3600000;
+    const time = getReadableTime(timeInMiliSeconds);
+
+    expect(time).toBe('1:00:00');
+  });
+
+  test('getReadableTime should report the correct time to be 1 hour 30 minutes', () => {
+    const timeInMiliSeconds = 5400000;
+    const time = getReadableTime(timeInMiliSeconds);
+
+    expect(time).toBe('1:30:00');
+  });
+});
