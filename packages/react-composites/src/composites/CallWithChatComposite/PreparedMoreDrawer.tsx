ed8efd4fd84aaa74db630be47c54b23f0b52bcfa
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-18 08:21:59 -0700

    Disable control bar for CallWithChat and Calling Composite when on Hold (#2215)
    * VideoTile call state
    
    * Change files
    
    * Update comment
    
    * add beta tag to videotilecallstate prop
    
    * Update api.md
    
    * Updating api.md
    
    * Tweak prop name
    
    * Update changelog
    
    * Tweak prop name
    
    * updating api.md
    
    * Tweaking types
    
    * update changelog
    
    * Update api.md
    
    * Return participant state from videoGallerySelector and wire it in gallery
    
    * Change files
    
    * Tweak changelog
    
    * Fixing build
    
    * Tweaking state conversion logic in videoGallerySelector
    
    * Localization for video tile participant state prop
    
    * Conditionally remove pstn code from participant item
    
    * Update api.md
    
    * Update Api.md
    
    * resolving PR comments
    
    * Change files
    
    * Debugging video tile participant state display logic
    
    * update api.md
    
    * Resolving PR comments
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * fixing conditional compilation
    
    * Fixing conditional compilation errors
    
    * Change files
    
    * Updating types to match video tile participant state
    
    * updating api.md
    
    * update video gallery logic
    
    * debugging
    
    * Change files
    
    * Remove duplicate interface
    
    * update Media Gallery
    
    * update vigeogallery tile creation
    
    * update mediagallery on render
    
    * update media gallery styles
    
    * Update locale strings to match figma
    
    * build api files
    
    * add tests for gridview and horizontal view
    
    * add tests for 1 video part and joining
    
    * remove test.only
    
    * update tests
    
    * add screensharing tests and fix typing issue
    
    * update api
    
    * fix Ringing error
    
    * update tests
    
    * fix deps array
    
    * add third case for ringing
    
    * fix ringing state
    
    * update names of tests
    
    * add new helper functions
    
    * remove test only
    
    * update participant item state name
    
    * update API docs
    
    * update initial state of PSTN and 1:n
    
    * add more button to call composite
    
    * move CallWithChatMoreButton to be reused
    
    * update control bar
    
    * add context menu with hold button
    
    * add hold button to more button
    
    * fix hold button strings
    
    * update strings for hold and more button
    
    * fix CC and isEnabled
    
    * Change files
    
    * add hold screen and timer
    
    * moved styles
    
    * update styles and timer
    
    * Change files
    
    * add people button to overflow
    
    * update api docs
    
    * add hold page
    
    * fix cc error
    
    * fix cc
    
    * update hold pane time string
    
    * fix cc
    
    * add more button to CallWithChat
    
    * fix control bar state when on hold
    
    * remove bad imports
    
    * add hold button to moreDrawer
    
    * Disable buttons in Calling and CallwithChat
    
    * fix lint
    
    * update for new people button location in Beta
    
    * remove test.only
    
    * remove not needed import
    
    * update cc
    
    * fix prop issue
    
    * Change files
    
    * update more button disabled
    
    * added testing names
    
    * Using consistent logic for disabling buttons in lobby and hold screens
    
    * Remove unneeded conditional flags
    
    * Tweak utils code
    
    * merging main
    
    * Fixing conditional errors
    
    * Update api
    
    * Disabling buttons in callwithchat when call is on hold
    
    * Debug conditional compilation
    
    * Tweaking conditional compilation
    
    * Update api
    
    * Update js bundle automation snapshots
    
    * Update js bundle automation snapshots
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * Debugging regression
    
    * Update composite automation snapshots
    
    * Update composite automation snapshots
    
    * debugging failing snaps
    
    * Update composite automation snapshots
    
    Co-authored-by: Anjul Garg <anjulgarg@live.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/react-composites/src/composites/CallWithChatComposite/PreparedMoreDrawer.tsx
+++ packages/react-composites/src/composites/CallWithChatComposite/PreparedMoreDrawer.tsx
@@ -16,6 +16,7 @@ export interface PreparedMoreDrawerProps {
   callControls?: boolean | CallWithChatControlOptions;
   /* @conditional-compile-remove(PSTN-calls) */
   onClickShowDialpad: () => void;
+  disableButtonsForHoldScreen?: boolean;
 }
 
 /** @private */
