cbccff82aefa1dbd513bb4e18bce6505a13ce99d
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-18 00:22:31 +0000

    added a anippet

---
+++ packages/storybook/stories/snippets/ReactIndex.snippet.tsx
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/App';
+
+ReactDOM.render(
+  <div className="wrapper">
+    <App />
+  </div>,
+  document.getElementById('root')
+);
