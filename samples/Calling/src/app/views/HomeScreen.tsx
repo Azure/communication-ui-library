1d1326219b4a65464f0ecbe8a8cf33e4de36d20c
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-19 16:29:27 -0700

    PSTN fix ups (#2237)
    * fix gallery logic and sample app dailing
    
    * nits
    
    * Change files
    
    * update to regex
    
    * update phone number formatting
    
    Co-authored-by: Anjul Garg <anjulgarg@live.com>

--- samples/Calling/src/app/views/HomeScreen.tsx
+++ samples/Calling/src/app/views/HomeScreen.tsx
@@ -190,7 +190,7 @@ export const HomeScreen = (props: HomeScreenProps): JSX.Element => {
                        * We need to pass in the formatting for the phone number string in the onChange handler
                        * to make sure the phone number is in E.164 format.
                        */
-                      const phoneNumber = '+' + newValue?.replace('[^0-9]', '');
+                      const phoneNumber = '+' + newValue?.replace(/\D/g, '');
                       setDialpadParticipant(phoneNumber);
                     }}
                   />
