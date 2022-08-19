3751bfbc47774628ee5429cb780c76609bec7b66
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-19 22:15:36 +0000

    fix build issues

--- packages/storybook/stories/snippets/ReactIndex.snippet.tsx
+++ packages/storybook/stories/snippets/ReactIndex.snippet.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App.snippet';
 
 ReactDOM.render(
   <div className="wrapper">
