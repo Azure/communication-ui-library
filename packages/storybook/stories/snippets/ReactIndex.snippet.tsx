5ddf7f374975e40c89c3503cbe9d1ce5fb3e75f4
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-19 21:47:51 +0000

    edit pages to reference the setup page

--- packages/storybook/stories/snippets/ReactIndex.snippet.tsx
+++ packages/storybook/stories/snippets/ReactIndex.snippet.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app/App';
+import App from './App';
 
 ReactDOM.render(
   <div className="wrapper">
