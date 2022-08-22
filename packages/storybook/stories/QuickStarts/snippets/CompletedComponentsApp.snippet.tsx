d7c63661a16c19f8f43796d7e1f32b263253c370
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-22 17:29:52 +0000

    register icons in component snippet

--- packages/storybook/stories/QuickStarts/snippets/CompletedComponentsApp.snippet.tsx
+++ packages/storybook/stories/QuickStarts/snippets/CompletedComponentsApp.snippet.tsx
@@ -1,10 +1,13 @@
-import { FluentThemeProvider } from '@azure/communication-react';
-import { Stack } from '@fluentui/react';
+import { DEFAULT_COMPONENT_ICONS, FluentThemeProvider } from '@azure/communication-react';
+import { Stack, registerIcons } from '@fluentui/react';
 import React from 'react';
 import { CallingComponents } from './CallingComponents';
 import { ChatComponents } from './ChatComponents';
 
 function CompletedComponentsApp(): JSX.Element {
+  // If you don't want to provide custom icons, you can register the default ones included with the library.
+  // This will ensure that all the icons are rendered correctly.
+  registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
   const stackStyle = {
     root: {
       width: '100%'
