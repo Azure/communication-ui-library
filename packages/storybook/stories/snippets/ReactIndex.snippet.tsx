e9388ac70eece014cb91cd53d74960430d4ef9b7
dmceachernmsft <94866715+dmceachernmsft@users.noreply.github.com>
2022-08-23 23:40:28 +0000

    update docs

--- packages/storybook/stories/snippets/ReactIndex.snippet.tsx
+++ packages/storybook/stories/snippets/ReactIndex.snippet.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App.snippet';
+import { App } from './App.snippet'; // this should point to your App.tsx file
 
 ReactDOM.render(
   <div className="wrapper">
