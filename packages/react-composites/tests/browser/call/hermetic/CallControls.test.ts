4b7f05d81a07fca7db8a592f172a4a7067b7ae03
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-08-24 23:17:14 +0530

    Deflake a class of browser test failures (#2252)

--- packages/react-composites/tests/browser/call/hermetic/CallControls.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/CallControls.test.ts
@@ -12,7 +12,7 @@ test.describe('CallControls tests', async () => {
     initialState.devices.microphones = [];
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
     await waitForSelector(page, dataUiId(IDS.videoGallery));
-    expect(await stableScreenshot(page, { dismissTooltips: true })).toMatchSnapshot('no-mics.png');
+    expect(await stableScreenshot(page)).toMatchSnapshot('no-mics.png');
   });
 
   test('CallControls when number of available cameras drops to zero', async ({ page, serverUrl }) => {
@@ -20,7 +20,7 @@ test.describe('CallControls tests', async () => {
     initialState.devices.cameras = [];
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
     await waitForSelector(page, dataUiId(IDS.videoGallery));
-    expect(await stableScreenshot(page, { dismissTooltips: true })).toMatchSnapshot('no-videos.png');
+    expect(await stableScreenshot(page)).toMatchSnapshot('no-videos.png');
   });
 });
 
@@ -28,6 +28,6 @@ test.describe('Call composite custom button injection tests', () => {
   test('injected buttons appear', async ({ page, serverUrl }) => {
     const initialState = defaultMockCallAdapterState([defaultMockRemoteParticipant('Paul Bridges')]);
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState, { injectCustomButtons: 'true' }));
-    expect(await stableScreenshot(page, { dismissTooltips: true })).toMatchSnapshot(`custom-buttons.png`);
+    expect(await stableScreenshot(page)).toMatchSnapshot(`custom-buttons.png`);
   });
 });
