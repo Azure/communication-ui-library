edc1b66809db2d723148d8c47e42772935b2fef3
mgamis-msft <miguelgamis@microsoft.com>
2022-09-02 14:27:01 -0700

    skip room page test for stable

--- packages/react-composites/tests/browser/call/hermetic/PageState.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/PageState.test.ts
@@ -2,7 +2,13 @@
 // Licensed under the MIT license.
 
 import { expect } from '@playwright/test';
-import { stableScreenshot, waitForPageFontsLoaded, waitForSelector, dataUiId } from '../../common/utils';
+import {
+  stableScreenshot,
+  waitForPageFontsLoaded,
+  waitForSelector,
+  dataUiId,
+  isTestProfileStableFlavor
+} from '../../common/utils';
 import { buildUrlWithMockAdapter, defaultMockCallAdapterState, test } from './fixture';
 
 test.describe('Page state tests', async () => {
@@ -43,6 +49,7 @@ test.describe('Page state tests', async () => {
     expect(await stableScreenshot(page)).toMatchSnapshot('removed-from-call-page.png');
   });
   test('Page when local participant tries to join a room that cannot be not found', async ({ page, serverUrl }) => {
+    test.skip(isTestProfileStableFlavor());
     const initialState = defaultMockCallAdapterState();
     initialState.page = 'roomNotFound';
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
@@ -51,6 +58,7 @@ test.describe('Page state tests', async () => {
     expect(await stableScreenshot(page)).toMatchSnapshot('room-not-found-page.png');
   });
   test('Page when local participant tries to join a room that they are not invited to', async ({ page, serverUrl }) => {
+    test.skip(isTestProfileStableFlavor());
     const initialState = defaultMockCallAdapterState();
     initialState.page = 'notInvitedToRoom';
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
