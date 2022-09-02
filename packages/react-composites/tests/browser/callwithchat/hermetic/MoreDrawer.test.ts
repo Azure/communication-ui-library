fdcb9b795ce5e5799ea7aa5587d8beec61efdee7
Prathmesh Prabhu <prprabhu@microsoft.com>
2022-09-03 00:13:06 +0530

    migrate speaker tests

--- packages/react-composites/tests/browser/callwithchat/hermetic/MoreDrawer.test.ts
+++ packages/react-composites/tests/browser/callwithchat/hermetic/MoreDrawer.test.ts
@@ -6,7 +6,7 @@ import { dataUiId, isTestProfileDesktop, pageClick, stableScreenshot } from '../
 import { loadCallPage, test } from './fixture';
 import { expect } from '@playwright/test';
 
-test.describe('CallWithChat Composite CallWithChat Page Tests', () => {
+test.describe.only('CallWithChat Composite CallWithChat Page Tests', () => {
   // eslint-disable-next-line no-empty-pattern
   test.beforeEach(async ({}, testInfo) => {
     test.skip(isTestProfileDesktop(testInfo), 'There is no More Drawer on desktop');
@@ -19,11 +19,30 @@ test.describe('CallWithChat Composite CallWithChat Page Tests', () => {
     expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-more-drawer-screen.png`);
   });
 
-  test.only('More Drawer Speaker submenu opens and displays correctly on mobile', async ({ page, serverUrl }) => {
+  test('Speaker Submenu click on a new audio device displays correctly on mobile', async ({ page, serverUrl }) => {
     const callState = defaultMockCallAdapterState([defaultMockRemoteParticipant('Paul Bridges')]);
+    callState.devices = {
+      ...callState.devices,
+      speakers: [
+        { id: 'speaker1', name: '1st Speaker', deviceType: 'Speaker', isSystemDefault: true },
+        { id: 'speaker2', name: '2nd Speaker', deviceType: 'Speaker', isSystemDefault: false }
+      ]
+    };
     await loadCallPage(page, serverUrl, callState);
+
     await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
     await pageClick(page, 'div[role="menu"] >> text=Speaker');
     expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-more-drawer-submenu-speaker-screen.png`);
+
+    await pageClick(page, 'div[role="menu"] >> text="2nd Speaker"');
+
+    await page.pause();
+
+    // Need to open again because submenu is dismissed automatically after selection
+    await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
+    expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-more-drawer-new-selected-speaker-screen.png`);
+
+    await pageClick(page, 'div[role="menu"] >> text=Speaker');
+    expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-more-drawer-submenu-speaker-select.png`);
   });
 });
--- packages/react-composites/tests/browser/callwithchat/live/CallWithChatComposite.test.ts
+++ packages/react-composites/tests/browser/callwithchat/live/CallWithChatComposite.test.ts
@@ -54,43 +54,6 @@ test.describe('CallWithChat Composite CallWithChat Page Tests', () => {
     expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-gallery-screen-with-people-pane.png`);
   });
 
-  test('Speaker Submenu click on a new audio device displays correctly on mobile', async ({ pages }, testInfo) => {
-    const page = pages[1];
-    if (!isTestProfileDesktop(testInfo)) {
-      await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
-      const moreDrawerSpeakerDiv = await page.$('div[role="menu"] >> text=Speaker');
-      await moreDrawerSpeakerDiv?.click();
-      const submenuNewAudioDeviceDiv = await page.$('div[role="menu"] >> text="Fake Audio Output 1"');
-      await submenuNewAudioDeviceDiv?.click();
-      //need to open again because submenu is dismissed automatically after selection
-      await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
-      const moreDrawerSpeakerDiv_copy = await page.$('div[role="menu"] >> text=Speaker');
-      await moreDrawerSpeakerDiv_copy?.click();
-      await page.$('div[role="menu"] >> text="Fake Audio Output 1"');
-      expect(await stableScreenshot(page)).toMatchSnapshot(`call-with-chat-more-drawer-submenu-speaker-select.png`);
-    }
-  });
-
-  test('More Drawer menu opens and displays new selected speaker device correctly on mobile', async ({
-    pages
-  }, testInfo) => {
-    const page = pages[1];
-    if (!isTestProfileDesktop(testInfo)) {
-      // Select new audio device in submenu drawer
-      await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
-      const moreDrawerSpeakerDiv = await page.$('div[role="menu"] >> text=Speaker');
-      await moreDrawerSpeakerDiv?.click();
-      const submenuNewAudioDeviceDiv = await page.$('div[role="menu"] >> text="Fake Audio Output 1"');
-      await submenuNewAudioDeviceDiv?.click();
-
-      // Display MoreDrawer to view newly selected audio device
-      await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
-      expect(await stableScreenshot(page)).toMatchSnapshot(
-        `call-with-chat-more-drawer-new-selected-speaker-screen.png`
-      );
-    }
-  });
-
   test('More Drawer Microphone submenu opens and displays correctly on mobile', async ({ pages }, testInfo) => {
     const page = pages[1];
     if (!isTestProfileDesktop(testInfo)) {
