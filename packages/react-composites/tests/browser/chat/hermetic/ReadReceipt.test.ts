47a720fc24f62102a47bd60677baef74c62f89d8
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-09-02 22:10:41 +0530

    browser tests: Fix flaky read reciept test (#2291)

--- packages/react-composites/tests/browser/chat/hermetic/ReadReceipt.test.ts
+++ packages/react-composites/tests/browser/chat/hermetic/ReadReceipt.test.ts
@@ -1,12 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { ChatParticipant } from '@azure/communication-chat';
-import { CommunicationIdentifier } from '@azure/communication-common';
-import { toFlatCommunicationIdentifier } from '@internal/acs-ui-common';
-import { ElementHandle, expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { temporarilyShowHiddenChatComposite } from '../../common/hermeticChatTestHelpers';
 import { sendMessage, waitForMessageDelivered, waitForMessageSeen } from '../../common/chatTestHelpers';
-import { dataUiId, stableScreenshot, waitForSelector } from '../../common/utils';
+import {
+  dataUiId,
+  perStepLocalTimeout,
+  screenshotOnFailure,
+  stableScreenshot,
+  waitForSelector
+} from '../../common/utils';
 import { buildUrlForChatAppUsingFakeAdapter, DEFAULT_FAKE_CHAT_ADAPTER_ARGS, test } from './fixture';
 
 test.describe('Chat Composite E2E Tests', () => {
@@ -18,17 +22,20 @@ test.describe('Chat Composite E2E Tests', () => {
         participantsWithHiddenComposites: [messageReader]
       })
     );
-    await setParticipantAbleToSeeMessages(page, messageReader, false);
 
-    const testMessageText = 'How the turn tables';
-    await sendMessage(page, testMessageText);
+    await sendMessage(page, 'How the turn tables');
     await waitForMessageDelivered(page);
     expect(await stableScreenshot(page, { stubMessageTimestamps: true })).toMatchSnapshot('sent-messages.png');
 
-    await setParticipantAbleToSeeMessages(page, messageReader, true);
+    await temporarilyShowHiddenChatComposite(page, messageReader);
     await waitForMessageSeen(page);
 
-    await page.locator(dataUiId('chat-composite-message-status-icon')).click();
+    // Can't use `pageClick()` here because we want the tooltips to be shown.
+    // `pageClick()` moves the mouse away explicitly to dismiss the tooltips.
+    await screenshotOnFailure(
+      page,
+      async () => await page.click(dataUiId('chat-composite-message-status-icon'), { timeout: perStepLocalTimeout() })
+    );
     await waitForSelector(page, dataUiId('chat-composite-message-tooltip'));
     expect(await stableScreenshot(page, { stubMessageTimestamps: true, dismissTooltips: false })).toMatchSnapshot(
       'read-message-tooltip-text.png'
@@ -46,52 +53,22 @@ test.describe('Chat Composite E2E Tests', () => {
       })
     );
 
-    DEFAULT_FAKE_CHAT_ADAPTER_ARGS.remoteParticipants.forEach(async (participant) => {
-      await setParticipantAbleToSeeMessages(page, participant, false);
-    });
-
-    const testMessageText = 'How the turn tables';
-    await sendMessage(page, testMessageText);
+    await sendMessage(page, 'How the turn tables');
     await waitForMessageDelivered(page);
 
-    DEFAULT_FAKE_CHAT_ADAPTER_ARGS.remoteParticipants.forEach(async (participant) => {
-      await setParticipantAbleToSeeMessages(page, participant, true);
-    });
+    for (const participant of DEFAULT_FAKE_CHAT_ADAPTER_ARGS.remoteParticipants) {
+      await temporarilyShowHiddenChatComposite(page, participant);
+    }
     await waitForMessageSeen(page);
 
-    await page.locator(dataUiId('chat-composite-message')).first().click();
-    await page.locator(dataUiId('chat-composite-message-action-icon')).first().click();
-    await waitForSelector(page, '[id="chat-composite-message-contextual-menu"]');
-    await page.locator(dataUiId('chat-composite-message-contextual-menu-read-info')).click();
-    await waitForSelector(page, '[id="chat-composite-message-contextual-menu-read-name-list"]');
+    await screenshotOnFailure(page, async () => {
+      await page.locator(dataUiId('chat-composite-message')).first().click();
+      await page.locator(dataUiId('chat-composite-message-action-icon')).first().click();
+      await waitForSelector(page, '[id="chat-composite-message-contextual-menu"]');
+      await page.locator(dataUiId('chat-composite-message-contextual-menu-read-info')).click();
+      await waitForSelector(page, '[id="chat-composite-message-contextual-menu-read-name-list"]');
+    });
+
     expect(await stableScreenshot(page)).toMatchSnapshot('read-message-contextualMenu.png');
   });
 });
-
-async function getHiddenCompositeHandleOfParticipant(
-  page: Page,
-  participantIdentifier: CommunicationIdentifier
-): Promise<ElementHandle<SVGElement | HTMLElement>> {
-  return await waitForSelector(
-    page,
-    `[id="hidden-composite-${toFlatCommunicationIdentifier(participantIdentifier)}"]`,
-    {
-      state: 'attached'
-    }
-  );
-}
-
-async function setParticipantAbleToSeeMessages(
-  page: Page,
-  participant: ChatParticipant,
-  isAble: boolean
-): Promise<void> {
-  const messageReaderCompositeHandle = await getHiddenCompositeHandleOfParticipant(page, participant.id);
-  if (isAble) {
-    // Display the hidden composite so that sent messages will be seen
-    await messageReaderCompositeHandle.evaluate((node) => (node.style.display = 'block'));
-  } else {
-    // Do not display the hidden composite so that messages sent will not be seen
-    await messageReaderCompositeHandle.evaluate((node) => (node.style.display = 'none'));
-  }
-}
