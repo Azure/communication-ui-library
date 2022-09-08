e3e52f1a2a817d61bbc44dec558c005645e6e803
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-09-08 22:49:21 +0000

    Update CallWithChatComposite Stories to have formFactor and the args to have stronger typing

--- packages/storybook/stories/CallWithChatComposite/JoinExample.stories.tsx
+++ packages/storybook/stories/CallWithChatComposite/JoinExample.stories.tsx
@@ -6,13 +6,22 @@ import { Stack } from '@fluentui/react';
 import { Meta } from '@storybook/react/types-6-0';
 import React, { useState, useEffect } from 'react';
 import { COMPOSITE_FOLDER_PREFIX, compositeExperienceContainerStyle } from '../constants';
-import { controlsToAdd, defaultCallWithChatCompositeHiddenControls } from '../controlsUtils';
+import { ArgsFrom, controlsToAdd, defaultCallWithChatCompositeHiddenControls } from '../controlsUtils';
 import { getDocs } from './CallWithChatCompositeDocs';
 import { CallWithChatExampleProps } from './snippets/CallWithChat.snippet';
 import { CallWithChatExperienceWithErrorChecks } from './snippets/CallWithChatWithErrorChecks.snippet';
 import { ConfigJoinMeetingHintBanner } from './Utils';
 
-const JoinTeamsMeetingStory = (args, context): JSX.Element => {
+const storyControls = {
+  userId: controlsToAdd.userId,
+  token: controlsToAdd.token,
+  endpointUrl: controlsToAdd.endpointUrl,
+  displayName: controlsToAdd.displayName,
+  teamsMeetingLink: controlsToAdd.teamsMeetingLink,
+  formFactor: controlsToAdd.formFactor
+};
+
+const JoinTeamsMeetingStory = (args: ArgsFrom<typeof storyControls>, context): JSX.Element => {
   const [meetingProps, setMeetingProps] = useState<CallWithChatExampleProps>();
 
   useEffect(() => {
@@ -52,11 +61,7 @@ export default {
   title: `${COMPOSITE_FOLDER_PREFIX}/CallWithChatComposite/Join Teams Meeting`,
   component: CallWithChatComposite,
   argTypes: {
-    token: controlsToAdd.token,
-    userId: controlsToAdd.userId,
-    endpointUrl: controlsToAdd.endpointUrl,
-    displayName: controlsToAdd.displayName,
-    teamsMeetingLink: controlsToAdd.teamsMeetingLink,
+    ...storyControls,
     // Hiding auto-generated controls
     ...defaultCallWithChatCompositeHiddenControls
   },
