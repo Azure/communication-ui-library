8d657411e883bb97859c82459adc2a3d1a326c2b
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-09-13 00:07:21 +0000

    create new view for the composite

---
+++ packages/react-composites/src/composites/CallComposite/pages/UnsupportedBrowser.tsx
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import React from 'react';
+
+/**
+ * @internal
+ */
+export type UnsupportedBrowserProps = {
+  onTroubleshootingClick: () => void;
+};
+
+/**
+ *
+ * @internal
+ */
+export const UnsupportedBrowser = (props: UnsupportedBrowserProps): JSX.Element => {
+  return <></>;
+};
