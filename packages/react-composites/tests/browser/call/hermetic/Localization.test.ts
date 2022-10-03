a06bff6eba575f8ea33fa295517bc87193274ec9
Edward Lee <edwardlee@microsoft.com>
2022-10-03 16:05:37 -0700

    Remove localization beta tests

--- packages/react-composites/tests/browser/call/hermetic/Localization.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/Localization.test.ts
@@ -2,13 +2,20 @@
 // Licensed under the MIT license.
 
 import { buildUrlWithMockAdapter, defaultMockCallAdapterState, test } from './fixture';
-import { waitForCallCompositeToLoad, stableScreenshot, waitForSelector, dataUiId } from '../../common/utils';
+import {
+  waitForCallCompositeToLoad,
+  stableScreenshot,
+  waitForSelector,
+  dataUiId,
+  isTestProfileStableFlavor
+} from '../../common/utils';
 import { expect } from '@playwright/test';
 import { IDS } from '../../common/constants';
 
 test.describe('Localization tests', async () => {
   // TODO(prprabhu) Rename this test once metrics show that it has been stabilized.
   test('Configuration page title and participant button in call should be localized', async ({ page, serverUrl }) => {
+    test.skip(!isTestProfileStableFlavor());
     const initialState = defaultMockCallAdapterState();
     initialState.page = 'configuration';
     initialState.call = undefined;
@@ -18,6 +25,7 @@ test.describe('Localization tests', async () => {
   });
 
   test('Participant button in call should be localized', async ({ page, serverUrl }) => {
+    test.skip(!isTestProfileStableFlavor());
     const initialState = defaultMockCallAdapterState();
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState, { useFrLocale: 'true' }));
     await waitForSelector(page, dataUiId(IDS.videoGallery));
