a639e1f2d235e67ce8de2a00756df54b37890b14
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-10-04 20:56:17 +0000

    fix build issue

--- packages/react-composites/src/composites/CallComposite/CallComposite.tsx
+++ packages/react-composites/src/composites/CallComposite/CallComposite.tsx
@@ -29,6 +29,7 @@ import { modalLayerHostStyle } from '../common/styles/ModalLocalAndRemotePIP.sty
 import { useId } from '@fluentui/react-hooks';
 /* @conditional-compile-remove(one-to-n-calling) */ /* @conditional-compile-remove(PSTN-calls) */
 import { HoldPage } from './pages/HoldPage';
+/* @conditional-compile-remove(unsupported-browser) */
 import { UnsupportedBrowserPage } from './pages/UnsupportedBrowser';
 
 /**
@@ -300,11 +301,11 @@ const MainScreen = (props: MainScreenProps): JSX.Element => {
     case unsupportedBrowserPageTrampoline():
       pageElement = (
         <>
-          {props.options?.onBrowserTroubleShootingClick && (
-            /* @conditional-compile-remove(unsupported-browser) */ <UnsupportedBrowserPage
-              onTroubleShootingClick={props.options?.onBrowserTroubleShootingClick}
-            />
-          )}
+          {
+            /* @conditional-compile-remove(unsupported-browser) */ props.options?.onBrowserTroubleShootingClick && (
+              <UnsupportedBrowserPage onTroubleShootingClick={props.options?.onBrowserTroubleShootingClick} />
+            )
+          }
         </>
       );
   }
