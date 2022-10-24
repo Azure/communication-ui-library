8fc34dbe6742c7c0d5e7749e5563181064637ce7
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-10-24 17:27:41 +0000

    add tests for PSTN and ACS call

--- packages/react-composites/tests/browser/call/hermetic/LobbyScreen.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/LobbyScreen.test.ts
@@ -2,18 +2,77 @@
 // Licensed under the MIT license.
 
 import { expect } from '@playwright/test';
-import { buildUrlWithMockAdapter, defaultMockCallAdapterState, test } from './fixture';
-import type { MockCallAdapterState } from '../../../common';
+import {
+  buildUrlWithMockAdapter,
+  defaultMockCallAdapterState,
+  defaultMockRemoteParticipant,
+  defaultMockRemotePSTNParticipant,
+  test
+} from './fixture';
 import { IDS } from '../../common/constants';
-import { dataUiId, stableScreenshot, waitForSelector } from 'browser/common/utils';
+import { dataUiId, isTestProfileStableFlavor, stableScreenshot, waitForSelector } from '../../common/utils';
 
 test.describe('Lobby page tests', async () => {
   test('Lobby page shows correct strings when joining a group call', async ({ page, serverUrl }) => {
-    const initialState = defaultMockCallAdapterState();
+    const dina = defaultMockRemoteParticipant('Dina');
+    /**
+     * remote participants reflect as this state for a moment before the user connects.
+     *
+     * Dina exists to demonstrate that we don't see the 'Calling' string when joining a group call as a flash.
+     */
+    dina.state = 'Idle';
+    const participants = [dina];
+    const initialState = defaultMockCallAdapterState(participants);
+
+    if (initialState.call) {
+      initialState.call.state = 'Connecting';
+    }
     initialState.page = 'lobby';
+
+    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
 
     await waitForSelector(page, dataUiId(IDS.lobbyScreenTitle));
 
     expect(await stableScreenshot(page)).toMatchSnapshot('lobbyPage-groupCall.png');
+  });
+
+  test('lobby page shows correct strings when starting a ACS outbound call', async ({ page, serverUrl }) => {
+    test.skip(isTestProfileStableFlavor());
+
+    const joel = defaultMockRemoteParticipant('Joel');
+    joel.state = 'Connecting';
+    const participants = [joel];
+    const initialState = defaultMockCallAdapterState(participants);
+
+    if (initialState.call) {
+      initialState.call.state = 'Connecting';
+    }
+    initialState.page = 'lobby';
+
+    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
+
+    await waitForSelector(page, dataUiId(IDS.lobbyScreenTitle));
+
+    expect(await stableScreenshot(page)).toMatchSnapshot('lobbyPage-ACScall.png');
+  });
+
+  test('lobby page shows correct strings when starting a PSTN outbound call', async ({ page, serverUrl }) => {
+    test.skip(isTestProfileStableFlavor());
+
+    const ellie = defaultMockRemotePSTNParticipant('15556667777');
+    ellie.state = 'Ringing';
+    const participants = [ellie];
+    const initialState = defaultMockCallAdapterState(participants);
+
+    if (initialState.call) {
+      initialState.call.state = 'Connecting';
+    }
+    initialState.page = 'lobby';
+
+    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
+
+    await waitForSelector(page, dataUiId(IDS.lobbyScreenTitle));
+
+    expect(await stableScreenshot(page)).toMatchSnapshot('lobbyPage-PSTNcall.png');
   });
 });
