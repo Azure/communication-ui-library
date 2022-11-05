b218b4a44c434d6c6c7c736ee3c9f768d795ab35
Anjul Garg <anjulgarg@live.com>
2022-11-04 17:22:27 -0700

    Working video gallery first test

--- packages/react-components/src/components/VideoGallery.test.tsx
+++ packages/react-components/src/components/VideoGallery.test.tsx
@@ -1,31 +1,26 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { initializeIcons, Stack } from '@fluentui/react';
+import { initializeIcons } from '@fluentui/react';
 import Enzyme, { mount, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { VideoGalleryLocalParticipant } from '../types';
 import { VideoGallery } from './VideoGallery';
 
-// create a document div with purple background
 const mockLocalRenderElement = document.createElement('div');
-mockLocalRenderElement.style.height = '50px';
-mockLocalRenderElement.style.width = '50px';
-mockLocalRenderElement.style.backgroundColor = 'purple';
 mockLocalRenderElement.innerHTML = '<div>LOCAL PARTICIPANT</div>';
 
 const mockLocalParticipant: VideoGalleryLocalParticipant = {
   userId: 'localParticipant',
   isMuted: false,
-  displayName: 'localParticipant',
+  displayName: 'Local Participant',
   videoStream: {
     id: 1,
     isAvailable: true,
     isReceiving: true,
     isMirrored: true,
-    renderElement: mockLocalRenderElement
+    renderElement: undefined
   },
   isScreenSharingOn: false
 };
@@ -37,20 +32,25 @@ describe('VideoGallery', () => {
   });
 
   test('should render local video tile as the only tile', () => {
-    const root = mountVideoGalleryWithDefaults();
-    // create jest snapshot
+    const localParticipant = { ...mockLocalParticipant };
+    const onCreateLocalStreamView = jest.fn();
+    const root = mountVideoGalleryWithDefaults({ onCreateLocalStreamView, localParticipant });
+    expect(onCreateLocalStreamView).toHaveBeenCalledTimes(1);
+    root.setProps({
+      localParticipant: {
+        ...localParticipant,
+        videoStream: {
+          ...localParticipant.videoStream,
+          renderElement: mockLocalRenderElement
+        }
+      },
+      onCreateLocalStreamView: onCreateLocalStreamView
+    });
+    expect(onCreateLocalStreamView).toHaveBeenCalledTimes(1);
     expect(root.render()).toMatchSnapshot('VideoGallery-with-local-participant');
   });
 });
 
-const mountVideoGalleryWithDefaults = (): ReactWrapper => {
-  let root;
-  act(() => {
-    root = mount(
-      <Stack style={{ height: '100px', width: '100px', position: 'relative' }}>
-        <VideoGallery localParticipant={mockLocalParticipant} />
-      </Stack>
-    );
-  });
-  return root;
+const mountVideoGalleryWithDefaults = ({ onCreateLocalStreamView, localParticipant }): ReactWrapper => {
+  return mount(<VideoGallery localParticipant={localParticipant} onCreateLocalStreamView={onCreateLocalStreamView} />);
 };
