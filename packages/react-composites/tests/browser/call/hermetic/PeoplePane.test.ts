fdf2f68ca9e316a7ed94e0c8f3220120c01d330e
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-11-06 06:41:37 +0000

    Split participant list test

--- packages/react-composites/tests/browser/call/hermetic/PeoplePane.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/PeoplePane.test.ts
@@ -9,10 +9,11 @@ import {
   defaultMockRemoteParticipant,
   test
 } from './fixture';
-import { expect } from '@playwright/test';
+import { expect, TestInfo } from '@playwright/test';
 import {
   dataUiId,
   isTestProfileDesktop,
+  isTestProfileMobile,
   isTestProfileStableFlavor,
   pageClick,
   perStepLocalTimeout,
@@ -21,48 +22,84 @@ import {
   waitForSelector
 } from '../../common/utils';
 import { IDS } from '../../common/constants';
+import { MockCallAdapterState } from 'common';
+
+test.describe('Participant list flyout tests', () => {
+  test.skip(!participantListShownAsFlyout());
+
+  test('participant list loads correctly', async ({ page, serverUrl }) => {
+    await page.goto(
+      buildUrlWithMockAdapter(serverUrl, participantListInitialState(), { callInvitationUrl: 'testUrl' })
+    );
+    await pageClick(page, dataUiId('call-composite-participants-button'));
+    const buttonCallOut = await waitForSelector(page, '.ms-Callout');
+    // This will ensure no animation is happening for the callout
+    await buttonCallOut.waitForElementState('stable');
+    expect(await stableScreenshot(page)).toMatchSnapshot(`video-gallery-page-participants-flyout.png`);
+  });
+});
+
+test.describe('Participant list side pane tests', () => {
+  test('participant list loads correctly', async ({ page, serverUrl }, testInfo) => {
+    test.skip(!participantListShownAsSidePane(testInfo));
+
+    await page.goto(
+      buildUrlWithMockAdapter(serverUrl, participantListInitialState(), { callInvitationUrl: 'testUrl' })
+    );
+    await pageClick(page, dataUiId('call-composite-participants-button'));
+    await waitForSelector(page, dataUiId('call-composite-people-pane'));
+    expect(await stableScreenshot(page)).toMatchSnapshot(`video-gallery-page-participants-flyout.png`);
+  });
+});
+
+test.describe('Participant list full screen pane tests', () => {
+  test('participant list loads correctly', async ({ page, serverUrl }, testInfo) => {
+    test.skip(!participantListShownAsFullScreenPane(testInfo));
+
+    await page.goto(
+      buildUrlWithMockAdapter(serverUrl, participantListInitialState(), { callInvitationUrl: 'testUrl' })
+    );
+    await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
+    const drawerPeopleMenuDiv = await page.$('div[role="menu"] >> text=People');
+    await drawerPeopleMenuDiv?.click();
+
+    await waitForSelector(page, dataUiId('call-composite-people-pane'));
+    await waitForPiPiPToHaveLoaded(page, { skipVideoCheck: true });
+
+    expect(await stableScreenshot(page)).toMatchSnapshot(`video-gallery-page-participants-flyout.png`);
+  });
+});
+
+const participantListInitialState = (): MockCallAdapterState => {
+  const paul = defaultMockRemoteParticipant('Paul Bridges');
+  addVideoStream(paul, true);
+  paul.isSpeaking = true;
+  const initialState = defaultMockCallAdapterState([
+    paul,
+    defaultMockRemoteParticipant('Eryka Klein'),
+    defaultMockRemoteParticipant('Fiona Harper')
+  ]);
+  addDefaultMockLocalVideoStreamState(initialState);
+  return initialState;
+};
+
+const participantListShownAsFlyout = (): boolean => {
+  /* @conditional-compile-remove(one-to-n-calling) */
+  return false;
+  return true;
+};
+
+const participantListShownAsSidePane = (testInfo: TestInfo): boolean => {
+  return isTestProfileDesktop(testInfo) && !participantListShownAsFlyout();
+};
+
+const participantListShownAsFullScreenPane = (testInfo: TestInfo): boolean => {
+  return isTestProfileMobile(testInfo) && !participantListShownAsFlyout();
+};
 
 // TODO(prprabhu): Merge the two tests below in a single `describe`
 // after metrics show that the tests have been stabilized.
 test.describe('Call Composite E2E CallPage Tests', () => {
-  // TODO: Split this test into multiple tests: one for beta/desktop, beta/mobile, stable each.
-  // Do this after the test has been stabilized. Keep the same name for flakiness analysis.
-  test('participant list loads correctly', async ({ page, serverUrl }, testInfo) => {
-    const paul = defaultMockRemoteParticipant('Paul Bridges');
-    addVideoStream(paul, true);
-    paul.isSpeaking = true;
-    const participants = [
-      paul,
-      defaultMockRemoteParticipant('Eryka Klein'),
-      defaultMockRemoteParticipant('Fiona Harper')
-    ];
-
-    const initialState = defaultMockCallAdapterState(participants);
-    addDefaultMockLocalVideoStreamState(initialState);
-
-    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState, { callInvitationUrl: 'testUrl' }));
-
-    if (!isTestProfileDesktop(testInfo) && !isTestProfileStableFlavor()) {
-      await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
-      const drawerPeopleMenuDiv = await page.$('div[role="menu"] >> text=People');
-      await drawerPeopleMenuDiv?.click();
-    } else {
-      await pageClick(page, dataUiId('call-composite-participants-button'));
-    }
-
-    if (isTestProfileStableFlavor()) {
-      const buttonCallOut = await waitForSelector(page, '.ms-Callout');
-      // This will ensure no animation is happening for the callout
-      await buttonCallOut.waitForElementState('stable');
-    } else {
-      await waitForSelector(page, dataUiId('call-composite-people-pane'));
-      if (!isTestProfileDesktop(testInfo)) {
-        await waitForPiPiPToHaveLoaded(page, { skipVideoCheck: true });
-      }
-    }
-    expect(await stableScreenshot(page)).toMatchSnapshot(`video-gallery-page-participants-flyout.png`);
-  });
-
   test('participant list opens and displays ellipses if passing in custom icon for desktop', async ({
     page,
     serverUrl
