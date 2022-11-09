9900eb5e5a3126af84ab46162e2ff43bc01fb952
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-11-09 06:47:00 +0000

    Start duplicating change files for beta release

---
+++ common/scripts/changelog/change.mjs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import fs from 'fs';
+import path from 'path';
+
+import { BEACHBALL, CHANGE_DIR, CHANGE_DIR_BETA } from './constants.mjs';
+import { exec, exec_output } from '../lib/index.mjs';
+
+const NEW_CHANGE_FILE_REGEXP = /\s*A\s*change\/(.*\.json)\s*/;
+
+async function main() {
+    await exec(`node ${BEACHBALL}`);
+    await duplicateChangeFiles();
+}
+
+async function duplicateChangeFiles() {
+    const gitLogStdout = await exec_output(`git log -1 --name-status`);
+    const newChangeFiles = parseNewChangeFiles(gitLogStdout);
+    if (newChangeFiles.length === 0) {
+        return;
+    }
+
+    console.log(`Duplicating ${newChangeFiles.length} change files into ${CHANGE_DIR_BETA}`);
+    for (const file of newChangeFiles) {
+        fs.copyFileSync(path.join(CHANGE_DIR, file), path.join(CHANGE_DIR_BETA, file));
+    }
+    await exec(`git add ${CHANGE_DIR_BETA}`);
+    await exec(`git commit -m 'Duplicate change files for beta release'`);
+}
+
+function parseNewChangeFiles(stdout) {
+    const lines = stdout.split('\n');
+    const matches = lines.map(line => line.match(NEW_CHANGE_FILE_REGEXP)).filter(match => !!match);
+    // Extract the first capture group.
+    return matches.map(match => match[1]);
+}
+
+await main();
---
+++ common/scripts/changelog/constants.mjs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import path from 'path';
+import { REPO_ROOT } from '../lib/index.mjs';
+
+export const BEACHBALL = path.join(REPO_ROOT, 'common', 'config', 'node_modules', 'beachball', 'bin', 'beachball');
+export const CHANGE_DIR = path.join(REPO_ROOT, 'change');
+export const CHANGE_DIR_BETA = path.join(REPO_ROOT, 'change-beta');
