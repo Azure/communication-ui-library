c8d3d24cf8d17695a2992940a0fc753f94bf6f9e
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-11-10 07:01:42 +0000

    add placeholder check script

---
+++ common/scripts/changelog/check.mjs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+async function main() {
+  // TODO: Just check that _some_ file was added to change/ ???
+}
+
+await main();
