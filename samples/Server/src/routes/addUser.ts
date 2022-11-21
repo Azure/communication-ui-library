127382346275755db46e25aaef96ebd69a52077d
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2022-11-21 12:36:30 -0800

    managing threads with an explicit user id instead of managing all kno… (#2531)
    * managing threads with an explicit user id instead of managing all known threads
    
    * removing unnecessary bracket
    
    * making minor changes and verified callWithChat composite
    
    * Change files
    
    * Duplicate change files for beta release
    
    * fixing lint issues
    
    * comment to address why we are returning a 404
    
    * adding server side comment about why we are returning a 404
    
    * addressing PR comments

--- samples/Server/src/routes/addUser.ts
+++ samples/Server/src/routes/addUser.ts
@@ -2,9 +2,10 @@
 // Licensed under the MIT license.
 
 import { ChatClient } from '@azure/communication-chat';
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 import * as express from 'express';
 import { getEndpoint } from '../lib/envHelper';
-import { threadIdToModeratorCredentialMap } from '../lib/chat/threadIdToModeratorTokenMap';
+import { getAdminUser, getToken } from '../lib/identityClient';
 
 const router = express.Router();
 interface AddUserParam {
@@ -26,20 +27,32 @@ interface AddUserParam {
 router.post('/:threadId', async function (req, res, next) {
   const addUserParam: AddUserParam = req.body;
   const threadId = req.params['threadId'];
-  const moderatorCredential = threadIdToModeratorCredentialMap.get(threadId);
 
-  const chatClient = new ChatClient(getEndpoint(), moderatorCredential);
+  // create a user from the adminUserId and create a credential around that
+  const credential = new AzureCommunicationTokenCredential({
+    tokenRefresher: async () => (await getToken(getAdminUser(), ['chat', 'voip'])).token,
+    refreshProactively: true
+  });
+
+  const chatClient = new ChatClient(getEndpoint(), credential);
   const chatThreadClient = await chatClient.getChatThreadClient(threadId);
 
-  await chatThreadClient.addParticipants({
-    participants: [
-      {
-        id: { communicationUserId: addUserParam.Id },
-        displayName: addUserParam.DisplayName
-      }
-    ]
-  });
-  res.sendStatus(201);
+  try {
+    await chatThreadClient.addParticipants({
+      participants: [
+        {
+          id: { communicationUserId: addUserParam.Id },
+          displayName: addUserParam.DisplayName
+        }
+      ]
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    // we will return a 404 if the thread to join is not accessible by the server user.
+    // The server user needs to be in the thread in order to add someone.
+    // So we are returning back that we can't find the thread to add the client user to.
+    res.sendStatus(404);
+  }
 });
 
 export default router;
