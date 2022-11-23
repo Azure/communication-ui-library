ea3127765c80a816f547e835f7ee1141f23f83bb
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-11-23 21:10:34 +0000

    fix cc issues

--- packages/react-composites/src/composites/CallComposite/pages/UnsupportedBrowser.tsx
+++ packages/react-composites/src/composites/CallComposite/pages/UnsupportedBrowser.tsx
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/* @conditional-compile-remove(unsupported-browser) */
 import { EnvironmentInfo } from '@azure/communication-calling';
+/* @conditional-compile-remove(unsupported-browser) */
 import { IStackStyles, Stack } from '@fluentui/react';
 /* @conditional-compile-remove(unsupported-browser) */
 import { UnsupportedBrowser, UnsupportedBrowserVersion } from '@internal/react-components';
@@ -14,6 +16,7 @@ import { useLocale } from '../../localization';
  */
 export type UnsupportedBrowserPageProps = {
   onTroubleshootingClick?: () => void;
+  /* @conditional-compile-remove(unsupported-browser) */
   environmentInfo?: EnvironmentInfo;
 };
 
@@ -31,7 +34,9 @@ export const UnsupportedBrowserPage = (props: UnsupportedBrowserPageProps): JSX.
   /* @conditional-compile-remove(unsupported-browser) */
   const unsupportedBrowserVersionStrings = locale.component.strings.UnsupportedBrowserVersion;
 
+  /* @conditional-compile-remove(unsupported-browser) */
   let pageElement: JSX.Element | undefined;
+  /* @conditional-compile-remove(unsupported-browser) */
   if (!environmentInfo?.isSupportedBrowser) {
     pageElement = (
       <UnsupportedBrowser onTroubleshootingClick={onTroubleshootingClick} strings={unsupportedBrowserStrings} />
