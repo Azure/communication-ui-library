00f2c374f3b2bc0c51a0d2dee4e592e517b85991
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2022-12-06 23:11:53 +0000

    add new test for mobile

--- packages/react-composites/tests/browser/call/hermetic/ConfigurationPage.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/ConfigurationPage.test.ts
@@ -3,8 +3,17 @@
 
 import { buildUrlWithMockAdapter, defaultMockCallAdapterState, test } from './fixture';
 import { expect, Page } from '@playwright/test';
-import { dataUiId, stableScreenshot, waitForCallCompositeToLoad, waitForSelector } from '../../common/utils';
+import {
+  dataUiId,
+  isTestProfileDesktop,
+  isTestProfileMobile,
+  stableScreenshot,
+  waitForCallCompositeToLoad,
+  waitForSelector
+} from '../../common/utils';
+import { IDS } from '../../common/constants';
 import type { MockCallAdapterState } from '../../../common';
+import type { DeviceManagerState } from '@internal/calling-stateful-client';
 
 test.describe('Call Composite E2E Configuration Screen Tests', () => {
   test('composite pages load completely', async ({ page, serverUrl }) => {
@@ -33,18 +42,27 @@ test.describe('Call Composite E2E Configuration Screen Tests', () => {
     expect(await stableScreenshot(page)).toMatchSnapshot('call-configuration-page-with-call-details.png');
   });
 
-  test('Configuration screen should show no devices available', async ({ page, serverUrl }) => {
+  test('Configuration screen desktop should show no devices available', async ({ page, serverUrl }, testInfo) => {
+    test.skip(isTestProfileMobile(testInfo));
     const state = defaultMockConfigurationPageState();
-    state.devices = {
-      cameras: [],
-      microphones: [],
-      speakers: [],
-      isSpeakerSelectionAvailable: true,
-      unparentedViews: [],
-      deviceAccess: { audio: true, video: true }
-    };
+    state.devices = deviceManagerWithNoDevicesState();
     await page.goto(buildUrlWithMockAdapter(serverUrl, state));
-    expect(await stableScreenshot(page)).toMatchSnapshot(`call-configuration-page-no-devices.png`);
+    expect(await stableScreenshot(page)).toMatchSnapshot(`call-configuration-page-no-devices-desktop.png`);
+  });
+
+  test.only('Configuration screen mobile buttons disabled because no devices', async ({
+    page,
+    serverUrl
+  }, testInfo) => {
+    test.skip(isTestProfileDesktop(testInfo));
+    const state = defaultMockConfigurationPageState();
+    state.devices = deviceManagerWithNoDevicesState();
+
+    await page.goto(buildUrlWithMockAdapter(serverUrl, state));
+
+    await waitForSelector(page, dataUiId(IDS.configurationScreenDevicesButton));
+
+    expect(await stableScreenshot(page)).toMatchSnapshot(`call-configuration-page-no-devices-mobile.png`);
   });
 });
 
@@ -68,4 +86,15 @@ const stubLocalCameraName = async (page: Page): Promise<void> => {
       element.innerHTML = element.innerHTML.replace(/C:.*?y4m/g, 'Fake Camera');
     }
   });
+};
+
+const deviceManagerWithNoDevicesState = (): DeviceManagerState => {
+  return {
+    cameras: [],
+    microphones: [],
+    speakers: [],
+    isSpeakerSelectionAvailable: true,
+    unparentedViews: [],
+    deviceAccess: { audio: true, video: true }
+  };
 };
