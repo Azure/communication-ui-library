c4903a59d3dc1d25379a324cf9bfe177999082ba
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2022-12-16 12:40:18 -0800

    Add pinnedParticipants props to VideoGallery (#2582)
    * Add pinnedParticipants prop to VideoGallery. Added PinnedParticipantsLayout component
    
    * eslint disable line for unused vars
    
    * move videoGalleryUtils.ts to utils folder and renamed to videoGalleryLayoutUtils.ts
    
    * Handle pinnedParticipants when layout is default
    
    * Change files
    
    * suppress unused setter setPinnedParticipants
    
    * conditional compilation fix
    
    * treat pinnedParticipants prop as a controlled component
    
    * fix conditional-compilation
    
    * trying to add tests
    
    * unit tests for videoGalleryLayoutUtils
    
    * memoized layout props in VideoGallery and cleaned up code
    
    * Refactored common layout props into an interface for extension
    
    * make usePinnedParticipantLayout more readable
    
    * save a dependency
    
    * fix JSDoc for VideoGalleryResponsiveHorizontalGallery
    
    Co-authored-by: carocao-msft <96077406+carocao-msft@users.noreply.github.com>
    Co-authored-by: Anjul Garg <anjulgarg@live.com>

---
+++ packages/react-components/src/components/VideoGallery/Layout.ts
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { VideoGalleryRemoteParticipant } from '../../types';
+import { VideoGalleryStyles } from '../VideoGallery';
+
+/**
+ * Props for a layout component
+ *
+ * @private
+ */
+export interface LayoutProps {
+  /**
+   * Styles for the {@link DefaultLayout}
+   */
+  styles?: Omit<VideoGalleryStyles, 'root'>;
+  /** List of remote video particpants */
+  remoteParticipants?: VideoGalleryRemoteParticipant[];
+  /** Callback to render each remote participant */
+  onRenderRemoteParticipant: (participant: VideoGalleryRemoteParticipant, isVideoParticipant?: boolean) => JSX.Element;
+  /** List of dominant speaker userIds in the order of their dominance. 0th index is the most dominant. */
+  dominantSpeakers?: string[];
+  /** Component that contains local video content */
+  localVideoComponent?: JSX.Element;
+  /** Component that contains screen share content */
+  screenShareComponent?: JSX.Element;
+  /**
+   * Maximum number of participant remote video streams that is rendered.
+   * @defaultValue 4
+   */
+  maxRemoteVideoStreams?: number;
+  /**
+   * Width of parent element
+   */
+  parentWidth?: number;
+}
