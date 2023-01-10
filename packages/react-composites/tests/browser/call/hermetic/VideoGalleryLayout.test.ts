962a7dc3fd58242a8232f1df5ed406897fdfb5af
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2023-01-10 14:44:52 -0800

    UI Snapshot Test - Verify clicking on VideoTile ellipses opens contextual menu in Desktop and test Pin/Unpin (#2638)
    * UI Snapshot Test - Verify clicking on VideoTile ellipses opens contextual menu in Desktop and test Pin/Unpin
    
    * Change files
    
    * Duplicate change files for beta release
    
    * minor change
    
    * minor change
    
    * Change files
    
    * Duplicate change files for beta release
    
    * Update packages/react-composites CallComposite browser test snapshots
    
    * minor change
    
    * Change files
    
    * Duplicate change files for beta release
    
    * Update packages/react-composites CallComposite browser test snapshots
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/react-composites/tests/browser/call/hermetic/VideoGalleryLayout.test.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import {
+  addVideoStream,
+  buildUrlWithMockAdapter,
+  defaultMockCallAdapterState,
+  defaultMockRemoteParticipant,
+  test
+} from './fixture';
+import { expect } from '@playwright/test';
+import { dataUiId, waitForSelector, stableScreenshot, isTestProfileMobile, pageClick } from '../../common/utils';
+import { IDS } from '../../common/constants';
+
+test.describe('VideoGalleryLayout tests', async () => {
+  /* @conditional-compile-remove(pinned-participants) */
+  test('VideoTile pin/unpin the remote participant for Desktop', async ({ page, serverUrl }, testInfo) => {
+    test.skip(isTestProfileMobile(testInfo));
+    const paul = defaultMockRemoteParticipant('Paul Bridges');
+    addVideoStream(paul, true);
+
+    const participants = [paul];
+    const initialState = defaultMockCallAdapterState(participants);
+
+    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState));
+
+    await waitForSelector(page, dataUiId(IDS.videoGallery));
+
+    await waitForSelector(page, dataUiId(IDS.videoTileMoreOptionsButton));
+    await pageClick(page, dataUiId(IDS.videoTileMoreOptionsButton));
+
+    await waitForSelector(page, dataUiId('video-tile-pin-participant-button'));
+    await pageClick(page, dataUiId('video-tile-pin-participant-button'));
+
+    expect(await stableScreenshot(page)).toMatchSnapshot('video-tile-pinned.png');
+
+    await waitForSelector(page, dataUiId(IDS.videoTileMoreOptionsButton));
+    await pageClick(page, dataUiId(IDS.videoTileMoreOptionsButton));
+
+    expect(await stableScreenshot(page)).toMatchSnapshot('video-tile-unpin.png');
+  });
+});
