170894a80253fa081ce09676f10018d9e3453667
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-02-07 06:17:30 +0000

    remove unrealted changes

--- packages/react-composites/tests/common/index.ts
+++ packages/react-composites/tests/common/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
 // This module holds files that are used both by
 // - the test applications in ../app/*
 // - the browser tests in ../browser/*
