cdcf9b60921a0dac693683ab7fbf57a746d99753
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-02-14 04:54:47 +0000

    Fix e2e tests, cant pass custom buttons in args as functions cannot be json stringified

--- packages/react-composites/tests/browser/callwithchat/hermetic/CallControls.test.ts
+++ packages/react-composites/tests/browser/callwithchat/hermetic/CallControls.test.ts
@@ -33,101 +33,18 @@ test.describe('Custom call control options tests', () => {
   });
 
   test('Control bar custom buttons render correctly', async ({ page, serverUrl }) => {
-    const testOptions: CallWithChatCompositeOptions = {
-      callControls: {
-        cameraButton: false,
-        microphoneButton: false,
-        screenShareButton: false,
-        peopleButton: false,
-        chatButton: false,
-        onFetchCustomButtonProps: [
-          () => ({
-            placement: 'primary',
-            iconName: 'MessageEdit',
-            text: 'Primary Btn 1',
-            key: 'primary-button-1',
-            styles: {
-              root: {
-                background: '#ff6600'
-              }
-            }
-          }),
-          () => ({
-            placement: 'primary',
-            iconName: 'MessageEdit',
-            text: 'Primary Btn 2',
-            key: 'primary-button-2',
-            disabled: true
-          }),
-          () => ({
-            placement: 'primary',
-            iconName: 'MessageEdit',
-            text: 'Primary Btn 3',
-            key: 'primary-button-3',
-            showLabel: false
-          }),
-          () => ({
-            placement: 'primary',
-            iconName: 'MessageEdit',
-            text: 'Primary Btn 4',
-            key: 'primary-button-4',
-            disabled: false,
-            showLabel: false
-          }),
-          () => ({
-            placement: 'primary',
-            iconName: 'MessageEdit',
-            text: 'Primary Btn 5',
-            key: 'primary-button-5',
-            disabled: true,
-            showLabel: false
-          }),
-          () => ({
-            placement: 'secondary',
-            iconName: 'MessageRemove',
-            text: 'Secondary Btn 1',
-            key: 'secondary-button-1',
-            disabled: true,
-            showLabel: true
-          }),
-          () => ({
-            placement: 'secondary',
-            iconName: 'MessageRemove',
-            text: 'Secondary Btn 2',
-            key: 'secondary-button-2',
-            disabled: false,
-            showLabel: false
-          }),
-          () => ({
-            placement: 'secondary',
-            iconName: 'MessageRemove',
-            text: 'Secondary Btn 3',
-            key: 'secondary-button-3',
-            disabled: false,
-            showLabel: false
-          }),
-          () => ({
-            placement: 'overflow',
-            iconName: 'SendBoxSend',
-            text: 'Overflow Btn 1',
-            key: 'overflow-button-1',
-            disabled: false,
-            showLabel: false
-          }),
-          () => ({
-            placement: 'overflow',
-            iconName: 'SendBoxSend',
-            text: 'Overflow Btn 2',
-            key: 'overflow-button-2',
-            disabled: false,
-            showLabel: false
-          })
-        ]
-      }
-    };
     const callState = defaultMockCallAdapterState([defaultMockRemoteParticipant('Paul Bridges')]);
     await loadCallPage(page, serverUrl, callState, {
-      customCompositeOptions: JSON.stringify(testOptions)
+      injectCustomButtons: 'true',
+      customCompositeOptions: JSON.stringify({
+        callControls: {
+          cameraButton: false,
+          microphoneButton: false,
+          screenShareButton: false,
+          peopleButton: false,
+          chatButton: false
+        }
+      })
     });
 
     await pageClick(page, dataUiId('call-with-chat-composite-more-button'));
