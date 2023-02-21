44f898d06b37511b9e028da689bfa8a33b3e0eec
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-02-21 18:09:26 +0000

    add component for control bar

--- packages/react-components/src/components/VerticalGallery.tsx
+++ packages/react-components/src/components/VerticalGallery.tsx
@@ -9,13 +9,28 @@ export interface VerticalGalleryStyles extends BaseCustomStyles {
   /** Styles for each video tile in the vertical gallery */
   children?: IStyle;
   /** Styles for the previous button in the verticalGallery control bar */
-  previousButton?: IStyle;
-  /** styles for the next button in the verticalGallery control bar */
-  nextButton?: IStyle;
-  /** Styles for the page counter in the verticalGallery control bar */
-  counter?: IStyle;
+  controlBar?: VerticalGalleryControlBarStyles;
 }
 
+export interface VerticalGalleryControlBarStyles extends BaseCustomStyles {
+  /**
+   * styles for the next button in the VerticalGalleryControlBar
+   */
+  nextButton?: IStyle;
+  /**
+   * Styles for the previous button in the VerticalGalleryControlBar
+   */
+  previousButton?: IStyle;
+  /**
+   * Styles for the counter in the VerticalGalleryControlBar
+   */
+  counter?: IStyle;
+}
+/**
+ * Props for the VerticalGallery component
+ *
+ * @beta
+ */
 export interface VerticalGalleryProps {
   /** Video tiles for the remote participants in the vertical gallery */
   children: React.ReactNode;
@@ -25,6 +40,14 @@ export interface VerticalGalleryProps {
   childrenPerPage?: number;
 }
 
+interface VerticalGalleryControlBarProps {
+  onNextButtonClick?: () => void;
+  onPreviousButtonClick?: () => void;
+  buttonsDisabled?: { next: boolean; previous: boolean };
+  totalPages?: number;
+  styles?: VerticalGalleryControlBarStyles;
+}
+
 /**
  * VerticalGallery is a overflow gallery for participants in the {@link VideoGallery} component. Stacks
  * participants on the Y-axis of the VideoGallery for better use of horizontal space.
@@ -32,5 +55,9 @@ export interface VerticalGalleryProps {
  * @beta
  */
 export const VerticalGallery = (props: VerticalGalleryProps): JSX.Element => {
+  return <></>;
+};
+
+const VerticalGalleryControlBar = (props: VerticalGalleryControlBarProps): JSX.Element => {
   return <></>;
 };
