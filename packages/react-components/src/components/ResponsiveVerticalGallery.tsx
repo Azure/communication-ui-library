e7bb6762a905275a0917bc1fccf45f0c29641746
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-02-22 21:52:58 +0000

    add child calc function

--- packages/react-components/src/components/ResponsiveVerticalGallery.tsx
+++ packages/react-components/src/components/ResponsiveVerticalGallery.tsx
@@ -1,17 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { IStyle } from '@fluentui/react';
+import { IStyle, mergeStyles } from '@fluentui/react';
 import { _convertRemToPx } from '@internal/acs-ui-common';
 import React, { useRef } from 'react';
+import { _useContainerHeight } from './utils/responsive';
 import { VerticalGallery, VerticalGalleryStyles } from './VerticalGallery';
 
 export interface ResponsiveVerticalGalleryProps {
   children: React.ReactNode;
   containerStyles: IStyle;
   verticalGalleryStyles: VerticalGalleryStyles;
-  gapWidthRem: number;
-  buttonWidthRem?: number;
+  gapHeightRem: number;
+  controlBarHeightRem?: number;
 }
 
 /**
@@ -22,11 +23,24 @@ export interface ResponsiveVerticalGalleryProps {
  * @beta
  */
 export const ResponsiveVerticalGallery = (props: ResponsiveVerticalGalleryProps): JSX.Element => {
-  const { children, containerStyles, verticalGalleryStyles, childWidthRem, gapWidthRem, buttonWidthRem } = props;
+  const { children, containerStyles, verticalGalleryStyles, gapHeightRem, controlBarHeightRem } = props;
   const containerRef = useRef<HTMLDivElement>(null);
+  const containerHeight = _useContainerHeight(containerRef);
+
+  const topPadding = containerRef.current ? parseFloat(getComputedStyle(containerRef.current).paddingTop) : 0;
+  const bottomPadding = containerRef.current ? parseFloat(getComputedStyle(containerRef.current).paddingBottom) : 0;
+
+  const childrenPerPage = calculateChildrenPerPage({
+    numberOfChildren: React.Children.count(children),
+    containerHeight: (containerHeight ?? 0) - topPadding - bottomPadding,
+    gapHeightRem,
+    controlBarHeight: controlBarHeightRem ?? 2
+  });
   return (
-    <div ref={containerRef}>
-      <VerticalGallery />
+    <div ref={containerRef} className={mergeStyles(containerStyles)}>
+      <VerticalGallery childrenPerPage={childrenPerPage} styles={verticalGalleryStyles}>
+        {children}
+      </VerticalGallery>
     </div>
   );
 };
@@ -38,12 +52,13 @@ const calculateChildrenPerPage = (args: {
   numberOfChildren: number;
   containerHeight: number;
   gapHeightRem: number;
+  controlBarHeight: number;
 }): number => {
-  const { numberOfChildren, containerHeight, gapHeightRem } = args;
+  const { numberOfChildren, containerHeight, gapHeightRem, controlBarHeight } = args;
 
   const childMinHeightPx = 90;
   const gapHeightPx = _convertRemToPx(gapHeightRem);
-  const controlBarHeightPx = 32;
+  const controlBarHeightPx = _convertRemToPx(controlBarHeight);
 
   /** First check how many children can fit in containerHeight.
    *
@@ -71,4 +86,17 @@ const calculateChildrenPerPage = (args: {
   if (numberOfChildren <= numberOfChildrenInContainer) {
     return numberOfChildrenInContainer;
   }
+
+  /**
+   * For the paginastion we know the container height, the height of the button bar and the 2 times the gap
+   * height, top tile and bottom tile above control bar. So the child space is calculated as:
+   *
+   *      space = height - controlbar - (2 * gap)
+   */
+  const childSpace = containerHeight - controlBarHeightPx - 2 * gapHeightPx;
+  /**
+   * Now that we have the childrenSpace height we can figure out how many Children can fir in the childrenSpace.
+   * childrenSpace = n * childHeightMin + (n - 1) * gapWidth. isolate n and take the floor.
+   */
+  return Math.floor((childSpace + gapHeightPx) / (childMinHeightPx + gapHeightPx));
 };
