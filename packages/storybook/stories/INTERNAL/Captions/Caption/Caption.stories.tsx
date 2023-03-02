daec0bfddf43a590b0cd9e1d84e3d18b46d93816
carocao-msft <carolinecao@microsoft.com>
2023-03-02 09:42:38 -0800

    caption

---
+++ packages/storybook/stories/INTERNAL/Captions/Caption/Caption.stories.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { _Caption } from '@internal/react-components';
+import { Meta } from '@storybook/react/types-6-0';
+import React from 'react';
+import { COMPONENT_FOLDER_PREFIX } from '../../../constants';
+import { hiddenControl } from '../../../controlsUtils';
+
+const CaptionStory = (args): JSX.Element => {
+  return <_Caption userId={args.userId} userName={args.userName} caption={args.caption} />;
+};
+
+// This must be the only named export from this module, and must be named to match the storybook path suffix.
+// This ensures that storybook hoists the story instead of creating a folder with a single entry.
+export const Caption = CaptionStory.bind({});
+
+export default {
+  id: `${COMPONENT_FOLDER_PREFIX}-internal-Caption`,
+  title: `${COMPONENT_FOLDER_PREFIX}/Internal/Captions/Caption`,
+  component: _Caption,
+  argTypes: {
+    userId: { control: 'text', defaultValue: 'abcd' },
+    userName: { control: 'text', defaultValue: 'Caroline' },
+    caption: { control: 'text', defaultValue: 'Hello there' },
+    onRenderAvatar: hiddenControl
+  }
+} as Meta;
