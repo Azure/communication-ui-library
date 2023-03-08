f7dfcc36b39975a3eb6ac70e01662430434978bd
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2023-03-08 23:26:40 +0000

    skip callWithChat overflow gallery tests for mobile platforms

--- packages/react-composites/tests/browser/callwithchat/hermetic/OverflowGallery.test.ts
+++ packages/react-composites/tests/browser/callwithchat/hermetic/OverflowGallery.test.ts
@@ -5,48 +5,50 @@ import { expect } from '@playwright/test';
 import { MockCallAdapterState } from '../../../common';
 import { addVideoStream, defaultMockCallAdapterState, defaultMockRemoteParticipant } from '../../call/hermetic/fixture';
 import { IDS } from '../../common/constants';
-import { dataUiId, isTestProfileDesktop, pageClick, stableScreenshot, waitForSelector } from '../../common/utils';
+import { dataUiId, isTestProfileMobile, pageClick, stableScreenshot, waitForSelector } from '../../common/utils';
 import { loadCallPage, test } from './fixture';
 
-test.describe('Overflow gallery tests', async () => {
+test.describe.only('Overflow gallery tests', async () => {
   test('Overflow gallery should be present when people or chat pane are open', async ({
     page,
     serverUrl
   }, testInfo) => {
+    if (isTestProfileMobile(testInfo)) {
+      test.skip();
+    }
     const initialState = createInitialStateWithManyAudioParticipants();
     await loadCallPage(page, serverUrl, initialState);
 
     await waitForSelector(page, dataUiId(IDS.videoGallery));
     expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-many-participants.png');
 
-    if (isTestProfileDesktop(testInfo)) {
-      await waitForSelector(page, dataUiId('call-with-chat-composite-people-button'));
-      await pageClick(page, dataUiId('call-with-chat-composite-people-button'));
-      expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-people-pane-open.png');
-      await waitForSelector(page, dataUiId('call-with-chat-composite-chat-button'));
-      await pageClick(page, dataUiId('call-with-chat-composite-chat-button'));
-      expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-chat-pane-open.png');
-    }
+    await waitForSelector(page, dataUiId('call-with-chat-composite-people-button'));
+    await pageClick(page, dataUiId('call-with-chat-composite-people-button'));
+    expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-people-pane-open.png');
+    await waitForSelector(page, dataUiId('call-with-chat-composite-chat-button'));
+    await pageClick(page, dataUiId('call-with-chat-composite-chat-button'));
+    expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-chat-pane-open.png');
   });
 
   test('Overflow gallery should be present when people or chat pane are open in right-to-left', async ({
     page,
     serverUrl
   }, testInfo) => {
+    if (isTestProfileMobile(testInfo)) {
+      test.skip();
+    }
     const initialState = createInitialStateWithManyAudioParticipants();
     await loadCallPage(page, serverUrl, initialState, { rtl: 'true' });
 
     await waitForSelector(page, dataUiId(IDS.videoGallery));
     expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-many-participants-rtl.png');
 
-    if (isTestProfileDesktop(testInfo)) {
-      await waitForSelector(page, dataUiId('call-with-chat-composite-people-button'));
-      await pageClick(page, dataUiId('call-with-chat-composite-people-button'));
-      expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-people-pane-open-rtl.png');
-      await waitForSelector(page, dataUiId('call-with-chat-composite-chat-button'));
-      await pageClick(page, dataUiId('call-with-chat-composite-chat-button'));
-      expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-chat-pane-open-rtl.png');
-    }
+    await waitForSelector(page, dataUiId('call-with-chat-composite-people-button'));
+    await pageClick(page, dataUiId('call-with-chat-composite-people-button'));
+    expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-people-pane-open-rtl.png');
+    await waitForSelector(page, dataUiId('call-with-chat-composite-chat-button'));
+    await pageClick(page, dataUiId('call-with-chat-composite-chat-button'));
+    expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-chat-pane-open-rtl.png');
   });
 });
 
