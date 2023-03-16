1833fc36d95453c5f4d6a43a630863f7c9231fe9
carocao-msft <carolinecao@microsoft.com>
2023-03-16 11:13:09 -0700

    captions composite

---
+++ packages/react-components/src/components/styles/CaptionsBanner.style.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { mergeStyles } from '@fluentui/react';
+import { _pxToRem } from '@internal/acs-ui-common';
+
+export const gridContainerClassName = mergeStyles({
+  overflowY: 'scroll',
+  overflowX: 'hidden',
+  width: '100%',
+  height: _pxToRem(60),
+  display: 'grid',
+  gridTemplateColumns: '20% 80%',
+  alignItems: 'stretch',
+  columnGap: _pxToRem(16),
+  padding: _pxToRem(8)
+});
+
+export const displayNameClassName = mergeStyles({
+  fontWeight: 600,
+  fontSize: _pxToRem(12),
+  lineHeight: _pxToRem(30)
+});
+
+export const captionClassName = mergeStyles({
+  fontWeight: 400,
+  fontSize: _pxToRem(16),
+  lineHeight: _pxToRem(30)
+});
