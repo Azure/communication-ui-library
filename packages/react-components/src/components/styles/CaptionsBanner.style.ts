b145e2a33ece1f8d642b6efb61884cd95b6ddc4e
carocao-msft <carolinecao@microsoft.com>
2023-03-16 11:55:06 -0700

    captions composite

--- packages/react-components/src/components/styles/CaptionsBanner.style.ts
+++ packages/react-components/src/components/styles/CaptionsBanner.style.ts
@@ -4,6 +4,9 @@
 import { mergeStyles } from '@fluentui/react';
 import { _pxToRem } from '@internal/acs-ui-common';
 
+/**
+ * @private
+ */
 export const gridContainerClassName = mergeStyles({
   overflowY: 'scroll',
   overflowX: 'hidden',
@@ -16,12 +19,18 @@ export const gridContainerClassName = mergeStyles({
   padding: _pxToRem(8)
 });
 
+/**
+ * @private
+ */
 export const displayNameClassName = mergeStyles({
   fontWeight: 600,
   fontSize: _pxToRem(12),
   lineHeight: _pxToRem(30)
 });
 
+/**
+ * @private
+ */
 export const captionClassName = mergeStyles({
   fontWeight: 400,
   fontSize: _pxToRem(16),
