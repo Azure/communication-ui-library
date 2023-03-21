4988f54d80bc299cb3b7cdcba4da9a214806a93a
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2023-03-21 14:04:23 -0700

    add unit test for blocked message

--- packages/react-components/src/components/MessageThread.test.tsx
+++ packages/react-components/src/components/MessageThread.test.tsx
@@ -4,10 +4,12 @@
 import React from 'react';
 import { MessageThread } from './MessageThread';
 import { ChatMessage } from '../types';
+/* @conditional-compile-remove(dlp) */
+import { BlockedMessage } from '../types';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mountWithLocalization, createTestLocale } from './utils/testUtils';
-/* @conditional-compile-remove(date-time-customization) */
+/* @conditional-compile-remove(date-time-customization) @conditional-compile-remove(dlp) */
 import { COMPONENT_LOCALE_EN_US } from '../localization/locales';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -120,5 +122,28 @@ describe('onDisplayDateTimeString passed through messagethread should overwrite 
       testLocale
     );
     expect(component.text()).toContain('Yesterday');
+  });
+});
+
+/* @conditional-compile-remove(dlp) */
+describe('Message blocked should display default blocked text correctly', () => {
+  test('Should locale string for default message blocked by policy"', async () => {
+    const testLocale = createTestLocale({ messageThread: { yesterday: Math.random().toString() } });
+    const sampleMessage: BlockedMessage = {
+      messageType: 'blocked',
+
+      senderId: 'user3',
+      senderDisplayName: 'Sam Fisher',
+      messageId: Math.random().toString(),
+      createdOn: twentyFourHoursAgo(),
+      mine: false,
+      attached: false,
+      contentType: 'text'
+    };
+    const component = mountWithLocalization(
+      <MessageThread userId="user1" messages={[sampleMessage]} showMessageDate={true} />,
+      testLocale
+    );
+    expect(component.text()).toContain(testLocale.strings.messageThread.blockedContentText);
   });
 });
