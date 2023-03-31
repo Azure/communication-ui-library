2cb4fd669ee5890e30466fbaa34aa6d3efb433f6
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2023-03-31 19:33:14 +0000

    Revert "[InlineImages] Update FileAdapter to include new method to download authenticated attachments from teams (#2827)"
    This reverts commit d246999b75f8e1934d4e5759b824e3dfab6b7cc4.

--- common/config/babel/.babelrc.js
+++ common/config/babel/.babelrc.js
@@ -36,8 +36,6 @@ process.env['COMMUNICATION_REACT_FLAVOR'] === 'stable' &&
         'teams-adhoc-call',
         // Joining calls using teams token
         'teams-identity-support',
-        // Chat teams interop to display images in chat messages
-        'teams-inline-images',
         'unsupported-browser',
         // Support Calling SDK isReceiving flag, shows a loading spinner on the video tile when isAvailable is true but isReceiving is false
         'video-stream-is-receiving-flag',
