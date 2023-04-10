560e26a366564ec477ee19462aa47a2fd1a729e0
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2023-04-10 14:03:33 -0700

    Update error message for unsupported thread type

--- packages/react-composites/src/composites/CallWithChatComposite/adapter/parseTeamsUrl.ts
+++ packages/react-composites/src/composites/CallWithChatComposite/adapter/parseTeamsUrl.ts
@@ -2,6 +2,17 @@
 // Licensed under the MIT license.
 
 /**
+ * @private
+ */
+const unsupportedThreadType = ['@thread.tacv2', '@thread.skype'];
+
+/**
+ * @private
+ */
+const TEAMS_LIMITATION_LEARN_MORE =
+  'https://learn.microsoft.com/en-us/azure/communication-services/concepts/join-teams-meeting#limitations-and-known-issues';
+
+/**
  * Get chat thread from a teams url.
  * As per documented on the Chat SDK: https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/chat/meeting-interop?pivots=platform-web#get-a-teams-meeting-chat-thread-for-a-communication-services-user
  *
@@ -14,8 +25,10 @@ export const getChatThreadFromTeamsLink = (teamsMeetingLink: string): string => 
   threadId = decodeURIComponent(threadId);
   // Extract just the chat guid from the link, stripping away the call locator ID
   threadId = threadId.split(/^(.*?@thread\.v2)/gm)[1];
-
   if (!threadId || threadId.length === 0) {
+    if (unsupportedThreadType.some((t) => teamsMeetingLink.includes(t))) {
+      throw new Error(`Teams Channel Meetings are not currently supported, read more ${TEAMS_LIMITATION_LEARN_MORE}`);
+    }
     throw new Error('Could not get chat thread from teams link');
   }
 
