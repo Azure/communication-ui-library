8f76e55857a5eabc454ceb98f9b554f2c59a306e
carocao-msft <carolinecao@microsoft.com>
2023-04-11 01:46:28 -0700

    cc

--- packages/react-composites/src/composites/common/CaptionsSettingModal.tsx
+++ packages/react-composites/src/composites/common/CaptionsSettingModal.tsx
@@ -2,18 +2,21 @@
 // Licensed under the MIT license.
 import React from 'react';
 import { _CaptionsSettingModal } from '@internal/react-components';
-import { usePropsFor } from '../CallComposite/hooks/usePropsFor';
+import { useAdaptedSelector } from '../CallComposite/hooks/useAdaptedSelector';
+import { useHandlers } from '../CallComposite/hooks/useHandlers';
 
 /** @private */
 export const CaptionsSettingModal = (props: {
   showCaptionsSettingModal: boolean;
   onDismissCaptionsSetting: () => void;
 }): JSX.Element => {
-  const captionsSettingModalProps = usePropsFor(_CaptionsSettingModal);
+  const captionsSettingModalProps = useAdaptedSelector(_CaptionsSettingModal);
+  const handlers = useHandlers(_CaptionsSettingModal);
 
   return (
     <_CaptionsSettingModal
       {...captionsSettingModalProps}
+      {...handlers}
       showModal={props.showCaptionsSettingModal}
       onDismissCaptionsSetting={props.onDismissCaptionsSetting}
     />
