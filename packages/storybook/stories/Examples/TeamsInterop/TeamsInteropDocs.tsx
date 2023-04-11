3afad359e726668bb1c4379166184f0f94eaa1d2
John <109105353+jpeng-ms@users.noreply.github.com>
2023-04-11 15:34:07 -0700

    updated snippet link

--- packages/storybook/stories/Examples/TeamsInterop/TeamsInteropDocs.tsx
+++ packages/storybook/stories/Examples/TeamsInterop/TeamsInteropDocs.tsx
@@ -2,11 +2,10 @@
 // Licensed under the MIT license.
 import { Description, Heading, Subheading, Source, Title } from '@storybook/addon-docs';
 import React from 'react';
+const InlineImageText = require('!!raw-loader!../../MessageThread/snippets/WithInlineImageMessage.snippet.tsx').default;
 const CallComponentText = require('!!raw-loader!./snippets/CallComponent.snippet.tsx').default;
 const ComplianceBannerText = require('!!raw-loader!./snippets/ComplianceBanner.snippet.tsx').default;
 const LobbyComponentText = require('!!raw-loader!./snippets/Lobby.snippet.tsx').default;
-const InlineImageText = require('!!raw-loader!./snippets/InlineImage.snippet.tsx').default;
-
 
 export const getDocs: () => JSX.Element = () => {
   return (
@@ -30,7 +29,9 @@ export const getDocs: () => JSX.Element = () => {
       <Heading>Create a Lobby Component</Heading>
       <Source code={LobbyComponentText} />
       <Title>Inline Image Support</Title>
-      <Description>In the Chat scenarios where Teams user sends inline image to ACS user, there are few additional setup needed.</Description>
+      <Description>
+        In the Chat scenarios where Teams user sends inline image to ACS user, there are few additional setup needed.
+      </Description>
       <Heading>Enable Inline Image Support</Heading>
       <Source code={InlineImageText} />
     </>
