27921108ef5dd819945f82ea7f165f9ac6d775d2
John <109105353+jpeng-ms@users.noreply.github.com>
2023-04-12 13:23:05 -0700

    Update InlineImageAdapter.snippet.tsx

--- packages/storybook/stories/Examples/TeamsInterop/snippets/InlineImageAdapter.snippet.tsx
+++ packages/storybook/stories/Examples/TeamsInterop/snippets/InlineImageAdapter.snippet.tsx
@@ -1,18 +1,10 @@
-import { AbortSignalLike } from '@azure/abort-controller';
-import {
-  CommunicationUserIdentifier,
-  AzureCommunicationTokenCredential,
-  CommunicationTokenRefreshOptions
-} from '@azure/communication-common';
-
+import { CommunicationUserIdentifier, AzureCommunicationTokenCredential } from '@azure/communication-common';
 import {
   ChatAdapter,
   ChatComposite,
   fromFlatCommunicationIdentifier,
-  toFlatCommunicationIdentifier,
   useAzureCommunicationChatAdapter
 } from '@azure/communication-react';
-
 import { Stack } from '@fluentui/react';
 import React, { useCallback, useEffect, useMemo } from 'react';
 
@@ -25,25 +17,15 @@ interface ChatScreenProps {
   endChatHandler(isParticipantRemoved: boolean): void;
 }
 
-export const ChatScreen = (props: ChatScreenProps): JSX.Element => {
-  const { displayName, endpointUrl, threadId, token, userId, endChatHandler } = props;
+const ChatScreen = (props: ChatScreenProps): JSX.Element => {
+  const { displayName, endpointUrl, threadId, token, userId } = props;
 
-  const adapterAfterCreate = useCallback(
-    async (adapter: ChatAdapter): Promise<ChatAdapter> => {
-      adapter.on('participantsRemoved', (listener) => {
-        const removedParticipantIds = listener.participantsRemoved.map((p) => toFlatCommunicationIdentifier(p.id));
-        if (removedParticipantIds.includes(userId)) {
-          const removedBy = toFlatCommunicationIdentifier(listener.removedBy.id);
-          endChatHandler(removedBy !== userId);
-        }
-      });
-      adapter.on('error', (e) => {
-        console.error(e);
-      });
-      return adapter;
-    },
-    [endChatHandler, userId]
-  );
+  const adapterAfterCreate = useCallback(async (adapter: ChatAdapter): Promise<ChatAdapter> => {
+    adapter.on('error', (e) => {
+      console.error(e);
+    });
+    return adapter;
+  }, []);
 
   const adapterArgs = useMemo(
     () => ({
@@ -51,7 +33,7 @@ export const ChatScreen = (props: ChatScreenProps): JSX.Element => {
       userId: fromFlatCommunicationIdentifier(userId) as CommunicationUserIdentifier,
       displayName,
       // the credential is used to fetch preview images
-      credential: createAutoRefreshingCredential(userId, token),
+      credential: new AzureCommunicationTokenCredential(token),
       threadId
     }),
 
@@ -82,32 +64,4 @@ export const ChatScreen = (props: ChatScreenProps): JSX.Element => {
     );
   }
   return <>Initializing...</>;
-};
-
-const postRefreshTokenParameters = {
-  method: 'POST'
-};
-
-/**
- * Create credentials that auto-refresh asynchronously.
- */
-export const createAutoRefreshingCredential = (userId: string, token: string): AzureCommunicationTokenCredential => {
-  const options: CommunicationTokenRefreshOptions = {
-    token: token,
-    tokenRefresher: refreshTokenAsync(userId),
-    refreshProactively: true
-  };
-
-  return new AzureCommunicationTokenCredential(options);
-};
-
-const refreshTokenAsync = (userIdentity: string): ((abortSignal?: AbortSignalLike) => Promise<string>) => {
-  return async (): Promise<string> => {
-    const response = await fetch(`/refreshToken/${userIdentity}`, postRefreshTokenParameters);
-    if (response.ok) {
-      return (await response.json()).token;
-    } else {
-      throw new Error('could not refresh token');
-    }
-  };
 };
