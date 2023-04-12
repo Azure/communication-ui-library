510a5b81756fa6761cdc3e18b13f0e4c3a65831e
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-04-12 14:43:32 -0700

    Unblock PR failures by updating user agent used in e2e tests (#2922)

--- samples/tests/playwright.config.ts
+++ samples/tests/playwright.config.ts
@@ -38,8 +38,7 @@ const config: PlaywrightTestConfig = {
         },
         // using chromium useragent from windows to avoid breaking the unsupportedBrowser page
         contextOptions: {
-          userAgent:
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42'
+          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0'
         }
       }
     }
