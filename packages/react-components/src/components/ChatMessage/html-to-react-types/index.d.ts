5941c2380b676376fc0d74f99aba1732def8f6a0
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-04-12 11:24:39 -0700

    Type definitions for the html-to-react library

---
+++ packages/react-components/src/components/ChatMessage/html-to-react-types/index.d.ts
@@ -0,0 +1,8 @@
+declare module 'html-to-react' {
+  import Parser from './lib/parser';
+  import ProcessingInstructions from './lib/processing-instructions';
+  import IsValidNodeDefinitions from './lib/is-valid-node-definitions';
+  import ProcessNodeDefinitions from './lib/process-node-definitions';
+
+  export { Parser, ProcessingInstructions, IsValidNodeDefinitions, ProcessNodeDefinitions };
+}
---
+++ packages/react-components/src/components/ChatMessage/html-to-react-types/utils.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function createElement(
+  node: HTMLElement,
+  index: number,
+  data: any,
+  children: HTMLElement[]
+): ReturnType<typeof React.createElement>;
