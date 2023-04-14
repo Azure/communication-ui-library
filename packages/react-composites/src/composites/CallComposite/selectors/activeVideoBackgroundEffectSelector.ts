3b4145f0a66879d88d9cfd2cee63dc77efb8647b
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2023-04-14 17:56:29 +0000

    minor change

--- packages/react-composites/src/composites/CallComposite/selectors/activeVideoBackgroundEffectSelector.ts
+++ packages/react-composites/src/composites/CallComposite/selectors/activeVideoBackgroundEffectSelector.ts
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/* @conditional-compile-remove(video-background-effects) */
 import * as reselect from 'reselect';
+/* @conditional-compile-remove(video-background-effects) */
 import { getSelectedVideoEffect } from './baseSelectors';
+/* @conditional-compile-remove(video-background-effects) */
 import { SelectedVideoBackgroundEffect } from '../adapter/CallAdapter';
 
+/* @conditional-compile-remove(video-background-effects) */
 /**
  * @private
  */
