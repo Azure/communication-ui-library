68efd616b76315e7c3e3f6819dfc47ec7bf6e548
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-04-18 13:36:23 -0700

    Update storybook

--- packages/storybook/stories/SendBox/snippets/Mentions.snippet.tsx
+++ packages/storybook/stories/SendBox/snippets/Mentions.snippet.tsx
@@ -30,7 +30,7 @@ const trigger = '@';
 export const MentionsExample: () => JSX.Element = () => (
   <FluentThemeProvider>
     <div style={{ width: '31.25rem', height: '20rem' }}>
-      {/* <div style={{ width: '31.25rem', height: '17rem' }} /> Spacer for layout */}
+      <div style={{ width: '31.25rem', height: '17rem' }} /> {/*Spacer for layout*/}
       <SendBox
         onSendMessage={async () => {
           return;
