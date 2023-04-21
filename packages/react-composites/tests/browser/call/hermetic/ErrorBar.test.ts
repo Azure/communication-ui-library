39a3894b9c03bee1ad05136f23e999d76fabce03
Porter Nan <jinan@microsoft.com>
2023-04-21 01:34:44 -0700

    Set different time to errors

--- packages/react-composites/tests/browser/call/hermetic/ErrorBar.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/ErrorBar.test.ts
@@ -28,14 +28,14 @@ test.describe('Error bar tests', async () => {
     const initialState = defaultMockCallAdapterState();
     initialState.latestErrors = {
       'Call.unmute': {
-        timestamp: new Date(10000),
+        timestamp: new Date(20000),
         name: 'Failure to unmute',
         message: 'Could not unmute',
         target: 'Call.unmute',
         innerError: new Error('Inner error of failure to unmute')
       },
       'Call.stopVideo': {
-        timestamp: new Date(10000),
+        timestamp: new Date(30000),
         name: 'Failure to stop video',
         message: 'Could not stop video',
         target: 'Call.stopVideo',
