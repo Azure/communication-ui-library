896a2b1ba353034cbf2c172770bd0f7c5a4237d6
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-04-26 11:27:16 -0700

    Disable warning for external type override

--- packages/react-components/src/components/ChatMessage/MentionSuggestionRenderer.tsx
+++ packages/react-components/src/components/ChatMessage/MentionSuggestionRenderer.tsx
@@ -20,6 +20,7 @@ export const defaultMentionSuggestionRenderer = (suggestion: Mention): JSX.Eleme
 // Add the custom HTML tag to the JSX.IntrinsicElements interface
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface IntrinsicElements {
       'msft-mention': React.HTMLAttributes<'msft-mention'> & {
