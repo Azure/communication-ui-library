f3d78053c90083bb3524b33b63f0b5f80d8c7664
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-04-28 15:37:53 -0700

    Remove public API usage of ComponentSlotStyle and redefine type internally (#2996)

--- packages/react-components/src/components/MessageThread.tsx
+++ packages/react-components/src/components/MessageThread.tsx
@@ -39,7 +39,6 @@ import {
   IPersona,
   Theme
 } from '@fluentui/react';
-import { ComponentSlotStyle } from '@fluentui/react-northstar';
 import { LiveAnnouncer } from 'react-aria-live';
 import { delay } from './utils/delay';
 import {
@@ -72,6 +71,7 @@ import { AttachmentDownloadResult } from './FileDownloadCards';
 import { useTheme } from '../theming';
 /* @conditional-compile-remove(at-mention) */
 import { AtMentionOptions } from './AtMentionFlyout';
+import { ComponentSlotStyle } from '../types/ComponentSlotStyle';
 
 const isMessageSame = (first: ChatMessage, second: ChatMessage): boolean => {
   return (
