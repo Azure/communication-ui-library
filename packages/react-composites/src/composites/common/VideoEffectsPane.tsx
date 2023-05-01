697d37906884329b645bf4d5d8895edfd8e6c369
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2023-05-01 19:02:41 +0000

    minor change

--- packages/react-composites/src/composites/common/VideoEffectsPane.tsx
+++ packages/react-composites/src/composites/common/VideoEffectsPane.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import React, { useState } from 'react';
+import React from 'react';
+/* @conditional-compile-remove(video-background-effects) */
+import { useState } from 'react';
 /* @conditional-compile-remove(video-background-effects) */
 import { useCallback, useMemo } from 'react';
 /* @conditional-compile-remove(video-background-effects) */
