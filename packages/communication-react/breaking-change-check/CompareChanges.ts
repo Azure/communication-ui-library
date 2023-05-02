463138a56c86eecc32901c8e89c76cebfe979913
Porter Nan <jiangnanhello@live.com>
2023-05-02 07:34:20 +0000

    Add breaking change check pipeline

---
+++ packages/communication-react/breaking-change-check/CompareChanges.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+export type CheckBreakingChanges<OLD, NEW extends OLD> = NEW;
+
+export type ExcludeList =
+  | 'GetCallingSelector'
+  | 'getCallingSelector'
+  | 'getChatSelector'
+  | 'GetChatSelector'
+  | 'usePropsFor';
---
+++ packages/communication-react/breaking-change-check/checkBreakingChanges.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { CheckBreakingChanges, ExcludeList } from './CompareChanges';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export type BreakingChangeTest = CheckBreakingChanges<
+  Omit<typeof import('../dist/communication-react'), ExcludeList>,
+  typeof import('./snapshots/communication-react')
+>;
