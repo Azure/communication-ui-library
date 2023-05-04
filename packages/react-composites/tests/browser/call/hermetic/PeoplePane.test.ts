fe79206dcfde1760f426d0b0954acd6b5141dd40
carocao-msft <carolinecao@microsoft.com>
2023-05-04 14:17:28 -0700

    test

--- packages/react-composites/tests/browser/call/hermetic/PeoplePane.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/PeoplePane.test.ts
@@ -172,6 +172,26 @@ test.describe('Participant list side pane tests', () => {
 
     expect(await stableScreenshot(page)).toMatchSnapshot(`participant-menu-item-flyout.png`);
   });
+
+  test('participant list opens and do not overlap with error bar', async ({ page, serverUrl }) => {
+    const initialState = participantListInitialState();
+    initialState.latestErrors = {
+      'Call.startVideo': {
+        // Add 24 hours to current time to ensure the error is not dismissed by default
+        timestamp: new Date(Date.now() + 3600 * 1000 * 24),
+        name: 'Failure to start video',
+        message: 'Could not start video',
+        target: 'Call.startVideo',
+        innerError: new Error('Inner error of failure to start video')
+      }
+    };
+    await page.goto(buildUrlWithMockAdapter(serverUrl, initialState, { callInvitationUrl: 'testUrl' }));
+    await pageClick(page, dataUiId('call-composite-participants-button'));
+    const buttonCallOut = await waitForSelector(page, '.ms-Callout');
+    // This will ensure no animation is happening for the callout
+    await buttonCallOut.waitForElementState('stable');
+    expect(await stableScreenshot(page)).toMatchSnapshot('people-pane-with-error-bar.png');
+  });
 });
 
 test.describe('Participant list full screen pane with drawer tests', () => {
