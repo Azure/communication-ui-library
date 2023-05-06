bcd85735438a15e0cabbdbcdb8c27f46b45d1f21
Alex Kwan <alkwa@microsoft.com>
2023-05-05 17:07:03 -0700

    updating conditional flags on these lines for a stable build

--- samples/Calling/src/app/styles/HomeScreen.styles.ts
+++ samples/Calling/src/app/styles/HomeScreen.styles.ts
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { FontWeights, IStackTokens, IStyle, mergeStyles } from '@fluentui/react';
+import { IStackTokens, mergeStyles } from '@fluentui/react';
 /* @conditional-compile-remove(PSTN-calls) */
-import { IStackStyles } from '@fluentui/react';
+import { FontWeights, IStackStyles, IStyle } from '@fluentui/react';
 
 export const imgStyle = mergeStyles({
   width: '16.5rem',
