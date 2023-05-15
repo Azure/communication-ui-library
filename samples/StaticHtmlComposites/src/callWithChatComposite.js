30ecedc7fd1264d8e58201ff9a95e2834f234d5d
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-05-15 10:43:25 -0700

    Bump dependencies to react 18 (#3045)

--- samples/StaticHtmlComposites/src/callWithChatComposite.js
+++ samples/StaticHtmlComposites/src/callWithChatComposite.js
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 import { CallWithChatComposite, createAzureCommunicationCallWithChatAdapter } from '@azure/communication-react';
 
@@ -17,6 +17,11 @@ export const loadCallWithChatComposite = async function (args, htmlElement, prop
     endpoint: endpoint,
     locator: locator
   });
-  ReactDOM.render(React.createElement(CallWithChatComposite, { ...props, adapter }, null), htmlElement);
+
+  if (!htmlElement) {
+    throw new Error('Failed to find the root element');
+  }
+
+  createRoot(htmlElement).render(React.createElement(CallWithChatComposite, { ...props, adapter }, null));
   return adapter;
 };
