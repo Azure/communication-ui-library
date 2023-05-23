0ce962136c004478b54eb6ba167404a52d2ba671
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-05-23 18:06:28 +0000

    create test file

---
+++ packages/react-composites/tests/browser/call/hermetic/LocalVideoTile.test.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { buildUrlWithMockAdapter, defaultMockCallAdapterState, test } from './fixture';
+
+test.describe('Call Composite E2E Configuration Screen Tests', () => {});
