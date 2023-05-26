ae96fb965560790da22b7c45d3af81a1e54baa30
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2023-05-25 17:00:36 -0700

    Fixed start captions console error and add loading screen  (#3021)
    * start captions error fix
    
    * Change files
    
    * Duplicate change files for beta release
    
    * Update packages/react-composites CallWithChatComposite browser test snapshots
    
    * Update packages/react-composites CallComposite browser test snapshots
    
    * start captions error fix
    
    * tests
    
    * Update packages/react-composites CallWithChatComposite browser test snapshots
    
    * fixlint
    
    * fixlint
    
    * Update packages/react-composites CallComposite browser test snapshots
    
    * name change
    
    * test
    
    * Update packages/react-composites CallComposite browser test snapshots
    
    * captions
    
    * build fix
    
    * uprdate
    
    ---------
    
    Signed-off-by: carocao-msft <96077406+carocao-msft@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>

--- packages/react-composites/src/composites/CallComposite/Strings.tsx
+++ packages/react-composites/src/composites/CallComposite/Strings.tsx
@@ -511,4 +511,9 @@ export interface CallCompositeStrings {
    * list of key value pairs that pairs language code to language names
    */
   captionsAvailableLanguageStrings?: CaptionsAvailableLanguageStrings;
+  /* @conditional-compile-remove(close-captions) */
+  /**
+   * captions banner loading spinner label
+   */
+  captionsBannerSpinnerText?: string;
 }
