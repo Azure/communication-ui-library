47746fe84f89ee8f4797a776df7b0f0bc2973aa3
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-06-02 00:45:51 +0000

    hook guards when callagent is being made in SDK

--- packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
+++ packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
@@ -1211,6 +1211,7 @@ const useAzureCommunicationCallAdapterGeneric = <
   const [adapter, setAdapter] = useState<Adapter | undefined>(undefined);
   // Ref needed for cleanup to access the old adapter created asynchronously.
   const adapterRef = useRef<Adapter | undefined>(undefined);
+  const creatingAdapterRef = useRef<boolean>(false);
 
   const afterCreateRef = useRef<((adapter: Adapter) => Promise<Adapter>) | undefined>(undefined);
   const beforeDisposeRef = useRef<((adapter: Adapter) => Promise<void>) | undefined>(undefined);
@@ -1243,7 +1244,6 @@ const useAzureCommunicationCallAdapterGeneric = <
           adapterRef.current.dispose();
           adapterRef.current = undefined;
         }
-
         let newAdapter: Adapter | undefined = undefined;
         if (adapterKind === 'AzureCommunication') {
           // This is just the type check to ensure that displayName is defined.
@@ -1255,6 +1255,11 @@ const useAzureCommunicationCallAdapterGeneric = <
           if (options && !('roleHint' in options)) {
             throw new Error('Unreachable code, provided a options without roleHint.');
           }
+          if (creatingAdapterRef.current) {
+            console.warn('Adapter is already being created, skipping creation.');
+            return;
+          }
+          creatingAdapterRef.current = true;
           newAdapter = (await createAzureCommunicationCallAdapter({
             credential,
             displayName: displayName,
@@ -1264,6 +1269,11 @@ const useAzureCommunicationCallAdapterGeneric = <
             /* @conditional-compile-remove(rooms) */ options
           })) as Adapter;
         } else if (adapterKind === 'Teams') {
+          if (creatingAdapterRef.current) {
+            console.warn('Adapter is already being created, skipping creation.');
+            return;
+          }
+          creatingAdapterRef.current = true;
           /* @conditional-compile-remove(teams-identity-support) */
           newAdapter = (await createTeamsCallAdapter({
             credential,
@@ -1283,17 +1293,14 @@ const useAzureCommunicationCallAdapterGeneric = <
           newAdapter = await afterCreateRef.current(newAdapter);
         }
         adapterRef.current = newAdapter;
+        creatingAdapterRef.current = false;
         setAdapter(newAdapter);
       })();
     },
     // Explicitly list all arguments so that caller doesn't have to memoize the `args` object.
     [
-      adapterRef,
-      afterCreateRef,
-      beforeDisposeRef,
       credential,
       locator,
-      adapterKind,
       userId,
       displayName,
       /* @conditional-compile-remove(PSTN-calls) */
