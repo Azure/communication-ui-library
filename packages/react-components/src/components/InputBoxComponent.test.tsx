1a3134eef0e90d1530d97f8e8996167c3ad62dc8
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-06-05 09:25:30 -0700

    Added a test with a custom trigger

--- packages/react-components/src/components/InputBoxComponent.test.tsx
+++ packages/react-components/src/components/InputBoxComponent.test.tsx
@@ -118,6 +118,69 @@ describe('InputBoxComponent should show mention popover', () => {
 });
 
 /* @conditional-compile-remove(mention) */
+describe('InputBoxComponent should show mention popover for a custom trigger', () => {
+  const suggestions: Mention[] = [
+    {
+      id: '1',
+      displayText: 'Test User1'
+    },
+    {
+      id: 'everyone',
+      displayText: 'Everyone'
+    }
+  ];
+  const trigger = '***';
+  const renderInputBoxComponent = (): void => {
+    render(
+      <InputBoxComponent
+        inlineChildren={true}
+        textValue=""
+        placeholderText="Enter a message"
+        onChange={() => {
+          // onChange function
+        }}
+        maxLength={1000}
+        mentionLookupOptions={{
+          trigger,
+          onQueryUpdated: async (query: string) => {
+            const filtered = suggestions.filter((suggestion) => {
+              return suggestion.displayText.toLocaleLowerCase().startsWith(query.toLocaleLowerCase());
+            });
+            return Promise.resolve(filtered);
+          }
+        }}
+      >
+        <div></div>
+      </InputBoxComponent>
+    );
+  };
+
+  const checkExpectedSuggestions = async (): Promise<void> => {
+    for (let i = 0; i < suggestions.length; i++) {
+      // Check that all suggestions are presented
+      const contextMenuItem = await screen.findByText(suggestions[i].displayText);
+      expect(contextMenuItem.classList.contains('ms-Persona-primaryText')).toBe(true);
+    }
+  };
+
+  test('Show mention popover when trigger is in the beginning of the input box', async () => {
+    renderInputBoxComponent();
+    // Find the input field
+    const input = await screen.findByPlaceholderText('Enter a message');
+    act(() => {
+      // Focus on the input field
+      input.focus();
+    });
+    await waitFor(async () => {
+      // Type into the input field
+      await userEvent.keyboard(trigger);
+    });
+    expect((input as HTMLInputElement).value).toBe(trigger);
+    await checkExpectedSuggestions();
+  });
+});
+
+/* @conditional-compile-remove(mention) */
 describe('InputBoxComponent should not show mention popover', () => {
   test("Shouldn't show mention popover when there no space before trigger or trigger is not in the beginning of the input box", async () => {
     const suggestions: Mention[] = [
