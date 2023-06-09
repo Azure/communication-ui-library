b9707cdf8b79b42fff0d75fdc463dbd3574d152f
Porter Nan <jiangnanhello@live.com>
2023-06-09 21:01:37 +0000

    Add comment to exclude list

--- packages/communication-react/breaking-change-check/CompareChanges.ts
+++ packages/communication-react/breaking-change-check/CompareChanges.ts
@@ -2,6 +2,9 @@
 // Licensed under the MIT license.
 export type CheckBreakingChanges<OLD, NEW extends OLD> = NEW;
 
+// These are the list we intentionally make breaking changes,
+// we will still check removed property and property names/types compatibility,
+// But it will skip the check for new properties in the interface (and we don't consider them as breaking change)
 export type ExcludeList =
   | 'GetCallingSelector'
   | 'getCallingSelector'
