cefa2f37056d2fd4a7335f24ddb6407f4d2c8a5b
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-06-19 13:17:40 -0700

    Simplify version detection

--- packages/storybook/scripts/common.mjs
+++ packages/storybook/scripts/common.mjs
@@ -2,14 +2,11 @@
 // Licensed under the MIT license.
 
 export * from '../../../common/scripts/lib/index.mjs';
-import { exec_output } from '../../../common/scripts/lib/exec.mjs';
-import { quote } from 'shell-quote';
 
 // This function sets additional environment variables for the storybook build steps,
 // to workaround issues between node 18 and storybook 6
 // It can be removed with storybook 7 potentially.
 export async function getExtraEnv() {
-  const nodeVersion = await exec_output(quote(['node', '-v']));
-  const nodeMajorVersion = parseInt(nodeVersion.split('.')[0].slice(1));
+  const nodeMajorVersion = parseInt(process.version.split('.')[0].slice(1));
   return nodeMajorVersion > 16 ? { NODE_OPTIONS: '--openssl-legacy-provider' } : undefined;
 }
