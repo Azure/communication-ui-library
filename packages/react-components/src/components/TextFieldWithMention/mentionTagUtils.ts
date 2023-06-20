2e8a013f050b77405343212c9a64b0d6bd3523a9
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2023-06-20 14:44:34 -0700

    Fixed selection is incorrect after deletion

--- packages/react-components/src/components/TextFieldWithMention/mentionTagUtils.ts
+++ packages/react-components/src/components/TextFieldWithMention/mentionTagUtils.ts
@@ -312,7 +312,7 @@ export const updateHTML = (props: UpdateHTMLProps): { updatedHTML: string; updat
       escapeHTMLChars(oldPlainText.substring(0, startIndex)) +
       changeWithSkippedChars +
       escapeHTMLChars(oldPlainText.substring(oldPlainTextEndIndex));
-    return { updatedHTML, updatedSelectionIndex: oldPlainTextEndIndex + change.length };
+    return { updatedHTML, updatedSelectionIndex: undefined };
   }
   let result = '';
   let lastProcessedHTMLIndex = 0;
@@ -471,6 +471,8 @@ export const updateHTML = (props: UpdateHTMLProps): { updatedHTML: string; updat
             oldPlainTextEndIndex,
             mentionTagLength
           });
+          console.log('Leah updateMentionTagResult', updateMentionTagResult);
+
           result += updateMentionTagResult.result;
           lastProcessedHTMLIndex = updateMentionTagResult.htmlIndex;
           // no need to handle plainTextSelectionEndIndex as the change will be added later
