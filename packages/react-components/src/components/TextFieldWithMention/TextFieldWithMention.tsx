ecde4ae74c2f534c41b762ca33278f4ef4167f30
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-06-20 17:00:17 -0700

    Fixed cursor position during mention editing

--- packages/react-components/src/components/TextFieldWithMention/TextFieldWithMention.tsx
+++ packages/react-components/src/components/TextFieldWithMention/TextFieldWithMention.tsx
@@ -686,6 +686,17 @@ export const TextFieldWithMention = (props: TextFieldWithMentionProps): JSX.Elem
           });
         }}
         onSelect={(e) => {
+          // update selection if needed
+          if (caretIndex !== undefined) {
+            // sometimes setting selectionRage in effect for updating caretIndex doesn't work as expected and
+            // onSelect still returns outdated value for cursor position
+            // e.g. when user select some text and a first name in a mention then delete or type something else
+            if (caretIndex !== e.currentTarget.selectionStart || caretIndex !== e.currentTarget.selectionEnd) {
+              e.currentTarget.setSelectionRange(caretIndex, caretIndex);
+            }
+            setCaretIndex(undefined);
+            return;
+          }
           handleOnSelect({
             event: e,
             inputTextValue,
