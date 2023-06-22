b6be7c08f2362d3d23890e4bcb8686d5754ee4d7
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-06-22 15:02:55 -0700

    Update side pane heading styles and correct video effects side pane width (#3179)

--- packages/react-composites/src/composites/common/styles/ParticipantContainer.styles.ts
+++ packages/react-composites/src/composites/common/styles/ParticipantContainer.styles.ts
@@ -38,8 +38,9 @@ export const sidePaneContainerTokens: IStackTokens = {
  */
 export const sidePaneHeaderStyles: IStackItemStyles = {
   root: {
-    lineHeight: '1.25rem',
-    padding: '0.25rem',
+    lineHeight: '1.5rem',
+    fontSize: '1.125rem',
+    padding: '0.75rem 0.75rem 0.75rem 0.25rem',
     fontWeight: '600'
   }
 };
