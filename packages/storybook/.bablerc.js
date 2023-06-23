b8a363389649323b46d7786915c44d75e31f407c
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-06-23 15:50:45 -0700

    Storybook updates

---
+++ packages/storybook/.bablerc.js
@@ -0,0 +1,33 @@
+const commonConfig = require('../../common/config/babel/.babelrc.js');
+
+module.exports = {
+  ...commonConfig,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          chrome: 100
+        }
+      }
+    ],
+    '@babel/preset-react'
+  ],
+  plugins: []
+};
+
+// {
+//   "sourceType": "unambiguous",
+//   "presets": [
+//     [
+//       "@babel/preset-env",
+//       {
+//         "targets": {
+//           "chrome": 100
+//         }
+//       }
+//     ],
+//     "@babel/preset-react"
+//   ],
+//   "plugins": []
+// }
