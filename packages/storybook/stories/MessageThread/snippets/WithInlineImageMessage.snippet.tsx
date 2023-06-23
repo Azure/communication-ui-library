d0d91f707ac0fb72669de5402c9d28a001abb30e
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2023-06-23 09:50:58 -0700

    fix errors in storybook

--- packages/storybook/stories/MessageThread/snippets/WithInlineImageMessage.snippet.tsx
+++ packages/storybook/stories/MessageThread/snippets/WithInlineImageMessage.snippet.tsx
@@ -3,8 +3,7 @@ import {
   MessageThread,
   Message,
   FileMetadata,
-  AttachmentDownloadResult,
-  TeamsInteropFileMetadata
+  AttachmentDownloadResult
 } from '@azure/communication-react';
 import React from 'react';
 
@@ -18,7 +17,7 @@ export const MessageThreadWithInlineImageExample: () => JSX.Element = () => {
     return [
       {
         // blobUrl: URL.createObjectURL(blob);
-        blobUrl: attachment.attachmentType === 'inlineImage' ? attachment.previewUrl : ''
+        blobUrl: attachment.attachmentType === 'inlineImage' ? attachment.previewUrl ?? '' : ''
       }
     ];
   };
