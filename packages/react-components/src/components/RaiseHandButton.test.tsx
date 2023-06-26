0f69db92871f6498fa7c8d10230be2c7ccd675b3
Ruslan Zdor <ruslanzdor@microsoft.com>
2023-06-26 10:10:12 -0700

    fix unit test

--- packages/react-components/src/components/RaiseHandButton.test.tsx
+++ packages/react-components/src/components/RaiseHandButton.test.tsx
@@ -1,20 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-/* @conditional-compile-remove(raise-hand) */
 import React from 'react';
-/* @conditional-compile-remove(raise-hand) */
 import { RaiseHandButton } from './RaiseHandButton';
-/* @conditional-compile-remove(raise-hand) */
 import { createTestLocale, renderWithLocalization } from './utils/testUtils';
-/* @conditional-compile-remove(raise-hand) */
 import { registerIcons } from '@fluentui/react';
-/* @conditional-compile-remove(raise-hand) */
 import { screen } from '@testing-library/react';
 
-/* @conditional-compile-remove(raise-hand) */
 describe('RaiseHandButton strings should be localizable and overridable', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     registerIcons({
       icons: {
         controlbuttonraisehand: <></>,
@@ -22,25 +16,26 @@ describe('RaiseHandButton strings should be localizable and overridable', () => 
       }
     });
   });
-
   test('Should localize button label', async () => {
     const testLocale = createTestLocale({
-      raiseHandButton: { offLabel: Math.random().toString(), onLabel: Math.random().toString() }
+      raiseHandButton: { offLabel: 'Raise Hand', onLabel: 'Lower Hand' }
     });
-    renderWithLocalization(<RaiseHandButton showLabel={true} />, testLocale);
-    expect(screen.getByRole('button').textContent).toBe(testLocale.strings.raiseHandButton.offLabel);
-    expect(screen.getByRole('button').setAttribute('checked', 'true'));
-    expect(screen.getByRole('button').textContent).toBe(testLocale.strings.raiseHandButton.onLabel);
+    const { rerender } = renderWithLocalization(<RaiseHandButton showLabel={true} />, testLocale);
+    expect(screen.getByRole('button').textContent).toBe('Raise Hand');
+
+    rerender(<RaiseHandButton showLabel={true} checked={true} />);
+    expect(screen.getByRole('button').textContent).toBe('Lower Hand');
   });
 
   test('Should override button label with `strings` prop', async () => {
     const testLocale = createTestLocale({
-      raiseHandButton: { offLabel: Math.random().toString(), onLabel: Math.random().toString() }
+      raiseHandButton: { offLabel: 'Raise Hand', onLabel: 'Lower Hand' }
     });
-    const raiseHandButtonStrings = { offLabel: Math.random().toString(), onLabel: Math.random().toString() };
-    renderWithLocalization(<RaiseHandButton showLabel={true} strings={raiseHandButtonStrings} />, testLocale);
-    expect(screen.getByRole('button').textContent).toBe(testLocale.strings.raiseHandButton.offLabel);
-    expect(screen.getByRole('button').setAttribute('checked', 'true'));
-    expect(screen.getByRole('button').textContent).toBe(testLocale.strings.raiseHandButton.onLabel);
+    const strings = { offLabel: 'Local Raise Hand', onLabel: 'Local Lower Hand' };
+    const { rerender } = renderWithLocalization(<RaiseHandButton showLabel={true} strings={strings} />, testLocale);
+    expect(screen.getByRole('button').textContent).toBe('Local Raise Hand');
+
+    rerender(<RaiseHandButton showLabel={true} checked={true} strings={strings} />);
+    expect(screen.getByRole('button').textContent).toBe('Local Lower Hand');
   });
 });
