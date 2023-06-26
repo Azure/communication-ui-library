a20a40a323a02bf24da0ce73b93f77636d7964fa
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2023-06-26 13:20:46 -0700

    [Release] Remove mention feature from in-progress features and add pre-release changelog (#3271)
    * Change files
    
    * Duplicate change files for beta release
    
    * Remove mention from in progress features

--- common/config/babel/.babelrc.js
+++ common/config/babel/.babelrc.js
@@ -77,8 +77,6 @@ process.env['COMMUNICATION_REACT_FLAVOR'] !== 'beta' &&
         'in-progress-beta-feature-demo',
         // Feature for updates needed for Click to Call (C2C) scenarios
         'click-to-call',
-        // Mention feature
-        'mention',
         // Feature for call transfer
         'call-transfer',
         // Optimal Video count feature,
