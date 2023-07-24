0caa02d8c04b29f8da4827bc1965fef3173f27b4
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-07-24 22:02:19 +0000

    arbitrary change to test pr conditions

--- packages/react-components-v2/src/index.ts
+++ packages/react-components-v2/src/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/// ARBITRARY CHANGE FOR TESTING PR CONDITIONS
+
 /** @internal */
 const _REMOVE_ME_ONCE_IMPLEMENTATION_IS_ADDED = 'REMOVE_ME';
 
