be561d0ede0269a9c1db7da73a57a292eee4fa5e
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-07-26 16:30:45 +0000

    remoe test.only

--- packages/react-composites/tests/browser/call/hermetic/RoomsConfigurationScreen.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/RoomsConfigurationScreen.test.ts
@@ -6,19 +6,19 @@ import { expect } from '@playwright/test';
 import { stableScreenshot, waitForCallCompositeToLoad } from '../../common/utils';
 
 test.describe('Rooms Call Configuration Screen Tests for different roles', () => {
-  test.only('All configurations are enabled for Presenter', async ({ page, serverUrl }) => {
+  test('All configurations are enabled for Presenter', async ({ page, serverUrl }) => {
     await page.goto(buildUrlWithMockAdapter(serverUrl, { ...defaultMockConfigurationPageState('Presenter') }));
     await waitForCallCompositeToLoad(page);
     expect(await stableScreenshot(page)).toMatchSnapshot(`rooms-configuration-page-presenter.png`);
   });
 
-  test.only('All configurations are enabled for Attendee', async ({ page, serverUrl }) => {
+  test('All configurations are enabled for Attendee', async ({ page, serverUrl }) => {
     await page.goto(buildUrlWithMockAdapter(serverUrl, { ...defaultMockConfigurationPageState('Attendee') }));
     await waitForCallCompositeToLoad(page);
     expect(await stableScreenshot(page)).toMatchSnapshot(`rooms-configuration-page-attendee.png`);
   });
 
-  test.only('Only configurations for speaker are enabled for Consumer', async ({ page, serverUrl }) => {
+  test('Only configurations for speaker are enabled for Consumer', async ({ page, serverUrl }) => {
     await page.goto(buildUrlWithMockAdapter(serverUrl, { ...defaultMockConfigurationPageState('Consumer') }));
     await waitForCallCompositeToLoad(page);
     expect(await stableScreenshot(page)).toMatchSnapshot(`rooms-configuration-page-consumer.png`);
