a061630f99cefba9e589e6704dcf9fcd6363ee93
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-08-01 11:14:43 -0700

    Add localization to v2 packlet (#3392)

---
+++ packages/react-components-v2/src/ExampleComponent/ExampleComponent.strings.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ * Locale strings of {@link ExampleComponent}.
+ *
+ * @public
+ */
+export interface ExampleComponentStrings {
+  /** Label for the ExampleComponent. This is just an example. */
+  label: string;
+}
