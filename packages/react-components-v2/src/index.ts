49ce3bef456ba3632154b96a9a3eb1d4b92081c2
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-08-01 17:08:52 +0000

    remove from stable builds

--- packages/react-components-v2/src/index.ts
+++ packages/react-components-v2/src/index.ts
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+/* @conditional-compile-remove(version-2) */
 export * from './public.index';
 
 // Internal only exports
+/* @conditional-compile-remove(version-2) */
 export * from './ExampleComponent';
+
+export {};
