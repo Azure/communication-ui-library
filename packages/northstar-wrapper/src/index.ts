bea8b89aede9113fb125d3cd2d87d80ae2f777a2
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-08-04 13:55:14 -0700

    Remove northstar completely.

--- packages/northstar-wrapper/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-import { Chat, ChatItemProps, MenuProps, ShorthandValue } from '@fluentui/react-northstar';
-
-export { Chat };
-export type { ChatItemProps, MenuProps, ShorthandValue };
