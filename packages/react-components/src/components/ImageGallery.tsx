0c435daa5fb89607c6fe7ffc640b7d8c462a9c86
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2023-08-09 14:22:11 -0700

    Fix host ids

--- packages/react-components/src/components/ImageGallery.tsx
+++ packages/react-components/src/components/ImageGallery.tsx
@@ -160,7 +160,7 @@ export const ImageGallery = (props: ImageGalleryProps): JSX.Element => {
         isOpen={images.length > 0}
         onDismiss={onDismiss}
         overlay={{ styles: { ...overlayStyles(theme, isDarkTheme), ...styles?.overlay } }}
-        layerProps={{ id: modalLayerHostId }}
+        layerProps={{ hostId: props.modalLayerHostId }}
         styles={{ main: focusTrapZoneStyle, scrollableContent: scrollableContentStyle, ...styles?.modal }}
       >
         <div className={mergeStyles(imageContainer, styles?.imageContainer)}>
@@ -176,7 +176,7 @@ export const ImageGallery = (props: ImageGalleryProps): JSX.Element => {
         </div>
       </Modal>
       <Layer
-        id={modalLayerHostId}
+        hostId={modalLayerHostId}
         insertFirst={true}
         styles={{
           content: mergeStyles(headerStyle(theme, isDarkTheme), styles?.header)
