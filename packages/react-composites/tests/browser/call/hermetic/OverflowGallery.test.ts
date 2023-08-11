d3cb8803bd8d0fb182ab55af92c975e5ce29386b
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-08-11 21:41:43 +0000

    cc test

--- packages/react-composites/tests/browser/call/hermetic/OverflowGallery.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/OverflowGallery.test.ts
@@ -265,6 +265,7 @@ test.describe('Overflow gallery tests', async () => {
     expect(await stableScreenshot(page)).toMatchSnapshot('overflow-gallery-with-1-joining-1-hold-participants.png');
   });
 
+  /* @conditional-compile-remove(gallery-layouts) */
   test('Overflow gallery can be moved to the top along with the local tile', async ({ page, serverUrl }, testInfo) => {
     test.skip(!isTestProfileDesktop(testInfo));
     const reina = defaultMockRemoteParticipant('Reina Takizawa');
