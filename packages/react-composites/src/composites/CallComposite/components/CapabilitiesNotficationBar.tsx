47880f456594599f3844b68d9790332aa3f137fb
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2023-08-14 06:48:12 +0000

    CapabilitiesNotificationBar copying ErrorBar

--- packages/react-composites/src/composites/CallComposite/components/CapabilitiesNotficationBar.tsx
+++ packages/react-composites/src/composites/CallComposite/components/CapabilitiesNotficationBar.tsx
@@ -3,7 +3,7 @@
 
 import { CapabilitiesChangeInfo, CapabilityResolutionReason } from '@azure/communication-calling';
 import { IMessageBarProps, MessageBar, MessageBarType, Stack } from '@fluentui/react';
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 /**
  * @beta
@@ -25,7 +25,7 @@ export interface ActiveNotification {
   /**
    * Type of error that is active.
    */
-  type: 'turnVideoOn' | 'unmuteMic' | 'shareScreen';
+  type: CapabalitiesNotificationType;
 
   isPresent: boolean;
 
@@ -46,31 +46,51 @@ export interface CapabilitiesNotificationBarStrings {
   turnVideoOnOffCapabilityLost?: string;
 }
 
+type CapabalitiesNotificationType = 'turnVideoOn' | 'unmuteMic' | 'shareScreen';
+interface DismissedNotification {
+  type: CapabalitiesNotificationType;
+  dismissedAt: Date;
+  activeSince?: Date;
+}
+
 /**
  * Notification bar for capabilities
  * @beta
  */
 export const CapabilitiesNotificationBar = (props: CapabilitiesNotificationBarProps): JSX.Element => {
-  console.log('props.capabilitiesChangesInfo: ', props.capabilitiesChangeInfo);
+  // Timestamp for when this comopnent is first mounted.
+  // Never updated through the lifecycle of this component.
+  const mountTimestamp = useRef(new Date(Date.now()));
 
-  const notifications: ActiveNotification[] = [];
-  if (props.capabilitiesChangeInfo?.newValue) {
-    Object.entries(props.capabilitiesChangeInfo.newValue).forEach((c) => {
-      if (c[0] === 'turnVideoOn') {
-        notifications.push({ type: 'turnVideoOn', ...c[1], timestamp: new Date(Date.now()) });
-      }
-      if (c[0] === 'unmuteMic') {
-        notifications.push({ type: 'unmuteMic', ...c[1], timestamp: new Date(Date.now()) });
-      }
-      if (c[0] === 'shareScreen') {
-        notifications.push({ type: 'shareScreen', ...c[1], timestamp: new Date(Date.now()) });
-      }
-    });
-  }
+  const [dismissedNotifications, setDismissedNotifications] = useState<DismissedNotification[]>([]);
+
+  const activeNotifications: ActiveNotification[] = useMemo(() => {
+    const activeNotifications: ActiveNotification[] = [];
+    if (props.capabilitiesChangeInfo?.newValue) {
+      Object.entries(props.capabilitiesChangeInfo.newValue).forEach((c) => {
+        if (c[0] === 'turnVideoOn') {
+          activeNotifications.push({ type: 'turnVideoOn', ...c[1], timestamp: new Date(Date.now()) });
+        }
+        if (c[0] === 'unmuteMic') {
+          activeNotifications.push({ type: 'unmuteMic', ...c[1], timestamp: new Date(Date.now()) });
+        }
+        if (c[0] === 'shareScreen') {
+          activeNotifications.push({ type: 'shareScreen', ...c[1], timestamp: new Date(Date.now()) });
+        }
+      });
+    }
+    return activeNotifications;
+  }, [props.capabilitiesChangeInfo?.newValue]);
+
+  useEffect(() => {
+    setDismissedNotifications(dropDismissalsForInactiveNotifications(activeNotifications, dismissedNotifications));
+  }, [activeNotifications, dismissedNotifications]);
+
+  const toShow = activeNotificationsToShow(activeNotifications, dismissedNotifications, mountTimestamp.current);
 
   return (
     <Stack data-ui-id="capabilities-notification-bar-stack">
-      {notifications.map((notification) => {
+      {toShow.map((notification) => {
         return (
           <MessageBar
             key={notification.type}
@@ -92,7 +112,7 @@ export const CapabilitiesNotificationBar = (props: CapabilitiesNotificationBarPr
             messageBarType={MessageBarType.warning}
             dismissIconProps={{ iconName: 'ErrorBarClear' }}
             onDismiss={() => {
-              console.log();
+              setDismissedNotifications(dismissNotification(dismissedNotifications, notification));
             }}
           >
             {getNotificationMessage(notification.type, notification.isPresent)}
@@ -103,7 +123,7 @@ export const CapabilitiesNotificationBar = (props: CapabilitiesNotificationBarPr
   );
 };
 
-const getNotificationMessage = (type: 'turnVideoOn' | 'unmuteMic' | 'shareScreen', isPresent: boolean): string => {
+const getNotificationMessage = (type: CapabalitiesNotificationType, isPresent: boolean): string => {
   switch (type) {
     case 'turnVideoOn':
       return isPresent ? 'Your camera has been enabled' : 'Your camera has been disabled';
@@ -112,4 +132,80 @@ const getNotificationMessage = (type: 'turnVideoOn' | 'unmuteMic' | 'shareScreen
     case 'shareScreen':
       return isPresent ? 'Your screen share has been enabled' : 'Your screen share has been disabled';
   }
+};
+
+const dropDismissalsForInactiveNotifications = (
+  activeNotifications: ActiveNotification[],
+  dismissedNotifications: DismissedNotification[]
+): DismissedNotification[] => {
+  const active = new Map();
+  for (const message of activeNotifications) {
+    active.set(message.type, message);
+  }
+  const shouldDeleteDismissal = (dismissed: DismissedNotification): boolean =>
+    dismissed.activeSince === undefined && active.get(dismissed.type) === undefined;
+
+  if (dismissedNotifications.some((dismissed) => shouldDeleteDismissal(dismissed))) {
+    return dismissedNotifications.filter((dismissed) => !shouldDeleteDismissal(dismissed));
+  }
+  return dismissedNotifications;
+};
+
+const activeNotificationsToShow = (
+  activeNotifications: ActiveNotification[],
+  dismissedNotifications: DismissedNotification[],
+  mountTimestamp?: Date
+): ActiveNotification[] => {
+  const dismissed: Map<CapabalitiesNotificationType, DismissedNotification> = new Map();
+  for (const error of dismissedNotifications) {
+    dismissed.set(error.type, error);
+  }
+
+  return activeNotifications.filter((error) => {
+    if (mountTimestamp && error.timestamp && mountTimestamp > error.timestamp) {
+      // Error has a timestamp and it is older than when the component was mounted.
+      return false;
+    }
+
+    const dismissal = dismissed.get(error.type);
+    if (!dismissal) {
+      // This error was never dismissed.
+      return true;
+    }
+    if (!error.timestamp) {
+      // No timestamp associated with the error. In this case, the existence of a dismissal is enough to suppress the error.
+      return false;
+    }
+    // Error has an associated timestamp, so compare with last dismissal.
+    return error.timestamp > dismissal.dismissedAt;
+  });
+};
+
+const dismissNotification = (
+  dismissedNotification: DismissedNotification[],
+  toDismiss: ActiveNotification
+): DismissedNotification[] => {
+  const now = new Date(Date.now());
+  for (const error of dismissedNotification) {
+    if (error.type === toDismiss.type) {
+      // Bump the timestamp for latest dismissal of this error to now.
+      error.dismissedAt = now;
+      error.activeSince = toDismiss.timestamp;
+      return Array.from(dismissedNotification);
+    }
+  }
+
+  const toDismissTimestamp = toDismiss.timestamp ?? now;
+
+  // Record that this error was dismissed for the first time right now.
+  return [
+    ...dismissedNotification,
+    {
+      type: toDismiss.type,
+      // the error time could be sometimes later than the button click time, which cause the dismiss not working
+      // so we set the dismiss time to the later one
+      dismissedAt: now > toDismissTimestamp ? now : toDismissTimestamp,
+      activeSince: toDismiss.timestamp
+    }
+  ];
 };
