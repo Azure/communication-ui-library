365ab777274e9c0b069dbc720f48e56aa92699ea
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2023-08-17 15:02:08 -0700

    Change titleIconTestId

--- packages/react-components/src/components/ImageGallery.test.tsx
+++ packages/react-components/src/components/ImageGallery.test.tsx
@@ -15,20 +15,22 @@ describe('ImageGallery default layout tests', () => {
       }
     });
   });
-  const titleIconTestId = 'titleIconTestId';
+  const titleIconTestId1 = 'titleIconTestId1';
+  const titleIconTestId2 = 'titleIconTestId2';
+
   const imageInfo = {
     imageUrl: 'images/inlineImageExample1.png',
     saveAsName: 'saveAsName',
     altText: 'altText',
     title: 'title',
-    titleIcon: <div data-testid={titleIconTestId}></div>
+    titleIcon: <div data-testid={titleIconTestId1}></div>
   };
   const imageInfo2 = {
     imageUrl: 'images/inlineImageExample2.png',
     saveAsName: 'saveAsName2',
     altText: 'altText2',
     title: 'title2',
-    titleIcon: <div data-testid={titleIconTestId}></div>
+    titleIcon: <div data-testid={titleIconTestId2}></div>
   };
   const renderImageGalleryComponent = (
     images?: Array<ImageGalleryImageProps>,
@@ -54,7 +56,7 @@ describe('ImageGallery default layout tests', () => {
     renderImageGalleryComponent();
     const image: HTMLImageElement = await screen.findByRole('img', { name: 'image-gallery-main-image' });
     const title: HTMLElement = await screen.findByText(imageInfo.title);
-    const titleIcon: HTMLElement = await screen.findByTestId(titleIconTestId);
+    const titleIcon: HTMLElement = await screen.findByTestId(titleIconTestId1);
     expect(image.src).toContain(imageInfo.imageUrl);
     expect(image.alt).toBe(imageInfo.altText);
 
@@ -66,7 +68,7 @@ describe('ImageGallery default layout tests', () => {
     renderImageGalleryComponent([imageInfo, imageInfo2], 1);
     const image: HTMLImageElement = await screen.findByRole('img', { name: 'image-gallery-main-image' });
     const title: HTMLElement = await screen.findByText(imageInfo2.title);
-    const titleIcon: HTMLElement = await screen.findByTestId(titleIconTestId);
+    const titleIcon: HTMLElement = await screen.findByTestId(titleIconTestId2);
     expect(image.src).toContain(imageInfo2.imageUrl);
     expect(image.alt).toBe(imageInfo2.altText);
 
