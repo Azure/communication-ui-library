2ad41041045ac2f2b70e3a6196696ad5066454e5
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-08-17 22:50:32 +0000

    remove debug

--- samples/Calling/src/app/views/CallCompositeContainer.tsx
+++ samples/Calling/src/app/views/CallCompositeContainer.tsx
@@ -26,9 +26,6 @@ export const CallCompositeContainer = (props: CallCompositeContainerProps): JSX.
       onNetworkingTroubleShootingClick,
       callControls: {
         legacyControlBarExperience: false
-      },
-      galleryOptions: {
-        layout: 'speaker'
       }
     }),
     []
