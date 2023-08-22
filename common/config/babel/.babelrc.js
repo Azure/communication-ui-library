237c30cc0a68a8da59f7144922fb2f745e697997
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2023-08-22 15:35:55 -0700

    fix typo

--- common/config/babel/.babelrc.js
+++ common/config/babel/.babelrc.js
@@ -12,7 +12,7 @@ process.env['COMMUNICATION_REACT_FLAVOR'] !== 'beta' &&
         // Flag to add API only available in beta calling SDK to mocks and internal types.
         // This feature should be stabilized whenever calling SDK is stabilized.
         'calling-beta-sdk',
-        // Flag to add API only available in beta calling SDK to mocks and internal types.
+        // Flag to add API only available in beta chat SDK to mocks and internal types.
         // This feature should be stabilized whenever chat SDK is stabilized.
         'chat-beta-sdk',
         // Flag to add API only available in the communication-common beta v3
