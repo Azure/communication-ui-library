f821d94cd0f109a1e5d75cfa911bad1c1a9de85e
carocao-msft <carolinecao@microsoft.com>
2023-08-25 13:12:37 -0700

    ui change

--- packages/react-composites/src/composites/common/CaptionsBanner.tsx
+++ packages/react-composites/src/composites/common/CaptionsBanner.tsx
@@ -76,9 +76,7 @@ export const CaptionsBanner = (props: { isMobile: boolean }): JSX.Element => {
       {
         /* @conditional-compile-remove(close-captions) */ <div className={containerClassName}>
           <Stack horizontalAlign="center">
-            <Stack.Item
-              style={{ width: props.isMobile ? mobileViewBannerWidth : desktopViewBannerWidth}}
-            >
+            <Stack.Item style={{ width: props.isMobile ? mobileViewBannerWidth : desktopViewBannerWidth }}>
               <_CaptionsBanner
                 {...captionsBannerProps}
                 {...handlers}
