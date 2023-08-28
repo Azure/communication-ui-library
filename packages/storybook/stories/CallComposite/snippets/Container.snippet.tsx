8d23e7b16a0646384def8e4290f5626586f33d10
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2023-08-28 20:55:10 +0000

    Trim locator entered in storybook join existring call preview and validate if it is a roomId

--- packages/storybook/stories/CallComposite/snippets/Container.snippet.tsx
+++ packages/storybook/stories/CallComposite/snippets/Container.snippet.tsx
@@ -29,10 +29,14 @@ const isTeamsMeetingLink = (link: string): boolean => link.startsWith('https://t
 const isGroupID = (id: string): boolean => validateUUID(id);
 
 const createCallAdapterLocator = (locator: string): CallAdapterLocator | undefined => {
-  if (isTeamsMeetingLink(locator)) {
+  if (locator === '') {
+    return undefined;
+  } else if (isTeamsMeetingLink(locator)) {
     return { meetingLink: locator };
   } else if (isGroupID(locator)) {
     return { groupId: locator };
+  } else if (/^\d+$/.test(locator)) {
+    return { roomId: locator };
   }
   return undefined;
 };
@@ -47,7 +51,7 @@ export const ContosoCallContainer = (props: ContainerProps): JSX.Element => {
     }
   }, [props.token]);
 
-  const locator = useMemo(() => createCallAdapterLocator(props.locator), [props.locator]);
+  const locator = useMemo(() => createCallAdapterLocator(props.locator.trim()), [props.locator]);
 
   const adapter = useAzureCommunicationCallAdapter(
     {
