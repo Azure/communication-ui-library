33da9ee458938da80c564069cdb2859acfd678ad
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-09-08 13:28:34 -0700

    Added workerIdleMemoryLimit

--- common/config/jest/jest.config.js
+++ common/config/jest/jest.config.js
@@ -50,6 +50,11 @@ module.exports = {
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
+
   // The directory where Jest should output its coverage files
-  coverageDirectory: '../../../temp/jest/coverage/' // communication-ui-library/temp/jest/coverage
+  coverageDirectory: '../../../temp/jest/coverage/', // communication-ui-library/temp/jest/coverage
+
+  // Specifies the memory limit for workers before they are recycled and
+  // is primarily a work- around for https://github.com/jestjs/jest/issues/11956
+  workerIdleMemoryLimit: 0.7,
 };
