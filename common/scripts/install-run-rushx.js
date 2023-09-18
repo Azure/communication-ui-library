16946325462b793be20f2aa6bf73a96b67965042
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2023-09-18 19:04:12 +0000

    revert auto generated files

--- common/scripts/install-run-rushx.js
+++ common/scripts/install-run-rushx.js
@@ -18,7 +18,7 @@ var __webpack_exports__ = {};
   !*** ./lib-esnext/scripts/install-run-rushx.js ***!
   \*************************************************/
 
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See the @microsoft/rush package's LICENSE file for license information.
 require('./install-run-rush');
 //# sourceMappingURL=install-run-rushx.js.map
