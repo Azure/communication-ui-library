cf2f3a6b478da464132d424a631f82461cb3a4f6
Patrick Latter <73612854+palatter@users.noreply.github.com>
2023-09-19 14:33:17 -0700

    [FluentUI] Resolve issue with read receipt layout and appearance for fluent migration. (#3594)

--- packages/react-components/src/components/MessageThread.tsx
+++ packages/react-components/src/components/MessageThread.tsx
@@ -411,6 +411,16 @@ const memoizeAllMessages = memoizeFnAll(
                   : {}
             }}
             body={{ className: chatMessageRenderStyles.body }}
+            statusIcon={
+              <div
+                className={mergeStyles(
+                  { paddingLeft: '0.25rem' },
+                  styles?.messageStatusContainer ? styles.messageStatusContainer(message.mine ?? false) : ''
+                )}
+              >
+                {message.status ? messageStatusRenderer(message.status) : undefined}
+              </div>
+            }
           >
             {onRenderMessage === undefined
               ? defaultChatMessageRenderer({ ...messageProps, messageStatusRenderer })
