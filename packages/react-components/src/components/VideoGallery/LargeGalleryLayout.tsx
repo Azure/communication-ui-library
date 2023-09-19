36e8d9afb5aadc0e2991da774387f005ee2df483
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-09-19 16:43:30 +0000

    fix cc

--- packages/react-components/src/components/VideoGallery/LargeGalleryLayout.tsx
+++ packages/react-components/src/components/VideoGallery/LargeGalleryLayout.tsx
@@ -40,15 +40,14 @@ export const LargeGalleryLayout = (props: LargeGalleryProps): JSX.Element => {
     styles,
     maxRemoteVideoStreams,
     parentWidth,
-    /* @conditional-compile-remove(vertical-gallery) */
-    parentHeight,
+    /* @conditional-compile-remove(vertical-gallery) */ /* @conditional-compile-remove(gallery-layouts) */ parentHeight,
     pinnedParticipantUserIds = [],
     /* @conditional-compile-remove(vertical-gallery) */ overflowGalleryPosition = 'HorizontalBottom'
   } = props;
 
   const isNarrow = parentWidth ? isNarrowWidth(parentWidth) : false;
 
-  /* @conditional-compile-remove(vertical-gallery) */
+  /* @conditional-compile-remove(vertical-gallery) */ /* @conditional-compile-remove(gallery-layouts) */
   const isShort = parentHeight ? isShortHeight(parentHeight) : false;
 
   const maxStreamsTrampoline = (): number => {
