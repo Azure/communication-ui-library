85eeeb1b0a8eb82286ad30ad6c26e591ae34679e
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2023-09-21 13:22:29 -0700

    Increase timeout for a test

--- packages/react-composites/playwright.config.common.ts
+++ packages/react-composites/playwright.config.common.ts
@@ -60,7 +60,7 @@ const config: PlaywrightTestConfig = {
   outputDir: outputDir,
   // Extend per-test timeout for local debugging so that developers can single-step through
   // the test in playwright inspector.
-  timeout: process.env.LOCAL_DEBUG ? 10 * MINUTE : 1 * MINUTE,
+  timeout: process.env.LOCAL_DEBUG ? 10 * MINUTE : 2 * MINUTE,
 
   // Do not allow `.only` to be committed to the codebase. `.only` should only be used for diagnosing issues.
   forbidOnly: !!process.env.CI,
@@ -71,7 +71,7 @@ const config: PlaywrightTestConfig = {
   // Applies to all projects
   use: {
     headless: !process.env.LOCAL_DEBUG,
-    video: 'on-first-retry'
+    video: 'retain-on-failure'
   },
 
   projects: [
