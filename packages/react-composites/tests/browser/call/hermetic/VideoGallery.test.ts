cc0f12eb37e5df27ccd1e555b72bd60e41ac6476
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-09-26 18:58:54 +0000

    remove test.only

--- packages/react-composites/tests/browser/call/hermetic/VideoGallery.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/VideoGallery.test.ts
@@ -219,7 +219,7 @@ test.describe('VideoGallery tests', async () => {
   });
 
   /* @conditional-compile-remove(gallery-layouts) */
-  test.only('VideoGallery layouts looks correct on mobile', async ({ page, serverUrl }, testInfo) => {
+  test('VideoGallery layouts looks correct on mobile', async ({ page, serverUrl }, testInfo) => {
     test.skip(!isTestProfileMobile(testInfo));
     const paul = defaultMockRemoteParticipant('Paul Bridges');
     const vasily = defaultMockRemoteParticipant('Vasily Pupkin');
