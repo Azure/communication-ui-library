8856df0d3eb4b066986529ea602ff087a4bcdf10
Mohtasim <mbellah@microsoft.com>
2023-10-11 19:17:44 -0700

    Applying dark opacity around reaction on the tile
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- packages/react-components/src/components/VideoTile.tsx
+++ packages/react-components/src/components/VideoTile.tsx
@@ -415,7 +415,8 @@ export const VideoTile = (props: VideoTileProps): JSX.Element => {
                   {
                     display: 'flex',
                     justifyContent: 'center',
-                    alignItems: 'center'
+                    alignItems: 'center',
+                    backgroundColor: reaction ? 'rgba(0, 0, 0, 0.5)' : 'transparent'
                   }
                 )} >
               <div style={{
