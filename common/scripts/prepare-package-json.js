61319d9763fd121c1cf91369534dc7e80e9a6808
Alex Kwan <alkwa@microsoft.com>
2023-10-20 10:26:00 -0700

    adding additional logging

--- common/scripts/prepare-package-json.js
+++ common/scripts/prepare-package-json.js
@@ -4,15 +4,24 @@ const fs =  require('fs');
 const main = () => {
     // making an assumption the working directory is packages/communication-react
     const packagePath = path.resolve(process.cwd(), 'package.json');
+    console.log('location of package.json to clean up : ' +packagePath);
     
     // open up the package.json as JSON
     const packageJson = JSON.parse(fs.readFileSync(packagePath));
 
     // remove extraneous properties we don't need in the final NPM package
     const result = { ...packageJson };
+    console.log('does package.json contain devDependencies before cleanup :'+ !!result.devDependencies)
+    console.log('does package.json contain scripts after cleanup :'+ !!result.scripts)
+    console.log('does package.json contain beachball before cleanup :'+ !!result.beachball)
+
     delete result.devDependencies;
     delete result.scripts;
     delete result.beachball;
+    
+    console.log('does package.json contain devDependencies after cleanup :'+ !!result.devDependencies)
+    console.log('does package.json contain scripts before cleanup :'+ !!result.scripts)
+    console.log('does package.json contain beachball after cleanup :'+ !!result.beachball)
   
     // write back to the package.json
     fs.writeFileSync(packagePath, JSON.stringify(result, null, 2));
