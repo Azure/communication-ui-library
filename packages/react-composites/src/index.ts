4209030eee8a10dab815c51ed4b25188b7c86b0a
edwardlee-msft <edwardlee@microsoft.com>
2023-10-20 17:09:07 -0700

    Update index.ts
    Signed-off-by: edwardlee-msft <edwardlee@microsoft.com>

--- packages/react-composites/src/index.ts
+++ packages/react-composites/src/index.ts
@@ -9,4 +9,4 @@ export { _useCompositeLocale } from './composites/localization';
 export { _createAzureCommunicationCallWithChatAdapterFromAdapters } from './composites/CallWithChatComposite/adapter/AzureCommunicationCallWithChatAdapter';
 export type { _ChatThreadRestError, _FakeChatAdapters, _FakeChatAdapterArgs, _MockFileUpload } from './mocks';
 export { _useFakeChatAdapters } from './mocks';
-export { _MockCallAdapter } from './composites/CallComposite/MockCallAdapter'
+export { _MockCallAdapter } from './composites/CallComposite/MockCallAdapter';
