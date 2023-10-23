efbf85daa725ca1d3176336f263218c0f624a3b0
Alex Kwan <alkwa@microsoft.com>
2023-10-22 20:26:10 -0700

    retaining scripts property so we can run postpack

--- common/scripts/prepare-package-json.js
+++ common/scripts/prepare-package-json.js
@@ -11,18 +11,10 @@ const main = () => {
 
     // remove extraneous properties we don't need in the final NPM package
     const result = { ...packageJson };
-    console.log('does package.json contain devDependencies before cleanup :'+ !!result.devDependencies)
-    console.log('does package.json contain scripts after cleanup :'+ !!result.scripts)
-    console.log('does package.json contain beachball before cleanup :'+ !!result.beachball)
 
     delete result.devDependencies;
-    delete result.scripts;
     delete result.beachball;
     
-    console.log('does package.json contain devDependencies after cleanup :'+ !!result.devDependencies)
-    console.log('does package.json contain scripts before cleanup :'+ !!result.scripts)
-    console.log('does package.json contain beachball after cleanup :'+ !!result.beachball)
-  
     // write back to the package.json
     fs.writeFileSync(packagePath, JSON.stringify(result, null, 2));
 }
