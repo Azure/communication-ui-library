6798e070ac0bd3d8ad22287244030f358568ac55
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2023-11-05 01:00:25 -0800

    Prevent loop of switching between scrollable and paginated horizontal gallery (#3730)
    * Prevent possible endless loop of switching between scrollable and paginated horizontal gallery in VideoGallery
    
    * Change files
    
    * Duplicate change files for beta release
    
    * fix and memoize scrollable horizontal gallery container style
    
    * lint fix
    
    * fix scrollable horizontal gallery width for other video gallery layouts
    
    * dependecy array lint fix

--- packages/react-components/src/components/VideoGallery/DefaultLayout.tsx
+++ packages/react-components/src/components/VideoGallery/DefaultLayout.tsx
@@ -120,6 +120,7 @@ export const DefaultLayout = (props: DefaultLayoutProps): JSX.Element => {
         }}
         /* @conditional-compile-remove(gallery-layouts) */
         layout={'default'}
+        parentWidth={parentWidth}
       />
     );
   }, [
@@ -129,7 +130,8 @@ export const DefaultLayout = (props: DefaultLayoutProps): JSX.Element => {
     styles?.horizontalGallery,
     /* @conditional-compile-remove(vertical-gallery) */ overflowGalleryPosition,
     setIndexesToRender,
-    /* @conditional-compile-remove(vertical-gallery) */ styles?.verticalGallery
+    /* @conditional-compile-remove(vertical-gallery) */ styles?.verticalGallery,
+    parentWidth
   ]);
 
   return (
