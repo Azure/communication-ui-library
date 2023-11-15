65ca092e834045416ae5f2860434fc6a1f2f0037
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2023-11-15 00:30:34 +0000

    add adapter jest tests

---
+++ packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.test.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+import {
+  CommonCallAdapterOptions,
+  createAzureCommunicationCallAdapterFromClient
+} from './AzureCommunicationCallAdapter';
+import { MockCallAgent, MockCallClient } from '../../CallWithChatComposite/adapter/TestUtils';
+import { StatefulCallClient } from '@internal/calling-stateful-client';
+
+describe('Adapter is created as expected', () => {
+  test('when cerating an adapter without sounds provided we should not see it in state', async () => {
+    const mockCallClient = new MockCallClient() as unknown as StatefulCallClient;
+
+    const mockCallAgent = new MockCallAgent();
+    const locator = { participantIds: ['some user id'] };
+
+    const adapter = await createAzureCommunicationCallAdapterFromClient(mockCallClient, mockCallAgent, locator);
+    expect(adapter).toBeDefined();
+    expect(adapter.getState().sounds).toBeUndefined();
+  });
+
+  test('when creating an adapter with sounds provided we should see it in state', async () => {
+    const mockCallClient = new MockCallClient() as unknown as StatefulCallClient;
+
+    const mockCallAgent = new MockCallAgent();
+    const locator = { participantIds: ['some user id'] };
+    const options: CommonCallAdapterOptions = {
+      soundOptions: {
+        callingSounds: {
+          callEnded: { path: 'test/path/ended' },
+          callRinging: { path: 'test/path/ringing' }
+        }
+      }
+    };
+
+    const adapter = await createAzureCommunicationCallAdapterFromClient(
+      mockCallClient,
+      mockCallAgent,
+      locator,
+      options
+    );
+    expect(adapter).toBeDefined();
+    expect(adapter.getState().sounds).toBeDefined();
+    expect(adapter.getState().sounds?.callEnded).toBeDefined();
+    expect(adapter.getState().sounds?.callRinging).toBeDefined();
+  });
+});
