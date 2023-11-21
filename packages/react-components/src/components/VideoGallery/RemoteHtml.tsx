44deed4636f21dd22ebc57a3bbb11c8b3b1fde5c
shaunasong <93549644+ShaunaSong@users.noreply.github.com>
2023-11-21 09:21:49 -0800

    remove the display name

--- packages/react-components/src/components/VideoGallery/RemoteHtml.tsx
+++ packages/react-components/src/components/VideoGallery/RemoteHtml.tsx
@@ -4,19 +4,16 @@
 import React from 'react';
 import { StreamMedia } from '../StreamMedia';
 import { VideoTile } from '../VideoTile';
-import { _formatString } from '@internal/acs-ui-common';
-
 /**
  * A memoized version of VideoTile for rendering the remote screen share stream. React.memo is used for a performance
  * boost by memoizing the same rendered component to avoid rerendering this when the parent component rerenders.
  * https://reactjs.org/docs/react-api.html#reactmemo
  */
 export const RemoteHtml = React.memo((props: { userId: string; displayName?: string; renderElement?: HTMLElement }) => {
-  const { displayName, renderElement } = props;
+  const { renderElement } = props;
 
   return (
     <VideoTile
-      displayName={displayName}
       renderElement={renderElement ? <StreamMedia videoStreamElement={renderElement} loadingState="none" /> : undefined}
     />
   );
