f6b98c55fc20f1ce21fbfded6c98d42200404c9b
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2023-12-21 13:15:45 -0800

    test

--- packages/react-composites/tests/browser/chat/hermetic/FileSharing.test.ts
+++ packages/react-composites/tests/browser/chat/hermetic/FileSharing.test.ts
@@ -319,7 +319,7 @@ test.describe('Inline Image Message Thread', async () => {
     );
     // wait for the to be loaded before comparing the snapshots
     // await page.evaluate(() => document.fonts.ready);
-    // await page.waitForTimeout(5000);
+    await page.waitForTimeout(5000);
 
     expect(
       await stableScreenshot(page, {
