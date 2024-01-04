c314e7e0bf6c7853bf6d38b125a4b0d477b3b908
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-01-04 22:10:05 +0000

    stable build fix

--- samples/Calling/src/app/views/HomeScreen.tsx
+++ samples/Calling/src/app/views/HomeScreen.tsx
@@ -197,6 +197,8 @@ export const HomeScreen = (props: HomeScreenProps): JSX.Element => {
                 required={true}
                 onChange={(_, option) => {
                   option && setChosenCallOption(option);
+                  /* @conditional-compile-remove(teams-adhoc-call) */
+                  /* @conditional-compile-remove(teams-identity-support) */
                   setTeamsIdFormatError(false);
                 }}
               />
