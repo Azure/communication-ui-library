7880492e141a23f391a3fc961cf88be1c8846347
Mohtasim <mbellah@microsoft.com>
2024-01-11 21:47:20 -0800

    reaction button selector api signature update to beta
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- packages/calling-component-bindings/src/callControlSelectors.ts
+++ packages/calling-component-bindings/src/callControlSelectors.ts
@@ -10,7 +10,6 @@ import {
   getDeviceManager,
   getIsMuted,
   getIsScreenSharingOn,
-  getLocalParticipantReaction,
   getLocalVideoStreams
 } from './baseSelectors';
 /* @conditional-compile-remove(reaction) */
