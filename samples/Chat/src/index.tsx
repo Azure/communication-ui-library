e9f9de2ddf5584dca2eb1185c3aa040026eeefcc
Patrick Latter <73612854+palatter@users.noreply.github.com>
2024-01-31 06:29:23 +0000

    Update ACS sample apps with commit ID

--- samples/Chat/src/index.tsx
+++ samples/Chat/src/index.tsx
@@ -7,7 +7,6 @@ import './index.css';
 import App from './app/App';
 import { SwitchableFluentThemeProvider } from './app/theming/SwitchableFluentThemeProvider';
 import { ShakeToSendLogs } from './app/utils/ShakeToSendLogs';
-import { BuildVersionOverlay } from './app/utils/BuildVersionOverlay';
 
 const domNode = document.getElementById('root');
 if (!domNode) {
@@ -18,7 +17,6 @@ createRoot(domNode).render(
   <React.StrictMode>
     <SwitchableFluentThemeProvider scopeId="SampleChatApp">
       <div className="wrapper">
-        <BuildVersionOverlay />
         <App />
         <ShakeToSendLogs />
       </div>
