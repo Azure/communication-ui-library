43e8d0e7435914f3ff6a92505c6e0cc5609ebadc
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-02-02 17:14:14 -0800

    Revert telemetry version change

--- packages/acs-ui-common/src/telemetry.ts
+++ packages/acs-ui-common/src/telemetry.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import * as telemetryVersion from './telemetryVersion';
+import telemetryVersion from './telemetryVersion';
 
 /**
  * @private
