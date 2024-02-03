017c12af8770e0205611051be733a2f553bb4490
Mohtasim <mbellah@microsoft.com>
2024-02-02 17:13:08 -0800

    call with chat adapter update
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- packages/react-composites/src/composites/CallWithChatComposite/state/CallWithChatAdapterState.ts
+++ packages/react-composites/src/composites/CallWithChatComposite/state/CallWithChatAdapterState.ts
@@ -189,6 +189,8 @@ export function mergeCallAdapterStateIntoCallWithChatAdapterState(
     /* @conditional-compile-remove(video-background-effects) */
     onResolveVideoEffectDependency: callAdapterState.onResolveVideoEffectDependency,
     /* @conditional-compile-remove(video-background-effects) */
-    selectedVideoBackgroundEffect: callAdapterState.selectedVideoBackgroundEffect
+    selectedVideoBackgroundEffect: callAdapterState.selectedVideoBackgroundEffect,
+    /* @conditional-compile-remove(reaction) */
+    reactions: callAdapterState.reactions
   };
 }
