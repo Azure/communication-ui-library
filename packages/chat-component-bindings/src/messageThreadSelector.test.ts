e21fe6db1e8b3cb0dcec6f309f1665972ff1f933
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-02-06 17:48:44 -0800

    Add messageThreadSelector tests

---
+++ packages/chat-component-bindings/src/messageThreadSelector.test.ts
@@ -0,0 +1,118 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ChatMessageWithStatus } from '@internal/chat-stateful-client';
+import { messageThreadSelector } from './messageThreadSelector';
+import { ChatAttachment, ChatParticipant } from '@azure/communication-chat';
+
+// Make it type safe when update messageThreadSelector
+const messageThreadSelectorResultFunc = (messageThreadSelector as any).resultFunc;
+
+describe('messageThreadSelector tests', () => {
+  test('should parse text messages correctly', async (): Promise<void> => {
+    const messageText = 'Hello';
+    const result = messageThreadSelectorResultFunc(
+      '1',
+      getChatMessages(messageText),
+      Date.now(),
+      false,
+      [],
+      getChatParticipants()
+    );
+
+    expect(result.messages).toMatchObject([
+      {
+        content: messageText
+      }
+    ]);
+  });
+
+  test('should parse HTML messages correctly', async (): Promise<void> => {
+    const messageText = '<p>Hello</p>';
+    const result = messageThreadSelectorResultFunc(
+      '1',
+      getChatMessages(messageText),
+      Date.now(),
+      false,
+      [],
+      getChatParticipants()
+    );
+
+    expect(result.messages).toMatchObject([
+      {
+        content: messageText
+      }
+    ]);
+  });
+
+  test('should parse HTML messages with image tag correctly without attachments', async (): Promise<void> => {
+    const messageText = '<p>Hello <img alt="image" src="" id="1"></p>';
+    const result = messageThreadSelectorResultFunc(
+      '1',
+      getChatMessages(messageText),
+      Date.now(),
+      false,
+      [],
+      getChatParticipants()
+    );
+    expect(result.messages).toMatchObject([
+      {
+        content: messageText
+      }
+    ]);
+  });
+
+  test('should parse HTML messages with image tag correctly with attachments', async (): Promise<void> => {
+    const messageText = '<p>Hello <img alt="image" src="" id="1"></p>';
+    const imageContent = 'data:image/png;base64,iVBORw0KGgoAAAA';
+    const expectedContent = '<p>Hello <img alt="image" src="' + imageContent + '" id="1"></p>';
+
+    const result = messageThreadSelectorResultFunc(
+      '1',
+      getChatMessages(messageText, [{ id: '1', attachmentType: 'image', previewUrl: 'testURL' }], {
+        testURL: imageContent
+      }),
+      Date.now(),
+      false,
+      [],
+      getChatParticipants()
+    );
+    expect(result.messages).toMatchObject([
+      {
+        content: expectedContent
+      }
+    ]);
+  });
+});
+
+const getChatParticipants = () => {
+  const participants: { [key: string]: ChatParticipant } = {};
+  participants['1'] = {
+    id: { communicationUserId: '1' },
+    displayName: 'User1'
+  };
+  return participants;
+};
+
+const getChatMessages = (
+  messageText: string,
+  attachments?: ChatAttachment[],
+  resourceCache?: Record<string, string>
+) => {
+  const chatMessages: { [key: string]: ChatMessageWithStatus } = {};
+  chatMessages['0'] = {
+    id: '0',
+    clientMessageId: '0',
+    sequenceId: '0',
+    status: 'delivered',
+    type: 'html',
+    version: '1',
+    createdOn: new Date(),
+    content: {
+      message: messageText,
+      attachments: attachments
+    },
+    resourceCache: resourceCache
+  };
+  return chatMessages;
+};
