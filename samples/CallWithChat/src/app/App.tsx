5beb5b4ce0d87dc5ce367205a2562e12be8f4bdd
Ruslan Zdor <ruslanzdor@microsoft.com>
2024-02-12 13:39:30 -0800

    fix threadid join in url

--- samples/CallWithChat/src/app/App.tsx
+++ samples/CallWithChat/src/app/App.tsx
@@ -25,7 +25,7 @@ import {
   isOnIphoneAndNotSafari
 } from './utils/AppUtils';
 /* @conditional-compile-remove(meeting-id) */
-import { ensureJoinableMeetingIdPushedToUrl, getMeetingIdFromUrl } from './utils/AppUtils';
+import { ensureJoinableMeetingIdPushedToUrl, getMeetingIdFromUrl, getThreadIdFromUrl } from './utils/AppUtils';
 import { CallScreen } from './views/CallScreen';
 import { HomeScreen } from './views/HomeScreen';
 import { UnsupportedBrowserPage } from './views/UnsupportedBrowserPage';
@@ -70,7 +70,7 @@ const App = (): JSX.Element => {
   const joiningExistingCallWithChat: boolean =
     (!!getGroupIdFromUrl() && !!getExistingThreadIdFromURL()) ||
     !!getTeamsLinkFromUrl() ||
-    /* @conditional-compile-remove(meeting-id) */ !!getMeetingIdFromUrl();
+    /* @conditional-compile-remove(meeting-id) */ (!!getMeetingIdFromUrl() && !!getExistingThreadIdFromURL());
 
   switch (page) {
     case 'home': {
@@ -152,7 +152,8 @@ const generateCallWithChatArgs = async (
     if ('meetingLink' in teamsLocator) {
       locator = teamsLocator;
     } else {
-      locator = { callLocator: teamsLocator, chatThreadId: threadId ? threadId : '' };
+      const chatThreadId = threadId ?? getThreadIdFromUrl();
+      locator = { callLocator: teamsLocator, chatThreadId: chatThreadId ? chatThreadId : '' };
     }
     if ('meetingLink' in teamsLocator) {
       ensureJoinableTeamsLinkPushedToUrl(teamsLocator);
