3fbf840af7f55b7189b41f34af1bd012cb99f178
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-02-29 14:49:04 -0800

    Improve generate-api-diff output to better see errors (#4192)

--- common/scripts/generate-api-diff.mjs
+++ common/scripts/generate-api-diff.mjs
@@ -15,7 +15,7 @@
 import { exit } from 'process';
 import yargs from 'yargs/yargs';
 import featureDefinitions from '../config/babel/features.js';
-import { exec } from './lib/exec.mjs';
+import { exec as execInternal } from './lib/exec.mjs';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -34,22 +34,39 @@ const filesToBackup = [
   path.join(__dirname, '../../packages/communication-react/review/beta/communication-react.api.md'),
   path.join(__dirname, '../../packages/communication-react/review/stable/communication-react.api.md'),
   path.join(__dirname, '../config/babel/features.js')
-]
+];
+
+// This process runs the build and api generate commands locally.
+// During this process, the development flavor is switched to beta-release and/or stable.
+// This variable is used to store the initial development flavor to restore to once the cmd finishes.
+let initialDevelopmentFlavor = undefined;
+
+// This variable is used to store the commands that were run during the process.
+// This is used to display the commands that were run in the logs in the event of an error
+// to allow the developer to easily reproduce the error.
+let commandsRun = []
 
 async function main() {
-  let wasSuccessful = true;
+  let result = undefined;
   await setup();
+
   try {
-    await generateApiJsons();
+    result = await generateApiJsons();
   } catch (e) {
-    wasSuccessful = false;
     console.error(e);
   } finally {
     await cleanup();
   }
 
-  if (!wasSuccessful) {
-    exit(-1);
+  // Ouput the result of the process _after_ the cleanup process has run to ensure the logs are not lost
+  if (result) {
+    console.log(`\nDone generating api.json files, files can be found at:`);
+    console.log(`  - ${result.baselineFilePath}`);
+    console.log(`  - ${result.featureFilePath}`);
+    console.log(`Please upload these files to https://apiview.dev/ for review.`);
+  } else {
+    console.error(`\nScript failed to generate api.json files.\nInspect the logs above for more information on errors.\nAlternatively, this tool ran the following commands that you can run locally to reproduce the error: \n${commandsRun.map(c => `  - ${c}`).join('\n')}`);
+    exit(1);
   }
 }
 
@@ -60,21 +77,28 @@ async function setup() {
     fs.rmSync(destinationDir, { recursive: true });
   }
 
+  // Backup the files that will be modified by the build process
   backupFiles(filesToBackup);
+
+  // Store the active development flavor to restore to once the cmd finishes
+  initialDevelopmentFlavor = getBuildFlavor();
 }
 
 /** To be called at the end of the script to restore the files that were modified by the build process */
 async function cleanup() {
+  // Restore the files that were modified by the build process
   restoreFiles(filesToBackup);
+
+  // restore the initial development flavor used before the cmd started
+  if (initialDevelopmentFlavor) {
+    await execInternal(`rush switch-flavor:${initialDevelopmentFlavor}`);
+  }
 }
 
 /**
  * Generate the baseline.api.json and feature.api.json files.
  */
 async function generateApiJsons() {
-  // Store the active development flavor to restore to once the cmd finishes
-  const initialDevelopmentFlavor = getBuildFlavor();
-
   const { feature } = parseArgs(process.argv);
   const { features, inProgressFeatures, stabilizedFeatures } = featureDefinitions;
 
@@ -96,81 +120,31 @@ async function generateApiJsons() {
     await exec('rush switch-flavor:stable');
   }
 
-  modifyFeatureDefinitionsFile(feature, isAlphaFeature ? 'alphaToBeta' : 'betaToStable');
   console.log(`Generating baseline.api.json file`);
-  const baselineFilePath = await generateApiFile(false);
+  const baselineFilePath = await generateApiFile('baseline.api.json');
 
-  modifyFeatureDefinitionsFile(feature, isAlphaFeature ? 'betaToAlpha' : 'stableToBeta');
   console.log(`Generating feature.api.json file`);
-  const featureFilePath = await generateApiFile(true);
+  exec(`rush stage-feature -f ${feature} -o ${isAlphaFeature ? 'alphaToBeta' : 'betaToStable'}`);
+  const featureFilePath = await generateApiFile('feature.api.json');
 
-  // restore the flavor
-  await exec(`rush switch-flavor:${initialDevelopmentFlavor}`);
-
-  console.log(`Done generating api.json files, files can be found at:`);
-  console.log(`  - ${baselineFilePath}`);
-  console.log(`  - ${featureFilePath}`);
-  console.log(`Please upload these files to apiview.dev for review.`);
+  return { baselineFilePath, featureFilePath };
 }
 
-/**
- * Modify the babelrc file to enable/disable beta features.
- *
- * @param {string} option - The option to modify the babelrc file with
- * @returns {Promise<void>}
- * @example
- * `modifyFeatureDefinitionsFile(featureName, 'alphaToBeta')`
- * `modifyFeatureDefinitionsFile(featureName, 'betaToAlpha')`
- * `modifyFeatureDefinitionsFile(featureName, 'betaToStable')`
- * `modifyFeatureDefinitionsFile(featureName, 'stableToBeta')`
- */
-async function modifyFeatureDefinitionsFile(feature, option) {
-
-  const adjustedFeatureDefinitions = { ...featureDefinitions };
-  switch (option) {
-    case 'alphaToBeta':
-      // delete from array:
-      adjustedFeatureDefinitions.inProgressFeatures = adjustedFeatureDefinitions.inProgressFeatures.filter(
-        (f) => f !== feature
-      );
-      break;
-    case 'betaToAlpha':
-      // add to array:
-      adjustedFeatureDefinitions.inProgressFeatures.push(feature);
-      break;
-    case 'betaToStable':
-      // delete from array:
-      adjustedFeatureDefinitions.features = adjustedFeatureDefinitions.features.filter((f) => f !== feature);
-      // add to array:
-      adjustedFeatureDefinitions.stabilizedFeatures.push(feature);
-      break;
-    case 'stableToBeta':
-      // delete from array:
-      adjustedFeatureDefinitions.stabilizedFeatures = adjustedFeatureDefinitions.stabilizedFeatures.filter(
-        (f) => f !== feature
-      );
-      // add to array:
-      adjustedFeatureDefinitions.features.push(feature);
-      break;
-    default:
-      console.error(`ERROR: Invalid option "${option}" provided to modifyFeatureDefinitionsFile()`);
-      exit(-1);
-  }
-
-  const featureDefinitionsFile = path.join(__dirname, '../config/babel/features.js');
-  fs.writeFileSync(featureDefinitionsFile, `module.exports = ${JSON.stringify(adjustedFeatureDefinitions, null, 2)};`);
+async function exec(cmd) {
+  commandsRun.push(cmd);
+  await execInternal(cmd);
 }
 
 /**
  * @returns {Promise<string>} The path to the generated api.json file
  */
-async function generateApiFile(isBaseline) {
-  await exec('rush build -o @azure/communication-react');
+async function generateApiFile(filename) {
+  await exec('rush build -v -o @azure/communication-react');
   if (!fs.existsSync(destinationDir)) {
     fs.mkdirSync(destinationDir, { recursive: true });
   }
 
-  const destinationFile = `${destinationDir}/${isBaseline ? 'baseline.api.json' : 'feature.api.json'}`;
+  const destinationFile = `${destinationDir}/${filename}`;
   fs.copyFileSync(
     path.join(__dirname, '../../packages/communication-react/temp/communication-react.api.json'),
     destinationFile
@@ -204,7 +178,7 @@ function parseArgs(argv) {
       feature: {
         alias: 'f',
         type: 'string',
-        describe: 'Feature name to generate api files for. This should match the name in the babelrc file'
+        describe: 'Feature name to generate api files for. This should match the name in the features.js file'
       }
     })
     .parseSync();
