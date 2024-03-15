65aefc94259c849c2c6263e2d18996c6ef7fdbec
dmceachernmsft <dmceachern@microsoft.com>
2024-03-15 11:42:31 -0700

    remove from features.js

--- common/config/babel/features.js
+++ common/config/babel/features.js
@@ -56,8 +56,6 @@ module.exports = {
     "in-progress-beta-feature-demo",
     // Feature for call transfer
     "call-transfer",
-    // Optimal Video Count
-    "optimal-video-count",
     // Feature for capabilities
     "capabilities",
     // Feature for large Gallery layout
@@ -135,8 +133,6 @@ module.exports = {
     "capabilities",
     // Closed captions
     "close-captions",
-    // Optimal Video Count
-    "optimal-video-count",
     // raise hands feature
     "raise-hand",
     // feature  for vertical overflow gallery
