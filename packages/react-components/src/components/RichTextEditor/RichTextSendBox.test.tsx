db3494556c4ab6a0dd1f17ffebdf030a3fc64204
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-03-19 15:06:41 -0700

    Test update

--- packages/react-components/src/components/RichTextEditor/RichTextSendBox.test.tsx
+++ packages/react-components/src/components/RichTextEditor/RichTextSendBox.test.tsx
@@ -1,14 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/* @conditional-compile-remove(rich-text-editor) */
 import React from 'react';
-/* @conditional-compile-remove(rich-text-editor) */
 import { RichTextSendBox } from './RichTextSendBox';
-/* @conditional-compile-remove(rich-text-editor) */
 import { renderWithLocalization, createTestLocale } from '../utils/testUtils';
 
-/* @conditional-compile-remove(rich-text-editor) */
 describe('RichTextSendBox should work with localization', () => {
   test('Should use localized string', async () => {
     const testLocale = createTestLocale({ richTextSendBox: { placeholderText: Math.random().toString() } });
