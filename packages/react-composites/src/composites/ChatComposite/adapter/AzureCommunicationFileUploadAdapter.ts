e9cebdb9f754cafa73da986182c155115ad9c1fd
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2024-03-22 14:26:22 -0700

    Only set fileSharingMetadata when fileUploads present.

--- packages/react-composites/src/composites/ChatComposite/adapter/AzureCommunicationFileUploadAdapter.ts
+++ packages/react-composites/src/composites/ChatComposite/adapter/AzureCommunicationFileUploadAdapter.ts
@@ -189,18 +189,20 @@ export class AzureCommunicationFileUploadAdapter implements FileUploadAdapter {
  * @param fileUploadUiState {@link FileUploadsUiState}
  * @private
  */
-export const convertFileUploadsUiStateToMessageMetadata = (fileUploads?: FileUploadsUiState): FileSharingMetadata => {
-  const fileMetadata: AttachmentMetadata[] = [];
+export const convertFileUploadsUiStateToMessageMetadata = (
+  fileUploads?: FileUploadsUiState
+): FileSharingMetadata | undefined => {
   if (fileUploads) {
+    const fileMetadata: AttachmentMetadata[] = [];
     Object.keys(fileUploads).forEach((key) => {
       const file = fileUploads[key];
       if (!file.error && file.metadata) {
         fileMetadata.push(file.metadata);
       }
     });
+    return { fileSharingMetadata: JSON.stringify(fileMetadata) };
   }
-
-  return { fileSharingMetadata: JSON.stringify(fileMetadata) };
+  return undefined;
 };
 
 /* @conditional-compile-remove(file-sharing) */
