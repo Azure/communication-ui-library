8821fb008539f60a1352a5f36ad7333286204b8d
dmceachernmsft <dmceachern@microsoft.com>
2024-03-28 11:52:19 -0700

    add missing jsDoc

--- packages/react-composites/src/composites/CallComposite/styles/LocalDeviceSettings.styles.ts
+++ packages/react-composites/src/composites/CallComposite/styles/LocalDeviceSettings.styles.ts
@@ -78,6 +78,9 @@ export const optionIconStyles = mergeStyles({
   verticalAlign: 'text-top'
 });
 
+/**
+ * @private
+ */
 export const deviceSelectionContainerStyles: IStackStyles = {
   root: {
     /**
