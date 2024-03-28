76e8c7a9e69d09e03837e32e23bfeb30cd03876e
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-03-28 04:30:43 +0000

    fix unit tests

--- packages/react-components/src/components/VideoEffects/VideoBackgroundEffectsPicker.test.tsx
+++ packages/react-components/src/components/VideoEffects/VideoBackgroundEffectsPicker.test.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { _VideoBackgroundEffectsPicker } from './VideoBackgroundEffectsPicker';
 
 const TEST_PICKER_OPTIONS = [
@@ -44,20 +44,26 @@ const TEST_PICKER_OPTIONS = [
 ];
 
 describe('Background Picker should have the correct number of tiles', () => {
-  test('Background Picker should have 7 tiles when items are set to wrap', async () => {
-    const { container } = render(<_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow="wrap" />);
+  test.only('Background Picker should have 7 tiles when items are set to wrap', async () => {
+    const { container } = render(
+      <_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow="wrap" ignoreFocusOnMount={true} />
+    );
     expect(container.querySelectorAll('[data-ui-id="video-effects-item"]').length).toBe(TEST_PICKER_OPTIONS.length);
   });
 
   test('Background Picker should have 7 tiles and 2 hidden tiles spanning 3 rows when items per row is set to 3', async () => {
-    const { container } = render(<_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow={3} />);
+    const { container } = render(
+      <_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow={3} ignoreFocusOnMount={true} />
+    );
     expect(container.querySelectorAll('[data-ui-id="video-effects-item"]').length).toBe(7);
     expect(container.querySelectorAll('[data-ui-id="video-effects-hidden-item"]').length).toBe(2);
     expect(container.querySelectorAll('[data-ui-id="video-effects-picker-row"]').length).toBe(3);
   });
 
   test('Background Picker should have 7 tiles and 1 hidden tile spanning 4 rows when items per row is set to 2', async () => {
-    const { container } = render(<_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow={2} />);
+    const { container } = render(
+      <_VideoBackgroundEffectsPicker options={TEST_PICKER_OPTIONS} itemsPerRow={2} ignoreFocusOnMount={true} />
+    );
     expect(container.querySelectorAll('[data-ui-id="video-effects-item"]').length).toBe(7);
     expect(container.querySelectorAll('[data-ui-id="video-effects-hidden-item"]').length).toBe(1);
     expect(container.querySelectorAll('[data-ui-id="video-effects-picker-row"]').length).toBe(4);
