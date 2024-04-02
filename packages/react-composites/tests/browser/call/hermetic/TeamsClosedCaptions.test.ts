c30d2105d6e65d6757ec90499de4fad993a426c2
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2024-04-02 18:50:26 +0000

    test

--- packages/react-composites/tests/browser/call/hermetic/TeamsClosedCaptions.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/TeamsClosedCaptions.test.ts
@@ -103,9 +103,8 @@ test.describe('Captions buttons in call control', () => {
     await page.goto(buildUrlWithMockAdapter(serverUrl, initialState, { newControlBarExperience: 'true' }));
     await pageClick(page, dataUiId('common-call-composite-more-button'));
     if (isTestProfileMobile(testInfo)) {
-      page.keyboard.press('PageDown');
-      page.keyboard.press('PageDown');
-      page.keyboard.press('PageDown');
+      const captionsButton = page.locator(`[id="common-call-composite-captions-settings-button"]`);
+      captionsButton.scrollIntoViewIfNeeded();
     }
     expect(await stableScreenshot(page)).toMatchSnapshot(`caption-button-teams-call.png`);
   });
