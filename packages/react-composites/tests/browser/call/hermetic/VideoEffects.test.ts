b9c6ed3eb6bae3957be117c0a9e1a0d260530db6
Mohtasim <mbellah@microsoft.com>
2024-04-03 06:27:17 +0600

    fix url for background effect tests
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- packages/react-composites/tests/browser/call/hermetic/VideoEffects.test.ts
+++ packages/react-composites/tests/browser/call/hermetic/VideoEffects.test.ts
@@ -234,37 +234,37 @@ test.describe('Video background effects error tests', async () => {
 const videoBackgroundImages = [
   {
     key: 'ab1',
-    url: '/backgrounds/abstract1.jpg',
+    url: 'assets/backgrounds/abstract1.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab2',
-    url: '/backgrounds/abstract2.jpg',
+    url: 'assets/backgrounds/abstract2.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab3',
-    url: '/backgrounds/abstract3.jpg',
+    url: 'assets/backgrounds/abstract3.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab4',
-    url: '/backgrounds/room1.jpg',
+    url: 'assets/backgrounds/room1.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab5',
-    url: '/backgrounds/room2.jpg',
+    url: 'assets/backgrounds/room2.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab6',
-    url: '/backgrounds/room3.jpg',
+    url: 'assets/backgrounds/room3.jpg',
     tooltipText: 'Custom Background'
   },
   {
     key: 'ab7',
-    url: '/backgrounds/room4.jpg',
+    url: 'assets/backgrounds/room4.jpg',
     tooltipText: 'Custom Background'
   }
 ];
