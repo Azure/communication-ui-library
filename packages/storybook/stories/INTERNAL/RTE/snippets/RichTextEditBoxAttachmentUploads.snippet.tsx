ead70c5e05f0c7045a36c1c1557963ed5a365ee4
Patrick Latter <73612854+palatter@users.noreply.github.com>
2024-04-03 15:40:36 -0700

    Add attachments

--- packages/storybook/stories/INTERNAL/RTE/snippets/RichTextEditBoxAttachmentUploads.snippet.tsx
+++ packages/storybook/stories/INTERNAL/RTE/snippets/RichTextEditBoxAttachmentUploads.snippet.tsx
@@ -1,46 +1,49 @@
 import { FluentThemeProvider } from '@azure/communication-react';
 import React from 'react';
-import { ChatMessageComponentAsRichTextEditBox } from '@internal/react-components';
+import {
+  ChatMessageComponentAsRichTextEditBox,
+  ChatMessageComponentAsRichTextEditBoxProps
+} from '@internal/react-components';
 
 export const RichTextEditBoxAttachmentUploadsExample: () => JSX.Element = () => {
   const timeoutRef = React.useRef<NodeJS.Timeout>();
   const delayForSendButton = 300;
 
+  const attachmentMetadata = [
+    {
+      id: '1',
+      name: 'Sample.pdf',
+      progress: 0.75
+    },
+    {
+      id: '2',
+      name: 'SampleXl.xlsx',
+      progress: 0.33
+    }
+  ];
+
+  const props: ChatMessageComponentAsRichTextEditBoxProps = {
+    onSubmit: async (message, metadata, options) => {
+      timeoutRef.current = setTimeout(() => {
+        alert(`sent message: ${message} `);
+      }, delayForSendButton);
+    },
+    message: {
+      messageType: 'chat',
+      content: 'Hi! How are you?',
+      contentType: 'richtext/html',
+      messageId: '1',
+      createdOn: new Date(),
+      attachments: attachmentMetadata
+    },
+    strings: {}
+  };
+
   return (
     <FluentThemeProvider>
       <div style={{ width: '31.25rem' }}>
-        <ChatMessageComponentAsRichTextEditBox
-          onSubmit={async (message) => {
-            timeoutRef.current = setTimeout(() => {
-              alert(`sent message: ${message} `);
-            }, delayForSendButton);
-          }}
-          message={{
-            messageType: 'chat',
-            content: 'Hi! How are you?',
-            contentType: 'richtext/html',
-            messageId: '1',
-            createdOn: new Date()
-          }}
-          strings={{}}
-        />
+        <ChatMessageComponentAsRichTextEditBox {...props} />
       </div>
     </FluentThemeProvider>
   );
 };
-
-// activeAttachmentUploads={[
-//     {
-//       id: '1',
-//       name: 'Sample.pdf',
-//       progress: 0.75
-//     },
-//     {
-//       id: '2',
-//       name: 'SampleXl.xlsx',
-//       progress: 0.33
-//     }
-//   ]}
-//   onSendMessage={async () => {
-//     return;
-//   }}
