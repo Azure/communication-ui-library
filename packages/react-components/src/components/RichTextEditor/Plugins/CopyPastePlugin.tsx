533577bc4e8f92058731cb6af1066f8547bf35c1
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2024-04-03 16:10:49 -0700

    Remove all parents and add tests

--- packages/react-components/src/components/RichTextEditor/Plugins/CopyPastePlugin.tsx
+++ packages/react-components/src/components/RichTextEditor/Plugins/CopyPastePlugin.tsx
@@ -17,14 +17,29 @@ export default class CopyPastePlugin implements EditorPlugin {
   dispose(): void {}
 
   onPluginEvent(event: PluginEvent): void {
-    // We don't support the pasting options such as paste as image yet.
-    if (event.eventType === CompatiblePluginEventType.BeforePaste && event.pasteType === CompatiblePasteType.Normal) {
-      event.fragment.querySelectorAll('img').forEach((image) => {
-        if (image.parentElement?.childNodes.length === 1) {
-          image.parentElement?.remove();
-        }
-        image.remove();
-      });
-    }
+    removeImageElement(event);
   }
 }
+
+/**
+ * @internal
+ * Exported only for unit testing
+ */
+export const removeImageElement = (event: PluginEvent): void => {
+  // We don't support the pasting options such as paste as image yet.
+  if (event.eventType === CompatiblePluginEventType.BeforePaste && event.pasteType === CompatiblePasteType.Normal) {
+    event.fragment.querySelectorAll('img').forEach((image) => {
+      // If the image is the only child of its parent, remove all the parents of this img element before removing the image element.
+      let parentNode = image.parentElement;
+      const parentsToRemove: Array<HTMLElement | null> = [];
+      while (parentNode?.childNodes.length === 1) {
+        parentsToRemove.push(parentNode);
+        parentNode = parentNode.parentElement;
+      }
+      parentsToRemove.forEach((parent) => {
+        parent?.remove();
+      });
+      image.remove();
+    });
+  }
+};
