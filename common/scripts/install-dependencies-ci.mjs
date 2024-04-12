8ecef461bf56ff4010a054803a92a94355f28661
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-04-12 15:32:52 +0000

    move install commands to script

---
+++ common/scripts/install-dependencies-ci.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+///
+/// This script is used to run install dependency commands in CI.
+/// `yml` files are not good at reusing commands, so this script helps us to write-once and reuse.
+///
+
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+async function main(args) {
+  const flavor = args[2];
+  if (!flavor) {
+    throw new Error('Usage: node install-dependencies-ci.mjs <flavor>');
+  }
+
+  // Install rush based on the version in rush.json.
+  // If we do not install that specific version, the first rush command has to reinstall rush.
+  const rushVersion = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../rush.json'))).rushVersion;
+  execSync(`npm install -g @microsoft/rush@${rushVersion}`);
+
+  // Force build flavor. This is necessary in CI to catch downstream dependency issues.
+  // For example, we had an issue where our stable falvor was not catching that the
+  // `@azure/communication-calling-effects` package should've only be installed in beta flavor
+  // and was failing to error out in stable flavor.
+  execSync(`node ./common/scripts/force-build-flavor.mjs ${flavor}`);
+
+  // Finally, install node module dependencies based on the flavor.
+  execSync(`rush switch-flavor:${flavor}`);
+}
+
+main(process.argv);
