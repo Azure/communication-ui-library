0009763b01d9895895ecc4dec06be41005896c27
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2024-04-15 16:00:29 -0700

    [Improvement] Storybook 8 Migration Packlet (#4454)

---
+++ packages/storybook8/scripts/build.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { getBuildFlavor, exec } from './common.mjs';
+import { quote } from 'shell-quote';
+
+async function main() {
+  if (getBuildFlavor() === 'stable') {
+    console.log('Skipping storybook build for stable build');
+    return;
+  }
+  await exec(quote(['npx', 'storybook', 'build', '--loglevel', 'warn', ...process.argv.slice(2)]));
+}
+
+await main();
