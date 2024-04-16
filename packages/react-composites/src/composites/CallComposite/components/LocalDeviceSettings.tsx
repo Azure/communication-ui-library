2e682fd98e4737b55c9f00a7cdfb0e9828460c83
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-04-16 21:12:04 +0000

    Improve detecting if camera permission is available by using Permissions API in the StatefulCallClient

--- packages/react-composites/src/composites/CallComposite/components/LocalDeviceSettings.tsx
+++ packages/react-composites/src/composites/CallComposite/components/LocalDeviceSettings.tsx
@@ -5,7 +5,6 @@ import { AudioDeviceInfo, VideoDeviceInfo } from '@azure/communication-calling';
 import { Dropdown, IDropdownOption, Label, mergeStyles, Stack } from '@fluentui/react';
 
 import { DefaultButton } from '@fluentui/react';
-/* @conditional-compile-remove(call-readiness) */
 import { useEffect } from 'react';
 import { useTheme, VideoStreamOptions, _DevicePermissionDropdown } from '@internal/react-components';
 import React from 'react';
@@ -116,16 +115,12 @@ export const LocalDeviceSettings = (props: LocalDeviceSettingsType): JSX.Element
   const cameraPermissionGranted = props.cameraPermissionGranted;
   const micPermissionGranted = props.microphonePermissionGranted;
 
-  let roleCanUseCamera = true;
-  let roleCanUseMic = true;
-
-  roleCanUseCamera = role === 'Consumer' ? false : true;
-  roleCanUseMic = role === 'Consumer' ? false : true;
+  const roleCanUseCamera = role === 'Consumer' ? false : true;
+  const roleCanUseMic = role === 'Consumer' ? false : true;
 
   // TODO: speaker permission is tied to microphone permission (when you request 'audio' permission using the SDK) its
   // actually granting access to query both microphone and speaker. However the browser popup asks you explicity for
   // 'microphone'. This needs investigation on how we want to handle this and maybe needs follow up with SDK team.
-  /* @conditional-compile-remove(call-readiness) */
   useEffect(() => {
     if (cameraPermissionGranted) {
       adapter.queryCameras();
