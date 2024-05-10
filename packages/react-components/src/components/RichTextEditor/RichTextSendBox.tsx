75688c9d8da38c9594adba20b48d0c9f22e183f1
Patrick Latter <73612854+palatter@users.noreply.github.com>
2024-05-10 13:53:47 -0700

    Update richtextsendbox to use options

--- packages/react-components/src/components/RichTextEditor/RichTextSendBox.tsx
+++ packages/react-components/src/components/RichTextEditor/RichTextSendBox.tsx
@@ -151,7 +151,7 @@ export interface RichTextSendBoxProps {
   /**
    * Callback function used when the send button is clicked.
    */
-  onSendMessage: (content: string) => Promise<void>;
+  onSendMessage: (content: string, options?: MessageOptions) => Promise<void>;
   /**
    * Optional callback called when user is typing
    */
