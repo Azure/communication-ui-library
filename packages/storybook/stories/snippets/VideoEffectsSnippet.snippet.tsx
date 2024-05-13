0d4d97e1593c7516b7802ab90fe655377d2c3617
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2024-05-13 20:31:29 +0000

    updated API ref

--- packages/storybook/stories/snippets/VideoEffectsSnippet.snippet.tsx
+++ packages/storybook/stories/snippets/VideoEffectsSnippet.snippet.tsx
@@ -1,33 +1,42 @@
 import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
-import { AzureCommunicationCallAdapterOptions } from '@azure/communication-react';
-import { onResolveVideoEffectDependencyLazy } from '@azure/communication-react';
-import { CallComposite, CallCompositeOptions, useAzureCommunicationCallAdapter } from '@azure/communication-react';
-import { PartialTheme, Stack, Theme } from '@fluentui/react';
+import {
+  AzureCommunicationCallAdapterOptions,
+  onResolveVideoEffectDependencyLazy,
+  CallComposite,
+  CallCompositeOptions,
+  useAzureCommunicationCallAdapter
+} from '@azure/communication-react';
+import { Stack } from '@fluentui/react';
 import React, { useMemo } from 'react';
 
 export type ContainerProps = {
   userId: CommunicationUserIdentifier;
   token: string;
   groupId: string;
-  formFactor?: 'desktop' | 'mobile';
-  fluentTheme?: PartialTheme | Theme;
   options?: CallCompositeOptions;
 };
 
+/* Video background images are passed to the composite to provide the user with a list of video background images to choose from.
+  Each video background image is defined by an object with the following properties:
+  - key: A unique key that identifies the video background image.
+  - url: The URL of the video background image.
+  - tooltipText: The tooltip text that is displayed when the user hovers over the video background image in the video effects menu.
+ */
 const videoBackgroundImages = [
   {
-    key: 'ab1',
-    url: '/assets/backgrounds/contoso.png',
-    tooltipText: 'Custom Background'
+    key: 'background1',
+    url: '/assets/backgrounds/room.png',
+    tooltipText: 'Room Background'
   },
   {
-    key: 'ab1',
-    url: '/assets/backgrounds/contoso.png',
-    tooltipText: 'Custom Background'
+    key: 'background2',
+    url: '/assets/backgrounds/office.png',
+    tooltipText: 'Office Background'
   }
 ];
+
 export const ContosoCallContainer = (props: ContainerProps): JSX.Element => {
-  const { userId, token, formFactor, fluentTheme, options, groupId } = props;
+  const { userId, token, options, groupId } = props;
   const credential = useMemo(() => {
     try {
       return new AzureCommunicationTokenCredential(token);
@@ -60,7 +69,7 @@ export const ContosoCallContainer = (props: ContainerProps): JSX.Element => {
   if (adapter) {
     return (
       <Stack>
-        <CallComposite adapter={adapter} formFactor={formFactor} fluentTheme={fluentTheme} options={options} />
+        <CallComposite adapter={adapter} options={options} />
       </Stack>
     );
   }
