40d797f692f8b5cbdcf59cdb80b6dc39d1869c65
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2024-05-15 06:47:14 +0000

    Fix local participant item to show menu icon when hovered

--- packages/react-components/src/components/ParticipantItem.tsx
+++ packages/react-components/src/components/ParticipantItem.tsx
@@ -166,7 +166,7 @@ export const ParticipantItem = (props: ParticipantItemProps): JSX.Element => {
 
   const strings = { ...localeStrings, ...props.strings };
   const participantStateString = participantStateStringTrampoline(props, strings);
-  const showMenuIcon = !me && !participantStateString && (itemHovered || !menuHidden);
+  const showMenuIcon = !participantStateString && (itemHovered || !menuHidden);
 
   // For 'me' show empty name so avatar will get 'Person' icon, when there is no name
   const meAvatarText = displayName?.trim() || '';
