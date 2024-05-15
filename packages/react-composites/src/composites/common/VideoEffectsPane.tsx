17767507a74555817ade4e5d52d49f46d46b41cc
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-05-15 23:22:33 +0000

    remove unused import

--- packages/react-composites/src/composites/common/VideoEffectsPane.tsx
+++ packages/react-composites/src/composites/common/VideoEffectsPane.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { useCallback, useMemo } from 'react';
 
