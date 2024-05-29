d98d564b7df2f066b6ca208b49e1a7339ed44973
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-05-29 16:48:00 -0700

    Revert playwright setup changes that will be commited to a separate branch

--- common/config/playwright/playwright.config.common.ts
+++ common/config/playwright/playwright.config.common.ts
@@ -67,12 +67,7 @@ export const config: PlaywrightTestConfig<TestOptions> = {
     }
   ],
   expect: {
-    // composites tests use toMatchSnapshot
     toMatchSnapshot: {
-      maxDiffPixels: 1
-    },
-    // components tests use toHaveScreenshot
-    toHaveScreenshot: {
       maxDiffPixels: 1
     }
   }
