5527b297da94006106219eb896a388c59b609d39
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-05-29 20:22:12 -0700

    threshold update

--- common/config/playwright/playwright.config.common.ts
+++ common/config/playwright/playwright.config.common.ts
@@ -74,7 +74,7 @@ export const config: PlaywrightTestConfig<TestOptions> = {
       // maxDiffPixels: 1,
       // make stricter comparison for colors, default value was 0.2
       // which didn't caught some color differences for gray colors
-      threshold: 0
+      threshold: 0.05
     }
   }
 };
