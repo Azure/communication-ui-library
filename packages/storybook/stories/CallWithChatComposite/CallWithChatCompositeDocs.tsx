f838285dfe48bcde7e66f902360d883a67c99959
John <109105353+jpeng-ms@users.noreply.github.com>
2024-06-04 09:16:52 -0700

    Update CallWithChatCompositeDocs.tsx

--- packages/storybook/stories/CallWithChatComposite/CallWithChatCompositeDocs.tsx
+++ packages/storybook/stories/CallWithChatComposite/CallWithChatCompositeDocs.tsx
@@ -229,9 +229,7 @@ export const Docs: () => JSX.Element = () => {
           When using the `CallWithChat` Composite in a Teams interop scenario, the Azure Communication Services end user
           can recieve file attachments from Teams user without any additional setup. Simply join the Teams meeting as an
           Azure Communication Services end user, as soon as the Teams user sends a file from the Teams client, the Azure
-          Communication Services end user will be able to see shared files in the chat thread. Please check out the
-          tutorial for [Enable file sharing using UI Library in Teams Interoperability
-          Chat](https://learn.microsoft.com/en-us/azure/communication-services/tutorials/file-sharing-tutorial-interop-chat)
+          Communication Services end user will be able to see shared files in the chat thread.
         </Description>
       </div>
 
