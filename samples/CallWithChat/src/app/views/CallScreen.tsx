7177428f36174b0abce75a97d09e4e3751a462a0
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2024-06-25 00:02:32 +0000

    add refresh hook to apps

--- samples/CallWithChat/src/app/views/CallScreen.tsx
+++ samples/CallWithChat/src/app/views/CallScreen.tsx
@@ -111,6 +111,24 @@ export const CallScreen = (props: CallScreenProps): JSX.Element => {
     };
   }, []);
 
+  // Update stale mobile browsers
+  useEffect(() => {
+    /**
+     * We want to make sure that the page is up to date. If for example a browser is dismissed
+     * on mobile, the page will be stale when opened again. This event listener will reload the page
+     */
+    window.addEventListener('pageshow', (event) => {
+      if (event.persisted) {
+        window.location.reload();
+      }
+    });
+    return () => {
+      window.removeEventListener('pageshow', () => {
+        window.location.reload();
+      });
+    };
+  }, []);
+
   const callIdRef = useRef<string>();
   const { currentTheme, currentRtl } = useSwitchableFluentTheme();
   const isMobileSession = useIsMobile();
