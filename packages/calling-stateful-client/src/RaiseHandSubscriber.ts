a56c6973bda36f3c1e3318652717c18340280bde
Ruslan Zdor <ruslanzdor@microsoft.com>
2024-07-02 11:04:23 -0700

    remove change

--- packages/calling-stateful-client/src/RaiseHandSubscriber.ts
+++ packages/calling-stateful-client/src/RaiseHandSubscriber.ts
@@ -5,7 +5,6 @@ import { RaiseHandCallFeature, RaisedHandChangedEvent } from '@azure/communicati
 import { CallContext } from './CallContext';
 import { CallIdRef } from './CallIdRef';
 import { toFlatCommunicationIdentifier } from '@internal/acs-ui-common';
-
 /**
  * @private
  */
