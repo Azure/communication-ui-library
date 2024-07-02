b0dc9a306be914ed18ceadf748e718a19ec9c177
Edward Lee <edwardlee@microsoft.com>
2024-07-02 15:58:44 -0700

    Move Soft mute to beta and address arb comment

--- common/config/babel/features.js
+++ common/config/babel/features.js
@@ -19,14 +19,14 @@ module.exports = {
     "hide-attendee-name",
     // Demo feature. Used in live-documentation of conditional compilation. Do not use in production code.
     "in-progress-beta-feature-demo",
+    // Get join conference information
+    'teams-meeting-conference',
     // Feature for Rich Text Editor (RTE) composite support
     "rich-text-editor-composite-support",
     // Feature for Rich Text Editor (RTE) image upload support
     "rich-text-editor-image-upload",
     // Feature for showing notifications
-    "notifications",
-    // Soft Mute feature for ACS and Interop calls
-    "soft-mute"
+    "notifications"
   ],
   beta: [
     "call-readiness",
@@ -78,7 +78,9 @@ module.exports = {
     // Feature for Rich Text Editor (RTE) support
     'rich-text-editor',
     // Feature to support file sharing in ACS chats
-    "file-sharing-acs"
+    "file-sharing-acs",
+    // Soft Mute feature for ACS and Interop calls
+    "soft-mute"
   ],
   stable: [
     // Demo feature. Used in live-documentation of conditional compilation.
@@ -95,8 +97,6 @@ module.exports = {
     // Feature for end call options 
     'end-call-options',
      // Feature to support file sharing in Teams interoperability chats
-     "file-sharing-teams-interop",
-    // Get join conference information
-    'teams-meeting-conference'
+     "file-sharing-teams-interop"
   ]
 }
