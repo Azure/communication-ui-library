8c3b0d3ef4a638559fe00231397d9483079a85d3
Edward Lee <edwardlee@microsoft.com>
2024-07-03 09:54:25 -0700

    Resolve feature.js issue

--- common/config/babel/features.js
+++ common/config/babel/features.js
@@ -19,8 +19,6 @@ module.exports = {
     "hide-attendee-name",
     // Demo feature. Used in live-documentation of conditional compilation. Do not use in production code.
     "in-progress-beta-feature-demo",
-    // Get join conference information
-    'teams-meeting-conference',
     // Feature for Rich Text Editor (RTE) composite support
     "rich-text-editor-composite-support",
     // Feature for Rich Text Editor (RTE) image upload support
@@ -96,7 +94,9 @@ module.exports = {
     'meeting-id',
     // Feature for end call options 
     'end-call-options',
-     // Feature to support file sharing in Teams interoperability chats
-     "file-sharing-teams-interop"
+    // Feature to support file sharing in Teams interoperability chats
+    "file-sharing-teams-interop",
+    // Get join conference information
+    'teams-meeting-conference'
   ]
 }
