c99d4785fece9babb4f2e2e8cad9a2b408b602d2
AmyL219 <longamy@microsoft.com>
2024-07-03 16:27:17 -0700

    Structure update

--- packages/storybook8/stories/Components/SendBox/snippets/SendBox.snippet.tsx
+++ packages/storybook8/stories/Components/SendBox/snippets/SendBox.snippet.tsx
@@ -1,5 +1,9 @@
-import { SendBox, FluentThemeProvider } from '@azure/communication-react';
+import { SendBox, FluentThemeProvider, DEFAULT_COMPONENT_ICONS } from '@azure/communication-react';
+import { initializeIcons, registerIcons } from '@fluentui/react';
 import React from 'react';
+
+initializeIcons();
+registerIcons({ icons: DEFAULT_COMPONENT_ICONS });
 
 export const SendBoxExample: () => JSX.Element = () => (
   <FluentThemeProvider>
