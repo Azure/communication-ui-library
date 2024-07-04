0b1a601e34acb13040bf1b693f2e856430adec0d
AmyL219 <longamy@microsoft.com>
2024-07-04 15:50:29 -0700

    Update

--- packages/storybook8/stories/controlsUtils.ts
+++ packages/storybook8/stories/controlsUtils.ts
@@ -335,7 +335,7 @@ export const controlsToAdd = {
       'Rick, Daryl, Michonne, Dwight, Pam, Michael, Jim, Kevin, Creed, Angela, Andy, Stanley, Meredith, Phyllis, Oscar, Ryan, Kelly, Andy, Toby, Darryl, Gabe, Erin',
     name: 'Remote participants (comma separated)'
   },
-  remoteParticipants: { control: 'object', defaultValue: defaultRemoteParticipants, name: 'Remote participants' },
+  remoteParticipants: { control: 'object', name: 'Remote participants' },
   requiredDisplayName: {
     control: 'text',
     defaultValue: 'John Smith',
