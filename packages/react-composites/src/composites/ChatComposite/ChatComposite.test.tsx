a44d43f4a38fa36796dc4ef1f1752c73d8326d5e
Patrick Latter <73612854+palatter@users.noreply.github.com>
2024-07-05 11:56:25 -0700

    Fix chat composite test

--- packages/react-composites/src/composites/ChatComposite/ChatComposite.test.tsx
+++ packages/react-composites/src/composites/ChatComposite/ChatComposite.test.tsx
@@ -1,18 +1,49 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { ChatAdapter } from './adapter/ChatAdapter';
+import { ChatAdapter, ChatAdapterState } from './adapter/ChatAdapter';
 import { registerIcons } from '@fluentui/react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { COMPOSITE_LOCALE_ZH_TW } from '../localization/locales/zh-TW/CompositeLocale';
 import { ChatComposite } from './ChatComposite';
 import React from 'react';
 
 function createMockChatAdapter(): ChatAdapter {
   const chatAdapter = {} as ChatAdapter;
-
+  chatAdapter.onStateChange = jest.fn();
+  chatAdapter.offStateChange = jest.fn();
+  chatAdapter.fetchInitialData = jest.fn();
+  chatAdapter.loadPreviousChatMessages = jest.fn();
+  chatAdapter.getState = jest.fn(
+    (): ChatAdapterState => ({
+      userId: { kind: 'communicationUser', communicationUserId: 'test' },
+      displayName: 'test',
+      thread: {
+        chatMessages: {},
+        participants: {},
+        threadId: 'test',
+        readReceipts: [],
+        typingIndicators: [],
+        latestReadTime: new Date()
+      },
+      latestErrors: {},
+      error: undefined
+    })
+  );
   return chatAdapter;
 }
+
+// Mock the richTextSendBoxWrapper component as it's lazy loaded in ChatComposite
+function MockedRichTextSendBoxWrapper(): JSX.Element {
+  return <div id="richTextSendBoxWrapper">Mocked RichTextSendboxWrapper</div>;
+}
+
+jest.mock('../common/RichTextSendBoxWrapper', () => {
+  return {
+    RichTextSendBoxWrapper: MockedRichTextSendBoxWrapper
+  };
+});
 
 describe('ChatAdapter', () => {
   beforeEach(() => {
@@ -24,20 +55,22 @@ describe('ChatAdapter', () => {
     });
   });
 
-  test('Rich text editor should show if its enabled', async () => {
+  test('Chat Composite should show richTextSendBox if it is enabled', async () => {
     const mockBaseCompositeProps = {
       fluentTheme: {},
       icons: {},
       locale: COMPOSITE_LOCALE_ZH_TW,
       rtl: true,
       onFetchAvatarPersonaData: jest.fn(),
-      richTextEditor: true
+      options: {
+        richTextEditor: true
+      }
     };
 
     const mockChatAdapter = createMockChatAdapter();
 
-    const { container } = render(<ChatComposite adapter={mockChatAdapter} {...mockBaseCompositeProps} />);
-
-    expect(container.querySelector('[data-testid="rich-text-editor-warpper"]')).toBeTruthy();
+    render(<ChatComposite adapter={mockChatAdapter} {...mockBaseCompositeProps} />);
+    const textToMatch = await screen.findByText(/Mocked RichTextSendboxWrapper/);
+    expect(textToMatch).toBeInTheDocument();
   });
 });
