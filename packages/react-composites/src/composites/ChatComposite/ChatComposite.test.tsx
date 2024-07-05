8f2dccc18181f7f317ea5dd8723d34ac3cb0d638
Patrick Latter <73612854+palatter@users.noreply.github.com>
2024-07-05 15:10:27 -0700

    Add cc

--- packages/react-composites/src/composites/ChatComposite/ChatComposite.test.tsx
+++ packages/react-composites/src/composites/ChatComposite/ChatComposite.test.tsx
@@ -1,14 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import { ChatAdapter, ChatAdapterState } from './adapter/ChatAdapter';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import { registerIcons } from '@fluentui/react';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import { render, screen } from '@testing-library/react';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import '@testing-library/jest-dom';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import { COMPOSITE_LOCALE_ZH_TW } from '../localization/locales/zh-TW/CompositeLocale';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import { ChatComposite } from './ChatComposite';
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 import React from 'react';
 
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 function createMockChatAdapter(): ChatAdapter {
   const chatAdapter = {} as ChatAdapter;
   chatAdapter.onStateChange = jest.fn();
@@ -35,16 +43,19 @@ function createMockChatAdapter(): ChatAdapter {
 }
 
 // Mock the richTextSendBoxWrapper component as it's lazy loaded in ChatComposite
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 function MockedRichTextSendBoxWrapper(): JSX.Element {
   return <div id="richTextSendBoxWrapper">Mocked RichTextSendboxWrapper</div>;
 }
 
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 jest.mock('../common/RichTextSendBoxWrapper', () => {
   return {
     RichTextSendBoxWrapper: MockedRichTextSendBoxWrapper
   };
 });
 
+/* @conditional-compile-remove(rich-text-editor-composite-support) */
 describe('ChatComposite', () => {
   beforeEach(() => {
     // Register icons used in ChatComposite to avoid warnings
