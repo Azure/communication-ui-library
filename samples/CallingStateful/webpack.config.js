a763e2b5111fb0c974e4509fb3373ce33a70c3d0
Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>
2024-07-12 14:51:55 -0700

    [Inbound Calling] Add Stateful Client Calling sample App (#4840)
    * Add new component
    
    * Add icons and Locale
    
    * add CC to notification
    
    * add storybook page
    
    * lint
    
    * Change files
    
    * Add style API
    
    * add customization updates and snippet
    
    * fix lint
    
    * Change files
    
    * app initial commit
    
    * it lives
    
    * update to support multiple call notifications
    
    * update API
    
    * create login component
    
    * update stable lock
    
    * simplify to 1 array
    
    * fix cte
    
    * add incoming Call Manager component
    
    * fix lint
    
    * add cc
    
    * fix lock

---
+++ samples/CallingStateful/webpack.config.js
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+module.exports = (env) => {
+  const babelConfig = require('./.babelrc.js');
+  const commonConfig = require('../../common/config/webpack/sampleapp.webpack.config')(__dirname, env, babelConfig);
+  return commonConfig;
+};
