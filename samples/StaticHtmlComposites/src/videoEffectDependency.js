3e1ea7c59f6ce09960e81d18f932516a3190621d
Porter Nan <jiangnanhello@live.com>
2024-07-16 21:20:53 +0000

    Add extra bundle for enable video effect

---
+++ samples/StaticHtmlComposites/src/videoEffectDependency.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { onResolveVideoEffectDependency } from '@azure/communication-react';
+
+export { onResolveVideoEffectDependency };
