aadceafea6a596cd7481cb5258a4c10879650a64
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2024-08-02 15:09:55 -0700

    Add comment

--- packages/react-composites/src/composites/ChatComposite/ImageUpload/ImageUploadUtils.tsx
+++ packages/react-composites/src/composites/ChatComposite/ImageUpload/ImageUploadUtils.tsx
@@ -274,6 +274,8 @@ export const cancelInlineImageUpload = (
   });
 
   if (imageUpload?.metadata.progress === 1 && imageUpload?.metadata.url) {
+    // The image id that we got back from the ChatSDK response is stored in the image src attribute,
+    // while the metadata id is the internal image id that we assigned to the image when it was pasted in.
     deleteInlineImageFromServer(imageUpload?.metadata.url, adapter);
   }
 };
