54d6ce8e8a755e4aee5cd20cd9fae363a4d8d6c9
alkwa-msft <79329532+alkwa-msft@users.noreply.github.com>
2024-08-07 16:28:00 -0700

    Example pages for LocalPreview, DeviceSettings, TeamsTheme, and Incoming Call Toast/Modal (#4982)
    * Added in Examples for DeviceSettings, Themes, and LocalPreview
    
    * adding in incoming call modal and toast examples
    
    * moving specific story snapshots to the correct subfolders
    
    * removing component directory for inbound calling notification

---
+++ packages/storybook8/stories/Examples/DeviceSettings/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta } from '@storybook/react';
+import { DeviceSettings as DeviceSettingsStory } from './DeviceSettingsDropDown.story';
+
+export const DeviceSettings = {
+  render: DeviceSettingsStory
+};
+
+export default {
+  id: 'examples-devicesettings',
+  title: 'Examples/Device Settings',
+  component: DeviceSettingsStory,
+  args: {}
+} as Meta;
