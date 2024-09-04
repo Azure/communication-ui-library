2fded05fa476057d81cf4cf80368241da3072a13
JoshuaLai <9044372+JoshuaLai@users.noreply.github.com>
2024-09-04 16:41:29 +0000

    Adding in logic to load icons properly

--- packages/storybook8/stories/Components/SendBox/RichTextSendBox/snippets/RichTextSendBoxAttachmentUploads.snippet.tsx
+++ packages/storybook8/stories/Components/SendBox/RichTextSendBox/snippets/RichTextSendBoxAttachmentUploads.snippet.tsx
@@ -1,5 +1,11 @@
-import { FluentThemeProvider, RichTextSendBox } from '@azure/communication-react';
+import { FluentThemeProvider, RichTextSendBox, DEFAULT_COMPONENT_ICONS } from '@azure/communication-react';
+import { initializeIcons, registerIcons } from '@fluentui/react';
+import { initializeFileTypeIcons } from '@fluentui/react-file-type-icons';
 import React from 'react';
+
+initializeIcons();
+initializeFileTypeIcons();
+registerIcons({ icons: { ...DEFAULT_COMPONENT_ICONS } });
 
 export const RichTextSendBoxAttachmentUploadsExample: () => JSX.Element = () => (
   <FluentThemeProvider>
