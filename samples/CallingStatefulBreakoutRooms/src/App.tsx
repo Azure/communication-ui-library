70a2a829c07c78dfe532cb687d86998004874c47
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2024-09-04 06:56:07 +0000

    Breakout rooms part 1: Manage call transitions

--- samples/CallingStatefulBreakoutRooms/src/App.tsx
+++ samples/CallingStatefulBreakoutRooms/src/App.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Call, CallAgent } from '@azure/communication-calling';
+import { BreakoutRoomsEventData, Call, CallAgent, Features } from '@azure/communication-calling';
 import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 import {
   CallAgentProvider,
@@ -13,7 +13,7 @@ import {
   StatefulCallClient
 } from '@azure/communication-react';
 import { initializeIcons, registerIcons } from '@fluentui/react';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import CallingComponents from './components/CallingComponents';
 
 initializeIcons();
@@ -28,6 +28,8 @@ function App(): JSX.Element {
   const [statefulCallClient, setStatefulCallClient] = useState<StatefulCallClient>();
   const [callAgent, setCallAgent] = useState<CallAgent>();
   const [call, setCall] = useState<Call>();
+  // Main meeting call is used to return to the main meeting from a breakout room
+  const [mainMeetingCall, setMainMeetingCall] = useState<Call>();
 
   useEffect(() => {
     const statefulCallClient = createStatefulCallClient({
@@ -58,9 +60,62 @@ function App(): JSX.Element {
 
   useEffect(() => {
     if (callAgent !== undefined) {
-      setCall(callAgent.join({ meetingLink }));
+      const call = callAgent.join({ meetingLink });
+      setCall(call);
+      // Set call as main meeting to return to from a breakout room
+      setMainMeetingCall(call);
     }
   }, [callAgent]);
+
+  // Function to return to the main meeting from a breakout room
+  const returnToMainMeeting = useCallback(async () => {
+    if (mainMeetingCall) {
+      setCall(mainMeetingCall);
+      if (mainMeetingCall.state === 'LocalHold') {
+        mainMeetingCall.resume();
+      }
+    }
+  }, [mainMeetingCall, setCall]);
+
+  // Breakout rooms update event handler
+  const onBreakoutRoomsUpdated = useCallback(
+    (eventData: BreakoutRoomsEventData) => {
+      if (eventData.type === 'join') {
+        // This case covers the scenario when the user joins a breakout room.
+        const breakoutRoomCall = eventData.data as Call;
+        setCall(breakoutRoomCall);
+      } else if (eventData.type === 'assignedBreakoutRooms') {
+        const breakoutRoom = eventData.data;
+        const assignedBreakoutRoom = mainMeetingCall?.feature(Features.BreakoutRooms).assignedBreakoutRooms;
+        if (breakoutRoom?.state === 'open') {
+          if (assignedBreakoutRoom && assignedBreakoutRoom.call && assignedBreakoutRoom.call.id !== call?.id) {
+            // This case covers the scenario when the user is re-assigned to another breakout room that is open.
+            const call = assignedBreakoutRoom.call as Call;
+            setCall(call);
+          }
+        } else {
+          if (assignedBreakoutRoom?.call) {
+            if (assignedBreakoutRoom.call.id !== call?.id) {
+              // This case covers the scenario when the user is re-assigned breakout room that is closed.
+              returnToMainMeeting();
+            }
+          } else {
+            // This case covers the scenario when the user is assigned breakout room that is closed or
+            // when the user is unassigned from a breakout room.
+            returnToMainMeeting();
+          }
+        }
+      }
+    },
+    [call, setCall, mainMeetingCall, returnToMainMeeting]
+  );
+
+  useEffect(() => {
+    if (call) {
+      // Subscribe to breakout rooms updated events
+      call.feature(Features.BreakoutRooms)?.on('breakoutRoomsUpdated', onBreakoutRoomsUpdated);
+    }
+  }, [call, onBreakoutRoomsUpdated]);
 
   return (
     <FluentThemeProvider>
