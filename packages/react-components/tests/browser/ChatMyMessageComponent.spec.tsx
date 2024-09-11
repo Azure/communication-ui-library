ea0573b836578aaca273db25b820be398e3a0328
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-09-11 16:40:29 -0700

    Test fix

--- packages/react-components/tests/browser/ChatMyMessageComponent.spec.tsx
+++ packages/react-components/tests/browser/ChatMyMessageComponent.spec.tsx
@@ -63,8 +63,7 @@ betaTest.describe('ChatMyMessageComponent keyboard navigation tests', () => {
     await page.keyboard.press('Enter');
     await expect(removeButton).not.toBeFocused();
     // more menu isn't open
-    expect(page.getByTestId('chat-composite-message-contextual-menu-edit-action')).not.toBeVisible();
-    expect(page.getByTestId('chat-composite-message-contextual-menu-remove-action')).not.toBeVisible();
+    expect(removeButton).not.toBeVisible();
   });
 });
 
