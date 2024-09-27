1d1eeefd39972372f4f335a5bcb354882ead5f9f
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-09-27 16:25:15 -0700

    Removed change files

--- common/scripts/changelog/utils.mjs
+++ common/scripts/changelog/utils.mjs
@@ -1,14 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-const NEW_CHANGE_FILE_REGEXP = /\s*(@azure-communication-react-.*\.json)\s*/;
-
 export function parseNewChangeFiles(stdout) {
     const lines = stdout.split('\n');
     // Filter out the lines that are not changefiles
     const matches = lines.filter(line => line.startsWith('A'));
-    console.log("1", matches.map(line => line.split('\t')[1]));
-    console.log("2", matches.map(match => match[1]));
-
+    // Parse the changefile name from the output
     return matches.map(line => line.split('\t')[1]);
 }
