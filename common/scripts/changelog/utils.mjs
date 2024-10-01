f423f549bc815dd0f6dd922b8494ddad8d51018e
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-10-01 11:30:44 -0700

    Comments update

--- common/scripts/changelog/utils.mjs
+++ common/scripts/changelog/utils.mjs
@@ -3,8 +3,8 @@
 
 export function parseNewChangeFiles(stdout) {
     const lines = stdout.split('\n');
-    // Filter out the lines that are not changefiles
-    const matches = lines.filter(line => line.startsWith('A'));
-    // Parse the changefile name from the output
+    // Filter out the empty lines (`lines` might have empty lines as part of the output)
+    const matches = lines.filter(line => line && line.toString().trim().length > 0);
+    // Parse the change file name from the output (remove the status and return only file information)
     return matches.map(line => line.split('\t')[1]);
 }
