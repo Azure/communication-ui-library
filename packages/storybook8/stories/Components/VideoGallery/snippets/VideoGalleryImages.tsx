fb1a211b4181e10fe807af2514e162429fa3f07c
edwardlee-msft <edwardlee@microsoft.com>
2024-10-03 14:24:06 -0700

    [bugfix][storybookv8] Use image of local screenshare for Videogallery screenshare presenter view  (#5237)
    * Update storybook v8 videogallery screenshare presenter view with proper presentation of new screenshare view
    
    * Linter

---
+++ packages/storybook8/stories/Components/VideoGallery/snippets/VideoGalleryImages.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ScreenSharingFromPresenterExample = (): JSX.Element => {
+  return (
+    <img
+      style={{ width: '100%' }}
+      src="images/video-gallery-screensharing-from-presenter.png"
+      alt="Call composite where local participant is screensharing"
+    />
+  );
+};
