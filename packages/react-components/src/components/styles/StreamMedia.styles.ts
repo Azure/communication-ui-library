065fed7b0ff9ad35e70facfd4701e534d30341f3
Porter Nan <jiangnanhello@live.com>
2024-10-24 09:35:47 +0000

    Remove line of isDarkedThemed

--- packages/react-components/src/components/styles/StreamMedia.styles.ts
+++ packages/react-components/src/components/styles/StreamMedia.styles.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 import { ISpinnerStyles, mergeStyles, Theme } from '@fluentui/react';
-import { isDarkThemed } from '../../theming/themeUtils';
 
 /**
  * @private
