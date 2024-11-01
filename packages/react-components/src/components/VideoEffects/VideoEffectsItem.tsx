a26f9224e9f65beedd0400e7af371090df27405d
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2024-10-31 20:02:18 -0700

    Update video effects picker to a radio group for better A11y control (#5373)
    * Update video effects picker to a radio group for better A11y control
    
    * Change files

--- packages/react-components/src/components/VideoEffects/VideoEffectsItem.tsx
+++ packages/react-components/src/components/VideoEffects/VideoEffectsItem.tsx
@@ -158,6 +158,8 @@ export const _VideoEffectsItem = (props: _VideoEffectsItemProps): JSX.Element =>
           componentRef={props.componentRef as React.RefObject<IButton>}
           ariaLabel={props.ariaLabel ?? (props.tooltipProps?.content as string) ?? props.itemKey}
           aria-disabled={props.disabled}
+          role="radio"
+          aria-checked={isSelected}
         >
           <Stack horizontalAlign={'center'} className={mergeStyles({ width: '100%' })}>
             {props.iconProps && (
