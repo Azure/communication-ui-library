b3765e28f12c5ddd763343b1984a86c25468c624
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2024-11-01 11:10:20 -0700

    simplify script

--- common/scripts/inject-signout-html.mjs
+++ common/scripts/inject-signout-html.mjs
@@ -60,40 +60,22 @@ function injectSignoutToIndexHtmlFile(filePath) {
           background-color: #e0e0e0;
       }
     </style>
-    <script type="text/javascript" src="https://alcdn.msauth.net/browser/2.35.0/js/msal-browser.min.js"></script>
   </head>`
     const content = `<div id="signout-container">
-      <div id="signout-bar" class="signout-bar" style="display: none;">
+      <div id="signout-bar" class="signout-bar" style="display: flex;">
         <button id="signoutButton" class="signout-button">Sign Out</button>
       </div>
     </div>
     <div id="root" class="Root"></div>
     <script>
       // Ignore: for internal testing only
-      document.addEventListener('DOMContentLoaded', function() {
-
-        const msalConfig = {
-          auth: {
-            clientId: '7899f900-b825-41c4-80af-f0247897d3c1',
-            redirectUri: window.location.origin
-          }
-        };
-        const msalInstance = new msal.PublicClientApplication(msalConfig);
-
-        function handleSignOut() {
-          const logoutRequest = {
-            postLogoutRedirectUri: window.location.origin
-          };
-          msalInstance.logout(logoutRequest);
-        }
-        var display = msalInstance.getAllAccounts.length > 0 ? 'flex' : 'none';
-        console.log(display);
-        document.getElementById('signout-bar').style.display = display;
-        document.getElementById('signoutButton').addEventListener('click', handleSignOut);
-      });
+      function handleSignOut() {
+        console.log("You have signed out!");
+        window.location.href = 'https://login.microsoftonline.com/common/oauth2/v2.0/logout';
+      }
+      document.getElementById('signoutButton').addEventListener('click', logout);
     </script>`;
     try {
-        
         const fileContent = readFileSync(filePath, 'utf-8');
         const updatedContent = fileContent
             .replace(new RegExp(bodySearchString, 'g'), content)
