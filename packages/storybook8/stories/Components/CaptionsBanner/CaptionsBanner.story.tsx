eb09de7e8a3734898de01e3474b982a62bf29058
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2024-12-03 18:46:18 +0000

    linter

--- packages/storybook8/stories/Components/CaptionsBanner/CaptionsBanner.story.tsx
+++ packages/storybook8/stories/Components/CaptionsBanner/CaptionsBanner.story.tsx
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { PrimaryButton, Stack } from '@fluentui/react';
 import { CaptionsInformation, CaptionsBanner as CaptionsBannerComponent } from '@azure/communication-react';
+import { PrimaryButton, Stack } from '@fluentui/react';
 import React, { useState } from 'react';
 import {
   GenerateMockNewCaption,
