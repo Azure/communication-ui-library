948458d5f1d839c44c14735a0b9c9e1363325614
Chukwuebuka Nwankwo <nwankwojustin93@gmail.com>
2024-12-05 21:26:43 +0000

    Addressed comments

--- packages/storybook8/stories/Concepts/TogetherMode/snippets/TogetherMode.snippet.tsx
+++ packages/storybook8/stories/Concepts/TogetherMode/snippets/TogetherMode.snippet.tsx
@@ -5,11 +5,6 @@ export const CallScreen = (): JSX.Element => {
   // Use usePropsFor to get properties for VideoGallery
   const videoGalleryProps = usePropsFor(VideoGallery);
 
-  // Logging remote participants' PPTLive stream without modifying the array
-  if (videoGalleryProps.isTogetherModeActive) {
-    console.log('Together Mode is active');
-  }
-
   // Display TogetherMode in VideoGallery, using the properties
   return <VideoGallery {...videoGalleryProps} />;
 };
