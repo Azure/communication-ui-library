d61f437322c8c75d2cef3dfaccd2be722d079432
dmceachernmsft <dmceachern@microsoft.com>
2024-12-05 17:23:50 -0800

    update context creation

--- packages/react-composites/src/composites/common/AudioProvider.tsx
+++ packages/react-composites/src/composites/common/AudioProvider.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React, { useContext, createContext } from 'react';
+import React, { useContext, createContext, useState, useEffect } from 'react';
 
 /**
  * @private
@@ -18,19 +18,32 @@ export interface ACSAudioProviderProps {
  */
 export const ACSAudioProvider = (props: ACSAudioProviderProps): JSX.Element => {
   const { audioContext, children } = props;
+  const [stateAudioContext, setStateAudioContext] = useState<AudioContext | undefined>(audioContext);
+
+  useEffect(() => {
+    // Create the AudioContext only when the component is rendered
+    const context = new AudioContext();
+    setStateAudioContext(context);
+
+    // Clean up the AudioContext when the component is unmounted
+    return () => {
+      context.close();
+    };
+  }, []);
+
   const alreadyWrapped = useAudio();
   if (alreadyWrapped) {
     return <>{children}</>;
   }
-  return <ACSAudioContext.Provider value={audioContext}>{props.children}</ACSAudioContext.Provider>;
+  return <ACSAudioContext.Provider value={stateAudioContext}>{props.children}</ACSAudioContext.Provider>;
 };
 
 /**
  * @private
  */
-const ACSAudioContext = createContext<AudioContext>(new AudioContext());
+const ACSAudioContext = createContext<AudioContext | undefined>(undefined);
 
 /**
  * @private
  */
-export const useAudio = (): AudioContext => useContext(ACSAudioContext);
+export const useAudio = (): AudioContext | undefined => useContext(ACSAudioContext);
