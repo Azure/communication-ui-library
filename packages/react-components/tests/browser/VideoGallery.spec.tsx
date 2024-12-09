693cc6da858a357b327cf4f23742f01463a4ea60
mgamis-msft <79475487+mgamis-msft@users.noreply.github.com>
2024-12-09 09:52:01 +0000

    Revert "first try to stabilize snapshots for VideoGallery component tests"
    This reverts commit 5de1759fa9022d33ab77b432162d1e593af6d5aa.

--- packages/react-components/tests/browser/VideoGallery.spec.tsx
+++ packages/react-components/tests/browser/VideoGallery.spec.tsx
@@ -29,7 +29,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-1-1-videogallery-with-audio-only-before-dominant-speakers.png');
     await component.update(
       <Stack styles={{ root: { width: '90vw', height: '90vh' } }}>
@@ -41,7 +40,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-1-2-videogallery-with-audio-only-after-dominant-speakers.png');
   });
 
@@ -69,7 +67,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-2-1-videogallery-with-some-video-before-dominant-speakers.png');
     await component.update(
       <Stack styles={{ root: { width: '90vw', height: '90vh' } }}>
@@ -81,7 +78,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-2-2-videogallery--with-some-video-after-dominant-speakers.png');
   });
 
@@ -104,7 +100,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-3-1-videogallery-with-screen-share-before-dominant-speakers.png');
     await component.update(
       <Stack styles={{ root: { width: '90vw', height: '90vw' } }}>
@@ -116,7 +111,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-3-2-videogallery-with-screen-share-after-dominant-speakers.png');
   });
 
@@ -139,7 +133,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-4-1-videogallery-before-spotlight.png');
     remoteParticipants[7].spotlight = { spotlightedOrderPosition: 1 };
     component.update(
@@ -152,7 +145,6 @@ test.describe('VGL - VideoGallery tests', () => {
         />
       </Stack>
     );
-    await (await component.elementHandle())?.waitForElementState('stable');
     await expect(component).toHaveScreenshot('VGL-4-2-videogallery-after-spotlight.png');
   });
 });
