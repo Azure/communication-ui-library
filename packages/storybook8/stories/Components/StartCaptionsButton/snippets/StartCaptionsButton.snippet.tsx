ef79009a98518bec8bca88cbdb9a33ed7cdaa2d8
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2024-12-13 09:27:07 -0800

    Make Start Captions Button a public component  (#5499)
    * start captions button
    
    * Change files
    
    * fix lint
    
    * update
    
    * update storybook

---
+++ packages/storybook8/stories/Components/StartCaptionsButton/snippets/StartCaptionsButton.snippet.tsx
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { StartCaptionsButton } from '@azure/communication-react';
+import { Stack } from '@fluentui/react';
+import React, { useState } from 'react';
+
+export const StartCaptionsButtonStory = (): JSX.Element => {
+  const [isCaptionsActive, setIsCaptionsActive] = useState<boolean>(false);
+
+  const onStartCaptions = async (): Promise<void> => {
+    setIsCaptionsActive(true);
+    Promise.resolve();
+  };
+
+  const onStopCaptions = async (): Promise<void> => {
+    setIsCaptionsActive(false);
+    Promise.resolve();
+  };
+
+  const onSetSpokenLanguage = async (language: string): Promise<void> => {
+    alert(`Spoken language set to ${language}`);
+    Promise.resolve();
+  };
+
+  return (
+    <Stack>
+      <StartCaptionsButton
+        onStartCaptions={onStartCaptions}
+        onStopCaptions={onStopCaptions}
+        onSetSpokenLanguage={onSetSpokenLanguage}
+        currentSpokenLanguage="en-us"
+        checked={isCaptionsActive}
+        showLabel
+      />
+    </Stack>
+  );
+};
