22f283e13d70a3d88aedc55f92aab103654cf2a2
vhuseinova-msft <98852890+vhuseinova-msft@users.noreply.github.com>
2024-12-18 14:47:58 -0800

    Updated tests readme.

--- packages/react-components/tests/browser/RichTextInputBoxComponent.spec.tsx
+++ packages/react-components/tests/browser/RichTextInputBoxComponent.spec.tsx
@@ -112,8 +112,3 @@ const addList = async (listButtonLabel: string, component: Locator): Promise<voi
   editor.hover();
   await editor.pressSequentially('Third line');
 };
-
-const formatButtonClick = async (component: Locator): Promise<void> => {
-  await component.getByTestId(formatButtonId).click();
-  await component.getByTestId('rich-text-editor-toolbar').waitFor({ state: 'visible' });
-};
