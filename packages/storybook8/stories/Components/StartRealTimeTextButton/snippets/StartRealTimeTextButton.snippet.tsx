4d446ab0805556664a17bbda8ab989b34e9108b0
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2025-01-30 01:38:14 +0000

    update

--- packages/storybook8/stories/Components/StartRealTimeTextButton/snippets/StartRealTimeTextButton.snippet.tsx
+++ packages/storybook8/stories/Components/StartRealTimeTextButton/snippets/StartRealTimeTextButton.snippet.tsx
@@ -5,13 +5,12 @@ import { CaptionsBanner, StartRealTimeTextButton, usePropsFor } from '@azure/com
 import React from 'react';
 
 export const StartRealTimeTextButtonStory = (): JSX.Element => {
-  
   /**
    * In this example snippet we show case how to add a button to start real-time text
    * The start button will enable real-time text for the local user so the user can start typing
    * Once the first real-time text message is sent, real-time text will be enabled automatically for all participants.
    * The start button is disabled when real-time text is on
-   **/
+   */
 
   const [isRealTimeTextOn, setIsRealTimeTextOn] = React.useState<boolean>(false);
   const captionsBannerProps = usePropsFor(CaptionsBanner);
