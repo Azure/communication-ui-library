ec0e5023ddbad6e58fa77bc3d44879e5808a779b
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2025-02-14 10:09:50 -0800

    Reorder ariaLabel fallback values

--- packages/react-components/src/components/ControlBarButton.tsx
+++ packages/react-components/src/components/ControlBarButton.tsx
@@ -149,7 +149,7 @@ export const ControlBarButton = (props: ControlBarButtonProps): JSX.Element => {
         styles={componentStyles}
         onRenderText={props.showLabel && props.onRenderText ? props.onRenderText : undefined}
         onRenderIcon={props.onRenderIcon ?? DefaultRenderIcon}
-        ariaLabel={props.splitButtonAriaLabel ?? props.ariaLabel ?? tooltipContent ?? labelText}
+        ariaLabel={props.splitButtonAriaLabel ?? props.ariaLabel ?? labelText ?? tooltipContent}
         allowDisabledFocus={props.allowDisabledFocus ?? true}
         aria-describedby={tooltipId}
         menuTriggerKeyCode={KeyCodes.down} // explicitly sets the keypress to activiate the split button drop down.
