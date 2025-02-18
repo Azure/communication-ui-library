423cbdb9b7dc10ccd512d0d9e40a1cd79e4c5a38
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2025-02-18 15:33:48 +0000

    Fix screenshare button text to be black in dark mode to match fluent dark mode primary button

--- packages/react-components/src/components/ScreenShareButton.tsx
+++ packages/react-components/src/components/ScreenShareButton.tsx
@@ -80,22 +80,26 @@ export const ScreenShareButton = (props: ScreenShareButtonProps): JSX.Element =>
 
 const screenshareButtonStyles = (theme: Theme): IButtonStyles => ({
   rootChecked: {
-    background: theme.palette.themePrimary,
-    color: DefaultPalette.white,
-    ':focus::after': { outlineColor: `${DefaultPalette.white} !important` }, // added !important to avoid override by FluentUI button styles
+    background: theme.semanticColors.primaryButtonBackground,
+    color: theme.semanticColors.primaryButtonText,
+    ':focus::after': { outlineColor: `${theme.palette.white} !important` }, // added !important to avoid override by FluentUI button styles
     '@media (forced-colors: active)': {
       border: '1px solid',
       borderColor: theme.palette.black
     }
   },
   rootCheckedHovered: {
-    background: theme.palette.themePrimary,
-    color: DefaultPalette.white,
-    ':focus::after': { outlineColor: `${DefaultPalette.white} !important` }, // added !important to avoid override by FluentUI button styles
+    background: theme.semanticColors.primaryButtonBackgroundHovered,
+    color: theme.semanticColors.primaryButtonTextHovered,
+    ':focus::after': { outlineColor: `${theme.palette.white} !important` }, // added !important to avoid override by FluentUI button styles
     '@media (forced-colors: active)': {
       border: '1px solid',
       borderColor: theme.palette.black
     }
   },
-  labelChecked: { color: DefaultPalette.white }
+  rootCheckedPressed: {
+    background: theme.semanticColors.primaryButtonBackgroundPressed,
+    color: theme.semanticColors.primaryButtonTextPressed
+  },
+  labelChecked: { color: theme.semanticColors.primaryButtonText }
 });
