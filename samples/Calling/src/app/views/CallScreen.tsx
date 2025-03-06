64433a6b925c5ce2ecaf20036efd4352780f02fe
Mohtasim Bellah <mbellah@microsoft.com>
2025-03-05 18:05:19 -0800

    Frame send to vision api - detect hand gestures
    Signed-off-by: Mohtasim Bellah <mbellah@microsoft.com>

--- samples/Calling/src/app/views/CallScreen.tsx
+++ samples/Calling/src/app/views/CallScreen.tsx
@@ -20,7 +20,7 @@ import {
   useTeamsCallAdapter
 } from '@azure/communication-react';
 import type { Profile, StartCallIdentifier, TeamsAdapterOptions } from '@azure/communication-react';
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import { createAutoRefreshingCredential } from '../utils/credential';
 import { WEB_APP_TITLE } from '../utils/AppUtils';
 import { CallCompositeContainer } from './CallCompositeContainer';
@@ -147,10 +147,147 @@ type AzureCommunicationCallScreenProps = CallScreenProps & {
 
 const AzureCommunicationCallScreen = (props: AzureCommunicationCallScreenProps): JSX.Element => {
   const { afterCreate, callLocator: locator, userId, ...adapterArgs } = props;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   if (!('communicationUserId' in userId)) {
     throw new Error('A MicrosoftTeamsUserIdentifier must be provided for Teams Identity Call.');
   }
+
+  useEffect(() => {
+    //console.log('AzureCommunicationCallScreen:: useEffect in action...');
+    async function startVideoStream() {
+      //console.log('Starting video stream...');
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing webcam:', error);
+      }
+    }
+
+    startVideoStream();
+  }, []);
+
+  useEffect(() => {
+    let animationFrameId: number;
+
+    async function analyzeFrame() {
+      const imageUrl = await captureFrame();
+
+      if (imageUrl && imageUrl.length > 10) {
+        console.log('image url - ', imageBase64ToBlob(imageUrl));
+        await detectHandGestures(imageUrl);
+      }
+      animationFrameId = requestAnimationFrame(analyzeFrame);
+    }
+
+    function startAnalyzing() {
+      animationFrameId = requestAnimationFrame(analyzeFrame);
+    }
+
+    startAnalyzing();
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+
+  async function captureFrame() {
+    console.log('Capturing frame...');
+    if (videoRef.current && canvasRef.current) {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      if (ctx) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      }
+      return canvas.toDataURL('image/jpeg');
+    }
+    return null;
+  }
+  async function detectHandGestures1(imageBase64: string) {
+    const CUSTOM_VISION_ENDPOINT = 'https://azureaiinsravan.cognitiveservices.azure.com';
+    const CUSTOM_VISION_KEY = 'Ffnb7EK1Z65PWAn9o31l5dxMV8kP1C6rIMAn2vbPRzZ3EidaEKjvJQQJ99BBACYeBjFXJ3w3AAAFACOGXhO6';
+    const PREDICTION_KEY = 'ebc77a8a52e04e9394125c19f2dc8a16';
+    const PROJECT_ID = 'daaea539-0d1a-456b-a0fc-31e121039d56';
+    const MODEL_NAME = 'FaceExpressionAndHandGestures';
+
+    const response = await fetch(
+      //`${CUSTOM_VISION_ENDPOINT}/customvision/v3.0/Prediction/${PROJECT_ID}/classify/iterations/${MODEL_NAME}/url`,
+      `https://azureaiinsravan.cognitiveservices.azure.com/customvision/v3.0/Prediction/daaea539-0d1a-456b-a0fc-31e121039d56/detect/iterations/FaceExpressionAndHandGestures/url`,
+      {
+        method: 'POST',
+        headers: {
+          'Ocp-Apim-Subscription-Key': CUSTOM_VISION_KEY,
+          'Prediction-Key': PREDICTION_KEY,
+          'Content-Type': 'application/json'
+        },
+        //body: imageBase64ToBlob(imageBase64)
+        body: JSON.stringify({
+          Url: 'https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/printed_text.jpg'
+        })
+      }
+    );
+
+    const data = await response.json();
+    console.log('Gesture detected data - \n');
+    console.log(data);
+  }
+  async function detectHandGestures(imageBase64: string) {
+    const CUSTOM_VISION_ENDPOINT = 'https://azureaiinsravan.cognitiveservices.azure.com';
+    const CUSTOM_VISION_KEY = 'Ffnb7EK1Z65PWAn9o31l5dxMV8kP1C6rIMAn2vbPRzZ3EidaEKjvJQQJ99BBACYeBjFXJ3w3AAAFACOGXhO6';
+    const PREDICTION_KEY = 'ebc77a8a52e04e9394125c19f2dc8a16';
+    const PROJECT_ID = 'daaea539-0d1a-456b-a0fc-31e121039d56';
+    const MODEL_NAME = 'FaceExpressionAndHandGestures';
+
+    const response = await fetch(
+      //`${CUSTOM_VISION_ENDPOINT}/customvision/v3.0/Prediction/${PROJECT_ID}/classify/iterations/${MODEL_NAME}/url`,
+      `https://azureaiinsravan.cognitiveservices.azure.com/customvision/v3.0/Prediction/daaea539-0d1a-456b-a0fc-31e121039d56/detect/iterations/FaceExpressionAndHandGestures/image`,
+      {
+        method: 'POST',
+        headers: {
+          'Ocp-Apim-Subscription-Key': CUSTOM_VISION_KEY,
+          'Prediction-Key': PREDICTION_KEY,
+          'Content-Type': 'application/octet-stream'
+        },
+        body: imageBase64ToBlob(imageBase64)
+      }
+    );
+
+    const data = await response.json();
+    console.log('Gesture detected data - \n');
+    console.log(data);
+  }
+  function imageBase64ToBlob(base64: string) {
+    const base64Data = base64.split(',')[1];
+    if (!base64Data) {
+      throw new Error('Invalid base64 string');
+    }
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    return new Blob([new Uint8Array(byteNumbers)], { type: 'image/jpeg' });
+  }
+  // function imageBase64ToBlob(base64: string) {
+  //   const base64Data = base64.split(',')[1];
+  //   console.log('base64Data image - ', base64Data);
+  //   if (!base64Data) {
+  //     throw new Error('Invalid base64 string');
+  //   }
+  //   const byteCharacters = atob(base64Data);
+  //   const byteNumbers = new Array(byteCharacters.length);
+  //   for (let i = 0; i < byteCharacters.length; i++) {
+  //     byteNumbers[i] = byteCharacters.charCodeAt(i);
+  //   }
+  //   return new Blob([new Uint8Array(byteNumbers)], { type: 'image/jpeg' });
+  // }
 
   const callAdapterOptions: AzureCommunicationCallAdapterOptions = useMemo(() => {
     return {
@@ -188,14 +325,110 @@ const AzureCommunicationCallScreen = (props: AzureCommunicationCallScreenProps):
     afterCreate
   );
 
-  return <CallCompositeContainer {...props} adapter={adapter} />;
+  detectHandGestures1('asdfa');
+  return (
+    <div>
+      <video ref={videoRef} autoPlay style={{ display: 'none' }} />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      <CallCompositeContainer {...props} adapter={adapter} />
+    </div>
+  );
 };
 
 const AzureCommunicationOutboundCallScreen = (props: AzureCommunicationCallScreenProps): JSX.Element => {
   const { afterCreate, targetCallees: targetCallees, userId, ...adapterArgs } = props;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   if (!('communicationUserId' in userId)) {
     throw new Error('A MicrosoftTeamsUserIdentifier must be provided for Teams Identity Call.');
+  }
+
+  useEffect(() => {
+    async function startVideoStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing webcam:', error);
+      }
+    }
+
+    startVideoStream();
+  }, []);
+
+  useEffect(() => {
+    let animationFrameId: number;
+
+    async function analyzeFrame() {
+      const imageUrl = await captureFrame();
+      if (imageUrl) {
+        await detectHandGestures(imageUrl);
+      }
+      animationFrameId = requestAnimationFrame(analyzeFrame);
+    }
+
+    function startAnalyzing() {
+      animationFrameId = requestAnimationFrame(analyzeFrame);
+    }
+
+    startAnalyzing();
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+
+  async function captureFrame() {
+    console.log('Capturing frame...');
+    if (videoRef.current && canvasRef.current) {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      if (ctx) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      }
+      return canvas.toDataURL('image/jpeg');
+    }
+    return null;
+  }
+  async function detectHandGestures(imageBase64: string) {
+    const CUSTOM_VISION_ENDPOINT = 'https://YOUR_CUSTOM_VISION_ENDPOINT/';
+    const CUSTOM_VISION_KEY = 'YOUR_CUSTOM_VISION_KEY';
+    const PROJECT_ID = 'YOUR_PROJECT_ID';
+    const MODEL_NAME = 'YOUR_MODEL_NAME';
+
+    const response = await fetch(
+      `${CUSTOM_VISION_ENDPOINT}/customvision/v3.0/Prediction/${PROJECT_ID}/classify/iterations/${MODEL_NAME}/image`,
+      {
+        method: 'POST',
+        headers: {
+          'Prediction-Key': CUSTOM_VISION_KEY,
+          'Content-Type': 'application/octet-stream'
+        },
+        body: imageBase64ToBlob(imageBase64)
+      }
+    );
+
+    const data = await response.json();
+    console.log('Gesture detected data - \n');
+    console.log(data);
+  }
+  function imageBase64ToBlob(base64: string) {
+    const base64Data = base64.split(',')[1];
+    if (!base64Data) {
+      throw new Error('Invalid base64 string');
+    }
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    return new Blob([new Uint8Array(byteNumbers)], { type: 'image/jpeg' });
   }
 
   const callAdapterOptions: AzureCommunicationCallAdapterOptions = useMemo(() => {
@@ -237,7 +470,13 @@ const AzureCommunicationOutboundCallScreen = (props: AzureCommunicationCallScree
     afterCreate
   );
 
-  return <CallCompositeContainer {...props} adapter={adapter} />;
+  return (
+    <div>
+      <video ref={videoRef} autoPlay style={{ display: 'none' }} />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      <CallCompositeContainer {...props} adapter={adapter} />
+    </div>
+  );
 };
 
 const convertPageStateToString = (state: CallAdapterState): string => {
