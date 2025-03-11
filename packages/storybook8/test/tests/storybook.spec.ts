73a46da26de48c1b4aea2a1db51c2f7a1410c86c
carocao-msft <carolinecao@microsoft.com>
2025-03-10 17:03:42 -0700

    update

--- packages/storybook8/test/tests/storybook.spec.ts
+++ packages/storybook8/test/tests/storybook.spec.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { expect, Locator, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { loadStory, getStoryIds } from '../utils.js';
 
 test('Take screenshots of all stories', async ({ page }) => {
@@ -10,27 +10,22 @@ test('Take screenshots of all stories', async ({ page }) => {
   for (const storyId of storyIds) {
     await loadStory(page, `docs/${storyId}`);
     await page.waitForSelector('#root');
-    await page.getByRole('button', { name: 'Go full screen [⌥ F]' }).click();
-    await expect(page).toHaveScreenshot(`${storyId}.png`);
-    // const hrefs = await page.locator("//div[contains(@class, 'sbdocs')").getAttribute('href');
-    // get all the elements that have a href attribute and id not in storyids
-    // const elements = await page.locator("//a[starts-with(@class, 'sbdocs')]");
-    const elements = await page.locator('a');
-    console.log(elements);
+    await expect(page).toHaveScreenshot(`story-${storyId}.png`);
+    const elements = await page.locator('p').locator('a');
     const linkCount = await elements.count();
-    console.log(linkCount);
     if (linkCount === 0) {
       console.log('No links found');
     } else {
       for (let i = 0; i < linkCount; i++) {
-        // //get the text of the quote
-        // const link = await elements.nth(i).getAttribute('href');
-        // //log it to the console
-        // console.log(`Quote: ${link}`);
-        // await page.goto(link, { waitUntil: 'networkidle' });
-        await elements.nth(i).click();
-        await page.waitForSelector('#root');
-        await expect(page).toHaveScreenshot(`${storyId}-${elements.innerText}.png`);
+        await elements
+          .nth(i)
+          .getAttribute('id')
+          .then((result) => {
+            console.log(result);
+            if (!(result !== null && result in storyIds)) {
+              console.log(elements.nth(i).getAttribute('href'));
+            }
+          });
       }
     }
   }
--- packages/storybook8/test/utils.ts
+++ packages/storybook8/test/utils.ts
@@ -23,15 +23,29 @@ export async function loadStory(page: Page, storyId: string) {
 export async function getStoryIds(page: Page): Promise<string[]> {
   await page.goto('https://azure.github.io/communication-ui-library/?path=/docs/overview--docs');
   await page.waitForSelector('#root');
-  const storyIds = await page.evaluate(() => {
-    const elements = document.querySelectorAll('a');
-    const ids: string[] = [];
-    for (const element of elements) {
-      if (element.getAttribute('id') !== null) {
-        ids.push(element.getAttribute('id') ?? '');
-      }
+  await page.locator('#composites').getByRole('button', { name: 'Collapse' }).click();
+  await page.locator('#components').getByRole('button', { name: 'Collapse' }).click();
+  await page.locator('#concepts').getByRole('button', { name: 'Collapse' }).click();
+  await page.locator('#examples').getByRole('button', { name: 'Collapse' }).click();
+  await page.locator('#stateful-client').getByRole('button', { name: 'Collapse' }).click();
+
+  const links = await page.getByRole('link');
+  const linkCount = await links.count();
+  const storyIds: string[] = [];
+  console.log(linkCount);
+  if (linkCount === 0) {
+    console.log('No links found');
+  } else {
+    for (let i = 0; i < linkCount; i++) {
+      await links
+        .nth(i)
+        .getAttribute('id')
+        .then((result) => {
+          if (result !== null) {
+            storyIds.push(result ?? '');
+          }
+        });
     }
-    return ids;
-  });
+  }
   return storyIds;
 }
