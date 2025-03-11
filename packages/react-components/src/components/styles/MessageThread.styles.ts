9729505365dd7b920e2fa1ca41bf0aac04521529
Patrick Latter <palatter@microsoft.com>
2025-03-11 14:16:28 -0700

    revert accidental change.

--- packages/react-components/src/components/styles/MessageThread.styles.ts
+++ packages/react-components/src/components/styles/MessageThread.styles.ts
@@ -262,8 +262,7 @@ export const useChatMessageStyles = makeStyles({
     minWidth: `${CHAT_MESSAGE_CONTAINER_MIN_WIDTH_REM}rem`,
     marginRight: '0rem',
     paddingBottom: '10px',
-    // Ensure the focus border around the message bubble doesn't overlap on top of more options button
-    zIndex: 2,
+    zIndex: CHAT_MESSAGE_ZINDEX,
     // This makes message bubble show border in high contrast mode making each message distinguishable
     ...shorthands.border('1px', 'solid', 'transparent'),
     '& > div:first-of-type': {
