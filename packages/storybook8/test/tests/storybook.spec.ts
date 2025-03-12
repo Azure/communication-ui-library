7ec3a5499c21f9232604ca9459b82c64dac59e54
carocao-msft <carolinecao@microsoft.com>
2025-03-12 16:54:14 -0700

    test

--- packages/storybook8/test/tests/storybook.spec.ts
+++ packages/storybook8/test/tests/storybook.spec.ts
@@ -4,29 +4,27 @@
 import { expect, test } from '@playwright/test';
 import { loadStory, getStoryIds } from '../utils.js';
 
-test('Take screenshots of all stories', async ({ page }) => {
+test('Check if embedded links inside each story is valid', async ({ page }) => {
   const storyIds = await getStoryIds(page);
 
   for (const storyId of storyIds) {
     await loadStory(page, `docs/${storyId}`);
     await page.waitForSelector('#root');
-    await expect(page).toHaveScreenshot(`story-${storyId}.png`);
-    const elements = await page.locator('p').locator('a');
-    const linkCount = await elements.count();
-    if (linkCount === 0) {
-      console.log('No links found');
-    } else {
-      for (let i = 0; i < linkCount; i++) {
-        await elements
-          .nth(i)
-          .getAttribute('id')
-          .then((result) => {
-            console.log(result);
-            if (!(result !== null && result in storyIds)) {
-              console.log(elements.nth(i).getAttribute('href'));
-            }
-          });
-      }
+    // Wait for the Storybook iframe to load
+    const iframe = await page.frameLocator('iframe#storybook-preview-iframe');
+    // Extract all links from the iframe
+    const links = await iframe
+      .locator('.sbdocs-a')
+      .evaluateAll((anchors) => anchors.map((a) => (a as HTMLLinkElement).href));
+    // check the links if they go through without errors
+    for (const link of links) {
+      console.log(`Checking link: ${storyId}-${link}`);
+      const response = await page.goto(link);
+      expect(response?.ok()).toBe(true);
+      const iframe = await page.frameLocator('iframe#storybook-preview-iframe');
+      // find h1 with id "error-message" and check if it is visible
+      const errorMessage = await iframe.locator('#error-message').isVisible();
+      expect(errorMessage).toBe(false);
     }
   }
 });
--- packages/storybook8/test/utils.ts
+++ packages/storybook8/test/utils.ts
@@ -3,15 +3,15 @@
 
 import { Page } from '@playwright/test';
 
+const testUrl = 'https://azure.github.io/communication-ui-library/';
+
 /**
  * Load a specific Storybook story for Playwright testing.
  * @param page - The Playwright page object.
  * @param storyId - The ID of the story to load.
  */
 export async function loadStory(page: Page, storyId: string) {
-  const url = `https://azure.github.io/communication-ui-library/?path=/${storyId}`;
-  console.log(url);
-  await page.goto(url, { waitUntil: 'networkidle' });
+  await page.goto(`${testUrl}/?path=/${storyId}`);
   await page.waitForSelector('#root');
 }
 
@@ -21,7 +21,7 @@ export async function loadStory(page: Page, storyId: string) {
  * @returns An array of story IDs.
  */
 export async function getStoryIds(page: Page): Promise<string[]> {
-  await page.goto('https://azure.github.io/communication-ui-library/?path=/docs/overview--docs');
+  await loadStory(page, '/docs/overview--docs');
   await page.waitForSelector('#root');
   await page.locator('#composites').getByRole('button', { name: 'Collapse' }).click();
   await page.locator('#components').getByRole('button', { name: 'Collapse' }).click();
@@ -32,7 +32,6 @@ export async function getStoryIds(page: Page): Promise<string[]> {
   const links = await page.getByRole('link');
   const linkCount = await links.count();
   const storyIds: string[] = [];
-  console.log(linkCount);
   if (linkCount === 0) {
     console.log('No links found');
   } else {
