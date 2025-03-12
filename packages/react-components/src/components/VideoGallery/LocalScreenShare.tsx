57a7c04be0aa2a42c0befe76d14f4325f3c3efe4
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2025-03-12 15:04:09 -0700

    Composite option to hide local screen share stream  (#5699)
    * update
    
    * Change files
    
    * update
    
    ---------
    
    Co-authored-by: Donald McEachern <94866715+dmceachernmsft@users.noreply.github.com>

--- packages/react-components/src/components/VideoGallery/LocalScreenShare.tsx
+++ packages/react-components/src/components/VideoGallery/LocalScreenShare.tsx
@@ -1,13 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { Icon, Stack, Text } from '@fluentui/react';
 import React, { useEffect } from 'react';
 import { useLocale } from '../../localization';
+import { useTheme } from '../../theming';
 import { CreateVideoStreamViewResult, VideoGalleryLocalParticipant, VideoStreamOptions } from '../../types';
 import { VideoTile } from '../VideoTile';
 import { StreamMedia } from '../StreamMedia';
 import { LoadingSpinner } from './RemoteScreenShare';
 import { _formatString } from '@internal/acs-ui-common';
+import {
+  screenSharingContainerStyle,
+  screenSharingNotificationContainerStyle,
+  screenSharingNotificationIconContainerStyle,
+  screenSharingNotificationIconStyle,
+  screenSharingNotificationTextStyle
+} from './styles/LocalScreenShare.styles';
 
 /**
  * A memoized version of local screen share component. React.memo is used for a performance
@@ -21,16 +30,19 @@ export const LocalScreenShare = React.memo(
     isAvailable?: boolean;
     onCreateLocalStreamView?: (options?: VideoStreamOptions) => Promise<void | CreateVideoStreamViewResult>;
     onDisposeLocalScreenShareStreamView?: () => Promise<void>;
+    localScreenShareView?: 'stream' | 'placeholderMessage';
   }) => {
     const {
       localParticipant,
       renderElement,
       isAvailable,
       onCreateLocalStreamView,
-      onDisposeLocalScreenShareStreamView
+      onDisposeLocalScreenShareStreamView,
+      localScreenShareView = 'stream'
     } = props;
     const locale = useLocale();
-    if (!renderElement) {
+    const theme = useTheme();
+    if (!renderElement && localScreenShareView === 'stream') {
       onCreateLocalStreamView && onCreateLocalStreamView();
     }
 
@@ -44,12 +56,37 @@ export const LocalScreenShare = React.memo(
     if (!localParticipant || !localParticipant.isScreenSharingOn) {
       return null;
     }
+    const localScreenSharingPlaceholder = (
+      <Stack horizontalAlign="center" verticalAlign="center" className={screenSharingContainerStyle}>
+        <Stack
+          horizontalAlign="center"
+          verticalAlign="center"
+          className={screenSharingNotificationContainerStyle(theme)}
+          tokens={{ childrenGap: '1rem' }}
+        >
+          <Stack horizontal verticalAlign="center" className={screenSharingNotificationIconContainerStyle}>
+            <Icon iconName="ControlButtonScreenShareStart" className={screenSharingNotificationIconStyle(theme)} />
+          </Stack>
+          <Text className={screenSharingNotificationTextStyle} aria-live="polite">
+            {locale.strings.videoGallery.screenIsBeingSharedMessage}
+          </Text>
+        </Stack>
+      </Stack>
+    );
 
     const displayName = !localParticipant?.displayName
       ? locale.strings.videoGallery.displayNamePlaceholder
       : localParticipant?.displayName;
 
     const loadingMessage = locale.strings.videoGallery.localScreenShareLoadingMessage;
+
+    if (localScreenShareView === 'placeholderMessage') {
+      return (
+        <VideoTile displayName={displayName} isMuted={localParticipant?.isMuted} onRenderPlaceholder={() => <></>}>
+          {localScreenSharingPlaceholder}
+        </VideoTile>
+      );
+    }
 
     return (
       <VideoTile
