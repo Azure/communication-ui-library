cfa420b8e7de4fb402e3a4f8df1c35732378b1e9
Patrick Latter <palatter@microsoft.com>
2025-03-18 13:50:53 -0700

    Temp disable lint

--- packages/react-components/src/components/RichTextEditor/RichTextSendBox.tsx
+++ packages/react-components/src/components/RichTextEditor/RichTextSendBox.tsx
@@ -606,12 +606,14 @@ const _isRichTextSendBox = Symbol('isRichTextSendBox');
 
 // Add the internal property to the component
 /* @conditional-compile-remove(rich-text-editor) */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 (RichTextSendBox as any)[_isRichTextSendBox] = true;
 
 /* @conditional-compile-remove(rich-text-editor) */
 /**
  * @internal
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isRichTextSendBox = (component: any): boolean => {
   return component?.[_isRichTextSendBox] === true;
 };
