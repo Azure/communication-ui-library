ee7ac4477c64e67ffb77a58702fdc5444af1ef91
dmceachernmsft <dmceachern@microsoft.com>
2025-03-20 14:45:40 -0700

    update useAdapted selector to always use the up to date callId

--- packages/react-composites/src/composites/CallComposite/hooks/useAdaptedSelector.ts
+++ packages/react-composites/src/composites/CallComposite/hooks/useAdaptedSelector.ts
@@ -52,14 +52,8 @@ export const useSelectorWithAdaptation = <
   });
 
   const callId = adapter.getState().call?.id;
-  const callConfigProps = useMemo(
-    () => ({
-      callId
-    }),
-    [callId]
-  );
 
-  const [props, setProps] = useState(selector(adaptState(adapter.getState()), selectorProps ?? callConfigProps));
+  const [props, setProps] = useState(selector(adaptState(adapter.getState()), selectorProps ?? { callId }));
   const propRef = useRef(props);
   propRef.current = props;
 
@@ -68,7 +62,7 @@ export const useSelectorWithAdaptation = <
       if (!mounted.current) {
         return;
       }
-      const newProps = selector(adaptState(state), selectorProps ?? callConfigProps);
+      const newProps = selector(adaptState(state), selectorProps ?? { callId: state.call?.id });
       if (propRef.current !== newProps) {
         setProps(newProps);
       }
@@ -77,7 +71,7 @@ export const useSelectorWithAdaptation = <
     return () => {
       adapter.offStateChange(onStateChange);
     };
-  }, [adaptState, adapter, selector, selectorProps, callConfigProps]);
+  }, [adaptState, adapter, selector, selectorProps, callId]);
   return props;
 };
 
