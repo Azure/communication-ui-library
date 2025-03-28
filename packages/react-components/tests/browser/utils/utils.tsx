445b85b2e041f5c7be5d1e1a8e78f476810f7b08
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2025-03-28 11:25:14 -0700

    Fix component tests

---
+++ packages/react-components/tests/browser/utils/utils.tsx
@@ -0,0 +1,16 @@
+/**
+ *  Helper function to detect whether a test is for a mobile broswer or not.
+ *  TestInfo comes from the playwright config which gives different information about what platform the
+ *  test is being run on.
+ * */
+export const isTestProfileMobile = (testProfileName: string): boolean => !isTestProfileDesktop(testProfileName);
+
+/**
+ *  Helper function to detect whether a test is for a mobile broswer or not.
+ *  TestInfo comes from the playwright config which gives different information about what platform the
+ *  test is being run on.
+ * */
+export const isTestProfileDesktop = (testProfileName: string): boolean => {
+  const testName = testProfileName.toLowerCase();
+  return testName.includes('desktop') ? true : false;
+};
