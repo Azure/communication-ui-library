a5a59b74c9a11eb7d782da40bca3010d3678f295
Jim Chou <77021369+jimchou-dev@users.noreply.github.com>
2025-03-28 13:02:21 -0700

    [Bugfix] Mobile ChatMyMessage attachment download (#5753)

--- packages/react-components/tests/browser/ChatMyMessageComponent.spec.tsx
+++ packages/react-components/tests/browser/ChatMyMessageComponent.spec.tsx
@@ -6,36 +6,42 @@ import { expect } from '@playwright/experimental-ct-react';
 import { test as betaTest } from './FlavoredBaseTest';
 import { Locator, Page } from 'playwright-core';
 import { TestChatMyMessageComponent } from './TestingComponents/TestChatMyMessageComponent';
+import { isTestProfileMobile } from './utils/utils';
 
 betaTest.describe('ChatMyMessageComponent rich text editor attachment tests', () => {
   betaTest.skip(({ isBetaBuild }) => !isBetaBuild, 'The tests should be run for beta flavor only');
 
-  betaTest('Edit box should not allow to save empty text when attachments are deleted', async ({ mount, page }) => {
-    const component = await mount(
-      <TestChatMyMessageComponent content={''} contentType={'text'} isRichTextEditorEnabled={true} />
-    );
+  betaTest(
+    'Edit box should not allow to save empty text when attachments are deleted',
+    async ({ mount, page }, testInfo) => {
+      const component = await mount(
+        <TestChatMyMessageComponent content={''} contentType={'text'} isRichTextEditorEnabled={true} />
+      );
 
-    await startMessageEditing(component, page);
-    await removeAttachmentAndSubmit(component, true);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
+      await removeAttachmentAndSubmit(component, true);
 
-    const editor = component.getByTestId('rooster-rich-text-editor');
-    await expect(editor).toBeVisible();
-    // make screenshot consistent
-    await editor.click();
+      const editor = component.getByTestId('rooster-rich-text-editor');
+      await expect(editor).toBeVisible();
+      // make screenshot consistent
+      await editor.click();
 
-    await expect(component).toHaveScreenshot(
-      'chat-my-message-component-rich-text-edit-box-empty-content-without-attachment.png'
-    );
-  });
+      await expect(component).toHaveScreenshot(
+        'chat-my-message-component-rich-text-edit-box-empty-content-without-attachment.png'
+      );
+    }
+  );
 
   betaTest(
     'Edit box should not allow to save empty html text when attachments are deleted',
-    async ({ mount, page }) => {
+    async ({ mount, page }, testInfo) => {
       const component = await mount(
         <TestChatMyMessageComponent content={'<div><br></div>'} contentType={'html'} isRichTextEditorEnabled={true} />
       );
 
-      await startMessageEditing(component, page);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
       await removeAttachmentAndSubmit(component, true);
 
       const editor = component.getByTestId('rooster-rich-text-editor');
@@ -51,12 +57,13 @@ betaTest.describe('ChatMyMessageComponent rich text editor attachment tests', ()
 
   betaTest(
     'Edit box should allow to save the message when content exists but attachments are deleted',
-    async ({ mount, page }) => {
+    async ({ mount, page }, testInfo) => {
       const component = await mount(
         <TestChatMyMessageComponent content={'test'} contentType={'text'} isRichTextEditorEnabled={true} />
       );
 
-      await startMessageEditing(component, page);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
       await removeAttachmentAndSubmit(component, true);
 
       await component.getByTestId('chat-composite-message').waitFor({ state: 'visible' });
@@ -72,46 +79,57 @@ betaTest.describe('ChatMyMessageComponent rich text editor attachment tests', ()
 betaTest.describe('ChatMyMessageComponent text editor attachment tests', () => {
   betaTest.skip(({ isBetaBuild }) => !isBetaBuild, 'The tests should be run for beta flavor only');
 
-  betaTest('Edit box should not allow to save empty text when attachments are deleted', async ({ mount, page }) => {
-    const component = await mount(
-      <TestChatMyMessageComponent content={''} contentType={'text'} isRichTextEditorEnabled={false} />
-    );
+  betaTest(
+    'Edit box should not allow to save empty text when attachments are deleted',
+    async ({ mount, page }, testInfo) => {
+      const component = await mount(
+        <TestChatMyMessageComponent content={''} contentType={'text'} isRichTextEditorEnabled={false} />
+      );
 
-    await startMessageEditing(component, page);
-    await removeAttachmentAndSubmit(component, false);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
+      await removeAttachmentAndSubmit(component, false);
 
-    const editor = component.getByRole('textbox');
-    await expect(editor).toBeVisible();
-    // make screenshot consistent
-    await editor.click();
+      const editor = component.getByRole('textbox');
+      await expect(editor).toBeVisible();
+      // make screenshot consistent
+      await editor.click();
 
-    await expect(component).toHaveScreenshot('chat-my-message-component-edit-box-empty-content-without-attachment.png');
-  });
+      await expect(component).toHaveScreenshot(
+        'chat-my-message-component-edit-box-empty-content-without-attachment.png'
+      );
+    }
+  );
 
-  betaTest('Edit box should allow to save empty html text when attachments are deleted', async ({ mount, page }) => {
-    await page.evaluate(() => document.fonts.ready);
-    const component = await mount(
-      <TestChatMyMessageComponent content={'<div><br></div>'} contentType={'html'} isRichTextEditorEnabled={false} />
-    );
+  betaTest(
+    'Edit box should allow to save empty html text when attachments are deleted',
+    async ({ mount, page }, testInfo) => {
+      await page.evaluate(() => document.fonts.ready);
+      const component = await mount(
+        <TestChatMyMessageComponent content={'<div><br></div>'} contentType={'html'} isRichTextEditorEnabled={false} />
+      );
 
-    await startMessageEditing(component, page);
-    await removeAttachmentAndSubmit(component, false);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
+      await removeAttachmentAndSubmit(component, false);
 
-    // the plain edit box is not empty, so the message should be visible
-    await component.getByTestId('chat-composite-message').waitFor({ state: 'visible' });
+      // the plain edit box is not empty, so the message should be visible
+      await component.getByTestId('chat-composite-message').waitFor({ state: 'visible' });
 
-    await expect(component).toHaveScreenshot(
-      'chat-my-message-component-edit-box-empty-html-content-without-attachment.png'
-    );
-  });
+      await expect(component).toHaveScreenshot(
+        'chat-my-message-component-edit-box-empty-html-content-without-attachment.png'
+      );
+    }
+  );
 
   betaTest(
     'Edit box should allow to save the message when content exists but attachments are deleted',
-    async ({ mount, page }) => {
+    async ({ mount, page }, testInfo) => {
       const component = await await mount(
         <TestChatMyMessageComponent content={'test'} contentType={'text'} isRichTextEditorEnabled={false} />
       );
-      await startMessageEditing(component, page);
+      const testProfileName = testInfo.project.name;
+      await startMessageEditing(component, page, testProfileName);
       await removeAttachmentAndSubmit(component, false);
 
       await component.getByTestId('chat-composite-message').waitFor({ state: 'visible' });
@@ -124,12 +142,18 @@ betaTest.describe('ChatMyMessageComponent text editor attachment tests', () => {
   );
 });
 
-const startMessageEditing = async (component: Locator, page: Page): Promise<void> => {
+const startMessageEditing = async (component: Locator, page: Page, testProfileName: string): Promise<void> => {
   await component.evaluate(() => document.fonts.ready);
   const messageBody = component.getByTestId('chat-composite-message');
   await expect(messageBody).toBeVisible();
   await expect(component.getByTestId('message-timestamp')).toBeVisible();
-  await component.getByTestId('message-timestamp').hover();
+  if (isTestProfileMobile(testProfileName)) {
+    // Mobile scenario: click instead of hover
+    await component.getByTestId('message-timestamp').click();
+  } else {
+    // Desktop scenario: hover
+    await component.getByTestId('message-timestamp').hover();
+  }
 
   await expect(component.getByTestId('chat-composite-message-action-icon')).toBeVisible();
 
