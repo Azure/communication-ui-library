b508bebf90d0ba6caba20215a6ff736cf4a030ae
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2025-04-03 19:26:53 +0000

    Add cjs build for js loaders

--- packages/acs-ui-javascript-loaders/rollup.config.mjs
+++ packages/acs-ui-javascript-loaders/rollup.config.mjs
@@ -3,7 +3,12 @@
 
 import { readFileSync } from 'node:fs';
 import commonConfig from '../../common/config/rollup/rollup.config.mjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)));
-
-export default commonConfig(packageJson);
+const commonConfigPackage = commonConfig(packageJson);
+const externalDeps = commonConfigPackage.external.filter((dep) => dep !== 'react-dom');
+const commonPlugins = commonConfigPackage.plugins.concat([nodeResolve()]);
+commonConfigPackage.external = externalDeps;
+commonConfigPackage.plugins = commonPlugins;
+export default commonConfigPackage;
