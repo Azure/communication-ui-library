44c91f1e1e5d7ce42061544efbbc7625dd3929a6
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2025-04-10 16:47:12 +0000

    fix test

--- common/config/jest/jestSetup.js
+++ common/config/jest/jestSetup.js
@@ -24,3 +24,35 @@ window.AudioContext = jest.fn().mockImplementation(() => {
 });
 // Add `ResizeObserver` to globals. Without this GridLayout jest tests fail with "ReferenceError: ResizeObserver is not defined"
 global.ResizeObserver = require('resize-observer-polyfill');
+
+Object.defineProperty(window, 'MediaStreamTrack', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+      contentHint: 'string',
+      enabled: true,
+      id: 'string',
+      kind: 'string',
+      label: 'string',
+      muted: true,
+      onended: jest.fn(),
+      onmute: jest.fn(),
+      onunmute: jest.fn(),
+      readyState: 'live',
+      applyConstraints: jest.fn(),
+      clone: jest.fn(),
+      getCapabilities: jest.fn(),
+      getConstraints: jest.fn(),
+      getSettings: jest.fn(),
+      stop: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+      isolated: false,
+      onisolationchange: jest.fn()
+  }))
+});
+
+Object.defineProperty(window, 'MediaStream', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => Object.create(MediaStream.prototype))
+});
