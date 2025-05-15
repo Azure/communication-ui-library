f3026ac30f731ba896d66f714162a0b5669f720b
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2025-05-15 11:44:36 -0700

    Fix up tests for stable

--- packages/acs-ui-common/src/identifier.test.ts
+++ packages/acs-ui-common/src/identifier.test.ts
@@ -28,6 +28,7 @@ test('phone number conversion from E.164 format', () => {
   const parsed = fromFlatCommunicationIdentifier('+15555555555');
   expect(isPhoneNumberIdentifier(parsed)).toBeTruthy;
   expect(parsed).toEqual({
+    /* @conditional-compile-remove(calling-beta-sdk) */
     isAnonymous: false,
     kind: 'phoneNumber',
     phoneNumber: '+15555555555'
@@ -38,6 +39,7 @@ test('Phone number conversions', () => {
   const parsed = fromFlatCommunicationIdentifier('4:OPAQUE');
   expect(isPhoneNumberIdentifier(parsed)).toBeTruthy();
   expect(parsed).toEqual({
+    /* @conditional-compile-remove(calling-beta-sdk) */
     isAnonymous: false,
     kind: 'phoneNumber',
     phoneNumber: 'OPAQUE'
