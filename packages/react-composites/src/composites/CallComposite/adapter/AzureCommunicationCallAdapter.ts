64bfd800de7e3c3e09e14ea2143d84a1d4172ad4
dmceachernmsft <dmceachern@microsoft.com>
2025-05-15 15:10:34 -0700

    Add call client disposal method to the stateful client

--- packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
+++ packages/react-composites/src/composites/CallComposite/adapter/AzureCommunicationCallAdapter.ts
@@ -671,6 +671,8 @@ export class AzureCommunicationCallAdapter<AgentType extends CallAgent | TeamsCa
     this.resetDiagnosticsForwarder();
     this.callClient.offStateChange(this.onClientStateChange);
     this.callAgent.dispose();
+    /* @conditional-compile-remove(calling-beta-sdk) */
+    this.callClient.dispose();
   }
 
   public async queryCameras(): Promise<VideoDeviceInfo[]> {
