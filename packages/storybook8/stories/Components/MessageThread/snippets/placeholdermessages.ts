33d7320fb5ad2e98648c300f5a6a7641a951f3e1
Leah Xia <107075081+Leah-Xia-Microsoft@users.noreply.github.com>
2025-05-22 17:22:24 -0700

    Add stroybook page for AdaptiveCard

--- packages/storybook8/stories/Components/MessageThread/snippets/placeholdermessages.ts
+++ packages/storybook8/stories/Components/MessageThread/snippets/placeholdermessages.ts
@@ -85,6 +85,114 @@ export const GetHistoryWithCustomMessages = (): (CustomMessage | ChatMessage)[] 
   ];
 };
 
+export const GetHistoryWithAdaptiveCardMessages = (adaptiveCardTitle?: string): (CustomMessage | ChatMessage)[] => {
+  // Adaptive Cards samples and templates can be found in https://adaptivecards.io/samples/
+  const adaptiveCard = {
+    type: 'AdaptiveCard',
+    body: [
+      {
+        type: 'TextBlock',
+        size: 'medium',
+        weight: 'bolder',
+        text: adaptiveCardTitle || 'Publish Adaptive Card Schema',
+        style: 'heading',
+        wrap: true
+      },
+      {
+        type: 'ColumnSet',
+        columns: [
+          {
+            type: 'Column',
+            items: [
+              {
+                type: 'Image',
+                style: 'person',
+                url: 'https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg',
+                altText: 'Matt Hidinger',
+                size: 'small'
+              }
+            ],
+            width: 'auto'
+          },
+          {
+            type: 'Column',
+            items: [
+              {
+                type: 'TextBlock',
+                weight: 'bolder',
+                text: 'Matt Hidinger',
+                wrap: true
+              },
+              {
+                type: 'TextBlock',
+                spacing: 'none',
+                text: 'Created 2005-05-22',
+                isSubtle: true,
+                wrap: true
+              }
+            ],
+            width: 'stretch'
+          }
+        ]
+      },
+      {
+        type: 'TextBlock',
+        text: 'Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.',
+        wrap: true
+      }
+    ],
+    actions: [
+      {
+        type: 'Action.ShowCard',
+        title: 'Set due date',
+        card: {
+          type: 'AdaptiveCard',
+          body: [
+            {
+              type: 'Input.Date',
+              label: 'Enter the due date',
+              id: 'dueDate'
+            },
+            {
+              type: 'Input.Text',
+              id: 'comment',
+              isMultiline: true,
+              label: 'Add a comment'
+            }
+          ],
+          actions: [
+            {
+              type: 'Action.Submit',
+              title: 'OK'
+            }
+          ],
+          $schema: 'http://adaptivecards.io/schemas/adaptive-card.json'
+        }
+      },
+      {
+        type: 'Action.OpenUrl',
+        title: 'View',
+        url: 'https://adaptivecards.io',
+        role: 'button'
+      }
+    ],
+    $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+    version: '1.5'
+  };
+
+  return [
+    ...GetHistoryChatMessages(),
+    {
+      messageType: 'custom',
+      // Custom message's payload can be any shape, this is just an example.
+      // Whatever is defined in the custom message's payload needs to be handled in onRenderMessage in MessageThread.
+      createdOn: new Date('2020-04-13T00:00:00.000+07:01'),
+      messageId: Math.random().toString(),
+      content: JSON.stringify(adaptiveCard)
+    }
+  ];
+};
+
 export const GetHistoryHTMLChatMessages = (): ChatMessage[] => {
   return GetHistoryChatMessages().map((message) => {
     return {
