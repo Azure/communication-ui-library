eb1a3b222734c72337f1c6646e6088508bc05dfd
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2025-05-27 10:53:37 -0700

    Migrate Chat sample

---
+++ samples/Chat/eslint.config.js
@@ -0,0 +1,124 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+
+const globals = require('globals');
+
+const { fixupConfigRules, fixupPluginRules } = require('@eslint/compat');
+
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const headers = require('eslint-plugin-headers');
+const js = require('@eslint/js');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
+      )
+    ),
+
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      headers
+    },
+
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': [
+        'warn',
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true
+        }
+      ],
+
+      eqeqeq: 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+
+      'headers/header-format': [
+        'error',
+        {
+          source: 'string',
+          style: 'line',
+          content: 'Copyright (c) Microsoft Corporation.\nLicensed under the MIT License.'
+        }
+      ],
+
+      'react/display-name': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_'
+        }
+      ],
+
+      curly: 'error',
+
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['@internal/*'],
+              message: 'Use @azure/communication-react instead.'
+            }
+          ]
+        }
+      ]
+    },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx', '**/mocks/*'],
+
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  },
+  globalIgnores([
+    '**/docs/',
+    '**/public/',
+    '**/dist/',
+    '**/node_modules/',
+    '**/preprocessed/',
+    '*.config.js',
+    '.babel*'
+  ])
+]);
