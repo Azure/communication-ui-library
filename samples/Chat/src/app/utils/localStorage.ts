bfcebd00d3f7bcdebea89277efab8d1c121889aa
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2025-06-10 10:15:51 -0700

    Avoid permissions API on Android WebView (#5888)

--- samples/Chat/src/app/utils/localStorage.ts
+++ samples/Chat/src/app/utils/localStorage.ts
@@ -12,22 +12,22 @@ export enum LocalStorageKeys {
  * Get display name from local storage.
  */
 export const getDisplayNameFromLocalStorage = (): string | null =>
-  window.localStorage.getItem(LocalStorageKeys.DisplayName);
+  window.localStorage?.getItem(LocalStorageKeys.DisplayName);
 
 /**
  * Save display name into local storage.
  */
 export const saveDisplayNameToLocalStorage = (displayName: string): void =>
-  window.localStorage.setItem(LocalStorageKeys.DisplayName, displayName);
+  window.localStorage?.setItem(LocalStorageKeys.DisplayName, displayName);
 
 /**
  * Get theme from local storage.
  */
 export const getThemeFromLocalStorage = (scopeId: string): string | null =>
-  window.localStorage.getItem(LocalStorageKeys.Theme + '_' + scopeId);
+  window.localStorage?.getItem(LocalStorageKeys.Theme + '_' + scopeId);
 
 /**
  * Save theme into local storage.
  */
 export const saveThemeToLocalStorage = (theme: string, scopeId: string): void =>
-  window.localStorage.setItem(LocalStorageKeys.Theme + '_' + scopeId, theme);
+  window.localStorage?.setItem(LocalStorageKeys.Theme + '_' + scopeId, theme);
