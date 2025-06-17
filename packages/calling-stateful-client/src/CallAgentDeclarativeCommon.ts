e9d0b5a936a0e4a3679331f00542fddde481d601
prabhjot-msft <97124699+prabhjot-msft@users.noreply.github.com>
2025-06-17 17:35:59 +0000

    minor

--- packages/calling-stateful-client/src/CallAgentDeclarativeCommon.ts
+++ packages/calling-stateful-client/src/CallAgentDeclarativeCommon.ts
@@ -172,11 +172,11 @@ export abstract class ProxyCallAgentCommon {
    * We can't directly override get function because it is proxied,
    * Add a getCommon function and call it in child class
    */
+  //eslint-disable-line @typescript-eslint/no-explicit-any
   protected getCommon<AgentType extends CallAgent | TeamsCallAgent, P extends keyof CallAgent>(
     target: AgentType,
     prop: P | 'incomingCalls'
   ): any {
-    // eslint-disable-line @typescript-eslint/no-explicit-any
     switch (prop) {
       case 'startCall': {
         return this._context.withErrorTeedToState((...args: Parameters<AgentType['startCall']>): CallCommon => {
