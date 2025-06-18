afbf2a213e0c2f12dc01f6d5de0485e203fd08b4
dmceachernmsft <dmceachern@microsoft.com>
2025-06-18 12:02:34 -0700

    only transpile

--- packages/storybook8/.storybook/main.ts
+++ packages/storybook8/.storybook/main.ts
@@ -93,7 +93,7 @@ const storybookConfig: StorybookConfig = {
       test: /\.(tsx?|jsx?)$/,
       loader: 'ts-loader',
       options: {
-        transpileOnly: DEVELOPMENT_BUILD ? true : false,
+        transpileOnly: true,
         compilerOptions: {
           // Create a unique output format to avoid collisions
           module: 'esnext',
