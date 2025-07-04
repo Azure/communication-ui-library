a7c9134f965deec86ed49f54aa105329c52ec01a
carocao-msft <96077406+carocao-msft@users.noreply.github.com>
2025-07-04 10:36:10 -0700

    Add isspeaking indicator for RTT (#5920)
    * isspeaking rtt
    
    * Change files

--- packages/calling-component-bindings/src/utils/SelectorUtils.ts
+++ packages/calling-component-bindings/src/utils/SelectorUtils.ts
@@ -1,10 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { toFlatCommunicationIdentifier } from '@internal/acs-ui-common';
 import { RemoteParticipantState } from '@internal/calling-stateful-client';
 
 /**
  * @private
  */
-export const checkIsSpeaking = (participant: RemoteParticipantState): boolean =>
-  participant.isSpeaking && !participant.isMuted;
+export const checkIsSpeaking = (
+  participant: RemoteParticipantState,
+  inProgressRealTimeTextParticipantsIds?: string[]
+): boolean =>
+  inProgressRealTimeTextParticipantsIds
+    ? (participant.isSpeaking && !participant.isMuted) ||
+      inProgressRealTimeTextParticipantsIds.includes(toFlatCommunicationIdentifier(participant.identifier))
+    : participant.isSpeaking && !participant.isMuted;
